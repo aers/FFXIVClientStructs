6f65da9a9ef55b3f05d070bdb80759df6ad79f3d
aers <aers00@gmail.com>
2020-12-07 21:52:25 -0800

    update

--- Component/GUI/AtkComponentButton.cs
+++ Component/GUI/AtkComponentButton.cs
@@ -13,5 +13,12 @@ namespace FFXIVClientStructs.Component.GUI
     public unsafe struct AtkComponentButton
     {
         [FieldOffset(0x0)] public AtkComponentBase AtkComponentBase;
+        // based on the text size
+        [FieldOffset(0xC0)] public short Left;
+        [FieldOffset(0xC2)] public short Top;
+        [FieldOffset(0xC4)] public short Right;
+        [FieldOffset(0xC6)] public short Bottom;
+        [FieldOffset(0xC8)] public AtkTextNode* ButtonTextNode;
+        [FieldOffset(0xD0)] public AtkResNode* ButtonBGNode;
     }
 }
