90c9feb694b5b17eef5ccdb8a62ed1ff7feafa46
Cara <git@caraxian.com>
2021-01-01 19:35:05 +1030

    Add fonts to AlignmentType

--- Component/GUI/ULD/ULDWidgetInfo.cs
+++ Component/GUI/ULD/ULDWidgetInfo.cs
@@ -13,6 +13,13 @@ namespace FFXIVClientStructs.Component.GUI.ULD
         BottomLeft = 0x6,
         Bottom = 0x7,
         BottomRight = 0x8,
+
+        Font0 = 0x00, // Standard font, Supports 0xE0## characters
+        Font1 = 0x10, // Used on title screen menu
+        Font2 = 0x20, // Item Level. 0123456789.%!+-   Non supported characters are replaced with '-'
+        Font3 = 0x30, // 
+        Font4 = 0x40, // Used for job names
+        Font5 = 0x50, // Damage Values. Numeric and ! only. Non supported characters are not drawn.
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x20)]
