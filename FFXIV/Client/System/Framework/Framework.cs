c234244242085b9bea58fc2dcb86f45127739a88
aers <aers00@gmail.com>
2021-01-23 19:52:16 -0800

    Update Framework.cs

--- FFXIV/Client/System/Framework/Framework.cs
+++ FFXIV/Client/System/Framework/Framework.cs
@@ -1,8 +1,8 @@
 ﻿using System.Runtime.InteropServices;
 
-namespace FFXIVClientStructs.FFXIV.Client.System
+namespace FFXIVClientStructs.FFXIV.Client.System.Framework
 {
-    // Client::System::Framework
+    // Client::System::Framework::Framework
 
     // size=0x2B50
     // ctor E8 ? ? ? ? 48 8B C8 48 89 05 ? ? ? ? EB 0A 48 8B CE 
@@ -10,5 +10,6 @@ namespace FFXIVClientStructs.FFXIV.Client.System
     public unsafe struct Framework
     {
         [FieldOffset(0x10)] public Client.System.Configuration.SystemConfig SystemConfig;
+        [FieldOffset(0x2F98)] public void* UIModule;
     }
 }
