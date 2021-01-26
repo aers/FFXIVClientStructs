9d7279a6970f6c7bcaa92c584a8479e8913fb086
Raymond Lynch <daemitus@gmail.com>
2021-01-25 20:24:43 -0500

    RaptureAtkModule stub

---
+++ FFXIV/Client/UI/RaptureAtkModule.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI
+{
+    // Client::UI::RaptureAtkModule
+    //   Component::GUI::AtkModule
+    //     Component::GUI::AtkModuleInterface
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x8)]  // either this is tiny, or huge
+    public unsafe struct RaptureAtkModule
+    {
+        [FieldOffset(0x0)] public void* vtbl;
+    }
+}
