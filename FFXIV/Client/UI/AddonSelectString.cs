abdbfbf8c46095669072c1c715f6a4ac242fc233
Raymond Lynch <daemitus@gmail.com>
2021-03-12 18:18:52 -0500

    AddonSelect(Icon)String and internal class

--- FFXIV/Client/UI/AddonSelectString.cs
+++ FFXIV/Client/UI/AddonSelectString.cs
@@ -6,16 +6,19 @@ namespace FFXIVClientStructs.FFXIV.Client.UI
     // Client::UI::AddonSelectString
     //   Component::GUI::AtkUnitBase
     //     Component::GUI::AtkEventListener
-    [StructLayout(LayoutKind.Explicit, Size = 0x28A)]
+    [StructLayout(LayoutKind.Explicit, Size = 0x2A8)]
     public unsafe struct AddonSelectString
     {
         [FieldOffset(0x0)] public AtkUnitBase AtkUnitBase;
-        [FieldOffset(0x238)] public void* vtbl238;
-        [FieldOffset(0x240)] public AtkStage* AtkStage;
-        [FieldOffset(0x248)] public char** SelectStrings;
-        [FieldOffset(0x258)] public void* unk258;
-        [FieldOffset(0x268)] public AtkComponentWindow* AtkComponentWindow;
-        [FieldOffset(0x270)] public AtkComponentList* AtkComponentList;
-        [FieldOffset(0x278)] public AddonSelectString* this278;
+        [FieldOffset(0x238)] public EventThing SelectStringThing;
+
+        [StructLayout(LayoutKind.Explicit, Size = 0x70)]
+        public unsafe struct EventThing
+        {
+            [FieldOffset(0x0)] public void* vtbl;
+            [FieldOffset(0x8)] public AtkStage* AtkStage;
+            [FieldOffset(0x38)] public AtkComponentList* AtkComponentList;
+            [FieldOffset(0x40)] public AddonSelectString* Addon;
+        }
     }
 }
