5f2eeb8ed4119e76f2324a2a7f353d97a601558b
Raymond Lynch <daemitus@gmail.com>
2021-03-13 20:03:07 -0500

    AddonInspectionResult

---
+++ FFXIV/Client/UI/AddonItemInspectionResult.cs
@@ -0,0 +1,14 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+using System.Runtime.InteropServices;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI
+{
+    // Client::UI::AddonItemInspectionResult
+    //   Component::GUI::AtkUnitBase
+    //     Component::GUI::AtkEventListener
+    [StructLayout(LayoutKind.Explicit, Size = 0x2F8)]
+    public unsafe struct AddonItemInspectionResult
+    {
+        [FieldOffset(0x0)] public AtkUnitBase AtkUnitBase;
+    }
+}
