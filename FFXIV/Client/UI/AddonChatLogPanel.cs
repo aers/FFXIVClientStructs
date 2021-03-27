796d558c4f13fefc1c18bb8403e3fca6c998173f
Cara <git@caraxian.com>
2021-03-28 10:27:22 +1030

    Create AddonChatLogPanel.cs

---
+++ FFXIV/Client/UI/AddonChatLogPanel.cs
@@ -0,0 +1,20 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+using System.Runtime.InteropServices;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI
+{
+    // Client::UI::AddonChatLogPanel
+    //   Component::GUI::AtkUnitBase
+    //     Component::GUI::AtkEventListener
+    [StructLayout(LayoutKind.Explicit, Size = 0x3D0)]
+    public unsafe struct AddonChatLogPanel
+    {
+        [FieldOffset(0x0)] public AtkUnitBase AtkUnitBase;
+        [FieldOffset(0x2B4)] public uint FirstLineVisible;
+        [FieldOffset(0x2B8)] public uint LastLineVisible;
+        [FieldOffset(0x2C0)] public uint Unknown2C0;
+        [FieldOffset(0x2C4)] public uint TotalLineCount;
+        [FieldOffset(0x2F8)] public uint MessagesAboveCurrent;
+        [FieldOffset(0x341)] public byte IsScrolledBottom;
+    }
+}
