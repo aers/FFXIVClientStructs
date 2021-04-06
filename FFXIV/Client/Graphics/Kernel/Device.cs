c525d19991c028c5bace673b40182682ca76b3cb
aers <aers00@gmail.com>
2021-04-06 09:42:15 -0700

    graphics stuff

---
+++ FFXIV/Client/Graphics/Kernel/Device.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Runtime.InteropServices;
+
+
+namespace FFXIVClientStructs.FFXIV.Client.Graphics.Kernel
+{
+    // Client::Graphics::Kernel::Device
+    //   Client::Graphics::Singleton
+    [StructLayout(LayoutKind.Explicit, Size = 0x210)]
+    public unsafe struct Device
+    {
+        [FieldOffset(0x80)] public SwapChain* SwapChain;
+    }
+}
