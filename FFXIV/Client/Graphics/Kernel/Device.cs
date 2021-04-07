3a44b8aa8615df9fd353269047cb9d0ca245aa24
aers <aers00@gmail.com>
2021-04-07 00:38:01 -0700

    DX11 objects in kernel device

--- FFXIV/Client/Graphics/Kernel/Device.cs
+++ FFXIV/Client/Graphics/Kernel/Device.cs
@@ -10,5 +10,9 @@ namespace FFXIVClientStructs.FFXIV.Client.Graphics.Kernel
     public unsafe struct Device
     {
         [FieldOffset(0x80)] public SwapChain* SwapChain;
+        [FieldOffset(0x94)] public int D3DFeatureLevel; // D3D_FEATURE_LEVEL enum
+        [FieldOffset(0x98)] public void* DXGIFactory; // IDXGIFactory1
+        [FieldOffset(0xA8)] public void* D3D11Device; // ID3D11Device1
+        [FieldOffset(0xB0)] public void* D3D11DeviceContext; // ID3D11DeviceContext1
     }
 }
