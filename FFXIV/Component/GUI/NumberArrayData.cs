1556669f566030b290daa8d8928a0aea61ab886f
aers <aers00@gmail.com>
2021-04-26 22:32:11 -0700

    more atk nonsense

---
+++ FFXIV/Component/GUI/NumberArrayData.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace FFXIVClientStructs.FFXIV.Component.GUI
+{
+    [StructLayout(LayoutKind.Explicit, Size = 0x28)]
+    public unsafe struct NumberArrayData
+    {
+        [FieldOffset(0x0)] public AtkArrayData AtkArrayData;
+        [FieldOffset(0x20)] public int* IntArray;
+    }
+}
