f88c03e635c9710b878469b0042daf4f65ac2b43
aers <aers00@gmail.com>
2021-06-24 23:10:48 -0700

    start resourcemanager

---
+++ FFXIV/Client/System/Resource/ResourceManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace FFXIVClientStructs.FFXIV.Client.System.Resource.Handle
+{
+    // Client::System::Resource::ResourceManager
+    // no vtbl
+
+    // size = 0x1728
+    // ctor E8 ? ? ? ? 48 89 05 ? ? ? ? 48 8B 08
+    [StructLayout(LayoutKind.Explicit, Size = 0x1728)]
+    public unsafe struct ResourceManager
+    {
+    }
+}
