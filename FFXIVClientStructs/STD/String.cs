f0e2d6744bec9de3d80a778f0ad4730e68dd5657
aers <aers00@gmail.com>
2021-07-17 17:42:22 -0700

    string type

---
+++ FFXIVClientStructs/STD/String.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace FFXIVClientStructs.STD
+{
+    // std::string aka std::basic_string from msvc
+    [StructLayout(LayoutKind.Explicit, Size=0x20)]
+    public unsafe struct String
+    {
+        [FieldOffset(0x0)] public byte* Ptr;
+        [FieldOffset(0x0)] public fixed byte Buf[16];
+        [FieldOffset(0x10)] public ulong Length;
+        [FieldOffset(0x18)] public ulong BufLength;
+    }
+}
