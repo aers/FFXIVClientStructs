6a14999f9962f5693ebe6f0c8b332ceb3e812625
liam <lmcintyre995@gmail.com>
2021-08-09 00:26:30 -0400

    tooltip stuff

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkStage.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkStage.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.InteropServices;
+using FFXIVClientStructs.Common;
 using FFXIVClientStructs.FFXIV.Client.UI;
 
 namespace FFXIVClientStructs.FFXIV.Component.GUI
@@ -9,9 +10,13 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI
     // size = 0x75DF8
     // ctor E8 ? ? ? ? 48 8B F8 48 89 BE ? ? ? ? 48 8B 43 10 
     [StructLayout(LayoutKind.Explicit, Size = 0x75DF8)]
-    public unsafe struct AtkStage
+    public unsafe partial struct AtkStage
     {
         [FieldOffset(0x0)] public AtkEventTarget AtkEventTarget;
         [FieldOffset(0x20)] public RaptureAtkUnitManager* RaptureAtkUnitManager;
+        [FieldOffset(0x168)] public AtkTooltipManager TooltipManager;
+        
+        [MemberFunction("E8 ? ? ? ? 0F BF D5")]
+        public partial AtkStage* GetSingleton();
     }
 }
