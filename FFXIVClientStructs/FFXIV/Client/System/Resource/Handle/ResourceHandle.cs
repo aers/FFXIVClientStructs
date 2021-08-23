48ee5f5711da2c4fcc764b8daccf48a7f56ef8b2
aers <aers00@gmail.com>
2021-08-23 14:29:10 -0700

    update resource handle

--- FFXIVClientStructs/FFXIV/Client/System/Resource/Handle/ResourceHandle.cs
+++ FFXIVClientStructs/FFXIV/Client/System/Resource/Handle/ResourceHandle.cs
@@ -5,12 +5,14 @@ namespace FFXIVClientStructs.FFXIV.Client.System.Resource.Handle
     // Client::System::Resource::Handle::ResourceHandle
     //   Client::System::Common::NonCopyable
 
-    // size = 0xA8
+    // size = 0xB0
     // ctor E8 ? ? ? ? 81 A3 ? ? ? ? ? ? ? ? 48 8D 05 ? ? ? ? 
-    [StructLayout(LayoutKind.Explicit, Size = 0xA8)]
+    [StructLayout(LayoutKind.Explicit, Size = 0xB0)]
     public unsafe struct ResourceHandle
     {
-        [FieldOffset(0x12)] public uint FileType; // "txt" "uld" etc from the header
+        [FieldOffset(0x08)] public uint Category;
+        [FieldOffset(0x0C)] public uint FileType; // "txt" "uld" etc from the header
+        [FieldOffset(0x10)] public uint Id;
         [FieldOffset(0x48)] public FFXIVClientStructs.STD.String FileName; // std::string
     }
 }
--- FFXIVClientStructs/FFXIV/Client/System/Resource/Handle/TextureResourceHandle.cs
+++ FFXIVClientStructs/FFXIV/Client/System/Resource/Handle/TextureResourceHandle.cs
@@ -6,9 +6,9 @@ namespace FFXIVClientStructs.FFXIV.Client.System.Resource.Handle
     //   Client::System::Resource::Handle::ResourceHandle
     //     Client::System::Common::NonCopyable
 
-    // size = 0x128
+    // size = 0x130
     // ctor 40 53 48 83 EC 30 48 8B 44 24 ? 48 8B D9 48 89 44 24 ? 48 8B 44 24 ? 48 89 44 24 ? E8 ? ? ? ? 81 A3 ? ? ? ? ? ? ? ?  
-    [StructLayout(LayoutKind.Explicit, Size = 0x128)]
+    [StructLayout(LayoutKind.Explicit, Size = 0x130)]
     public struct TextureResourceHandle
     {
         [FieldOffset(0x0)] public ResourceHandle ResourceHandle;
