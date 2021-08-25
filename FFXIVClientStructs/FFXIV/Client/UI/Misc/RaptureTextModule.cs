7181f15a25e30f873a5f0dd219ebfdfdd9748272
pohky <otoriosu@gmail.com>
2021-08-25 02:46:10 +0200

    Create RaptureTextModule.cs

---
+++ FFXIVClientStructs/FFXIV/Client/UI/Misc/RaptureTextModule.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+using FFXIVClientStructs.Common;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI.Misc {
+    // Client::UI::Misc::RaptureTextModule
+    // ctor E8 ?? ?? ?? ?? 48 8D B7 ?? ?? ?? ?? 4D 8B C4
+    [StructLayout(LayoutKind.Explicit, Size = 0xD50)]
+    public unsafe partial struct RaptureTextModule {
+
+        [MemberFunction("E9 ?? ?? ?? ?? 80 EA 20")]
+        public partial byte* GetAddonText(uint addonId);
+
+        [MemberFunction("E8 ?? ?? ?? ?? EB 55 FF 50 30")]
+        public partial byte* FormatAddonText2(uint addonId, int intParam1, int intParam2);
+
+        [MemberFunction("E8 ?? ?? ?? ?? 44 8B 7D 84")]
+        public partial byte* FormatAddonText3(uint addonId, int intParam1, int intParam2, int intParam3);
+    }
+}
