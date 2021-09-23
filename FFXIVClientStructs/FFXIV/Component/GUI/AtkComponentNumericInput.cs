90dce60823a40db3ef9ff732097301faa97805cc
Bruno Ferreira <chalkos@chalkos.net>
2021-09-23 12:06:24 +0100

    fix AtkComponentNumericInput->AtkTextNode

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentNumericInput.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentNumericInput.cs
@@ -9,10 +9,10 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI
     //       Component::GUI::AtkEventListener
 
     [StructLayout(LayoutKind.Explicit, Size = 0x338)]
-    public partial struct AtkComponentNumericInput
+    public unsafe partial struct AtkComponentNumericInput
     {
         [FieldOffset(0x0)] public AtkComponentInputBase AtkComponentInputBase;
-        [FieldOffset(0xC8)] public AtkTextNode AtkTextNode;
+        [FieldOffset(0xC8)] public AtkTextNode* AtkTextNode;
 
         [MemberFunction("40 53 48 83 EC 60 8B 81")]
         public partial void SetValue(int value);
