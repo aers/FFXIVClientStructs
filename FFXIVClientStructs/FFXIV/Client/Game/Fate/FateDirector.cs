73045a405ac721e86a0dec2ef64125881ae8dac0
pohky <otoriosu@gmail.com>
2021-10-01 00:11:58 +0200

    Update FateDirector

--- FFXIVClientStructs/FFXIV/Client/Game/Fate/FateDirector.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Fate/FateDirector.cs
@@ -1,15 +1,11 @@
 ﻿using System.Runtime.InteropServices;
 using FFXIVClientStructs.Attributes;
-using FFXIVClientStructs.FFXIV.Client.System.String;
-using FFXIVClientStructs.FFXIV.Common.Lua;
+using FFXIVClientStructs.FFXIV.Client.Game.Event;
 
 namespace FFXIVClientStructs.FFXIV.Client.Game.Fate {
     [StructLayout(LayoutKind.Explicit, Size = 0x4f8)]
     public unsafe partial struct FateDirector {
-        [FieldOffset(0x210)] public LuaState* LuaState;
-        
-        [FieldOffset(0x350)] public Utf8String FateName;
-        [FieldOffset(0x3B8)] public Utf8String FateDescription;
+        [FieldOffset(0x00)] public Director Director;
         
         [FieldOffset(0x4B8)] public byte FateLevel;
         [FieldOffset(0x4C0)] public uint FateNpcObjectId;
