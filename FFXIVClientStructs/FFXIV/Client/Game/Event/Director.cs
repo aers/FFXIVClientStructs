c34944aea37c67f3e3b862ef27813884a2c8352c
pohky <otoriosu@gmail.com>
2021-10-01 00:11:30 +0200

    Add Director

---
+++ FFXIVClientStructs/FFXIV/Client/Game/Event/Director.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+using FFXIVClientStructs.FFXIV.Client.System.String;
+
+namespace FFXIVClientStructs.FFXIV.Client.Game.Event {
+    [StructLayout(LayoutKind.Explicit, Size = 0x4B8)]
+    public unsafe struct Director {
+        [FieldOffset(0x00)] public LuaEventHandler LuaEventHandler;
+        [FieldOffset(0x350)] public Utf8String Unk_350_String;
+        [FieldOffset(0x3B8)] public Utf8String Unk_3B8_String;
+        [FieldOffset(0x420)] public Utf8String Unk_420_String;
+    }
+}
---
+++ FFXIVClientStructs/FFXIV/Client/Game/Event/EventHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+using FFXIVClientStructs.FFXIV.Client.System.String;
+
+namespace FFXIVClientStructs.FFXIV.Client.Game.Event {
+    [StructLayout(LayoutKind.Explicit, Size = 0x210)]
+    public unsafe struct EventHandler {
+        [FieldOffset(0xC8)] public Utf8String Unk_C8_String;
+        [FieldOffset(0x168)] public Utf8String Unk_168_String;
+    }
+}
