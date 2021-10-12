03f380a58fa9704410917b3c92778d531a43c691
Cara <git@caraxian.com>
2021-10-12 20:32:16 +1030

    Update AtkComponentNumericInput.cs

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentNumericInput.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentNumericInput.cs
@@ -13,6 +13,7 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI
     {
         [FieldOffset(0x0)] public AtkComponentInputBase AtkComponentInputBase;
         [FieldOffset(0xC8)] public AtkTextNode* AtkTextNode;
+        [FieldOffset(0x2F8)] public AtkUldComponentDataNumericInput Data;
 
         [MemberFunction("40 53 48 83 EC 60 8B 81")]
         public partial void SetValue(int value);
