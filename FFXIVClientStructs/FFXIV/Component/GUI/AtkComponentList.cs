980ccd2afc931f7701ba2e48f68351cf7a7f53df
Raymond <daemitus@gmail.com>
2021-10-19 13:48:20 -0400

    Update AtkComponentList

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentList.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentList.cs
@@ -17,6 +17,13 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI
         [FieldOffset(0xC8)] public AtkComponentScrollBar* AtkComponentScrollBarC8;
         [FieldOffset(0xF0)] public ListItem* ItemRendererList;
         [FieldOffset(0x118)] public int ListLength;
+        [FieldOffset(0x12C)] public int SelectedItemIndex; // 0-N, -1 when none.
+        [FieldOffset(0x130)] public int HeldItemIndex; // 0-N, -1 when none. While mouse is held down.
+        [FieldOffset(0x134)] public int HoveredItemIndex; // 0-N, -1 when none. While mouse is hovering.
+        // [FieldOffset(0x138)] public int SelectedItemIndex2; // Goes negative sometimes... strange.
+        [FieldOffset(0x148)] public AtkCollisionNode* HoveredItemCollisionNode;
+        [FieldOffset(0x150)] public int HoveredItemIndex2; // Repeat?
+        [FieldOffset(0x158)] public int HoveredItemIndex3; // Repeat?
 
         [StructLayout(LayoutKind.Explicit, Size = 0x18)]
         public struct ListItem
