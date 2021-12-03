f3800a6149423038cc5cbdd7c28c8f53e32b61f4
pohky <otoriosu@gmail.com>
2021-12-03 16:06:05 +0100

    Update UIState.cs

--- FFXIVClientStructs/FFXIV/Client/Game/UI/UIState.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/UIState.cs
@@ -12,13 +12,14 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.UI
     // all data in here is used for UI display
 
     // ctor E8 ? ? ? ? 48 8D 0D ? ? ? ? 48 83 C4 28 E9 ? ? ? ? 48 83 EC 28 33 D2 
-    [StructLayout(LayoutKind.Explicit, Size=0x15D2A)] // its at least this big, may be a few bytes bigger
+    [StructLayout(LayoutKind.Explicit, Size=0x168D8)] // its at least this big, may be a few bytes bigger
     public unsafe partial struct UIState
     {
         [FieldOffset(0x00)] public Hotbar Hotbar;
         [FieldOffset(0xA38)] public PlayerState PlayerState;
-        [FieldOffset(0x1120)] public Revive Revive;
-        [FieldOffset(0x1388)] public Telepo Telepo;
+        [FieldOffset(0x11C0)] public Revive Revive;
+        [FieldOffset(0x1448)] public Telepo Telepo;
+
         [FieldOffset(0x1960)] public Buddy Buddy;
         [FieldOffset(0x22E8)] public RelicNote RelicNote;
         [FieldOffset(0x9E98)] public Director* ActiveDirector;
