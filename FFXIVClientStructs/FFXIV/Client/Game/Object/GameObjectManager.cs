f60b501eefd442f34e97f49e4a19307f6b188796
pohky <otoriosu@gmail.com>
2021-12-05 10:05:12 +0100

    Update GameObjectManager.cs
    offsets up by 8

--- FFXIVClientStructs/FFXIV/Client/Game/Object/GameObjectManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Object/GameObjectManager.cs
@@ -6,12 +6,12 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Object
     [StructLayout(LayoutKind.Explicit, Size = 0x27E0)]
     public unsafe partial struct GameObjectManager
     {
-        [FieldOffset(0x05)] public byte Active;
-        [FieldOffset(0x10)] public fixed long ObjectList[424]; // size 424 * 8
-        [FieldOffset(0xD50)] public fixed long ObjectListFiltered[424];
-        [FieldOffset(0x1A90)] public fixed long ObjectList3[424];
-        [FieldOffset(0x27D0)] public int ObjectListFilteredCount;
-        [FieldOffset(0x27D4)] public int ObjectList3Count;
+        [FieldOffset(0x04)] public byte Active;
+        [FieldOffset(0x18)] public fixed long ObjectList[424]; // size 424 * 8
+        [FieldOffset(0xD58)] public fixed long ObjectListFiltered[424];
+        [FieldOffset(0x1A98)] public fixed long ObjectList3[424];
+        [FieldOffset(0x27D8)] public int ObjectListFilteredCount;
+        [FieldOffset(0x27DC)] public int ObjectList3Count;
 
         [StaticAddress("48 8D 35 ?? ?? ?? ?? 81 FA")]
         public static partial GameObjectManager* Instance();
