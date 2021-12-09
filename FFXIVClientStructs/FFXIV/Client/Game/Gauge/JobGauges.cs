c33fe04e05fae1244fd0918477a4b14c030f6348
Raymond <daemitus@gmail.com>
2021-12-09 08:57:42 -0500

    Monk and Reaper gauges

--- FFXIVClientStructs/FFXIV/Client/Game/Gauge/JobGauges.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Gauge/JobGauges.cs
@@ -1,22 +1,26 @@
 using System.Runtime.InteropServices;
 
-namespace FFXIVClientStructs.FFXIV.Client.Game.Gauge {
+namespace FFXIVClientStructs.FFXIV.Client.Game.Gauge
+{
     [StructLayout(LayoutKind.Explicit, Size = 0x08)]
-    public struct JobGauge {
+    public struct JobGauge
+    {
         // empty base class for other gauges, this only has the vtable
     }
 
     #region Healer
 
     [StructLayout(LayoutKind.Explicit, Size = 0x10)]
-    public struct WhiteMageGauge {
+    public struct WhiteMageGauge
+    {
         [FieldOffset(0x0A)] public short LilyTimer;
         [FieldOffset(0x0C)] public byte Lily;
         [FieldOffset(0x0D)] public byte BloodLily;
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x10)]
-    public struct ScholarGauge {
+    public struct ScholarGauge
+    {
         [FieldOffset(0x0A)] public byte Aetherflow;
         [FieldOffset(0x0B)] public byte FairyGauge;
         [FieldOffset(0x0C)] public short SeraphTimer;
@@ -24,17 +28,19 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Gauge {
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x10)]
-    public unsafe struct AstrologianGauge {
+    public unsafe struct AstrologianGauge
+    {
         [FieldOffset(0x08)] public short Timer;
         [FieldOffset(0x0C)] public byte Card;
         [FieldOffset(0x0D)] public fixed byte Seals[3];
 
         public AstrologianCard CurrentCard => (AstrologianCard)Card;
-        public AstrologianSeal[] CurrentSeals => new[] {(AstrologianSeal)Seals[0], (AstrologianSeal)Seals[1], (AstrologianSeal)Seals[2]};
+        public AstrologianSeal[] CurrentSeals => new[] { (AstrologianSeal)Seals[0], (AstrologianSeal)Seals[1], (AstrologianSeal)Seals[2] };
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x10)]
-    public struct SageGauge {
+    public struct SageGauge
+    {
         [FieldOffset(0x08)] public short AddersgallTimer;
         [FieldOffset(0x0A)] public byte Addersgall;
         [FieldOffset(0x0B)] public byte Addersting;
@@ -48,7 +54,8 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Gauge {
     #region MagicDPS
 
     [StructLayout(LayoutKind.Explicit, Size = 0x30)]
-    public struct BlackMageGauge {
+    public struct BlackMageGauge
+    {
         [FieldOffset(0x08)] public short EnochianTimer;
         [FieldOffset(0x0A)] public short ElementTimeRemaining;
         [FieldOffset(0x0C)] public sbyte ElementStance;
@@ -63,7 +70,8 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Gauge {
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x10)]
-    public struct SummonerGauge {
+    public struct SummonerGauge
+    {
         [FieldOffset(0x8)] public ushort SummonTimer; // millis counting down
         [FieldOffset(0xA)] public ushort AttunementTimer; // millis counting down
         [FieldOffset(0xC)] public byte ReturnSummon; // Pet sheet (23=Carbuncle, the only option now)
@@ -73,7 +81,8 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Gauge {
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x50)]
-    public struct RedMageGauge {
+    public struct RedMageGauge
+    {
         [FieldOffset(0x08)] public byte WhiteMana;
         [FieldOffset(0x09)] public byte BlackMana;
         [FieldOffset(0x0A)] public byte ManaStacks;
@@ -84,7 +93,8 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Gauge {
     #region RangeDPS
 
     [StructLayout(LayoutKind.Explicit, Size = 0x10)]
-    public struct BardGauge {
+    public struct BardGauge
+    {
         [FieldOffset(0x08)] public ushort SongTimer;
         [FieldOffset(0x0C)] public byte Repertoire;
         [FieldOffset(0x0D)] public byte SoulVoice;
@@ -92,7 +102,8 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Gauge {
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x10)]
-    public struct MachinistGauge {
+    public struct MachinistGauge
+    {
         [FieldOffset(0x08)] public short OverheatTimeRemaining;
         [FieldOffset(0x0A)] public short SummonTimeRemaining;
         [FieldOffset(0x0C)] public byte Heat;
@@ -102,7 +113,8 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Gauge {
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x10)]
-    public unsafe struct DancerGauge {
+    public unsafe struct DancerGauge
+    {
         [FieldOffset(0x08)] public byte Feathers;
         [FieldOffset(0x09)] public byte Esprit;
         [FieldOffset(0x0A)] public fixed byte DanceSteps[4];
@@ -116,26 +128,37 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Gauge {
     #region MeleeDPS
 
     [StructLayout(LayoutKind.Explicit, Size = 0x10)]
-    public struct MonkGauge {
-        [FieldOffset(0x08)] public byte Chakra;
+    public struct MonkGauge
+    {
+        // [FieldOffset(0x08)] public byte Chakra;            // Appears unused, pre-6.0 Chakra value
+        [FieldOffset(0x09)] public ChakraType BeastChakra1;   // CoeurlChakra = 1, RaptorChakra = 2, OpoopoChakra = 3 (only one value)
+        [FieldOffset(0x0A)] public ChakraType BeastChakra2;   // CoeurlChakra = 1, RaptorChakra = 2, OpoopoChakra = 3 (only one value)
+        [FieldOffset(0x0B)] public ChakraType BeastChakra3;   // CoeurlChakra = 1, RaptorChakra = 2, OpoopoChakra = 3 (only one value)
+        [FieldOffset(0x0C)] public NadiFlags Nadi;            // LunarNadi = 2, SolarNadi = 4 (If both then 2+4=6)
+        [FieldOffset(0x0E)] public ushort BlitzTimeRemaining; // 20 seconds
+
+        public ChakraType[] CurrentChakra => new[] { BeastChakra1, BeastChakra2, BeastChakra3 };
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x10)]
-    public struct DragoonGauge {
+    public struct DragoonGauge
+    {
         [FieldOffset(0x08)] public short BotdTimer;
         [FieldOffset(0x0A)] public byte BotdState;
         [FieldOffset(0x0B)] public byte EyeCount;
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x18)]
-    public struct NinjaGauge {
+    public struct NinjaGauge
+    {
         [FieldOffset(0x08)] public int HutonTimer;
         [FieldOffset(0x0C)] public byte Ninki;
         [FieldOffset(0x0D)] public byte HutonManualCasts;
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x10)]
-    public struct SamuraiGauge {
+    public struct SamuraiGauge
+    {
         [FieldOffset(0x0A)] public KaeshiAction Kaeshi;
         [FieldOffset(0x0B)] public byte Kenki;
         [FieldOffset(0x0C)] public byte MeditationStacks;
@@ -143,11 +166,13 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Gauge {
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x10)]
-    public struct ReaperGauge {
+    public struct ReaperGauge
+    {
         [FieldOffset(0x08)] public byte Soul;
         [FieldOffset(0x09)] public byte Shroud;
         [FieldOffset(0x0A)] public ushort EnshroudedTimeRemaining;
-        [FieldOffset(0x0C)] public byte VoidShroud;
+        [FieldOffset(0x0C)] public byte LemureShroud;
+        [FieldOffset(0x0D)] public byte VoidShroud;
     }
 
     #endregion
@@ -155,7 +180,8 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Gauge {
     #region Tanks
 
     [StructLayout(LayoutKind.Explicit, Size = 0x10)]
-    public struct DarkKnightGauge {
+    public struct DarkKnightGauge
+    {
         [FieldOffset(0x08)] public byte Blood;
         [FieldOffset(0x0A)] public ushort DarksideTimer;
         [FieldOffset(0x0C)] public byte DarkArtsState;
@@ -163,17 +189,20 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Gauge {
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x10)]
-    public struct PaladinGauge {
+    public struct PaladinGauge
+    {
         [FieldOffset(0x08)] public byte OathGauge;
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x10)]
-    public struct WarriorGauge {
+    public struct WarriorGauge
+    {
         [FieldOffset(0x08)] public byte BeastGauge;
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x10)]
-    public struct GunbreakerGauge {
+    public struct GunbreakerGauge
+    {
         [FieldOffset(0x08)] public byte Ammo;
         [FieldOffset(0x0A)] public short MaxTimerDuration;
         [FieldOffset(0x0C)] public byte AmmoComboStep;
