b092874bc2aef76a6b918169aa5339f2b7c9157d
Cara <git@caraxian.com>
2021-12-10 19:43:52 +1030

    Update AgentLobby.cs

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentLobby.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentLobby.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.InteropServices;
 using FFXIVClientStructs.FFXIV.Client.System.Framework;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace FFXIVClientStructs.FFXIV.Client.UI.Agent {
     [StructLayout(LayoutKind.Explicit, Size = 0xA90)]
@@ -7,7 +8,7 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Agent {
 
         public static AgentLobby* Instance() => (AgentLobby*) Framework.Instance()->GetUiModule()->GetAgentModule()->GetAgentByInternalId(AgentId.Lobby);
 
-        [FieldOffset(0x0)] public void* vtbl;
+        [FieldOffset(0x0)] public AgentInterface AgentInterface;
         [FieldOffset(0x820)] public byte DataCenter;
         [FieldOffset(0x840)] public uint IdleTime;
     }
