198a3c26979333ace3847d7b1b441912ac9393e7
pohky <otoriosu@gmail.com>
2021-12-17 06:59:54 +0100

    Update Object.cs

--- FFXIVClientStructs/FFXIV/Client/Graphics/Scene/Object.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Scene/Object.cs
@@ -10,6 +10,13 @@ namespace FFXIVClientStructs.FFXIV.Client.Graphics.Scene
     [StructLayout(LayoutKind.Explicit, Size = 0x80)]
     public unsafe struct Object
     {
+        [FieldOffset(0x18)] public Object* ParentObject;
+        [FieldOffset(0x20)] public Object* PreviousSiblingObject;
+        [FieldOffset(0x28)] public Object* NextSiblingObject;
         [FieldOffset(0x30)] public Object* ChildObject; // for humans this is a weapon
+
+        [FieldOffset(0x50)] public Vector3 Position;
+        [FieldOffset(0x60)] public Quarternion Rotation;
+        [FieldOffset(0x70)] public Vector3 Scale;
     }
 }
