60dcd288061994e48365ca2563527624fc3dfad8
pohky <otoriosu@gmail.com>
2021-12-18 20:13:34 +0100

    Update Object.cs

--- FFXIVClientStructs/FFXIV/Client/Graphics/Scene/Object.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Scene/Object.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.InteropServices;
+using FFXIVClientStructs.Attributes;
 
 namespace FFXIVClientStructs.FFXIV.Client.Graphics.Scene
 {
@@ -8,7 +9,7 @@ namespace FFXIVClientStructs.FFXIV.Client.Graphics.Scene
     // size = 0x80
     // ctor - inlined in all derived class ctors
     [StructLayout(LayoutKind.Explicit, Size = 0x80)]
-    public unsafe struct Object
+    public unsafe partial struct Object
     {
         [FieldOffset(0x18)] public Object* ParentObject;
         [FieldOffset(0x20)] public Object* PreviousSiblingObject;
@@ -18,5 +19,21 @@ namespace FFXIVClientStructs.FFXIV.Client.Graphics.Scene
         [FieldOffset(0x50)] public Vector3 Position;
         [FieldOffset(0x60)] public Quarternion Rotation;
         [FieldOffset(0x70)] public Vector3 Scale;
+
+        [VirtualFunction(2)]
+        public partial ObjectType GetObjectType();
+    }
+
+    public enum ObjectType {
+        Object = 0,
+        Terrain = 1,
+        BgObject = 2,
+        CharacterBase = 3,
+        VfxObject = 4,
+        Light = 5,
+        Unk_Type6 = 6,
+        EnvSpace = 7,
+        EnvLocation = 8,
+        Unk_Type9 = 9
     }
 }
