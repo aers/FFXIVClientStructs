507f3e0b2146e4067e5a459485eec289398c6048
pohky <otoriosu@gmail.com>
2022-01-12 12:40:12 +0100

    Update CharacterManager.cs

--- FFXIVClientStructs/FFXIV/Client/Game/Character/CharacterManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Character/CharacterManager.cs
@@ -11,5 +11,14 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Character
 
         [MemberFunction("E8 ?? ?? ?? ?? 48 89 84 1D")]
         public partial BattleChara* LookupBattleCharaByObjectId(int objectId);
+
+        [MemberFunction("E8 ?? ?? ?? ?? 48 8B F8 48 85 C0 74 3A 0F B7 4C 24")]
+        public partial BattleChara* LookupBattleCharaByName(string name, bool onlyPlayers = false, short world = -1);
+
+        [MemberFunction("E8 ?? ?? ?? ?? 49 8D 4D 20 48 89 44 24")]
+        public partial BattleChara* LookupBuddyByOwnerObject(BattleChara* owner);
+
+        [MemberFunction("E8 ?? ?? ?? ?? 48 3B F0 74 3A")]
+        public partial BattleChara* LookupPetByOwnerObject(BattleChara* owner);
     }
 }
