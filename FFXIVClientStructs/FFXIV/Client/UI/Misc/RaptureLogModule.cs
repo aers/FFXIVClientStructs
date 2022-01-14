d78ba8c50149ba0d0875189faf14ea1b0ee9b5fd
pohky <otoriosu@gmail.com>
2022-01-15 00:10:45 +0100

    Update RaptureLogModule.cs

--- FFXIVClientStructs/FFXIV/Client/UI/Misc/RaptureLogModule.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Misc/RaptureLogModule.cs
@@ -1,13 +1,39 @@
 ﻿using System.Runtime.InteropServices;
 using FFXIVClientStructs.Attributes;
+using FFXIVClientStructs.FFXIV.Client.System.String;
+using FFXIVClientStructs.FFXIV.Common.Log;
+using FFXIVClientStructs.FFXIV.Component.Excel;
 
 namespace FFXIVClientStructs.FFXIV.Client.UI.Misc {
     // Client::UI::Misc::RaptureLogModule
     // ctor E8 ?? ?? ?? ?? 4C 8D AF ?? ?? ?? ?? 49 8B CD
     [StructLayout(LayoutKind.Explicit, Size = 0x3480)]
     public unsafe partial struct RaptureLogModule {
+        [FieldOffset(0x00)] public LogModule LogModule;
+
+        [FieldOffset(0xE8)] public ExcelModuleInterface* ExcelModuleInterface;
+        [FieldOffset(0xF0)] public RaptureTextModule* RaptureTextModule;
+
+        [FieldOffset(0x528)] public fixed byte ChatTabs[5 * 0x928]; // array of 5 RaptureLogModuleTab
+
+        [FieldOffset(0x3470)] public LogMessageSource* MsgSourceArray;
+        [FieldOffset(0x3478)] public int MsgSourceArrayLength;
 
         [MemberFunction("E8 ?? ?? ?? ?? 44 03 FB")]
         public partial void ShowLogMessage(uint logMessageID);
+    }
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x10)]
+    public unsafe struct LogMessageSource {
+        [FieldOffset(0x00)] public ulong ContentId;
+        [FieldOffset(0x08)] public int LogMessageIndex;
+        [FieldOffset(0x0C)] public short World;
+        [FieldOffset(0x0E)] public short ChatType;
+    }
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x928)]
+    public unsafe struct RaptureLogModuleTab {
+        [FieldOffset(0x00)] public Utf8String Name;
+        [FieldOffset(0x68)] public Utf8String VisibleLogLines;
     }
 }
