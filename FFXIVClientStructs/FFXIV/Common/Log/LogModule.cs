d692f7fea1b9a8e44b5526e7fd176b9b37d9ef86
pohky <otoriosu@gmail.com>
2022-01-15 00:09:48 +0100

    Create LogModule.cs

---
+++ FFXIVClientStructs/FFXIV/Common/Log/LogModule.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+using FFXIVClientStructs.STD;
+
+namespace FFXIVClientStructs.FFXIV.Common.Log {
+    //Component::Log::LogModule
+    //  Component::Log::LogModuleInterface
+    [StructLayout(LayoutKind.Explicit, Size = 0x80)]
+    public unsafe struct LogModule {
+        [FieldOffset(0x08)] public ulong LocalPlayerContentId;
+
+        [FieldOffset(0x14)] public int LogMessageCount;
+
+        [FieldOffset(0x48)] public StdVector<int> LogMessageIndex;
+        [FieldOffset(0x60)] public StdVector<byte> LogMessageData;
+    }
+}
