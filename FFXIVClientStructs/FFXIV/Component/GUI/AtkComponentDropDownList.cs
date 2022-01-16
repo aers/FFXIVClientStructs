0c22b9c1f4241a47815c5729c41c2adb07ef6521
Raymond <daemitus@gmail.com>
2022-01-16 16:47:35 -0500

    Add AtkComponentDropDownList

---
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentDropDownList.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace FFXIVClientStructs.FFXIV.Component.GUI
+{
+    // Component::GUI::AtkComponentDropDownList
+    //   Component::GUI::AtkComponentBase
+    //     Component::GUI::AtkEventListener
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x224)]
+    public struct AtkComponentDropDownList
+    {
+        [FieldOffset(0x0)] public AtkComponentBase AtkComponentBase;
+    }
+}
