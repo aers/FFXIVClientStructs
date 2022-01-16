2cea7b91960c2d8f4635fd55a495bd0555b39418
Raymond <daemitus@gmail.com>
2022-01-16 16:47:18 -0500

    AddonSynthesize is AddonSynthesis

--- FFXIVClientStructs/FFXIV/Client/UI/AddonSynthesis.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonSynthesis.cs
@@ -4,11 +4,11 @@ using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace FFXIVClientStructs.FFXIV.Client.UI
 {
-    // Client::UI::AddonSynthesize
+    // Client::UI::AddonSynthesis
     //   Component::GUI::AtkUnitBase
     //     Component::GUI::AtkEventListener
     [StructLayout(LayoutKind.Explicit, Size = 0x838)]
-    public unsafe struct AddonSynthesize
+    public unsafe struct AddonSynthesis
     {
         [FieldOffset(0x0)] public AtkUnitBase AtkUnitBase;
         [FieldOffset(0x220)] public AtkCollisionNode* RootCollisionNode;
