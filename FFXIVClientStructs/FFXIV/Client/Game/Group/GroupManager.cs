9f0eab46131b0752246db07151cd892f69f4f9ff
goaaats <16760685+goaaats@users.noreply.github.com>
2022-01-16 20:49:22 +0100

    Add party id to GroupManager

--- FFXIVClientStructs/FFXIV/Client/Game/Group/GroupManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Group/GroupManager.cs
@@ -12,8 +12,8 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Group
         [FieldOffset(0x1180)] public fixed byte AllianceMembers[0x230 * 20]; // PartyMember type
         [FieldOffset(0x3D40)] public uint Unk_3D40;
         [FieldOffset(0x3D44)] public ushort Unk_3D44;
-        [FieldOffset(0x3D48)] public long Unk_3D48;
-        [FieldOffset(0x3D50)] public long Unk_3D50;
+        [FieldOffset(0x3D48)] public long PartyId; // both seem to be unique per party and replicated to every member
+        [FieldOffset(0x3D50)] public long PartyId_2;
         [FieldOffset(0x3D58)] public uint PartyLeaderIndex; // index of party leader in array
         [FieldOffset(0x3D5C)] public byte MemberCount;
         [FieldOffset(0x3D5D)] public byte Unk_3D5D;
