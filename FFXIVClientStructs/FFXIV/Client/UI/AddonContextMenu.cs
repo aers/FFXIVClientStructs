12ae8ab775b919884775fd59096d605d4bd22066
goaaats <16760685+goaaats@users.noreply.github.com>
2022-01-27 14:38:35 +0100

    cleanup AddonContextMenu

--- FFXIVClientStructs/FFXIV/Client/UI/AddonContextMenu.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonContextMenu.cs
@@ -6,9 +6,8 @@ namespace FFXIVClientStructs.FFXIV.Client.UI
     [StructLayout(LayoutKind.Explicit)]
     public struct AddonContextMenu
     {
-        [FieldOffset(0x0)] public unsafe AddonInterface AddonInterface;
+        [FieldOffset(0x0)] public AtkUnitBase AtkUnitBase;
         [FieldOffset(0x160)] public unsafe AtkValue* AtkValues;
         [FieldOffset(0x1CA)] public ushort AtkValuesCount;
-        [FieldOffset(0x690)] public bool IsInitialMenu;
     }
 }
