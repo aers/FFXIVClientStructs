12e55a28cb857832189d1be1be2492c566fffcf6
pohky <otoriosu@gmail.com>
2022-01-29 12:25:16 +0100

    Create WeaponState.cs

---
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/WeaponState.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace FFXIVClientStructs.FFXIV.Client.Game.UI {
+    [StructLayout(LayoutKind.Explicit, Size = 0x20)]
+    public struct WeaponState {
+        [FieldOffset(0x08)] public byte WeaponUnsheathed;
+        [FieldOffset(0x0C)] public float SheatheTimer;
+        [FieldOffset(0x10)] public float AutoSheathDelayTimer;
+        [FieldOffset(0x14)] public byte AutoSheatheState;
+        [FieldOffset(0x18)] public byte IsAutoAttacking;
+    }
+}
