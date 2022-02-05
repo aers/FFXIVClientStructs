6fa4d6e38c3892c64e11f45a45af0c2120922672
aers <aers00@gmail.com>
2022-02-05 02:48:06 -0800

    fix actionmanager

--- FFXIVClientStructs/FFXIV/Client/Game/ActionManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/ActionManager.cs
@@ -21,12 +21,6 @@ namespace FFXIVClientStructs.FFXIV.Client.Game {
         [MemberFunction("E8 ?? ?? ?? ?? 8B F8 3B DF")]
         public partial uint GetAdjustedActionId(uint actionID);
 
-        [MemberFunction("E8 ?? ?? ?? ?? 8B D6 41 8B CF")]
-        public partial float GetAdjustedRecastTime(ActionType actionType, uint actionID, byte a3 = 1);
-
-        [MemberFunction("E8 ?? ?? ?? ?? 33 D2 49 8B CE 66 44 0F 6E C0")]
-        public partial float GetAdjustedCastTime(ActionType actionType, uint actionID, byte a3 = 1, byte a4 = 0);
-
         [MemberFunction("E8 ?? ?? ?? ?? 0F 2F C7 0F 28 7C 24")]
         public partial float GetRecastTime(ActionType actionType, uint actionID);
 
@@ -41,6 +35,9 @@ namespace FFXIVClientStructs.FFXIV.Client.Game {
 
         [MemberFunction("E8 ?? ?? ?? ?? 0F 57 FF 48 85 C0")]
         public partial RecastDetail* GetRecastGroupDetail(int recastGroup);
+        
+        [MemberFunction("E8 ?? ?? ?? ?? 85 C0 75 75 83 FF 03")]
+        public partial uint CheckActionResources(ActionType actionType, uint actionId, void* actionData = null);
 
         [MemberFunction("E8 ?? ?? ?? ?? F3 0F 11 43 ?? 80 3B 00", IsStatic = true)]
         public static partial float GetActionRange(uint actionId);
@@ -50,9 +47,12 @@ namespace FFXIVClientStructs.FFXIV.Client.Game {
 
         [MemberFunction("E8 ?? ?? ?? ?? 48 8B 5C 24 ?? 48 83 C4 30 5F C3 33 D2", IsStatic = true)]
         public static partial int GetActionCost(ActionType actionType, uint actionId, byte a3, byte a4, byte a5, byte a6);
+        
+        [MemberFunction("E8 ?? ?? ?? ?? 8B D6 41 8B CF", IsStatic = true)]
+        public static partial float GetAdjustedRecastTime(ActionType actionType, uint actionID, byte a3 = 1);
 
-        [MemberFunction("E8 ?? ?? ?? ?? 85 C0 75 75 83 FF 03")]
-        public partial uint CheckActionResources(ActionType actionType, uint actionId, void* actionData = null);
+        [MemberFunction("E8 ?? ?? ?? ?? 33 D2 49 8B CE 66 44 0F 6E C0", IsStatic = true)]
+        public static partial float GetAdjustedCastTime(ActionType actionType, uint actionID, byte a3 = 1, byte* a4 = null);
 
         [MemberFunction("E8 ?? ?? ?? ?? 33 DB 8B C8", IsStatic = true)]
         public static partial ushort GetMaxCharges(uint actionId, uint level); // 0 for current level
