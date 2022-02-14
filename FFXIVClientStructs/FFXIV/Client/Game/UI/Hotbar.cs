fce10558e1968147f39b9b64799054ffdfca91b4
pohky <otoriosu@gmail.com>
2022-02-14 21:08:56 +0100

    Rename IsActionUnlocked
    renamed and moved it to UIState.
    old function marked as obsolete for now

--- FFXIVClientStructs/FFXIV/Client/Game/UI/Hotbar.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/Hotbar.cs
@@ -1,4 +1,6 @@
-﻿namespace FFXIVClientStructs.FFXIV.Client.Game.UI;
+﻿using System;
+
+namespace FFXIVClientStructs.FFXIV.Client.Game.UI;
 
 [StructLayout(LayoutKind.Explicit, Size = 0x08)]
 public partial struct Hotbar
@@ -6,6 +8,6 @@ public partial struct Hotbar
     [MemberFunction("48 83 EC 38 33 D2 C7 44 24 ?? ?? ?? ?? ?? 45 33 C9")]
     public partial void CancelCast();
 
-    [MemberFunction("E8 ?? ?? ?? ?? 88 45 80")]
+    [MemberFunction("E8 ?? ?? ?? ?? 88 45 80"), Obsolete("This is wrong. Use UIState.IsUnlockLinkUnlocked() instead.", false)]
     public partial bool IsActionUnlocked(uint actionId);
 }
