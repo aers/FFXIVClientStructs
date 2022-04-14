6a313d67e262d54dff86e9909f17dfd45317542e
Cara <git@caraxian.com>
2022-04-14 09:39:10 +0930

    Update RaptureGearsetModule.cs

--- FFXIVClientStructs/FFXIV/Client/UI/Misc/RaptureGearsetModule.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Misc/RaptureGearsetModule.cs
@@ -18,7 +18,7 @@ public unsafe struct RaptureGearsetModule
     [StructLayout(LayoutKind.Sequential, Size = 0xAF2C)]
     public struct Gearsets
     {
-        private fixed byte data[0xAF2C];
+        private fixed byte data[0x1C0 * 100];
 
         public GearsetEntry* this[int i]
         {
@@ -53,29 +53,31 @@ public unsafe struct RaptureGearsetModule
         public uint ItemID;
     }
 
-    [StructLayout(LayoutKind.Explicit, Size = 0x1BC)]
+    [StructLayout(LayoutKind.Explicit, Size = 0x1C0)]
     public struct GearsetEntry
     {
         [FieldOffset(0x000)] public byte ID;
         [FieldOffset(0x001)] public fixed byte Name[0x2F];
         [FieldOffset(0x31)] public byte ClassJob;
         [FieldOffset(0x32)] public byte GlamourSetLink;
-        [FieldOffset(0x33)] public GearsetFlag Flags;
-
-        [FieldOffset(0x34)] public fixed byte ItemsData[0x188];
-        [FieldOffset(0x34)] public GearsetItem MainHand;
-        [FieldOffset(0x50)] public GearsetItem OffHand;
-        [FieldOffset(0x6C)] public GearsetItem Head;
-        [FieldOffset(0x88)] public GearsetItem Body;
-        [FieldOffset(0xA4)] public GearsetItem Hands;
-        [FieldOffset(0xC0)] public GearsetItem Belt;
-        [FieldOffset(0xDC)] public GearsetItem Legs;
-        [FieldOffset(0xF8)] public GearsetItem Feet;
-        [FieldOffset(0x114)] public GearsetItem Ears;
-        [FieldOffset(0x130)] public GearsetItem Neck;
-        [FieldOffset(0x14C)] public GearsetItem Wrists;
-        [FieldOffset(0x168)] public GearsetItem RingRight;
-        [FieldOffset(0x184)] public GearsetItem RightLeft;
-        [FieldOffset(0x1A0)] public GearsetItem SoulStone;
+        [FieldOffset(0x37)] public GearsetFlag Flags;
+        
+        private const int ItemDataOffset = 0x38;
+        private const int ItemDataSize = 0x1C;
+        [FieldOffset(ItemDataOffset)] public fixed byte ItemsData[0x188];
+        [FieldOffset(ItemDataOffset + ItemDataSize * 00)] public GearsetItem MainHand;
+        [FieldOffset(ItemDataOffset + ItemDataSize * 01)] public GearsetItem OffHand;
+        [FieldOffset(ItemDataOffset + ItemDataSize * 02)] public GearsetItem Head;
+        [FieldOffset(ItemDataOffset + ItemDataSize * 03)] public GearsetItem Body;
+        [FieldOffset(ItemDataOffset + ItemDataSize * 04)] public GearsetItem Hands;
+        [FieldOffset(ItemDataOffset + ItemDataSize * 05)] public GearsetItem Belt;
+        [FieldOffset(ItemDataOffset + ItemDataSize * 06)] public GearsetItem Legs;
+        [FieldOffset(ItemDataOffset + ItemDataSize * 07)] public GearsetItem Feet;
+        [FieldOffset(ItemDataOffset + ItemDataSize * 08)] public GearsetItem Ears;
+        [FieldOffset(ItemDataOffset + ItemDataSize * 09)] public GearsetItem Neck;
+        [FieldOffset(ItemDataOffset + ItemDataSize * 10)] public GearsetItem Wrists;
+        [FieldOffset(ItemDataOffset + ItemDataSize * 11)] public GearsetItem RingRight;
+        [FieldOffset(ItemDataOffset + ItemDataSize * 12)] public GearsetItem RightLeft;
+        [FieldOffset(ItemDataOffset + ItemDataSize * 13)] public GearsetItem SoulStone;
     }
 }
