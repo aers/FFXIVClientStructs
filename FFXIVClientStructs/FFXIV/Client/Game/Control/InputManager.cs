19de84586578de15c3741f514924ffda03e749f1
Cara <git@caraxian.com>
2022-04-25 10:41:00 +0930

    Add `InputManager.IsAutoRunning`

---
+++ FFXIVClientStructs/FFXIV/Client/Game/Control/InputManager.cs
@@ -0,0 +1,8 @@
+﻿namespace FFXIVClientStructs.FFXIV.Client.Game.Control; 
+
+[StructLayout(LayoutKind.Explicit, Size = 0x8)]
+public unsafe partial struct InputManager {
+    
+    [MemberFunction("E8 ?? ?? ?? ?? 3A C3 74 0C", IsStatic = true)]
+    public static partial bool IsAutoRunning();
+}
