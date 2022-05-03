bac905b4db8a3710f8b62a872c758269003d91d5
pohky <otoriosu@gmail.com>
2022-05-03 16:50:16 +0200

    Create AddonActionBarX.cs

---
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonActionBarX.cs
@@ -0,0 +1,6 @@
+﻿namespace FFXIVClientStructs.FFXIV.Client.UI;
+
+[StructLayout(LayoutKind.Explicit, Size = 0x298)]
+public unsafe partial struct AddonActionBarX {
+    [FieldOffset(0x00)] public AddonActionBarBase AddonActionBarBase;
+}
