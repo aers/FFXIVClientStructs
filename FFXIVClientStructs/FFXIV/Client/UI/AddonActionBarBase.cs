925693b5006c8e6aeb1319e7c5bd193a6b8d817a
pohky <otoriosu@gmail.com>
2022-05-03 16:50:05 +0200

    Create AddonActionBarBase.cs

---
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonActionBarBase.cs
@@ -0,0 +1,11 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI; 
+
+[StructLayout(LayoutKind.Explicit, Size = 0x248)]
+public unsafe partial struct AddonActionBarBase {
+    [FieldOffset(0x00)] public AtkUnitBase AtkUnitBase;
+
+    [VirtualFunction(76)]
+    public partial void PulseActionBarSlot(int slotIndex);
+}
