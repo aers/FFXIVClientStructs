520ba103a37824d10b686600780a35b011d3290d
Cara <git@caraxian.com>
2022-05-13 16:11:20 +0930

    Update AgentSalvage.cs

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentSalvage.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentSalvage.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.System.Framework;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace FFXIVClientStructs.FFXIV.Client.UI.Agent; 
 
+[Agent(AgentId.Salvage)]
 [StructLayout(LayoutKind.Explicit, Size = 0x3E0)]
 public unsafe struct AgentSalvage {
+    public AgentSalvage* Instance() => Framework.Instance()->GetUiModule()->GetAgentModule()->GetAgentSalvage();
+    
     [FieldOffset(0x00)] public AgentInterface AgentInterface;
 
     [FieldOffset(0x398)] public InventoryItem* DesynthItemSlot;
