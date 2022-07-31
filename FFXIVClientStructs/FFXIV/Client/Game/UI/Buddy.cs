fa22b3988b0115ef12ac402d592e0600066f4b4e
Cara <git@caraxian.com>
2022-07-31 15:54:52 +0930

    Add Buddy.IsBuddyEquipUnlocked

--- FFXIVClientStructs/FFXIV/Client/Game/UI/Buddy.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/Buddy.cs
@@ -2,7 +2,7 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.UI;
 
 // ctor E8 ? ? ? ? 48 89 B3 ? ? ? ? 48 8D 05 ? ? ? ? 48 89 B3 ? ? ? ? 
 [StructLayout(LayoutKind.Explicit, Size = 0xED8)]
-public unsafe struct Buddy
+public unsafe partial struct Buddy
 {
     [StructLayout(LayoutKind.Explicit, Size = 0x198)]
     public struct BuddyMember
@@ -25,6 +25,7 @@ public unsafe struct Buddy
     [FieldOffset(0x198)] public BuddyMember Pet;
     [FieldOffset(0x330)] public fixed byte BattleBuddies[0x198 * 7]; // BuddyMember array for Squadron/Trust
     [FieldOffset(0xE58)] public BuddyMember* CompanionPtr;
+    [FieldOffset(0xE58)] private fixed byte BuddyEquipUnlock[1];
     [FieldOffset(0xE60)] public float TimeLeft;
     [FieldOffset(0xE73)] public fixed byte Name[21];
     [FieldOffset(0xE88)] public uint CurrentXP;
@@ -40,4 +41,13 @@ public unsafe struct Buddy
     [FieldOffset(0xE95)] public byte Mounted; // bool
     [FieldOffset(0xEA0)] public BuddyMember* PetPtr;
     [FieldOffset(0xEB0)] public BuddyMember* SquadronTrustPtr;
+
+    public bool IsBuddyEquipUnlocked(uint buddyEquipId) {
+        fixed(byte* p = BuddyEquipUnlock) {
+            return IsBuddyEquipUnlockedInternal(p, buddyEquipId);
+        }
+    }
+    
+    [MemberFunction("E9 ?? ?? ?? ?? 0F B7 50 02 41 B8", IsPrivate = true, IsStatic = true)]
+    private static partial bool IsBuddyEquipUnlockedInternal(void* ptr, uint buddyEquipId);
 }
