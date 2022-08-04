15e79803f3abbed011795bbb383f78b98e265753
aers <aers00@gmail.com>
2022-08-03 17:47:54 -0700

    Update Status (fix #222)

--- FFXIVClientStructs/FFXIV/Client/Game/Status.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Status.cs
@@ -4,11 +4,13 @@
 public struct Status
 {
     [FieldOffset(0x0)] public ushort StatusID;
+    // this contains different information depending on the type of status
+    // debuffs - stack count
+    // food/potions - ID of the food/potion in the ItemFood sheet
+    [FieldOffset(0x2)] public ushort Param;
+    // remains for compatibility
     [FieldOffset(0x2)] public byte StackCount;
-    [FieldOffset(0x3)] public byte Param;
-
     [FieldOffset(0x4)] public float RemainingTime;
-
     // objectID matching the entity that cast the effect - regens will be from the white mage ID etc
     [FieldOffset(0x8)] public uint SourceID;
 }
