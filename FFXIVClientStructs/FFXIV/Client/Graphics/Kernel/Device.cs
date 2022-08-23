1676923fa915b27f2715db120a9a6c0fef1a93bc
aers <aers00@gmail.com>
2022-08-23 00:27:56 -0700

    fix Device

--- FFXIVClientStructs/FFXIV/Client/Graphics/Kernel/Device.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Kernel/Device.cs
@@ -7,13 +7,13 @@ public unsafe partial struct Device
 {
     [FieldOffset(0x8)] public void* ContextArray; // Client::Graphics::Kernel::Context array
     [FieldOffset(0x10)] public void* RenderThread; // Client::Graphics::Kernel::RenderThread
-    [FieldOffset(0x58)] public SwapChain* SwapChain;
-    [FieldOffset(0x1E8)] public int D3DFeatureLevel; // D3D_FEATURE_LEVEL enum
-    [FieldOffset(0x1F0)] public void* DXGIFactory; // IDXGIFactory1
-    [FieldOffset(0x1F8)] public void* DXGIOutput; // IDXGIOutput6
-    [FieldOffset(0x200)] public void* D3D11Forwarder; // CID3D11Forwarder (ID3D11Device vtbl present here)
-    [FieldOffset(0x208)] public void* D3D11DeviceContext; // ID3D11DeviceContext5
-    [FieldOffset(0x218)] public void* ImmediateContext; // Client::Graphics::Kernel::Device::ImmediateContext
+    [FieldOffset(0x70)] public SwapChain* SwapChain;
+    [FieldOffset(0x220)] public int D3DFeatureLevel; // D3D_FEATURE_LEVEL enum
+    [FieldOffset(0x228)] public void* DXGIFactory; // IDXGIFactory1
+    [FieldOffset(0x230)] public void* DXGIOutput; // IDXGIOutput6
+    [FieldOffset(0x238)] public void* D3D11Forwarder; // CID3D11Forwarder (ID3D11Device vtbl present here)
+    [FieldOffset(0x240)] public void* D3D11DeviceContext; // ID3D11DeviceContext5
+    [FieldOffset(0x250)] public void* ImmediateContext; // Client::Graphics::Kernel::Device::ImmediateContext
 
     [StaticAddress("48 8B 0D ?? ?? ?? ?? 48 8D 54 24 ?? F3 0F 10 44 24", isPointer: true)]
     public static partial Device* Instance();
