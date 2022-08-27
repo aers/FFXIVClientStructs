22c569a7d06ed4d2ed6a5ba55440282d5d18ea46
Raymond <daemitus@gmail.com>
2022-08-27 08:30:46 -0400

    add AgentContentsFinder

---
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentContentsFinder.cs
@@ -0,0 +1,24 @@
+﻿using FFXIVClientStructs.FFXIV.Client.Game.Object;
+using FFXIVClientStructs.FFXIV.Client.System.Framework;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using System;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+// Client::UI::Agent::AgentContentsFinder
+//   Client::UI::Agent::AgentInterface
+//     Component::GUI::AtkModuleInterface::AtkEventInterface
+[Agent(AgentId.ContentsFinder)]
+[StructLayout(LayoutKind.Explicit, Size = 0x2050)]
+public unsafe partial struct AgentContentsFinder
+{
+    public static AgentContentsFinder* Instance() => (AgentContentsFinder*)Framework.Instance()->GetUiModule()->GetAgentModule()->GetAgentByInternalId(AgentId.ContentsFinder);
+
+    [FieldOffset(0x0)] public AgentInterface AgentInterface;
+
+    [MemberFunction("48 89 6C 24 ?? 48 89 74 24 ?? 57 48 81 EC ?? ?? ?? ?? 48 8B F9 41 0F B6 E8")]
+    public partial IntPtr OpenRegularDuty(uint contentsFinderCondition, byte a2 = 0);
+
+    [MemberFunction("E9 ?? ?? ?? ?? 8B 93 ?? ?? ?? ?? 48 83 C4 20")]
+    public partial IntPtr OpenRouletteDuty(byte roulette, byte a2 = 0);
+}
