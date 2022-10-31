1ff6e4e87fcda058970f82b307233fde36d38a88
pohky <otoriosu@gmail.com>
2022-10-31 12:32:46 +0100

    Create CameraManager.cs

---
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Scene/CameraManager.cs
@@ -0,0 +1,18 @@
+﻿namespace FFXIVClientStructs.FFXIV.Client.Graphics.Scene; 
+
+[StructLayout(LayoutKind.Explicit, Size = 0x100)]
+public unsafe partial struct CameraManager {
+
+	[FieldOffset(0x50)] public int CameraIndex;
+	[FieldOffset(0x58)] public fixed byte CameraArray[14 * 8]; //14 * Camera*
+
+	public Camera* CurrentCamera {
+		get {
+			fixed (byte* ptr = CameraArray)
+				return ((Camera**)ptr)[CameraIndex];
+		}
+	}
+
+	[StaticAddress("48 8B 05 ?? ?? ?? ?? 48 63 3A", isPointer: true)]
+	public static partial CameraManager* Instance();
+}
