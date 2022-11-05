8506108f3772ddcb608cf7cf0612ce6cb2b9bbf7
aers <aers00@gmail.com>
2022-11-04 17:55:44 -0700

    reso stuff

--- FFXIVClientStructs/FFXIV/Client/Graphics/Kernel/Device.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Kernel/Device.cs
@@ -10,6 +10,11 @@ public unsafe partial struct Device
     [FieldOffset(0x8)] public void* ContextArray; // Client::Graphics::Kernel::Context array
     [FieldOffset(0x10)] public void* RenderThread; // Client::Graphics::Kernel::RenderThread
     [FieldOffset(0x70)] public SwapChain* SwapChain;
+    [FieldOffset(0x7A)] public byte RequestResolutionChange;
+    [FieldOffset(0x88)] public uint Width;
+    [FieldOffset(0x8C)] public uint Height;
+    [FieldOffset(0x1C0)] public uint NewWidth;
+    [FieldOffset(0x1C4)] public uint NewHeight;
     [FieldOffset(0x220)] public int D3DFeatureLevel; // D3D_FEATURE_LEVEL enum
     [FieldOffset(0x228)] public void* DXGIFactory; // IDXGIFactory1
     [FieldOffset(0x230)] public void* DXGIOutput; // IDXGIOutput6
