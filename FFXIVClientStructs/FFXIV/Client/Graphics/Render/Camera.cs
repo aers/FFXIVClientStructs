dfc25162c4dee72f854f99d07c8d30bbf60f0371
chirp <ch1rp@protonmail.com>
2022-11-13 05:51:08 +0000

    render camera projection

--- FFXIVClientStructs/FFXIV/Client/Graphics/Render/Camera.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Render/Camera.cs
@@ -4,4 +4,10 @@
 public struct Camera
 {
     [FieldOffset(0x00)] public ReferencedClassBase ReferencedClassBase;
+
+    [FieldOffset(0x50)] public Matrix44 ProjectionMatrix;
+    [FieldOffset(0xA4)] public float FoV;
+    [FieldOffset(0xA8)] public float AspectRatio;
+    [FieldOffset(0xAC)] public float NearPlane;
+    [FieldOffset(0xB0)] public float FarPlane;
 }
