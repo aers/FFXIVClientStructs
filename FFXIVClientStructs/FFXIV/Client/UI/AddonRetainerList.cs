0d3e0a8517dbdefd7af1c8d8cd2b213154872b91
JeffyW <JeffyW@gmail.com>
2022-11-22 09:37:18 -0800

    A bunch of OnSetup signatures.

---
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonRetainerList.cs
@@ -0,0 +1,15 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI;
+
+// Client::UI::AddonRetainerTaskList
+//   Component::GUI::AtkUnitBase
+//     Component::GUI::AtkEventListener
+[StructLayout(LayoutKind.Explicit, Size = 0x8)]
+public unsafe partial struct AddonRetainerList
+{
+    [FieldOffset(0x0)] public AtkUnitBase AtkUnitBase;
+
+    [MemberFunction("48 89 5C 24 ?? 55 56 57 41 56 41 57 48 81 EC 00 01 00 00 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 84 ?? ?? ?? 00 00 49 8B F8 48 8B F1 E8 ?? ?? ?? ??")]
+    public partial void OnSetup(uint a2, byte* data);
+}
--- FFXIVClientStructs/FFXIV/Client/UI/AddonRetainerTaskResult.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonRetainerTaskResult.cs
@@ -6,9 +6,12 @@ namespace FFXIVClientStructs.FFXIV.Client.UI;
 //   Component::GUI::AtkUnitBase
 //     Component::GUI::AtkEventListener
 [StructLayout(LayoutKind.Explicit, Size = 0x258)]
-public unsafe struct AddonRetainerTaskResult
+public unsafe partial struct AddonRetainerTaskResult
 {
     [FieldOffset(0x0)] public AtkUnitBase AtkUnitBase;
     [FieldOffset(0x240)] public AtkComponentButton* ReassignButton;
     [FieldOffset(0x248)] public AtkComponentButton* ConfirmButton;
+
+    [MemberFunction("48 89 5C 24 ?? 55 56 57 48 83 EC 40 8B F2 49 8B F8 BA ?? ?? ?? ?? 48 8B D9 E8 ?? ?? ?? ??")]
+    public partial void OnSetup(uint a2, byte* data);
 }
