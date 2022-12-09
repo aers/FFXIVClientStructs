aa8cf2754333fcc98b3e10c0fc9461e4de22a49d
pohky <otoriosu@gmail.com>
2022-12-09 20:43:12 +0100

    Update Director.cs

--- FFXIVClientStructs/FFXIV/Client/Game/Event/Director.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Event/Director.cs
@@ -3,10 +3,15 @@
 namespace FFXIVClientStructs.FFXIV.Client.Game.Event;
 
 [StructLayout(LayoutKind.Explicit, Size = 0x4B8)]
-public struct Director
+public unsafe struct Director
 {
-    [FieldOffset(0x00)] public LuaEventHandler LuaEventHandler;
-    [FieldOffset(0x350)] public Utf8String String0;
-    [FieldOffset(0x3B8)] public Utf8String String1;
-    [FieldOffset(0x420)] public Utf8String String2;
+	[FieldOffset(0x00)] public LuaEventHandler LuaEventHandler;
+	[FieldOffset(0x330)] public EventHandlerInfo* EventHandlerInfo;
+	[FieldOffset(0x338)] public uint ContentId;
+	[FieldOffset(0x33C)] public byte ContentFlags; // 1 = Tourism (Explorer Mode)
+	[FieldOffset(0x340)] public byte Sequence;
+	[FieldOffset(0x342)] public fixed byte UnionData[10]; // I8A-I8J, UI8A-UI8J, Branch etc.
+	[FieldOffset(0x350)] public Utf8String String0;
+	[FieldOffset(0x3B8)] public Utf8String String1;
+	[FieldOffset(0x420)] public Utf8String String2;
 }
--- FFXIVClientStructs/FFXIV/Client/Game/Event/EventHandler.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Event/EventHandler.cs
@@ -3,5 +3,13 @@
 [StructLayout(LayoutKind.Explicit, Size = 0x210)]
 public unsafe struct EventHandler
 {
-    [FieldOffset(0x18)] public void* EventSceneModule;
+	[FieldOffset(0x18)] public EventSceneModuleUsualImpl* EventSceneModule;
+	[FieldOffset(0x20)] public EventHandlerInfo Info;
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0x38)]
+public struct EventHandlerInfo {
+	[FieldOffset(0x00)] public ushort ContentId;
+	[FieldOffset(0x02)] public ushort EventHandlerType;
+	[FieldOffset(0x04)] public bool IsActive;
 }
