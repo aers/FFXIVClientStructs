989048257ca6e27be43f443d31811062e4424836
JeffyW <JeffyW@gmail.com>
2023-01-07 13:29:33 -0800

    Remove OnSetup calls (which could be VirtualFunctions).  Need to look for a way to define virtual functions and auto-generate addresses.

--- FFXIVClientStructs/FFXIV/Client/UI/AddonRetainerTaskList.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonRetainerTaskList.cs
@@ -6,10 +6,7 @@ namespace FFXIVClientStructs.FFXIV.Client.UI;
 //   Component::GUI::AtkUnitBase
 //     Component::GUI::AtkEventListener
 [StructLayout(LayoutKind.Explicit, Size = 0x8)]
-public unsafe partial struct AddonRetainerTaskList
+public struct AddonRetainerTaskList
 {
     [FieldOffset(0x0)] public AtkUnitBase AtkUnitBase;
-
-    [MemberFunction("48 89 5C 24 ?? 55 56 57 48 81 EC 10 01 00 00 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 84 ?? ?? ?? ?? ?? 8B DA 49 8B F0 BA 03 00 00 00 48 8B F9 E8 ?? ?? ?? ??")]
-    public partial void OnSetup(uint a2, byte* data);
 }
--- FFXIVClientStructs/FFXIV/Client/UI/AddonSelectString.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonSelectString.cs
@@ -6,14 +6,11 @@ namespace FFXIVClientStructs.FFXIV.Client.UI;
 //   Component::GUI::AtkUnitBase
 //     Component::GUI::AtkEventListener
 [StructLayout(LayoutKind.Explicit, Size = 0x2A0)]
-public unsafe partial struct AddonSelectString
+public struct AddonSelectString
 {
     [FieldOffset(0x0)] public AtkUnitBase AtkUnitBase;
     [FieldOffset(0x220)] public PopupMenuDerive PopupMenu;
     
-    [MemberFunction("40 53 56 57 41 54 41 55 41 57 48 83 EC 48 4D 8B F8 44 8B E2 48 8B F1 E8 ?? ?? ?? ??")]
-    public partial void OnSetup(uint a2, byte* data);
-
     [StructLayout(LayoutKind.Explicit, Size = 0x68)]
     public struct PopupMenuDerive
     {
