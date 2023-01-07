1bbb48a7e1eca8c73a549400808f4e2a89f7fa14
JeffyW <JeffyW@gmail.com>
2023-01-07 13:59:28 -0800

    AddonChocoboBreedTraining

---
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonChocoboBreedTraining.cs
@@ -0,0 +1,15 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI;
+
+// Client::UI::AddonChocoboBreedTraining
+//   Component::GUI::AtkUnitBase
+//     Component::GUI::AtkEventListener
+[StructLayout(LayoutKind.Explicit, Size = 0x230)]
+public unsafe struct AddonChocoboBreedTraining
+{
+    [FieldOffset(0x0)] public AtkUnitBase AtkUnitBase;
+
+    [FieldOffset(0x220)] public AtkComponentButton* CommenceButton;
+    [FieldOffset(0x228)] public AtkComponentButton* CancelButton;
+}
