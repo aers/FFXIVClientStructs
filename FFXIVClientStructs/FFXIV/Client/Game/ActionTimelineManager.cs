dc577549bc7d81fdf82e52b98867ff82db85c53b
Asgard <95163444+AsgardXIV@users.noreply.github.com>
2023-01-10 11:40:56 -0700

    ActionTimelineManager tweaks

--- FFXIVClientStructs/FFXIV/Client/Game/ActionTimelineManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/ActionTimelineManager.cs
@@ -1,19 +1,25 @@
 ﻿namespace FFXIVClientStructs.FFXIV.Client.Game;
 
-[StructLayout(LayoutKind.Explicit)]
+[StructLayout(LayoutKind.Explicit, Size = 0x340)]
 public unsafe partial struct ActionTimelineManager
+{
+    [FieldOffset(0x10)] public ActionTimelineDriver Driver;
+
+    [FieldOffset(0x2C0)] public float OverallSpeed; // The overall speed which is applied to all slots as well as things like particles attached to the owner
+
+    [FieldOffset(0x2DC)] public ushort BaseOverride; // Forces base animation when character is in a Normal or AnimLock state
+
+    [FieldOffset(0x2DE)] public ushort LipsOverride; // Forces the character lips to play timeline
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0x1F0)]
+public unsafe partial struct ActionTimelineDriver
 {
     public const int TimelineSlotCount = 13;
 
-    [FieldOffset(0x0E0)] public unsafe fixed ushort TimelineIds[TimelineSlotCount]; // The timeline active in each slot or 0 when none
+    [FieldOffset(0xE0)] public unsafe fixed ushort TimelineIds[TimelineSlotCount]; // The timeline active in each slot or 0 when none
 
     [FieldOffset(0x154)] public unsafe fixed float TimelineSpeeds[TimelineSlotCount]; // Speed for each slot
-
-    [FieldOffset(0x2B0)] public float OverallSpeed; // The overall speed which is applied to all slots as well as things like particles attached to the owner
-
-    [FieldOffset(0x2CC)] public ushort BaseOverride; // Forces base animation when character is in a Normal or AnimLock state
-
-    [FieldOffset(0x2CE)] public ushort LipsOverride; // Forces the character lips to play timeline
 
     [MemberFunction("E8 ?? ?? ?? ?? 4C 8B BC 24 ?? ?? ?? ?? 4C 8D 9C 24 ?? ?? ?? ?? 49 8B 5B ??")]
     public partial void PlayTimeline(ushort actionTimelineId, void* a2 = null); // Determines which slot the timeline belongs in and then plays it on that slot
