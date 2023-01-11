89d151ea071c9a857983e5dd9bf918322ae8a65b
Asgard <95163444+AsgardXIV@users.noreply.github.com>
2023-01-10 21:05:41 -0700

    6.3 Ornament offsets

--- FFXIVClientStructs/FFXIV/Client/Game/Character/Ornament.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Character/Ornament.cs
@@ -6,10 +6,12 @@
 // ornament = accessory
 
 // ctor E8 ?? ?? ?? ?? 48 8B C8 EB 03 48 8B CF 48 8B 46 08 45 33 C9
-[StructLayout(LayoutKind.Explicit, Size = 0x1B10)]
+[StructLayout(LayoutKind.Explicit, Size = 0x1B30)]
 public struct Ornament
 {
     [FieldOffset(0x0)] public Character Character;
 
-    [FieldOffset(0x1B00)] public uint OrnamentId;
+    [FieldOffset(0x1B20)] public uint OrnamentId;
+    [FieldOffset(0x1B24)] public byte AttachmentPoint;
+
 }
