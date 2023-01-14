81d006ac20db7cd5574509d823c4fa3d83ed854b
Ottermandias <nickroederspam+git1@gmail.com>
2023-01-14 16:20:03 +0100

    Add some stuff for portrait agents.

---
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentBannerParty.cs
@@ -0,0 +1,73 @@
+﻿using FFXIVClientStructs.FFXIV.Client.Game.Object;
+using FFXIVClientStructs.FFXIV.Client.System.Framework;
+using FFXIVClientStructs.FFXIV.Client.System.String;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+// Client::UI::Agent::AgentBannerParty
+//   Client::UI::Agent::AgentBannerInterface
+//     Client::UI::Agent::AgentInterface
+//       Component::GUI::AtkModuleInterface::AtkEventInterface
+[Agent(AgentId.BannerParty)]
+[StructLayout(LayoutKind.Explicit, Size = 0x38)]
+public unsafe partial struct AgentBannerParty
+{
+    public static AgentBannerParty* Instance() => (AgentBannerParty*)Framework.Instance()->GetUiModule()->GetAgentModule()->GetAgentByInternalId(AgentId.BannerParty);
+
+
+    [FieldOffset(0x0)] public AgentInterface AgentInterface;
+
+    [FieldOffset(0x28)] public Storage* Data;
+
+    // Client::UI::Agent::AgentBannerInterface::Storage
+    // ctor E8 ?? ?? ?? ?? 48 8B F0 48 89 73 ?? C6 06
+    // destructed in Client::UI::Agent::AgentBannerInterface::dtor
+    [StructLayout(LayoutKind.Explicit, Size = 0x3BB0)]
+    public struct Storage
+    {
+        // vtable: 48 8D 05 ?? ?? ?? ?? 48 89 01 48 8B F9 7E 
+        // dtor: E8 ?? ?? ?? ?? 48 83 EF ?? 75 ?? BA ?? ?? ?? ?? 48 8B CE E8 ?? ?? ?? ?? 48 89 7D
+        [StructLayout(LayoutKind.Explicit, Size = 0x770)]
+        public struct Character
+        {
+            [FieldOffset(0x000)] public void** VTable;
+
+            [FieldOffset(0x2B0)] public void** SomeVTable;
+            [FieldOffset(0x2B8)] public int    Unk1;
+
+            [FieldOffset(0x018)] public Utf8String Name1;
+            [FieldOffset(0x080)] public Utf8String Name2;
+            [FieldOffset(0x0E8)] public Utf8String UnkString1;
+            [FieldOffset(0x150)] public Utf8String UnkString2;
+            [FieldOffset(0x1C0)] public Utf8String Job;
+            [FieldOffset(0x240)] public Utf8String UnkString3;
+            [FieldOffset(0x6F8)] public Utf8String Title;
+
+            [FieldOffset(0x2B0)] public CharaView CharaView;
+
+            [FieldOffset(0x5D0)] public AtkTexture AtkTexture;
+
+            [FieldOffset(0x768)] public void* SomePointer;
+
+        }
+
+        [FieldOffset(0x0000)] public void* Agent; // AgentBannerParty, maybe other Banner agents
+        [FieldOffset(0x0008)] public UIModule* UiModule;
+        [FieldOffset(0x0010)] public uint PortraitBitmask; // Maybe
+        [FieldOffset(0x0014)] public uint Unk2;
+
+        [FieldOffset(0x0020)] public Character Character1;
+        [FieldOffset(0x0790)] public Character Character2;
+        [FieldOffset(0x0F00)] public Character Character3;
+        [FieldOffset(0x1670)] public Character Character4;
+        [FieldOffset(0x1DE0)] public Character Character5;
+        [FieldOffset(0x2550)] public Character Character6;
+        [FieldOffset(0x2CC0)] public Character Character7;
+        [FieldOffset(0x3430)] public Character Character8;
+
+        [FieldOffset(0x3BA0)] public long  Unk3;
+        [FieldOffset(0x3BA8)] public long  Unk4;
+    }
+}
---
+++ FFXIVClientStructs/FFXIV/Client/UI/Misc/CharaView.cs
@@ -0,0 +1,55 @@
+﻿using FFXIVClientStructs.FFXIV.Client.Game;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI.Misc;
+
+// Client::UI::Misc::CharaView
+[StructLayout(LayoutKind.Explicit, Size = 0x2C8)]
+public unsafe partial struct CharaView
+{
+    [FieldOffset(0x0)] public void** VTable;
+
+
+    [FieldOffset(0x008)] public uint Unk8;
+    [FieldOffset(0x00C)] public uint CutsceneActorIndex; // 40 to 47
+    [FieldOffset(0x010)] public uint ScreenActorIndex;   // 0 to 7, not 240 to 247
+    [FieldOffset(0x014)] public uint Unk14;
+    [FieldOffset(0x020)] public void* Unk20;
+    [FieldOffset(0x028)] public Camera* Camera;
+
+    [FieldOffset(0x0C0)] public uint UnkC0;
+    [FieldOffset(0x030)] public ulong Unk30;
+    [FieldOffset(0x038)] public ulong Unk38;
+
+    [FieldOffset(0x0D0)] public UnkStruct UnkD0;
+    [FieldOffset(0x0E0)] public UnkStruct UnkE0;
+    [FieldOffset(0x0F0)] public UnkStruct UnkF0;
+    [FieldOffset(0x100)] public UnkStruct Unk100;
+    [FieldOffset(0x110)] public UnkStruct Unk110;
+    [FieldOffset(0x120)] public UnkStruct Unk120;
+    [FieldOffset(0x130)] public UnkStruct Unk130;
+    [FieldOffset(0x140)] public UnkStruct Unk140;
+    [FieldOffset(0x150)] public UnkStruct Unk150;
+    [FieldOffset(0x160)] public UnkStruct Unk160;
+    [FieldOffset(0x170)] public UnkStruct Unk170;
+    [FieldOffset(0x180)] public UnkStruct Unk180;
+    [FieldOffset(0x190)] public UnkStruct Unk190;
+    [FieldOffset(0x1A0)] public UnkStruct Unk1A0;
+
+    [FieldOffset(0x2B8)] public ushort Unk2B8;
+    [FieldOffset(0x290)] public ulong Unk290;
+    [FieldOffset(0x298)] public ulong Unk298;
+    [FieldOffset(0x2A0)] public ulong Unk2A0;
+    [FieldOffset(0x2A8)] public ulong Unk2A8;
+    [FieldOffset(0x2B0)] public ulong Unk2B0;
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x20)]
+    public struct UnkStruct
+    {
+        [FieldOffset(0x00)] public ushort Unk0;
+        [FieldOffset(0x03)] public byte Unk1;
+        [FieldOffset(0x04)] public byte Unk2;
+        [FieldOffset(0x08)] public ulong Unk3;
+        [FieldOffset(0x10)] public ulong Unk4;
+        [FieldOffset(0x18)] public ulong Unk5;
+    }
+}
