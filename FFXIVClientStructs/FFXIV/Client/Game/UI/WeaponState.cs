6625b6179ffe27b04302fc7621c41b61aa1f9d73
pohky <otoriosu@gmail.com>
2023-01-18 13:52:13 +0100

    Update WeaponState.cs

--- FFXIVClientStructs/FFXIV/Client/Game/UI/WeaponState.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/WeaponState.cs
@@ -3,9 +3,10 @@
 [StructLayout(LayoutKind.Explicit, Size = 0x20)]
 public struct WeaponState
 {
-    [FieldOffset(0x08)] public byte WeaponUnsheathed;
-    [FieldOffset(0x0C)] public float SheatheTimer;
-    [FieldOffset(0x10)] public float AutoSheathDelayTimer;
-    [FieldOffset(0x14)] public byte AutoSheatheState;
-    [FieldOffset(0x18)] public byte IsAutoAttacking;
+    [FieldOffset(0x08), Obsolete("Use IsUnsheathed instead.", true)] public byte WeaponUnsheathed;
+    [FieldOffset(0x08)] public bool IsUnsheathed;
+    [FieldOffset(0x0C)] public float SheatheCooldown;
+    [FieldOffset(0x10)] public float AutoSheathTimer;
+    [FieldOffset(0x14)] public bool AutoSheatheState;
+    [FieldOffset(0x18)] public bool IsAutoAttacking;
 }
