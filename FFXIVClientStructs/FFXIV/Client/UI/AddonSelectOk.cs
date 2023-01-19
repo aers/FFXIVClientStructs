40b2a61389f8e0c598f15de75e371f2e77a7bd21
marimelon <29140293+marimelon@users.noreply.github.com>
2023-01-20 02:18:34 +0900

    Add AddonSelectOk

---
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonSelectOk.cs
@@ -0,0 +1,14 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI;
+
+// Client::UI::AddonSelectOk
+//   Component::GUI::AtkUnitBase
+//     Component::GUI::AtkEventListener
+[StructLayout(LayoutKind.Explicit, Size = 0x2A8)]
+public unsafe struct AddonSelectOk
+{
+    [FieldOffset(0x0)] public AtkUnitBase AtkUnitBase;
+    [FieldOffset(0x220)] public AtkTextNode* PromptText;
+    [FieldOffset(0x228)] public AtkComponentButton* OkButton;
+}
