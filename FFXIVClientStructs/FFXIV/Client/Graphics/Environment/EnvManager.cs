c5be50b3485daa21ab5da860c5f9bc22c329b979
Asgard <95163444+AsgardXIV@users.noreply.github.com>
2023-01-24 10:36:01 -0700

    EnvManager

---
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Environment/EnvManager.cs
@@ -0,0 +1,14 @@
+﻿namespace FFXIVClientStructs.FFXIV.Client.Graphics.Environment;
+
+[StructLayout(LayoutKind.Explicit, Size = 0x122)]
+public unsafe partial struct EnvManager
+{
+    [StaticAddress("48 8B 35 ?? ?? ?? ?? 80 BE 73 03 00 00 80", 3, isPointer: true)]
+    public static partial EnvManager* Instance();
+
+    [FieldOffset(0x27)]
+    public byte ActiveWeather;
+
+    [FieldOffset(0x28)]
+    public float TransitionTime;
+}
