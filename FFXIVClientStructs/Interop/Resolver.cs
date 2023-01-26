195a87df1cb94d025c447e2c41d1e2ecbf68aa67
Caraxi <git@caraxian.com>
2023-01-27 09:21:25 +1030

    Remove automatic versioning
    Sets manual version to 1700 to be above the previous version
    Hopefully a better solution will be thought of in the future #308

--- FFXIVClientStructs/Interop/Resolver.cs
+++ FFXIVClientStructs/Interop/Resolver.cs
@@ -10,9 +10,7 @@ namespace FFXIVClientStructs.Interop;
 public sealed partial class Resolver
 {
     private static readonly Lazy<Resolver> Instance = new(() => new Resolver());
-
-    private static readonly Lazy<uint> version = new (() => uint.Parse(GitVersionInformation.CommitsSinceVersionSource));
-    public static uint Version => version.Value;
+    public static uint Version => 1700; // Manually set for now. https://github.com/aers/FFXIVClientStructs/issues/308
     
     private Resolver()
     {
