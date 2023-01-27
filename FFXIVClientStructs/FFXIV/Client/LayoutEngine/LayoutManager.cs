0c56b5320ec6a2ec8c906f984fbffa36fece9878
Asgard <95163444+AsgardXIV@users.noreply.github.com>
2023-01-27 06:00:49 -0700

    Festivals / Events (#310)

--- FFXIVClientStructs/FFXIV/Client/LayoutEngine/LayoutManager.cs
+++ FFXIVClientStructs/FFXIV/Client/LayoutEngine/LayoutManager.cs
@@ -4,12 +4,17 @@ namespace FFXIVClientStructs.FFXIV.Client.LayoutEngine;
 [StructLayout(LayoutKind.Explicit, Size = 0x98)]
 public unsafe partial struct LayoutManager {
     [FieldOffset(0x20)] public uint TerritoryTypeId;
+    [FieldOffset(0x38)] public uint FestivalStatus; // SetActiveFestivals will not allow a change when not 5 or 0
+    [FieldOffset(0x40)] public fixed uint ActiveFestivals[4];
     [FieldOffset(0x80)] public void* HousingController;
     [FieldOffset(0x90)] public IndoorAreaLayoutData* IndoorAreaData;
 
 
     [MemberFunction("E8 ?? ?? ?? ?? 33 C0 48 83 C4 38 C3 33 C0")]
     public partial void SetInteriorFixture(int floor, int part, int fixtureId, byte unk = 255);
+
+    [MemberFunction("E8 ?? ?? ?? ?? 8B C5 EB 6A")]
+    public partial void SetActiveFestivals(uint* festivalArray); // Array of exactly 4 festivals. Use 0 for none.
 }
 
 [StructLayout(LayoutKind.Explicit)]
