5f88e45ed04e8f3a6a6a4b100d3243befa5d8f3f
Koenari <koenari@koenari.de>
2023-01-31 22:01:13 +0100

    more mail

--- FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyMail.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyMail.cs
@@ -4,6 +4,13 @@
 public unsafe partial struct InfoProxyMail
 {
     [FieldOffset(0x00)] public InfoProxyInterface InfoProxyInterface;
+    [FieldOffset(0x10)] public byte NumLetters;
+    [FieldOffset(0x18)] public void* Unk18; //Local pointer (lower) maybe, but not aligned to 8?
+    //VAlues: 000001E1C6BB0102 (AllocBase: 000001E1C6530000) = +680102
+
+    [FieldOffset(0x24)] public byte NumAtachments;
+    [FieldOffset(0x27)] public byte NumLettersFromFriends;
+    [FieldOffset(0x28)] public byte NumPurchases;
     [FixedSizeArray<Entry>(130)]
     [FieldOffset(0x30)] public fixed byte List[130 * 0xa0];
     //0xCC0 After
@@ -19,7 +26,7 @@ public unsafe partial struct InfoProxyMail
         [FieldOffset(0x3C)] public bool Read;
 
         [FieldOffset(0x3F)] public fixed byte Sender[32];
-        [FieldOffset(0x5F)] public fixed byte Betreff[64];
+        [FieldOffset(0x5F)] public fixed byte MessagePreview[64];
 
         [StructLayout(LayoutKind.Explicit, Size = 0x8)]
         public partial struct ItemAttachment
