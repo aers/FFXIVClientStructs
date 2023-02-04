7c516ccc60130e03ab68890aa7bfe786b9040833
pohky <otoriosu@gmail.com>
2023-02-04 14:52:04 +0100

    add LimitBreakController

--- FFXIVClientStructs/FFXIV/Client/Game/UI/UIState.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/UIState.cs
@@ -32,6 +32,7 @@ public unsafe partial struct UIState
     [FieldOffset(0xAA58)] public Map Map;
 
     [FieldOffset(0xEA40)] public MarkingController MarkingController;
+    [FieldOffset(0xECF0)] public LimitBreakController LimitBreakController;
 
     [FieldOffset(0x11A88)] public ContentsFinder ContentsFinder;
 
