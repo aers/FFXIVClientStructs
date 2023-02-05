0af2279f6115fb32572e594321ffaab102df52f6
Caraxi <git@caraxian.com>
2023-02-05 19:02:01 +1030

    Update HousingManager.cs

--- FFXIVClientStructs/FFXIV/Client/Game/Housing/HousingManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Housing/HousingManager.cs
@@ -19,4 +19,23 @@ public unsafe partial struct HousingManager {
 
     [MemberFunction("E8 ?? ?? ?? ?? 48 8D 4B D0 33 D2")]
     public partial bool IsInside();
+    
+    [MemberFunction("E8 ?? ?? ?? ?? 0F B6 C0 3B 46 3C")]
+    public partial sbyte GetCurrentWard();
+
+    // 1 for Main Division, 2 for Subdivision
+    [MemberFunction("E8 ?? ?? ?? ?? 84 C0 74 0F 0F B6 C8")]
+    public partial byte GetCurrentDivision();
+
+    // Apartment / FC Room number
+    [MemberFunction("E8 ?? ?? ?? ?? 0F B7 D8 E8 ?? ?? ?? ?? 48 8B C8")]
+    public partial short GetCurrentRoom();
+
+    // -128 for Apartments in Main Division, -127 for Apartments in Subdivision
+    [MemberFunction("E8 ?? ?? ?? ?? 88 44 24 78")]
+    public partial sbyte GetCurrentPlot();
+    
+    // Unique Identifier
+    [MemberFunction("E8 ?? ?? ?? ?? 83 CA FF 48 8B D8 8D 4A 02")]
+    public partial long GetCurrentHouseId();
 }
