a63415413ef54d488dc8f3379a2f5d5d999c4281
Caraxi <git@caraxian.com>
2023-02-07 18:19:18 +1030

    Update AtkCounterNode.cs

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkCounterNode.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkCounterNode.cs
@@ -1,4 +1,5 @@
 ﻿using FFXIVClientStructs.FFXIV.Client.System.String;
+using FFXIVClientStructs.FFXIV.Client.System.Memory;
 
 namespace FFXIVClientStructs.FFXIV.Component.GUI;
 // Component::GUI::AtkCounterNode
@@ -9,7 +10,7 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI;
 // common CreateAtkNode function E8 ?? ?? ?? ?? 48 8B 4E 08 49 8B D5 
 // type 5
 [StructLayout(LayoutKind.Explicit, Size = 0x128)]
-public unsafe struct AtkCounterNode
+public unsafe partial struct AtkCounterNode : ICreatable
 {
     [FieldOffset(0x0)] public AtkResNode AtkResNode;
     [FieldOffset(0xA8)] public AtkUldPartsList* PartsList;
@@ -20,4 +21,7 @@ public unsafe struct AtkCounterNode
     [FieldOffset(0xB8)] public ushort TextAlign;
     [FieldOffset(0xBC)] public float Width;
     [FieldOffset(0xC0)] public Utf8String NodeText;
+    
+    [MemberFunction("E9 ?? ?? ?? ?? 45 33 C9 4C 8B C0 33 D2 B9 ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 85 C0 74 5D")]
+    public partial void Ctor();
 }
