22cd5134acb11c29f8c9be1849504591b0b639e4
wolfcomp <magnus1997.mho@gmail.com>
2023-02-11 00:18:40 +0100

    add some more data to ExplorationResultData

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentExplorationResult.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentExplorationResult.cs
@@ -26,11 +26,27 @@ public unsafe struct AgentExplorationResultInterface {
 
 [StructLayout(LayoutKind.Explicit, Size = 0x3CB0)]
 public unsafe partial struct ExplorationResultData {
-	[FixedSizeArray<AtkValue>(151)]
-	[FieldOffset(0x00)] public fixed byte ValueArray[0x10 * 151];
+    [FixedSizeArray<AtkValue>(151)]
+    [FieldOffset(0x00)] public fixed byte ValueArray[0x10 * 151];
+    
+    [FieldOffset(0x988)] public Utf8String Rating;
 
-	[FieldOffset(0x988)] public Utf8String Rating;
+    [FixedSizeArray<ExplorationResultDataItemReturn>(10)]
+    [FieldOffset(0xA00)] public fixed byte ItemReturn[0xB0 * 10];
 
-	[FixedSizeArray<Utf8String>(100)]
-	[FieldOffset(0x10E8)] public fixed byte StringArray[0x68 * 100];
+    [FieldOffset(0x10E0)] public byte ItemReturnListCount;
+
+    [FixedSizeArray<Utf8String>(100)]
+    [FieldOffset(0x10E8)] public fixed byte StringArray[0x68 * 100];
+    
+    [FixedSizeArray<Pointer<Utf8String>>(100)]
+    [FieldOffset(0x3988)] public fixed byte StringPointerArray[0x8 * 100];
+    [FieldOffset(0x3CA8)] public byte StringPointerListCount;
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0xB0)]
+public unsafe partial struct ExplorationResultDataItemReturn {
+    [FieldOffset(0x00)] public uint ItemId;
+    [FieldOffset(0x04)] public uint Quantity;
+    [FieldOffset(0x08)] public fixed byte UnknownBytes[0xA8];
 }
