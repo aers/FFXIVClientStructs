65274f40c851fdf061d143071a7470a7b35a7d4f
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-02-11 12:54:25 -0800

    Add AtkUnitBase, Remove Collapsed

--- FFXIVClientStructs/FFXIV/Client/UI/AddonFieldMarker.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonFieldMarker.cs
@@ -1,9 +1,11 @@
-﻿namespace FFXIVClientStructs.FFXIV.Client.UI;
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI;
 
 [StructLayout(LayoutKind.Explicit, Size = 0x598)]
 public unsafe partial struct AddonFieldMarker
 {
-    [FieldOffset(0x190)] public int Collapsed;
+    [FieldOffset(0x00)] public AtkUnitBase AtkUnitBase;
     [FieldOffset(0x230)] public int HoveredButtonIndex; // Index 0-8 of the currently moused over button (A-D, 1-4, Clear)
     
     [FixedSizeArray<AddonWaymarkInfo>(8)]
@@ -11,8 +13,6 @@ public unsafe partial struct AddonFieldMarker
     
     [FieldOffset(0x57C)] public int HoveredPresetIndex; // Index 0-4 of the currently moused over slot, -1 if not hovering over a slot
     [FieldOffset(0x580)] public byte SelectedPage;
-
-    public bool IsCollapsed => Collapsed != 0;
 }
 
 [StructLayout(LayoutKind.Explicit, Size = 0x18)]
