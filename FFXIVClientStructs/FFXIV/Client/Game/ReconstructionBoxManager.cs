d32884463b7d9298e60febf538638f5362c9e455
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-02-12 15:10:24 -0800

    Add ReconstructionBox

---
+++ FFXIVClientStructs/FFXIV/Client/Game/ReconstructionBoxManager.cs
@@ -0,0 +1,21 @@
+﻿namespace FFXIVClientStructs.FFXIV.Client.Game;
+
+[StructLayout(LayoutKind.Explicit, Size = 0x10)]
+public unsafe partial struct ReconstructionBoxManager
+{
+    [StaticAddress("0F 85 ?? ?? ?? ?? 45 33 C0 48 89 5C 24", 3)]
+    public static partial ReconstructionBoxManager* Instance();
+    
+    [FieldOffset(0x00)] public ReconstructionBoxData* ReconstructionBoxData;
+    [FieldOffset(0x08)] public void* UnknownDataPointer;
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0xA8)]
+public struct ReconstructionBoxData
+{
+    [FieldOffset(0xA0)] 
+    public ushort Donated;
+
+    [FieldOffset(0xA6)] 
+    public ushort Allowance;
+}
