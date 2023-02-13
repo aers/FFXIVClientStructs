1b888f7794041e6f475757f2606bd26abc8696bd
Caraxi <git@caraxian.com>
2023-02-13 21:57:40 +1030

    Add AgentArchiveItem

---
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentArchiveItem.cs
@@ -0,0 +1,23 @@
+using FFXIVClientStructs.FFXIV.Client.System.Framework;
+using FFXIVClientStructs.FFXIV.Client.System.String;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI.Agent; 
+
+[Agent(AgentId.ArchiveItem)]
+[StructLayout(LayoutKind.Explicit, Size = 0x30)]
+public unsafe partial struct AgentArchiveItem {
+    public static AgentArchiveItem* Instance() => (AgentArchiveItem*) Framework.Instance()->GetUiModule()->GetAgentModule()->GetAgentByInternalId(AgentId.ArchiveItem);
+    
+    [FieldOffset(0x00)] public AgentInterface AgentInterface;
+    [FieldOffset(0x28)] public ArchiveItem* ArchiveItem;
+    
+    [MemberFunction("E8 ?? ?? ?? ?? 48 8B 9C 24 ?? ?? ?? ?? C7 06")]
+    public partial void* ViewArchiveItem(uint itemId);
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0xB0)]
+public unsafe struct ArchiveItem {
+    [FieldOffset(0x10)] public uint ItemId;
+    [FieldOffset(0x18)] public Utf8String ItemName;
+}
