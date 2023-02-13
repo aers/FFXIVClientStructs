cfc00b8accee7b8c8183991d4da1288e31f38bd4
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-02-13 15:02:10 -0800

    Resolve Merge Conflict

--- FFXIVClientStructs/FFXIV/Client/Game/UI/ContentsFinder.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/ContentsFinder.cs
@@ -1,13 +1,10 @@
 ﻿namespace FFXIVClientStructs.FFXIV.Client.Game.UI; 
 
-[StructLayout(LayoutKind.Explicit, Size = 0x40)]
-public unsafe struct ContentsFinder {
-    public enum LootRule : byte {
-        Normal = 0,
-        GreedOnly = 1,
-        Lootmaster = 2
-    }
-
+[StructLayout(LayoutKind.Explicit, Size = 0xB0)]
+public unsafe struct ContentsFinder
+{
+    public static ContentsFinder* Instance() => &UIState.Instance()->ContentsFinder;
+    
     [FieldOffset(0x00)] public void* vtbl;
     [FieldOffset(0x10)] public LootRule LootRules;
     [FieldOffset(0x11)] public byte UnrestrictedParty;
@@ -16,4 +13,15 @@ public unsafe struct ContentsFinder {
     [FieldOffset(0x14)] public byte ExplorerMode;
     [FieldOffset(0x15)] public byte LevelSync;
     [FieldOffset(0x16)] public byte LimitedLevelingRoulette;
+
+    [FieldOffset(0x60)] public int EnteredQueueTimestamp;
+    [FieldOffset(0x7C)] public byte PositionInQueue;
+    [FieldOffset(0x87)] public byte AverageWaitTime; // In minutes
+    
+    public enum LootRule : byte 
+    {
+        Normal = 0,
+        GreedOnly = 1,
+        Lootmaster = 2
+    }
 }
