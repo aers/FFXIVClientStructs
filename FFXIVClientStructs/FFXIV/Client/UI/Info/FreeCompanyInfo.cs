93ba4108a429b4ec1d948b2f6b493f851e75cb2e
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-02-15 22:54:42 -0800

    Add FreeCompanyInfo

---
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/FreeCompanyInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace FFXIVClientStructs.FFXIV.Client.UI.Info;
+
+[StructLayout(LayoutKind.Explicit, Size = 0x6E8)]
+public unsafe struct FreeCompanyInfo
+{
+    [FieldOffset(0x30)] public ulong FreeCompanyId;
+    [FieldOffset(0x7C)] public byte* FreeCompanyName; // null-terminated cstring
+    [FieldOffset(0x93)] public byte* FounderName; // null-terminated cstring
+
+    [FixedSizeArray<RankInfo>(11)]
+    [FieldOffset(0x160)] public fixed byte RankInformation[0x58 * 11];
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0x58)]
+public unsafe struct RankInfo
+{
+    [FieldOffset(0x00)] public short MemberCount;
+    [FieldOffset(0x03)] public byte* RankName; // Null-terminated cstring
+}
--- FFXIVClientStructs/FFXIV/Client/UI/Info/InfoModule.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/InfoModule.cs
@@ -1,9 +1,15 @@
-﻿using FFXIVClientStructs.FFXIV.Client.System.String;
+﻿using FFXIVClientStructs.FFXIV.Client.System.Framework;
+using FFXIVClientStructs.FFXIV.Client.System.String;
 
 namespace FFXIVClientStructs.FFXIV.Client.UI.Info; 
 
 [StructLayout(LayoutKind.Explicit, Size = 0x1C70)]
-public unsafe struct InfoModule {
+public unsafe struct InfoModule
+{
+	public static InfoModule* Instance() => Framework.Instance()->UIModule->GetInfoModule();
+
+	[FieldOffset(0x19E0)] public FreeCompanyInfo FreeCompanyInfo;
+	
 	[FieldOffset(0x1978)] public fixed long InfoProxyArray[34];
 	[FieldOffset(0x1A88)] public ulong LocalContentId;
 	[FieldOffset(0x1A90)] public Utf8String UnkString0;
