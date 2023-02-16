8ca6dcc6c3785846f7b08ce6a5864756dde9e8d9
Haselnussbomber <mail@haselnussbomber.de>
2023-02-16 08:39:39 +0100

    Add AgentFishGuide (#347)

---
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentFishGuide.cs
@@ -0,0 +1,13 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+[Agent(AgentId.FishGuide)]
+[StructLayout(LayoutKind.Explicit, Size = 0x158)]
+public unsafe partial struct AgentFishGuide
+{
+    [FieldOffset(0x00)] public AgentInterface AgentInterface;
+
+    [MemberFunction("E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 0F B6 93 ?? ?? ?? ?? 48 8B CE")]
+    public partial void OpenForItemId(uint itemId, bool isSpearfishing);
+}
