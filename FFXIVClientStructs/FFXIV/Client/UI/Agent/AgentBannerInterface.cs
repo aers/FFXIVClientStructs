6f9045a221a4a1ce9886eec6b61fe984470d1b96
Ottermandias <nickroederspam+git1@gmail.com>
2023-02-16 18:41:25 +0100

    Add some AgentBanner info.

---
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentBannerInterface.cs
@@ -0,0 +1,58 @@
+﻿using FFXIVClientStructs.FFXIV.Client.System.String;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+// Client::UI::Agent::AgentBannerParty
+//   Client::UI::Agent::AgentBannerInterface
+//     Client::UI::Agent::AgentInterface
+[StructLayout(LayoutKind.Explicit, Size = 0x30)]
+public unsafe partial struct AgentBannerInterface
+{
+    [FieldOffset(0x0)] public AgentInterface AgentInterface;
+    [FieldOffset(0x28)] public Storage* Data;
+
+    // Client::UI::Agent::AgentBannerInterface::Storage
+    // Destroyed in Client::UI::Agent::AgentBannerInterface::dtor
+    [StructLayout(LayoutKind.Explicit, Size = 0x3B30)]
+    public partial struct Storage
+    {
+        public const int CharacterDataSize = 0x760;
+
+        // vtable: 48 8D 05 ?? ?? ?? ?? 48 89 01 48 8B F9 7E 
+        // dtor: E8 ?? ?? ?? ?? 48 83 EF ?? 75 ?? BA ?? ?? ?? ?? 48 8B CE E8 ?? ?? ?? ?? 48 89 7D
+        [StructLayout(LayoutKind.Explicit, Size = CharacterDataSize)]
+        public struct CharacterData
+        {
+            [FieldOffset(0x000)] public void** VTable;
+
+            [FieldOffset(0x018)] public Utf8String Name1;
+            [FieldOffset(0x080)] public Utf8String Name2;
+            [FieldOffset(0x0E8)] public Utf8String UnkString1;
+            [FieldOffset(0x150)] public Utf8String UnkString2;
+            [FieldOffset(0x1C0)] public Utf8String Job;
+            [FieldOffset(0x238)] public uint WorldId;
+            [FieldOffset(0x240)] public Utf8String UnkString3;
+
+            [FieldOffset(0x2B0)] public void* CharaView;
+            [FieldOffset(0x5D0)] public AtkTexture AtkTexture;
+
+            [FieldOffset(0x6E0)] public Utf8String Title;
+            [FieldOffset(0x750)] public void* SomePointer;
+
+        }
+
+        [FieldOffset(0x0000)] public void* Agent; // AgentBannerParty, maybe other Banner agents
+        [FieldOffset(0x0008)] public UIModule* UiModule;
+        [FieldOffset(0x0010)] public uint Unk1; // Maybe count or bitfield, but probably not
+        [FieldOffset(0x0014)] public uint Unk2;
+
+        public const int NumCharacters = 8;
+
+        [FixedSizeArray<CharacterData>(NumCharacters)]
+        [FieldOffset(0x00)] public fixed byte CharacterArray[CharacterDataSize * NumCharacters];
+
+        [FieldOffset(0x3B20)] public long Unk3;
+        [FieldOffset(0x3B28)] public long Unk4;
+    }
+}
--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentBannerMIP.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentBannerMIP.cs
@@ -2,15 +2,15 @@
 
 namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
 
-// Client::UI::Agent::AgentBannerParty
+// Client::UI::Agent::AgentBannerMIP
 //   Client::UI::Agent::AgentBannerInterface
 //     Client::UI::Agent::AgentInterface
 //       Component::GUI::AtkModuleInterface::AtkEventInterface
-[Agent(AgentId.BannerParty)]
+[Agent(AgentId.BannerMIP)]
 [StructLayout(LayoutKind.Explicit, Size = 0x38)]
-public unsafe partial struct AgentBannerParty
+public unsafe partial struct AgentBannerMIP
 {
-    public static AgentBannerParty* Instance() => (AgentBannerParty*)Framework.Instance()->GetUiModule()->GetAgentModule()->GetAgentByInternalId(AgentId.BannerParty);
+    public static AgentBannerParty* Instance() => (AgentBannerParty*)Framework.Instance()->GetUiModule()->GetAgentModule()->GetAgentByInternalId(AgentId.BannerMIP);
 
     [FieldOffset(0x0)] public AgentBannerInterface AgentBannerInterface;
 }
