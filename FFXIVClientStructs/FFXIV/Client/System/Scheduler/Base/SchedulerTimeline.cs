313789ef227c9da625b9bce897a3a137d372277e
Ottermandias <nickroederspam+git1@gmail.com>
2023-02-16 18:18:29 +0100

    Add some SchedulerResource info.

--- FFXIVClientStructs/FFXIV/Client/System/Scheduler/Base/SchedulerTimeline.cs
+++ FFXIVClientStructs/FFXIV/Client/System/Scheduler/Base/SchedulerTimeline.cs
@@ -4,12 +4,13 @@
 //   Client::System::Scheduler::Base::TimelineController
 //     Client::System::Scheduler::Base::SchedulerState
 
-// Size at least 0x248
 // ctor E8 ?? ?? ?? ?? 48 89 43 ?? 48 89 98
-[StructLayout(LayoutKind.Explicit, Size=0x248)]
+[StructLayout(LayoutKind.Explicit, Size=0x280)]
 public partial struct SchedulerTimeline
 {
     [FieldOffset(0)] public TimelineController TimelineController;
+
+    [FieldOffset(0x18C)] public uint OwningGameObjectIndex;
 
     [MemberFunction("E8 ?? ?? ?? ?? 83 7F ?? ?? 75 ?? 0F B6 87")]
     public partial ulong LoadTimelineResources();
