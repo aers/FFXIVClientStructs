605541e5863a7165bc0cc9014557431441abb472
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-02-15 23:34:17 -0800

    Add SatisfactionSupply (#339)
    * Add SatisfactionSupply
    
    * Rename file to match struct

---
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonSatisfactionSupply.cs
@@ -0,0 +1,22 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI;
+
+[StructLayout(LayoutKind.Explicit, Size = 0x670)]
+public unsafe partial struct AddonSatisfactionSupply
+{
+    [FieldOffset(0x00)] public AtkUnitBase AtkUnitBase;
+    [FieldOffset(0x224)] public int HoveredElementIndex; // Index 0-2 of the last hovered turn in element
+
+    [FixedSizeArray<AddonDeliveryItemInfo>(3)]
+    [FieldOffset(0x308)] public fixed byte DeliveryInfo[0x68 * 3];
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0x68)]
+public unsafe partial struct AddonDeliveryItemInfo
+{
+    [FieldOffset(0x00)] public uint ItemId;
+    
+    // The rest of this array are pointers to various other blocks of memory it seems.
+    // These pointers don't seem to be vfuncs
+}
