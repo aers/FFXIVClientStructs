172a3ec96e2887cf3a6b8b346c0d4657a91cb99e
Caraxi <git@caraxian.com>
2023-02-19 15:40:00 +1030

    AddonMacro

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentDragDrop.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentDragDrop.cs
@@ -7,7 +7,11 @@
 // common CreateAtkComponent function 8B FA 33 DB E8 ?? ?? ?? ?? 
 // type 17
 [StructLayout(LayoutKind.Explicit, Size = 0x110)]
-public struct AtkComponentDragDrop
+public unsafe partial struct AtkComponentDragDrop
 {
     [FieldOffset(0x0)] public AtkComponentBase AtkComponentBase;
+    [FieldOffset(0xF8)] public AtkComponentIcon* AtkComponentIcon;
+    
+    [MemberFunction("E8 ?? ?? ?? ?? 83 F8 FF 74 40")]
+    public partial int GetIconId();
 }
