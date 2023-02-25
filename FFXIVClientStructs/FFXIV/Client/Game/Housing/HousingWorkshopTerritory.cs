60ac609313eec09d0cd5479ff69a738f83814478
wolfcomp <magnus1997.mho@gmail.com>
2023-02-25 17:35:53 +0100

    fix airship faulty size and offset

--- FFXIVClientStructs/FFXIV/Client/Game/Housing/HousingWorkshopTerritory.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Housing/HousingWorkshopTerritory.cs
@@ -2,7 +2,7 @@
 public unsafe partial struct HousingWorkshopTerritory
 {
     [FixedSizeArray<AirshipData>(4)]
-    [FieldOffset(0x60)] public fixed byte AirshipDataList[0x1C8 * 4];
+    [FieldOffset(0x68)] public fixed byte AirshipDataList[0x1C0 * 4];
     
     [FieldOffset(0x7D8)] public byte ActiveAirshipId; // 0-3, 255 if none
     [FieldOffset(0x7D9)] public byte AirshipCount;
@@ -15,29 +15,27 @@ public unsafe partial struct HousingWorkshopTerritory
     [FieldOffset(0xB5E0)] public fixed byte SubmersibleDataPointerList[0x8 * 5]; // 0-3 is the same as SubmersibleDataList, 4 is the one you are currently using
 }
 
-[StructLayout(LayoutKind.Explicit, Size = 0x1C8)]
+[StructLayout(LayoutKind.Explicit, Size = 0x1C0)]
 public unsafe partial struct AirshipData
 {
-    [FieldOffset(0xC)] public int RegisterTime; // currently a theory will confirm on a later point when i craft a 2nd airship
-    [FieldOffset(0x14)] public byte RankId;
-    [FieldOffset(0x18)] public int ReturnTime;
-    [FieldOffset(0x1C)] public uint CurrentExp;
-    [FieldOffset(0x20)] public uint NextLevelExp;
+    [FieldOffset(0x4)] public int RegisterTime; // currently a theory will confirm on a later point when i craft a 2nd airship
+    [FieldOffset(0xC)] public byte RankId;
+    [FieldOffset(0x10)] public int ReturnTime;
+    [FieldOffset(0x14)] public uint CurrentExp;
+    [FieldOffset(0x18)] public uint NextLevelExp;
 
-    [FieldOffset(0x28)] public ushort HullId; // AirshipExplorationPart Key
-    [FieldOffset(0x30)] public ushort SternId; // AirshipExplorationPart Key
-    [FieldOffset(0x32)] public ushort BowId; // AirshipExplorationPart Key
-    [FieldOffset(0x34)] public ushort BridgeId; // AirshipExplorationPart Key
+    [FieldOffset(0x20)] public ushort HullId; // AirshipExplorationPart Key
+    [FieldOffset(0x22)] public ushort SternId; // AirshipExplorationPart Key
+    [FieldOffset(0x24)] public ushort BowId; // AirshipExplorationPart Key
+    [FieldOffset(0x26)] public ushort BridgeId; // AirshipExplorationPart Key
 
-    [FieldOffset(0x36)] public ushort Surveillance;
-    [FieldOffset(0x38)] public ushort Retrieval;
-    [FieldOffset(0x40)] public ushort Speed;
-    [FieldOffset(0x42)] public ushort Range;
-    [FieldOffset(0x44)] public ushort Favor;
+    [FieldOffset(0x28)] public ushort Surveillance;
+    [FieldOffset(0x30)] public ushort Retrieval;
+    [FieldOffset(0x32)] public ushort Speed;
+    [FieldOffset(0x34)] public ushort Range;
+    [FieldOffset(0x36)] public ushort Favor;
 
-    [FieldOffset(0x42)] public fixed byte Route[5]; // AirshipExplorationPoint Key
-
-    [FieldOffset(0x3F)] public fixed byte Name[20];
+    [FieldOffset(0x37)] public fixed byte Name[20];
 
     /// <summary>Gets the registered time as a <see cref="DateTime"/> object</summary>
     public DateTime GetRegisterTime() => DateTime.UnixEpoch.AddSeconds(RegisterTime);
