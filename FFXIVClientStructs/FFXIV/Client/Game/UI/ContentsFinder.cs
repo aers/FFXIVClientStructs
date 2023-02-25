878e14ea3abb3ce29f2396c03221abe9e552566d
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-02-24 19:46:05 -0800

    Add Boolean Properties

--- FFXIVClientStructs/FFXIV/Client/Game/UI/ContentsFinder.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/ContentsFinder.cs
@@ -7,23 +7,30 @@ public unsafe struct ContentsFinder
     
     [FieldOffset(0x00)] public void* vtbl;
     [FieldOffset(0x10)] public LootRule LootRules;
-    [FieldOffset(0x11)] public byte UnrestrictedParty;
-    [FieldOffset(0x12)] public byte MinimalIL;
-    [FieldOffset(0x13)] public byte SilenceEcho;
-    [FieldOffset(0x14)] public byte ExplorerMode;
-    [FieldOffset(0x15)] public byte LevelSync;
-    [FieldOffset(0x16)] public byte LimitedLevelingRoulette;
+    [Obsolete("Use Is[propertyName] boolean property instead", true)][FieldOffset(0x11)] public byte UnrestrictedParty;
+    [Obsolete("Use Is[propertyName] boolean property instead", true)][FieldOffset(0x12)] public byte MinimalIL;
+    [Obsolete("Use Is[propertyName] boolean property instead", true)][FieldOffset(0x13)] public byte SilenceEcho;
+    [Obsolete("Use Is[propertyName] boolean property instead", true)][FieldOffset(0x14)] public byte ExplorerMode;
+    [Obsolete("Use Is[propertyName] boolean property instead", true)][FieldOffset(0x15)] public byte LevelSync;
+    [Obsolete("Use Is[propertyName] boolean property instead", true)][FieldOffset(0x16)] public byte LimitedLevelingRoulette;
+    
+    [FieldOffset(0x11)] public bool IsUnrestrictedParty;
+    [FieldOffset(0x12)] public bool IsMinimalIL;
+    [FieldOffset(0x13)] public bool IsSilenceEcho;
+    [FieldOffset(0x14)] public bool IsExplorerMode;
+    [FieldOffset(0x15)] public bool IsLevelSync;
+    [FieldOffset(0x16)] public bool IsLimitedLevelingRoulette;
 
     [FieldOffset(0x60)] public int EnteredQueueTimestamp;
     [FieldOffset(0x7C)] public byte PositionInQueue;
     [FieldOffset(0x87)] public byte AverageWaitTime; // In minutes
     
     public DateTime GetEnteredQueueDateTime() => DateTime.UnixEpoch.AddSeconds(EnteredQueueTimestamp);
-    
-    public enum LootRule : byte 
-    {
-        Normal = 0,
-        GreedOnly = 1,
-        Lootmaster = 2
-    }
+}
+
+public enum LootRule : byte 
+{
+    Normal = 0,
+    GreedOnly = 1,
+    Lootmaster = 2
 }
