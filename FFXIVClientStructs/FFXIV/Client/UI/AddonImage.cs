b00c9cc09b49f81cd24717169cecc3a664533c93
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-02-25 19:13:37 -0800

    Add AddonImage (#368)

---
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonImage.cs
@@ -0,0 +1,17 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI;
+
+[Addon("_Image")]
+[StructLayout(LayoutKind.Explicit, Size = 0x290)]
+public unsafe struct AddonImage
+{
+    [FieldOffset(0x00)] public AtkUnitBase AtkUnitBase;
+
+    [FieldOffset(0x260)] public AtkResNode* ResNode1; // Both AtkResNode's appear to be the same node
+    [FieldOffset(0x268)] public AtkResNode* ResNode2;
+    [FieldOffset(0x270)] public AtkImageNode* ImageNode;
+    
+    [FieldOffset(0x27C)] public ushort Width;
+    [FieldOffset(0x280)] public ushort Height;
+}
