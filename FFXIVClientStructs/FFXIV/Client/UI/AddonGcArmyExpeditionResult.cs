3e641b35bec7a6e0d477269b25d2c81a1e11bf96
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-03-04 10:45:07 -0800

    Add AddonGcArmyExpeditionResult

---
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonGcArmyExpeditionResult.cs
@@ -0,0 +1,12 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI;
+
+[Addon("GcArmyExpeditionReport")]
+[StructLayout(LayoutKind.Explicit, Size = 0x228)]
+public unsafe struct AddonGcArmyExpeditionResult
+{
+    [FieldOffset(0x00)] public AtkUnitBase AtkUnitBase;
+
+    [FieldOffset(0x220)] public AtkComponentButton* CompleteButton;
+}
