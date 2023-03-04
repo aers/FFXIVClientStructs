decd5e97c6d430c23bd48fa36958efc4fc6aa324
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-03-04 11:33:41 -0800

    Add AddonGcArmyCapture

---
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonGcArmyCapture.cs
@@ -0,0 +1,20 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI;
+
+[Addon("GcArmyCapture")]
+[StructLayout(LayoutKind.Explicit, Size = 0x268)]
+public unsafe struct AddonGcArmyCapture
+{
+    [FieldOffset(0x00)] public AtkUnitBase AtkUnitBase;
+
+    [FieldOffset(0x220)] public AtkComponentButton* DeployButton;
+    [FieldOffset(0x228)] public AtkComponentList* MissionList;
+    [FieldOffset(0x230)] public AtkComponentList* ChosenRecruitList;
+    [FieldOffset(0x238)] public AtkTextNode* ScriptsTextNode;
+    [FieldOffset(0x240)] public AtkTextNode* ExpRewardTextNode;
+    [FieldOffset(0x248)] public AtkComponentBase* ExpRewardComponent;
+    [FieldOffset(0x250)] public AtkTextNode* ExpendituresTextNode;
+    [FieldOffset(0x258)] public AtkTextNode* MissionLevelTextNode;
+    [FieldOffset(0x260)] public AtkTextNode* MissionRequirementsTextNode;
+}
