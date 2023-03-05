e05041ab1927b607ff0ec12842c7c503bbdf480c
Haselnussbomber <mail@haselnussbomber.de>
2023-03-06 00:09:24 +0100

    Update AtkModule

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkModule.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkModule.cs
@@ -6,6 +6,10 @@
 public unsafe partial struct AtkModule
 {
     [FieldOffset(0x0)] public void* vtbl;
+
+    [FieldOffset(0x1B10)] public AtkUnitBase* IntersectingAddon;
+    [FieldOffset(0x1B18)] public AtkCollisionNode* IntersectingCollisionNode;
+
     [FieldOffset(0x1B48)] public AtkArrayDataHolder AtkArrayDataHolder;
 
     [VirtualFunction(9)]
@@ -16,6 +20,9 @@ public unsafe partial struct AtkModule
 
     [VirtualFunction(11)]
     public partial ExtendArrayData* GetExtendArrayData(int index);
+
+    [VirtualFunction(26)]
+    public partial bool IsAddonReady(uint addonId);
 
     [MemberFunction("E8 ?? ?? ?? ?? 84 C0 75 BA 40 84 FF")]
     public partial bool IsTextInputActive();
