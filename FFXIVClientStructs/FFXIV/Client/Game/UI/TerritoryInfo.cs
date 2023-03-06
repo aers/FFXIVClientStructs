73f1c24f22d191a821008eebd5007e0a563e9ba2
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-03-05 17:07:38 -0800

    Fix incorrect offsets

--- FFXIVClientStructs/FFXIV/Client/Game/UI/TerritoryInfo.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/TerritoryInfo.cs
@@ -7,8 +7,8 @@
 public unsafe partial struct TerritoryInfo
 {
     [FieldOffset(0x1C)] public int InSanctuary;
-    [FieldOffset(0x20)] public uint AreaPlaceNameID;
-    [FieldOffset(0x24)] public uint SubAreaPlaceNameID;
+    [FieldOffset(0x24)] public uint AreaPlaceNameID;
+    [FieldOffset(0x28)] public uint SubAreaPlaceNameID;
 
     [StaticAddress("48 8D 0D ?? ?? ?? ?? BA ?? ?? ?? ?? F3 0F 5C 05", 3)]
     public static partial TerritoryInfo* Instance();
