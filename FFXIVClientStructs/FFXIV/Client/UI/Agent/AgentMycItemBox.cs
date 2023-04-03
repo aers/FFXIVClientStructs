2795dfe0f8ddd7f5479fdcfdf54f6e98a5c0ee26
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-04-02 22:35:10 -0700

    Adjust Names, add Holster items

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentMycItemBox.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentMycItemBox.cs
@@ -11,19 +11,33 @@ public unsafe struct AgentMycItemBox
     [FieldOffset(0x40)] public MycLostFindsCache* LostFindsCache;
 }
 
-[StructLayout(LayoutKind.Explicit, Size = 0xAA4)]
+[StructLayout(LayoutKind.Explicit, Size = 0x1580)]
 public unsafe partial struct MycLostFindsCache
 {
     [FixedSizeArray<MycItemCategory>(7)] 
-    [FieldOffset(0x08)] public fixed byte ItemCategoryArray[7 * 0x184];
+    [FieldOffset(0x08)] public fixed byte ItemCacheArray[7 * 0x184];
 
-    [FieldOffset(0x08)] public MycItemCategory OffensiveCategory;
-    [FieldOffset(0x18C)] public MycItemCategory DefensiveCategory;
-    [FieldOffset(0x310)] public MycItemCategory RestorativeCategory;
-    [FieldOffset(0x494)] public MycItemCategory BeneficialCategory;
-    [FieldOffset(0x618)] public MycItemCategory TacticalCategory;
-    [FieldOffset(0x79C)] public MycItemCategory DetrimentalCategory;
-    [FieldOffset(0x920)] public MycItemCategory ItemRelatedCategory;
+    [FieldOffset(0x08)] public MycItemCategory OffensiveCache;
+    [FieldOffset(0x18C)] public MycItemCategory DefensiveCache;
+    [FieldOffset(0x310)] public MycItemCategory RestorativeCache;
+    [FieldOffset(0x494)] public MycItemCategory BeneficialCache;
+    [FieldOffset(0x618)] public MycItemCategory TacticalCache;
+    [FieldOffset(0x79C)] public MycItemCategory DetrimentalCache;
+    [FieldOffset(0x920)] public MycItemCategory ItemRelatedCache;
+    
+    [FixedSizeArray<MycItemCategory>(7)] 
+    [FieldOffset(0xAA4)] public fixed byte ItemHolsterArray[7 * 0x184];
+    
+    [FieldOffset(0xAA4)] public MycItemCategory OffensiveHolster;
+    [FieldOffset(0xC28)] public MycItemCategory DefensiveHolster;
+    [FieldOffset(0xDAC)] public MycItemCategory RestorativeHolster;
+    [FieldOffset(0xF30)] public MycItemCategory BeneficialHolster;
+    [FieldOffset(0x10B4)] public MycItemCategory TacticalHolster;
+    [FieldOffset(0x1238)] public MycItemCategory DetrimentalHolster;
+    [FieldOffset(0x13BC)] public MycItemCategory ItemRelatedHolster;
+
+    [FieldOffset(0x1550)] public int HolsterCurrentTab;
+    [FieldOffset(0x1558)] public int LastSelectedActionId;
 }
 
 [StructLayout(LayoutKind.Explicit, Size = 0x184)]
