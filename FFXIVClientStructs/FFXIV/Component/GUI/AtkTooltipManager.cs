1ca0e7e9d1f0648e9dac3e48a63e1cc239b2ebe6
Caraxi <git@caraxian.com>
2023-06-12 18:16:08 +0930

    Add AtkTooltipManager.ShowTooltip and HideTooltip

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkTooltipManager.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkTooltipManager.cs
@@ -1,4 +1,6 @@
-﻿namespace FFXIVClientStructs.FFXIV.Component.GUI;
+﻿using FFXIVClientStructs.FFXIV.Client.System.Memory;
+
+namespace FFXIVClientStructs.FFXIV.Component.GUI;
 
 // Component::GUI::AtkToolTipManager
 //      Component::GUI::AtkEventListener
@@ -14,13 +16,16 @@ public unsafe partial struct AtkTooltipManager
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x18)]
-    public struct AtkTooltipArgs
+    public partial struct AtkTooltipArgs : ICreatable
     {
         [FieldOffset(0x0)] public byte* Text;
         [FieldOffset(0x8)] public ulong TypeSpecificID;
         [FieldOffset(0x10)] public uint Flags;
         [FieldOffset(0x14)] public short Unk_14;
         [FieldOffset(0x16)] public byte Unk_16;
+
+        [MemberFunction("E8 ?? ?? ?? ?? 48 C7 C2")]
+        public partial void Ctor();
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x20)]
@@ -41,4 +46,26 @@ public unsafe partial struct AtkTooltipManager
 
     [MemberFunction("E8 ?? ?? ?? ?? 45 33 C9 45 8D 46 4A")]
     public partial void RemoveTooltip(AtkResNode* targetNode);
+
+    [MemberFunction("E8 ?? ?? ?? ?? EB 24 66 83 FA 22")]
+    public partial void ShowTooltip(
+        AtkTooltipType type,
+        ushort parentID,
+        AtkResNode* targetNode,
+        AtkTooltipArgs* tooltipArgs,
+        delegate* unmanaged[Stdcall] <float*, float*, void*> unkDelegate = null,
+        bool unk7 = false,
+        bool unk8 = true);
+    
+    [GenerateCStrOverloads] // cursed forced partial to make generator happy
+    public partial void ShowTooltip(ushort parentId, AtkResNode* targetNode, byte* tooltipString);
+    public partial void ShowTooltip(ushort parentId, AtkResNode* targetNode, byte* tooltipString) {
+        var args = stackalloc AtkTooltipArgs[1];
+        args->Ctor();
+        args->Text = tooltipString;
+        ShowTooltip(AtkTooltipType.Text, parentId, targetNode, args);
+    }
+    
+    [MemberFunction("E8 ?? ?? ?? ?? 41 F6 C5 20")]
+    public partial void HideTooltip(ushort parentId, bool unk = false);
 }
