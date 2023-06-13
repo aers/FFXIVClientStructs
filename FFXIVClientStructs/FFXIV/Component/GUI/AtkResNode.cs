5f0f05074af4f9aba64d8e979f1918c9d7892b01
Caraxi <git@caraxian.com>
2023-06-13 16:55:51 +0930

    Obsolete AtkResNode.Flags, Add AtkResNode.NodeFlags

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkResNode.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkResNode.cs
@@ -101,7 +101,9 @@ public unsafe partial struct AtkResNode : ICreatable
 
     // asm accesses these fields together so this is one 32bit field with priority+flags
     [FieldOffset(0x9C)] public ushort Priority;
+    [Obsolete("Use NodeFlags", false)]
     [FieldOffset(0x9E)] public short Flags;
+    [FieldOffset(0x9E)] public NodeFlags NodeFlags;
     [FieldOffset(0xA0)] public uint Flags_2; // bit 1 = has changes, ClipCount is bits 10-17, idk its a mess
     [FieldOffset(0xA0)] public uint DrawFlags;
 
