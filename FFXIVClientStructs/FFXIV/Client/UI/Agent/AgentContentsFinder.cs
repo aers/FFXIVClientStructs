a31572669c62cf095059dc87819f9ad513345932
wolfcomp <magnus1997.mho@gmail.com>
2023-06-15 19:32:35 +0200

    add a comment for future struct fixes with braking changes

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentContentsFinder.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentContentsFinder.cs
@@ -25,6 +25,7 @@ public unsafe partial struct AgentContentsFinder
     [FieldOffset(0x5C8), Obsolete("Poorly mapped")] public ContentsFinderRewards Reward2;
     [FieldOffset(0x810), Obsolete("Poorly mapped")] public ContentsFinderRewards BonusReward2;
 
+    // TODO split into 3 separate arrays, (8F0, EE0, 14D0), each with 5 entries, 8F0 seems to be the top row of rewards while 14D0 is the bottom row, EE0 is unknown
     [FixedSizeArray<ItemReward>(15)] 
     [FieldOffset(0x8F0)] public fixed byte ItemRewardArray[0x130 * 15]; // E8 ?? ?? ?? ?? 41 B8 ?? ?? ?? ?? 89 5E 2C
     
