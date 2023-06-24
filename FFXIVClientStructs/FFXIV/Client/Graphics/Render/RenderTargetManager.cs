431bd0f70324fb239cad1dac79abe7cf51fea4cd
Haselnussbomber <mail@haselnussbomber.de>
2023-06-24 11:31:04 +0200

    Add GetCharaViewTexture to RenderTargetManager

--- FFXIVClientStructs/FFXIV/Client/Graphics/Render/RenderTargetManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Render/RenderTargetManager.cs
@@ -45,5 +45,7 @@ public unsafe partial struct RenderTargetManager
 
     [StaticAddress("48 8B 0D ?? ?? ?? ?? 48 8B B1 ?? ?? ?? ??", 3, isPointer: true)]
     public static partial RenderTargetManager* Instance();
-    
+
+    [MemberFunction("48 8B 05 ?? ?? ?? ?? 8B CA 48 8B 84 C8")]
+    public partial Texture* GetCharaViewTexture(uint clientObjectIndex);
 }
