2eb32d7f7b1e3a00fd34d2cb3caae8d3b521b796
Caraxi <git@caraxian.com>
2023-06-28 17:53:33 +0930

    Update HousingOutdoorTerritory.cs

--- FFXIVClientStructs/FFXIV/Client/Game/Housing/HousingOutdoorTerritory.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Housing/HousingOutdoorTerritory.cs
@@ -11,5 +11,45 @@ public unsafe partial struct HousingOutdoorTerritory {
     /// <returns>IconID, or 0 if something went wrong</returns>
     [MemberFunction("40 56 57 48 83 EC 38 0F B6 FA")]
     public partial int GetPlotIcon(byte plot);
+    
+    [FixedSizeArray<PlotDetail>(60)]
+    [FieldOffset(0x96B8)] public fixed byte Plot[0x10 * 60];
+    
+    [FixedSizeArray<ApartmentBuildingState>(2)]
+    [FieldOffset(0x9A78)] public fixed byte ApartmentBuilding[2];
+
 }
 
+[StructLayout(LayoutKind.Explicit, Size = 0x10)]
+public struct PlotDetail {
+    [FieldOffset(0x00)] public PlotState State;
+    [FieldOffset(0x01)] public PlotSize Size;
+    [FieldOffset(0X02)] public bool IsOpen;
+    [FieldOffset(0x03)] public bool Unk03;
+    [FieldOffset(0x04)] public PlotOwnerType OwnerType;
+}
+
+[Flags]
+public enum ApartmentBuildingState : byte {
+    None = 0,
+    IsFull = 1,
+    IsOwn = 2,
+}
+
+public enum PlotSize : byte {
+    Small = 0,
+    Medium = 1,
+    Large = 2,
+}
+
+public enum PlotState : byte {
+    None = 0,
+    UnownedLand = 1,
+    OwnedLand = 2,
+    OwnedEstate = 3,
+}
+
+public enum PlotOwnerType : uint {
+    FreeCompany = 0,
+    Individual = 2,
+}
