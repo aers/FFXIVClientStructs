dd0b6dbd8975ee2e51e7ccffbeb8bd4ff6ce3a26
Caraxi <git@caraxian.com>
2023-07-28 19:31:27 +0930

    Add UiState.Chain

---
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/Chain.cs
@@ -0,0 +1,7 @@
+namespace FFXIVClientStructs.FFXIV.Client.Game.UI; 
+
+[StructLayout(LayoutKind.Explicit, Size = 0x8)]
+public struct Chain {
+    [FieldOffset(0x00)] public float RemainingTime;
+    [FieldOffset(0x04)] public float MaxTime;
+}
