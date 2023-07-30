6de85fd9bdea8d03859fd3996412fd56772c9f31
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-07-30 13:48:17 -0700

    Add SelectedItemIndex

--- FFXIVClientStructs/FFXIV/Client/UI/AddonNeedGreed.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonNeedGreed.cs
@@ -3,7 +3,7 @@
 namespace FFXIVClientStructs.FFXIV.Client.UI;
 
 [Addon("NeedGreed")]
-[StructLayout(LayoutKind.Explicit, Size = 0x2B8)]
+[StructLayout(LayoutKind.Explicit, Size = 0x510)]
 public unsafe partial struct AddonNeedGreed
 {
     [FieldOffset(0x00)] public AtkUnitBase AtkUnitBase;
@@ -11,12 +11,23 @@ public unsafe partial struct AddonNeedGreed
     // Limit is assumed to be 32, due to the addon having 32 pre-allocated list item renderers
     [FixedSizeArray<LootItemInfo>(32)] 
     [FieldOffset(0x228)] public fixed byte Items[0x28 * 32];
+
+    [FieldOffset(0x508)] public int SelectedItemIndex;
     
     // Contains other information for each loot item,
     // Such as time remaining, and a pointer to what is probably the tooltip string
+    [Obsolete("Use LootItemInfo, instead of AddonNeedGreed.LootItemInfo", false)]
     [StructLayout(LayoutKind.Explicit, Size = 0x28)]
     public struct LootItemInfo
     {
         [FieldOffset(0x00)] public uint ItemId;
     }
+}
+
+// Contains other information for each loot item,
+// Such as time remaining, and a pointer to what is probably the tooltip string
+[StructLayout(LayoutKind.Explicit, Size = 0x28)]
+public struct LootItemInfo
+{
+    [FieldOffset(0x00)] public uint ItemId;
 }
