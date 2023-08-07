a3ee4975b0a791ccac2047b1b505f4c94665e800
Haselnussbomber <mail@haselnussbomber.de>
2023-08-08 00:00:11 +0200

    Fix AtkUnitList.Count

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkUnitList.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkUnitList.cs
@@ -8,5 +8,5 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI;
 public unsafe struct AtkUnitList {
     [FieldOffset(0x0)] public void* vtbl;
     [FieldOffset(0x8)] public AtkUnitBase* AtkUnitEntries; // array of pointers 0x8-0x808
-    [FieldOffset(0x808)] public uint Count;
+    [FieldOffset(0x808)] public ushort Count;
 }
