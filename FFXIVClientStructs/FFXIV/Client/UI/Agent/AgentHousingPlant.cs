c0406b0aa1b8402d2978ff8c17176027a9b216da
pohky <otoriosu@gmail.com>
2023-08-19 02:47:29 +0200

    Create AgentHousingPlant.cs

---
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentHousingPlant.cs
@@ -0,0 +1,38 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI.Agent; 
+
+[Agent(AgentId.HousingPlant)]
+[StructLayout(LayoutKind.Explicit, Size = 0x950)]
+public unsafe partial struct AgentHousingPlant {
+    public static AgentHousingPlant* Instance() => (AgentHousingPlant*)AgentModule.Instance()->GetAgentByInternalId(AgentId.HousingPlant);
+    [FieldOffset(0x00)] public AgentInterface AgentInterface;
+
+    [FieldOffset(0x40)] public uint ContextAddonId;
+    [FieldOffset(0x44)] public uint PlotType; // 27 indoor, 14 outdoor, 15 ? - not really sure, changes how the context menu is used
+
+    [FixedSizeArray<SelectedItem>(2)]
+    [FieldOffset(0x48)] public fixed byte SelectedItems[2 * 0x10];
+    [FixedSizeArray<SelectedItem>(2)]
+    [FieldOffset(0x68)] public fixed byte SelectedItems2[2 * 0x10];
+    [FixedSizeArray<SelectableItem>(140)]
+    [FieldOffset(0x88)] public fixed byte SelectableItems[140 * 0x10];
+    [FieldOffset(0x948)] public byte SelectableItemCount;
+    //[FieldOffset(0x949)] public bool IsPlantPot_IsNotGardening; ?
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x10)]
+    public struct SelectedItem {
+        [FieldOffset(0x00)] public uint InventoryId;
+        [FieldOffset(0x04)] public ushort InventorySlot;
+        
+        [FieldOffset(0x08)] public uint ItemId;
+    }
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x10)]
+    public struct SelectableItem {
+        // looks like a Client::UI::RaptureAtkModule::ItemCache
+        [FieldOffset(0x00)] public void* ItemCache;
+        [FieldOffset(0x08)] public uint InventoryId;
+        [FieldOffset(0x0C)] public ushort InventorySlot;
+    }
+}
