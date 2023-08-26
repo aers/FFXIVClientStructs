1e52770d5367c8237ac78688774d136118994bf7
pohky <otoriosu@gmail.com>
2023-08-26 10:40:12 +0200

    Update PronounModule.cs

--- FFXIVClientStructs/FFXIV/Client/UI/Misc/PronounModule.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Misc/PronounModule.cs
@@ -1,5 +1,6 @@
 ﻿using FFXIVClientStructs.FFXIV.Client.Game.Object;
 using FFXIVClientStructs.FFXIV.Client.System.Framework;
+using FFXIVClientStructs.FFXIV.Component.Excel;
 
 namespace FFXIVClientStructs.FFXIV.Client.UI.Misc;
 
@@ -9,6 +10,14 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Misc;
 public unsafe partial struct PronounModule
 {
     public static PronounModule* Instance() => Framework.Instance()->GetUiModule()->GetPronounModule();
+
+    [FieldOffset(0x08)] public ExcelModuleInterface* ExcelModuleInterface;
+    [FieldOffset(0x10)] public RaptureTextModule* RaptureTextModule;
+    [FieldOffset(0x18)] public ExcelSheet* TextCommandParamSheet;
+    
+    [FieldOffset(0x290)] public GameObject* UiMouseOverTarget;
+    //[FieldOffset(0x298)] public TextChecker TextChecker;
+    [FieldOffset(0x390)] public UIModule* UiModule;
 
     [MemberFunction("E8 ?? ?? ?? ?? 48 8B 5C 24 ?? EB 0C")]
     [GenerateCStrOverloads]
