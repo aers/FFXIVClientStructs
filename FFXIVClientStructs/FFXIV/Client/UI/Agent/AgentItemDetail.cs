6eb6f79d8ee599a12f62264432649abb8568c5ca
Asriel Camora <asriel@camora.dev>
2023-09-14 19:14:51 -0700

    Add ItemDetail and MiragePrismPrismItemDetail agents

---
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentItemDetail.cs
@@ -0,0 +1,12 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+[Agent(AgentId.ItemDetail)]
+[StructLayout(LayoutKind.Explicit, Size = 0x150)]
+public unsafe struct AgentItemDetail
+{
+    [FieldOffset(0x00)] public AgentInterface AgentInterface;
+
+    [FieldOffset(0x138)] public uint ItemId;
+}
