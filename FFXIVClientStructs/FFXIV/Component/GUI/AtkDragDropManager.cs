5578966a7186ced443fdba4280c477abed571f6e
Asriel Camora <asriel@camora.dev>
2023-09-14 21:35:30 -0700

    Create and implement AtkDragDropInterface

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkDragDropManager.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkDragDropManager.cs
@@ -7,24 +7,29 @@ public unsafe struct AtkDragDropManager {
     //[FieldOffset(0x08)] public uint UnkNum_1; //some sort of identifier?
     //[FieldOffset(0x0C)] public int UnkNum_2; //ActionId, ItemId, InventorySlotIndex, ListIndex  etc.
 
-    //[FieldOffset(0x20)] public Utf8String UnkString;
-
     [FieldOffset(0x90)] public AtkStage* AtkStage;
 
     //the first 2 seem to point to some subclass of DragDrop, maybe some temp thing
-    //[FieldOffset(0x98)] public AtkComponentDragDrop* UnkDragDrop_1;
-    //[FieldOffset(0xA0)] public AtkComponentDragDrop* UnkDragDrop_2;
-    //[FieldOffset(0xA8)] public AtkComponentDragDrop* UnkDragDrop_3;
+    [FieldOffset(0x98)] public AtkDragDropInterface* DragDrop1;
+    
+	// vvv Not set in some cases where DragDropS isn't used (like with hotbars)
+    [FieldOffset(0xA0)] public AtkDragDropInterface* DragDrop2;
+    [FieldOffset(0xA8)] public AtkComponentDragDrop* DragDropS;
+	// ^^^
 
     //returns some sort of (event?)mask from some static array
-    //[FieldOffset(0xB0)] public delegate*unmanaged<int, ulong> UnkFunc;
+    //public const int MaskSize = 0x57;
+    //[FieldOffset(0xB0)] public delegate*unmanaged<int, ulong> GetMask;
 
     [FieldOffset(0xB8)] public short DragStartX;
-    [FieldOffset(0xBA)] public short DragStartY;
+	[FieldOffset(0xBA)] public short DragStartY;
 
-    //[FieldOffset(0xBB)] public byte UnkBool_1;
-    //[FieldOffset(0xBC)] public byte UnkBool_2;
-    //[FieldOffset(0xBD)] public byte UnkBool_3;
-    //[FieldOffset(0xBE)] public byte UnkBool_4;
-    //[FieldOffset(0xBF)] public byte UnkBool_5;
+    [FieldOffset(0xBC)] public bool IsDragging;
+    // True if the item was clicked on (user must click again to drop it somewhere)
+    [FieldOffset(0xBD)] public bool ReclickToDrop;
+    // Set to true once the mouse moves during a drag
+    [FieldOffset(0xBE)] public bool MouseMoved;
+    // False if dropping on anything not ui (like the world)
+    // Can't be false if ReclickToDrop is false
+    [FieldOffset(0xBF)] public bool IsNotDiscarding;
 }
