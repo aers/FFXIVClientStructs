f84a6507052d4f5722440edae62de0953d96319b
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-09-15 15:01:09 -0700

    Fix AddonLookingForGroupDetail

--- FFXIVClientStructs/FFXIV/Client/UI/AddonLookingForGroupDetail.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonLookingForGroupDetail.cs
@@ -16,14 +16,17 @@ public unsafe partial struct AddonLookingForGroupDetail {
     [FieldOffset(0x280)] public AtkComponentButton* SendTellButton;
     [FieldOffset(0x288)] public AtkComponentButton* AllianceBackButton; // Not visible in 8-man parties
     [FieldOffset(0x290)] public AtkComponentButton* BackButton;
+    [FieldOffset(0x2B8)] public AtkComponentButton* RelayPartyFinderInfoButton;
 
-    [FieldOffset(0x2C8)] public AtkImageNode* LookingForGroupImageNode;
+    [FieldOffset(0x2C0)] public AtkImageNode* LookingForGroupImageNode;
+    [FieldOffset(0x2C8)] public AtkImageNode* CategoryImageNode;
+
     [FieldOffset(0x2D0)] public AtkTextNode* PartyLeaderTextNode;
     [FieldOffset(0x2E0)] public AtkTextNode* TimeRemainingTextNode;
     [FieldOffset(0x2E8)] public AtkTextNode* DutyNameTextNode;
     [FieldOffset(0x2D8)] public AtkTextNode* LocationTextNode;
     [FieldOffset(0x2F0)] public AtkTextNode* ItemLevelTextNode;
-    [FieldOffset(0x2F0)] public AtkTextNode* StatusTextNode;
+    [FieldOffset(0x2F8)] public AtkTextNode* StatusTextNode;
     [FieldOffset(0x300)] public AtkTextNode* DescriptionTextNode;
 
     [FieldOffset(0x308)] public Utf8String DescriptionString;
