6e34b91faee07441f4429c384615ef06118a1cc9
WildWolf <magnus1997.mho@gmail.com>
2023-09-17 21:25:09 +0200

    fix format

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentMycBattleAreaInfo.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentMycBattleAreaInfo.cs
@@ -1,23 +1,21 @@
+using System.Runtime.InteropServices;
 using FFXIVClientStructs.Attributes;
 using FFXIVClientStructs.FFXIV.Client.System.String;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using FFXIVClientStructs.Interop.Attributes;
-using System.Runtime.InteropServices;
 
 namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
 
 [Agent(AgentId.MycBattleAreaInfo)]
 [StructLayout(LayoutKind.Explicit, Size = 0x48)]
-public unsafe partial struct AgentMycBattleAreaInfo
-{
+public unsafe partial struct AgentMycBattleAreaInfo {
     [FieldOffset(0x0)] public AgentInterface AgentInterface;
 
-    [FieldOffset(0x28)] public MycDynamicEventData* MycDynamicEventData; 
+    [FieldOffset(0x28)] public MycDynamicEventData* MycDynamicEventData;
 }
 
 [StructLayout(LayoutKind.Explicit, Size = 0x178)]
-public unsafe partial struct MycDynamicEventData
-{
+public unsafe partial struct MycDynamicEventData {
     [FieldOffset(0x0C)] public byte Count;
 
     [FixedSizeArray<MycDynamicEvent>(3)]
@@ -25,8 +23,7 @@ public unsafe partial struct MycDynamicEventData
 }
 
 [StructLayout(LayoutKind.Explicit, Size = 0x78)]
-public unsafe partial struct MycDynamicEvent
-{
+public unsafe partial struct MycDynamicEvent {
     [FieldOffset(0x00)] public uint TimeLeft;
     [FieldOffset(0x04)] public ushort Id;
     [FieldOffset(0x07)] public MycDynamicEventState State;
@@ -34,8 +31,7 @@ public unsafe partial struct MycDynamicEvent
     [FieldOffset(0x10)] public Utf8String Name;
 }
 
-public enum MycDynamicEventState : byte
-{
+public enum MycDynamicEventState : byte {
     None = 0,
     Register = 1,
     Commence = 2,
