e910fb891d0053872a2f22fb01c72c93aad1058e
Haselnussbomber <mail@haselnussbomber.de>
2023-09-30 03:29:06 +0200

    Fix ChocoboTaxiStandsBitmask

--- FFXIVClientStructs/FFXIV/Client/Game/UI/UIState.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/UIState.cs
@@ -49,19 +49,19 @@ public unsafe partial struct UIState {
     // Size: Number of rows in Aetheryte sheet >> 3
     [FieldOffset(0x16B34)] public fixed byte UnlockedAetherytesBitmask[200 >> 3];
 
-    // Ref: E8 ?? ?? ?? ?? 48 83 6F ?? ?? 75 06 48 89 77 68
+    // Ref: "E8 ?? ?? ?? ?? 48 83 6F ?? ?? 75 06 48 89 77 68"
     // Size: Number of rows in HowTo sheet >> 3
     [FieldOffset(0x16B4E)] public fixed byte UnlockedHowtoBitmask[288 >> 3];
 
     // Ref: g_Client::Game::UI::UnlockedCompanionsMask
-    //      direct ref: 48 8D 0D ?? ?? ?? ?? 0F B6 04 08 84 D0 75 10 B8 ?? ?? ?? ?? 48 8B 5C 24
-    //      relative to uistate: E8 ?? ?? ?? ?? 84 C0 75 A6 32 C0 (case for 0x355)
+    //      direct ref: "48 8D 0D ?? ?? ?? ?? 0F B6 04 08 84 D0 75 10 B8 ?? ?? ?? ?? 48 8B 5C 24"
+    //      relative to uistate: "E8 ?? ?? ?? ?? 84 C0 75 A6 32 C0" (case for 0x355)
     // Size: Number of rows in Companion sheet >> 3
     [FieldOffset(0x16B72)] public fixed byte UnlockedCompanionsBitmask[496 >> 3];
 
-    // 42 0F B6 04 30 44 84 C0
-    // Size: Number of rows in ChocoboTaxi sheet >> 3
-    [FieldOffset(0x16BB0)] public fixed byte ChocoboTaxiStandsBitmask[312 >> 3];
+    // Ref: "42 0F B6 04 30 44 84 C0"
+    // Size: Number of rows in ChocoboTaxiStand sheet >> 3
+    [FieldOffset(0x16BB0)] public fixed byte ChocoboTaxiStandsBitmask[87 >> 3];
 
     [StaticAddress("48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8B 8B ?? ?? ?? ?? 48 8B 01", 3)]
     public static partial UIState* Instance();
