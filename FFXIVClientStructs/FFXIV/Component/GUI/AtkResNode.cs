fb403833c06d2c8f2ec2842ef70773b53b7d68d0
wolfcomp <magnus1997.mho@gmail.com>
2023-10-02 10:38:07 +0200

    remove another obsolete field

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkResNode.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkResNode.cs
@@ -98,7 +98,6 @@ public unsafe partial struct AtkResNode : ICreatable {
     // asm accesses these fields together so this is one 32bit field with priority+flags
     [FieldOffset(0x9C)] public ushort Priority;
     [FieldOffset(0x9E)] public NodeFlags NodeFlags;
-    [FieldOffset(0xA0), Obsolete("Use DrawFlags", true)] public uint Flags_2; // bit 1 = has changes, ClipCount is bits 10-17, idk its a mess
     [FieldOffset(0xA0)] public uint DrawFlags;
 
     public bool IsVisible => NodeFlags.HasFlag(NodeFlags.Visible);
