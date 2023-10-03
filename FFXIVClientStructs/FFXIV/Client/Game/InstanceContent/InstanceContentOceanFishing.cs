99c0bb58493fc770a85ce0342348c03f51d0e0d5
Nukoooo <89713806+Nukoooo@users.noreply.github.com>
2023-10-03 14:21:07 +0800

    Update InstanceContentOceanFishing.cs

--- FFXIVClientStructs/FFXIV/Client/Game/InstanceContent/InstanceContentOceanFishing.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/InstanceContent/InstanceContentOceanFishing.cs
@@ -9,24 +9,24 @@ public struct InstanceContentOceanFishing {
 
     // Row ID for IKDRoute sheet
     // Each zone (and their time of day) can be extracted from sheet
-    [FieldOffset(0x1CC8)] public uint CurrentRoute;
+    [FieldOffset(0x1CD0)] public uint CurrentRoute;
 
-    [FieldOffset(0x1CD0)] public byte CurrentZone; // 0, 1, 2
+    [FieldOffset(0x1CD8)] public byte CurrentZone; // 0, 1, 2
 
     // InstanceContentDirector.ContentDirector.ContentTimeLeft - TimeOffset = time left in current zone
     // After changing zones, seems to tick down independent of the UI and then jump up
-    [FieldOffset(0x1CD8)] public uint TimeOffset;
+    [FieldOffset(0x1CE0)] public uint TimeOffset;
 
-    [FieldOffset(0x1CE0)] public bool SpectralCurrentActive;
+    [FieldOffset(0x1CE8)] public bool SpectralCurrentActive;
 
     // Row ID for IKDPlayerMissionCondition sheet
     // Description and required amount can be extracted from sheet
-    [FieldOffset(0x22D8)] public uint Mission1Type;
-    [FieldOffset(0x22DC)] public uint Mission2Type;
-    [FieldOffset(0x22E0)] public uint Mission3Type;
+    [FieldOffset(0x22E0)] public uint Mission1Type;
+    [FieldOffset(0x22E4)] public uint Mission2Type;
+    [FieldOffset(0x22E8)] public uint Mission3Type;
 
     // Progress can be larger than the mission's required amount
-    [FieldOffset(0x22E4)] public ushort Mission1Progress;
-    [FieldOffset(0x22E6)] public ushort Mission2Progress;
-    [FieldOffset(0x22E8)] public ushort Mission3Progress;
+    [FieldOffset(0x22EC)] public ushort Mission1Progress;
+    [FieldOffset(0x22EE)] public ushort Mission2Progress;
+    [FieldOffset(0x22F0)] public ushort Mission3Progress;
 }
