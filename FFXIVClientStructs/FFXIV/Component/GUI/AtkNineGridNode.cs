4b83de4a0f85419bc355bbc0530f2900428e95aa
aers <aers00@gmail.com>
2023-10-03 21:20:27 -0700

    Update AtkNineGridNode.cs

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkNineGridNode.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkNineGridNode.cs
@@ -16,11 +16,11 @@ public unsafe partial struct AtkNineGridNode : ICreatable {
     [FieldOffset(0xBC)] public short TopOffset;
     [FieldOffset(0xBE)] public short BottomOffset;
     [FieldOffset(0xC0)] public short LeftOffset;
-    [FieldOffset(0xC4)] public short RightOffset;
-    [FieldOffset(0xC8)] public uint BlendMode;
+    [FieldOffset(0xC2)] public short RightOffset;
+    [FieldOffset(0xC4)] public uint BlendMode;
 
     // bit 1 = parts type, bit 2 = render type
-    [FieldOffset(0xCC)] public byte PartsTypeRenderType;
+    [FieldOffset(0xC8)] public byte PartsTypeRenderType;
 
     [MemberFunction("E9 ?? ?? ?? ?? 45 33 C9 4C 8B C0 33 D2 B9 ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 85 C0 0F 84 ?? ?? ?? ?? 48 8B C8 48 83 C4 20 5B E9 ?? ?? ?? ?? 45 33 C9 4C 8B C0 33 D2 B9 ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 85 C0 0F 84 ?? ?? ?? ?? 48 8B C8 48 83 C4 20 5B E9 ?? ?? ?? ?? 45 33 C9 4C 8B C0 33 D2 B9 ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 85 C0 74 5D")]
     public partial void Ctor();
