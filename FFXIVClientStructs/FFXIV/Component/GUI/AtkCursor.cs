f1e55a82ee7648bd7d71c4cc00c4880f9854af84
Caraxi <git@caraxian.com>
2023-10-08 13:42:48 +1030

    Update AtkCursor struct

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkCursor.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkCursor.cs
@@ -1,15 +1,23 @@
 namespace FFXIVClientStructs.FFXIV.Component.GUI;
 
-[StructLayout(LayoutKind.Explicit, Size = 0x10)]
+[StructLayout(LayoutKind.Explicit, Size = 0x20)]
 public partial struct AtkCursor {
-    [FieldOffset(0x00)] public CursorType Type;
-    [FieldOffset(0x0E)] public byte Visible;
+    [FieldOffset(0x00)] public bool ShouldAutoHide;
+    [FieldOffset(0x08)] public bool IsAutoHidden;
+    [FieldOffset(0x0C)] public CursorType Type;
+    [FieldOffset(0x1A)] public bool IsVisible;
+    
+    [Obsolete("Use IsVisible", true)]
+    [FieldOffset(0x1A)] public byte Visible;
 
     [MemberFunction("48 83 EC ?? 80 79 ?? ?? 74 ?? C6 41")]
     public partial void Hide();
 
     [MemberFunction("48 83 EC 58 80 79 1A 00 75 6C")]
     public partial void Show();
+
+    [MemberFunction("E8 ?? ?? ?? ?? 83 BE ?? ?? ?? ?? ?? 41 B8")]
+    public partial void SetVisible(bool visible);
 
     [MemberFunction("E8 ?? ?? ?? ?? C6 47 1B 01")]
     public partial void SetCursorType(CursorType type, byte a3 = 0);
