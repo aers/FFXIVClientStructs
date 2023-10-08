b4b6476818d365f5a49f23b5fad6f2c7c56564f3
Caraxi <git@caraxian.com>
2023-10-08 14:06:25 +1030

    Update InputManager.cs

--- FFXIVClientStructs/FFXIV/Client/Game/Control/InputManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Control/InputManager.cs
@@ -5,4 +5,18 @@ public unsafe partial struct InputManager {
 
     [MemberFunction("E8 ?? ?? ?? ?? 3A C3 74 0C")]
     public static partial bool IsAutoRunning();
+
+    [StaticAddress("39 1D ?? ?? ?? ?? 74 14", 2)]
+    public static partial MouseButtonHoldState* GetMouseButtonHoldState();
+
+    public static bool IsLeftMouseDown() => GetMouseButtonHoldState()->HasFlag(MouseButtonHoldState.Left);
+    public static bool IsRightMouseDown() => GetMouseButtonHoldState()->HasFlag(MouseButtonHoldState.Right);
+    public static bool IsAnyMouseDown() => *GetMouseButtonHoldState() != 0;
+
+    [Flags]
+    public enum MouseButtonHoldState : byte {
+        None = 0,
+        Left = 1,
+        Right = 2,
+    }
 }
