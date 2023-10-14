4a0819f2b3b53b2d41f591a503b99914e5bc6ace
Haselnussbomber <mail@haselnussbomber.de>
2023-10-15 01:07:58 +0200

    Add AgentHUD.SetMainCommandEnabledState

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentHUD.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentHUD.cs
@@ -32,6 +32,9 @@ public unsafe partial struct AgentHUD {
 
     [FieldOffset(0x13F0)] public HudPartyMemberEnmity* PartyEnmityList;
 
+    [MemberFunction("E8 ?? ?? ?? ?? 41 B0 01 EB 27")]
+    public partial bool SetMainCommandEnabledState(uint mainCommandId, bool enabled);
+
     [MemberFunction("48 85 D2 74 7F 48 89 5C 24")]
     public partial void OpenContextMenuFromTarget(GameObject* gameObject);
 }
