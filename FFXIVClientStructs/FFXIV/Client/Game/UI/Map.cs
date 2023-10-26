1846533df4832cc116897fd23f95d9a748c55246
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-10-26 11:07:25 -0700

    Fix inconsistent wording

--- FFXIVClientStructs/FFXIV/Client/Game/UI/Map.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/Map.cs
@@ -21,7 +21,7 @@ public unsafe partial struct Map {
     [FieldOffset(0x3EA8)] public StdList<MarkerInfo> CustomTalk;
     [FieldOffset(0x3F50)] public StdList<MarkerInfo> GemstoneTraders;
     
-    [FieldOffset(0x1AF0), Obsolete("Use CurrentLevequest")] public StdVector<MapMarkerData> ActiveLevequestMarkerData;
+    [FieldOffset(0x1AF0), Obsolete("Use ActiveLevequest")] public StdVector<MapMarkerData> ActiveLevequestMarkerData;
     [FieldOffset(0x1B18), Obsolete("Use List<T> UnacceptedQuests")] public MapMarkerContainer QuestMarkerData;
     [FieldOffset(0x1B60), Obsolete("Use List<T> GuildLeveAssignments")] public MapMarkerContainer GuildLeveAssignmentMapMarkerData;
     [FieldOffset(0x1BA8), Obsolete("Use List<T> GuildOrderGuides")] public MapMarkerContainer GuildOrderGuideMarkerData;
