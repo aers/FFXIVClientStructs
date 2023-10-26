11973e5637e37664d2407694f2356964d2d46290
Asriel Camora <asriel@camora.dev>
2023-10-26 01:10:25 -0700

    Add explanatory comments

--- FFXIVClientStructs/STD/List.cs
+++ FFXIVClientStructs/STD/List.cs
@@ -6,9 +6,14 @@ public unsafe struct StdList<T> where T : unmanaged {
     public struct Node {
         public Node* Next;
         public Node* Previous;
+        // Unused in the head node
         public T Value;
     }
 
+    // First node is Head->Next
+    // Last node is Head->Previous
     public Node* Head;
+
+    // Size is the number of nodes in the list (excluding the head node)
     public ulong Size;
 }
