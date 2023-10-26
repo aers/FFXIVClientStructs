fbe3f4a8d98c69b4dfacd575b88fa3df65928dc0
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-10-26 00:35:57 -0700

    Add ChangeHudLayout member

--- FFXIVClientStructs/FFXIV/Client/UI/Misc/AddonConfig.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Misc/AddonConfig.cs
@@ -10,6 +10,16 @@ public unsafe partial struct AddonConfig {
     [FieldOffset(0x00)] public UserFileEvent UserFileEvent;
 
     [FieldOffset(0x50)] public AddonConfigData* ModuleData;
+
+    /// <summary>
+    /// Changes the current HUD Layout to the specific value
+    /// </summary>
+    /// <param name="layoutIndex">HUD Layout Index, 0-based</param>
+    /// <param name="unk1">Unknown, generally False</param>
+    /// <param name="unk2">Unknown, generally True</param>
+    /// <returns></returns>
+    [MemberFunction("E8 ?? ?? ?? ?? 33 C0 EB 15")]
+    public partial nint ChangeHudLayout(uint layoutIndex, bool unk1 = false, bool unk2 = true);
 }
 
 [StructLayout(LayoutKind.Explicit, Size = 0x9E90)]
