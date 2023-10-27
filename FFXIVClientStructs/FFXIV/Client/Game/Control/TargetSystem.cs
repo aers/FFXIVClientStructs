9db89242322cd6f1762725ce4bd6942fe2b8ade8
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-10-26 19:30:55 -0700

    Move fields into TargetSystem

--- FFXIVClientStructs/FFXIV/Client/Game/Control/TargetSystem.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Control/TargetSystem.cs
@@ -18,6 +18,13 @@ public unsafe partial struct TargetSystem {
     [FieldOffset(0x1A98)] public GameObjectArray ObjectFilterArray1;
     [FieldOffset(0x2D58)] public GameObjectArray ObjectFilterArray2;
     [FieldOffset(0x4018)] public GameObjectArray ObjectFilterArray3;
+    
+    // Indicates if addon windows such as the Map, Inventory, should be shown.
+    // Changes to false when interacting with things like Aethernets
+    // These two variables are forced to be opposites of each other
+    [FieldOffset(0x52E0)] public bool ShouldShowWindows;
+    [FieldOffset(0x5300)] public bool ShouldHideWindows;
+    // There are other boolean values around here
 
     [StaticAddress("48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 3B C6 0F 95 C0", 3)]
     public static partial TargetSystem* Instance();
