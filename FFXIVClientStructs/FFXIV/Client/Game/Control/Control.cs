3d411a56ba1b4059ceee08787effc3d4883a4c83
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-10-26 18:54:11 -0700

    Add ShouldShowWindows

--- FFXIVClientStructs/FFXIV/Client/Game/Control/Control.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Control/Control.cs
@@ -10,6 +10,12 @@ public unsafe partial struct Control {
     [FieldOffset(0x5AE8)] public uint LocalPlayerObjectId;
     [FieldOffset(0x5AF0)] public BattleChara* LocalPlayer;
 
+    /// <summary>
+    /// Indicates if addon windows such as the Map, Inventory, should be shown.
+    /// Changes to false when interacting with things like Aethernets
+    /// </summary>
+    [FieldOffset(0x5460)] public bool ShouldShowWindows;
+
     [StaticAddress("4C 8D 35 ?? ?? ?? ?? 85 D2", 3)]
     public static partial Control* Instance();
 }
