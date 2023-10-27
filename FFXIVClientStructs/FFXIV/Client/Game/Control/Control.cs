bc6b8bd023a6d466c10d1f1a00416e8dfb66d1d7
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-10-26 19:10:04 -0700

    Add ShouldHideWindows

--- FFXIVClientStructs/FFXIV/Client/Game/Control/Control.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Control/Control.cs
@@ -10,11 +10,11 @@ public unsafe partial struct Control {
     [FieldOffset(0x5AE8)] public uint LocalPlayerObjectId;
     [FieldOffset(0x5AF0)] public BattleChara* LocalPlayer;
 
-    /// <summary>
-    /// Indicates if addon windows such as the Map, Inventory, should be shown.
-    /// Changes to false when interacting with things like Aethernets
-    /// </summary>
+    // Indicates if addon windows such as the Map, Inventory, should be shown.
+    // Changes to false when interacting with things like Aethernets
+    // These two variables are forced to be opposites of each other
     [FieldOffset(0x5460)] public bool ShouldShowWindows;
+    [FieldOffset(0x5480)] public bool ShouldHideWindows;
 
     [StaticAddress("4C 8D 35 ?? ?? ?? ?? 85 D2", 3)]
     public static partial Control* Instance();
