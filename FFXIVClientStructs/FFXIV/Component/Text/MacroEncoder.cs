0c15c79bcebec7b1bb7fd2e36cc11a6ee86af7d8
pohky <otoriosu@gmail.com>
2023-10-30 01:34:48 +0100

    Update MacroEncoder.cs

--- FFXIVClientStructs/FFXIV/Component/Text/MacroEncoder.cs
+++ FFXIVClientStructs/FFXIV/Component/Text/MacroEncoder.cs
@@ -4,7 +4,7 @@ namespace FFXIVClientStructs.FFXIV.Component.Text;
 
 [StructLayout(LayoutKind.Explicit, Size = 0x310)]
 public unsafe partial struct MacroEncoder {
-    [FieldOffset(0x08)] public StdMap<Utf8String, byte> MacroCodeMap;
+    [FieldOffset(0x08)] public StdMap<Utf8String, MacroCodeDescription> MacroCodeMap;
     [FieldOffset(0x20)] public Utf8String EncoderError;
     [FieldOffset(0x88)] public Utf8String Str2;
     [FieldOffset(0xF0)] public Utf8String Str3;
@@ -13,4 +13,12 @@ public unsafe partial struct MacroEncoder {
     [FieldOffset(0x1D8)] public Utf8String ConditionalOp;
     [FieldOffset(0x240)] public Utf8String Str6;
     [FieldOffset(0x2A8)] public Utf8String Str7;
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x100)]
+    public struct MacroCodeDescription {
+        [FieldOffset(0x00)] public byte Id;
+
+        [FieldOffset(0x48)] public int ParameterCount;
+        [FieldOffset(0x4C)] public bool HasParameter;
+    }
 }
