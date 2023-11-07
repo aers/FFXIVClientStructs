ea61761139a05aad9d1e29b8a60eab548e7b54fb
pohky <otoriosu@gmail.com>
2023-11-07 07:37:15 +0100

    Update Camera.cs

--- FFXIVClientStructs/FFXIV/Client/Graphics/Render/Camera.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Render/Camera.cs
@@ -7,8 +7,9 @@ public struct Camera {
     [FieldOffset(0x00)] public ReferencedClassBase ReferencedClassBase;
 
     [FieldOffset(0x50)] public Matrix4x4 ProjectionMatrix;
-    [FieldOffset(0xA4)] public float FoV;
-    [FieldOffset(0xA8)] public float AspectRatio;
-    [FieldOffset(0xAC)] public float NearPlane;
-    [FieldOffset(0xB0)] public float FarPlane;
+    
+    [FieldOffset(0xA8)] public float FoV;
+    [FieldOffset(0xAC)] public float AspectRatio;
+    [FieldOffset(0xB0)] public float NearPlane;
+    [FieldOffset(0xB4)] public float FarPlane;
 }
