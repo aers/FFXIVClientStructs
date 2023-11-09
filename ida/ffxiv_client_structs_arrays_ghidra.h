c367eeb87dabe1e31931daeca9a9bf0a4a9161fc
WildWolf <magnus1997.mho@gmail.com>
2023-11-09 07:25:09 +0100

    update struct header files

--- ida/ffxiv_client_structs_arrays_ghidra.h
+++ ida/ffxiv_client_structs_arrays_ghidra.h
@@ -8088,11 +8088,7 @@ struct StdString /* Size=0x20 */
 
 struct Client_System_Resource_Handle_ResourceHandle /* Size=0xB0 */
 {
-    union {
-    /* 0x00 */ void* vtbl;
-    /* 0x00 */ void** vfunc;
     /* 0x00 */ Client_System_Resource_Handle_ResourceHandle_ResourceHandleVTable* VTable;
-    } _union_0x0;
     /* 0x08 */ Client_System_Resource_Handle_ResourceHandleType Type;
     /* 0x0C */ unsigned __int32 FileType;
     /* 0x10 */ unsigned __int32 Id;
@@ -10909,10 +10905,7 @@ struct Client_UI_Agent_AgentBannerInterface_Storage /* Size=0x3B30 */
 
 struct Component_GUI_AtkTexture /* Size=0x18 */
 {
-    union {
-    /* 0x00 */ void* vtbl;
     /* 0x00 */ Component_GUI_AtkTexture_AtkTextureVTable* VTable;
-    } _union_0x0;
     union {
     /* 0x08 */ Component_GUI_AtkTextureResource* Resource;
     /* 0x08 */ void* Crest;
@@ -15079,11 +15072,7 @@ struct Common_Configuration_ChangeEventInterface /* Size=0x18 */
 
 struct Client_UI_UIModule /* Size=0xEE030 */
 {
-    union {
-    /* 0x00000 */ void* vtbl;
-    /* 0x00000 */ void** vfunc;
     /* 0x00000 */ Client_UI_UIModule_UIModuleVTable* VTable;
-    } _union_0x0;
     /* 0x00008 */ void** AtkModuleEvent;
     /* 0x00010 */ void** ExcelLanguageEvent;
     /* 0x00018 */ Common_Configuration_ChangeEventInterface ChangeEventInterface;
@@ -15484,10 +15473,7 @@ struct Component_GUI_AtkComponentCheckBox /* Size=0x110 */
 
 struct Component_GUI_AtkDragDropInterface /* Size=0x30 */
 {
-    union {
-    /* 0x00 */ void* vtbl;
     /* 0x00 */ Component_GUI_AtkDragDropInterface_AtkDragDropInterfaceVTable* VTable;
-    } _union_0x0;
     /* 0x08 */ Component_GUI_AtkComponentNode* ComponentNode;
     /* 0x10 */ Component_GUI_AtkResNode* ActiveNode;
     /*      */ byte _gap_0x18[0x18];
@@ -15933,10 +15919,7 @@ struct Component_GUI_AtkTextureResourceManager /* Size=0x58 */
 
 struct Component_GUI_AtkModule /* Size=0x82A0 */
 {
-    union {
-    /* 0x0000 */ void* vtbl;
     /* 0x0000 */ Component_GUI_AtkModule_AtkModuleVTable* VTable;
-    } _union_0x0;
     /*        */ byte _gap_0x8[0x120];
     /* 0x0128 */ Component_GUI_AtkStage* AtkStage;
     /*        */ byte _gap_0x130[0x120];
