7a6160ac487984dbc8f2f2014aecf18e7e648359
pohky <otoriosu@gmail.com>
2023-11-27 04:17:42 +0100

    Update CharacterUtility.cs

--- FFXIVClientStructs/FFXIV/Client/Graphics/Scene/CharacterUtility.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Scene/CharacterUtility.cs
@@ -1,4 +1,3 @@
-using System.Runtime.CompilerServices;
 using FFXIVClientStructs.FFXIV.Client.Graphics.Kernel;
 using FFXIVClientStructs.FFXIV.Client.System.Resource.Handle;
 using FFXIVClientStructs.FFXIV.Common.Math;
@@ -9,26 +8,16 @@ namespace FFXIVClientStructs.FFXIV.Client.Graphics.Scene;
 public unsafe partial struct CharacterUtility {
     public const int ResourceHandleCount = 87;
 
-    [FieldOffset(0x0)]
-    public void* vtbl;
+    [FieldOffset(0x0)] public void* vtbl;
 
-    [FieldOffset(0x8)]
     [FixedSizeArray<Pointer<ResourceHandle>>(ResourceHandleCount)]
-    public fixed byte ResourceHandles[ResourceHandleCount * sizeof(ulong)];
+    [FieldOffset(0x8)] public fixed byte ResourceHandles[ResourceHandleCount * 8];
 
-    [FieldOffset(0x2F8)]
-    public ConstantBuffer* LegacyBodyDecalColorCBuffer;
-    [FieldOffset(0x300)]
-    public ConstantBuffer* FreeCompanyCrestColorCBuffer;
+    [FieldOffset(0x2F8)] public ConstantBuffer* LegacyBodyDecalColorCBuffer;
+    [FieldOffset(0x300)] public ConstantBuffer* FreeCompanyCrestColorCBuffer;
 
-    public ref ResourceHandle* ResourceHandle(int index)
-        => ref *(ResourceHandle**)Unsafe.AsPointer(ref ResourceHandles[0]);
-
-    public readonly ConstantBufferPointer<Vector4> LegacyBodyDecalColorTypedCBuffer
-        => new(LegacyBodyDecalColorCBuffer);
-
-    public readonly ConstantBufferPointer<Vector4> FreeCompanyCrestColorTypedCBuffer
-        => new(FreeCompanyCrestColorCBuffer);
+    public readonly ConstantBufferPointer<Vector4> LegacyBodyDecalColorTypedCBuffer => new(LegacyBodyDecalColorCBuffer);
+    public readonly ConstantBufferPointer<Vector4> FreeCompanyCrestColorTypedCBuffer => new(FreeCompanyCrestColorCBuffer);
 
     [StaticAddress("48 8B 05 ?? ?? ?? ?? 83 B9", 3, true)]
     public static partial CharacterUtility* Instance();
