70ffd9c509a7a19acacfe7efe5efcb3698e6a2e0
Soreepeong <soreepeong@gmail.com>
2023-12-09 22:48:02 +0900

    changes

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkInputManager.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkInputManager.cs
@@ -1,4 +1,3 @@
-using System.Runtime.CompilerServices;
 using FFXIVClientStructs.FFXIV.Client.System.String;
 using FFXIVClientStructs.FFXIV.Component.Text;
 
@@ -6,7 +5,7 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI;
 
 [StructLayout(LayoutKind.Explicit, Size = 0xCC0)]
 public struct AtkInputManager {
-    [FieldOffset(0x1c0)] public ClipboardReadWrite ClipMan;
+    [FieldOffset(0x1c0)] public ClipboardData ClipboardData;
     [FieldOffset(0x298)] public Utf8String CopyBufferRaw;
     [FieldOffset(0x300)] public Utf8String CopyBufferFiltered;
 }
