d1d2eb7607638818a192541e6a591361cd75b5de
Haselnussbomber <mail@haselnussbomber.de>
2023-12-09 23:12:47 +0100

    Change AtkInputManager to AtkTextInput

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkInputManager.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkInputManager.cs
@@ -1,11 +1,6 @@
-using FFXIVClientStructs.FFXIV.Client.System.Input;
-using FFXIVClientStructs.FFXIV.Client.System.String;
-
 namespace FFXIVClientStructs.FFXIV.Component.GUI;
 
-[StructLayout(LayoutKind.Explicit, Size = 0xCC0)]
-public struct AtkInputManager {
-    [FieldOffset(0x1c0)] public ClipBoard ClipboardData;
-    [FieldOffset(0x298)] public Utf8String CopyBufferRaw;
-    [FieldOffset(0x300)] public Utf8String CopyBufferFiltered;
+[StructLayout(LayoutKind.Explicit)]
+public unsafe struct AtkInputManager {
+    [FieldOffset(0)] public AtkTextInput* TextInput;
 }
