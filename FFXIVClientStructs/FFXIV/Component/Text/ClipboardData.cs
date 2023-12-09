62fec46d2021333714ee4cd68dc2502c6d360d6d
Soreepeong <soreepeong@gmail.com>
2023-12-09 22:50:12 +0900

    docs

--- FFXIVClientStructs/FFXIV/Component/Text/ClipboardData.cs
+++ FFXIVClientStructs/FFXIV/Component/Text/ClipboardData.cs
@@ -9,7 +9,8 @@ public unsafe partial struct ClipboardData {
     [FieldOffset(0x70)] public Utf8String CopyStagingText;
     
     /// <summary>
-    /// Writes to the system clipboard.
+    /// Writes to the system clipboard.<br />
+    /// Currently, <c>this</c> is unused; this might as well be a static function, but all uses of it are indirect.
     /// </summary>
     /// <param name="stringToCopy">[in] The string to copy with payloads.</param>
     /// <param name="copiedStringWithoutPayload">[inout] The copied string without payloads.</param>
@@ -17,21 +18,24 @@ public unsafe partial struct ClipboardData {
     public partial void WriteToSystemClipboard(Utf8String* stringToCopy, Utf8String* copiedStringWithoutPayload);
 
     /// <summary>
-    /// Retrieves the current system clipboard text.
+    /// Retrieves the current system clipboard text.<br />
+    /// Currently, <see cref="SystemClipboardText"/> is updated after this call.
     /// </summary>
     /// <returns>The text retrieved from the system clipboard.</returns>
     [VirtualFunction(2)]
     public partial Utf8String* GetSystemClipboardText();
 
     /// <summary>
-    /// Sets the copy staging text.
+    /// Sets the copy staging text.<br />
+    /// Currently, this effectively copies <paramref name="utf8String"/> to <see cref="CopyStagingText"/>.
     /// </summary>
     /// <param name="utf8String">[in] The text.</param>
     [VirtualFunction(3)]
     public partial void SetCopyStagingText(Utf8String* utf8String);
 
     /// <summary>
-    /// Sets the copy staging text to the system clipboard.
+    /// Sets the copy staging text to the system clipboard.<br />
+    /// Currently, this effectively calls <see cref="WriteToSystemClipboard"/>, using <see cref="CopyStagingText"/> as both parameters.
     /// </summary>
     [VirtualFunction(4)]
     public partial void ApplyCopyStagingText();
