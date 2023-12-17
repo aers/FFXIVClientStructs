71cc8138b5e28678f982b946051e154b2c69b58a
Haselnussbomber <mail@haselnussbomber.de>
2023-12-17 16:18:06 +0100

    Cleanup comments

--- FFXIVClientStructs/FFXIV/Client/Game/InstanceContent/InstanceContentDirector.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/InstanceContent/InstanceContentDirector.cs
@@ -4,9 +4,6 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.InstanceContent;
 public unsafe struct InstanceContentDirector {
     [FieldOffset(0x00)] public ContentDirector ContentDirector;
     //[FieldOffset(0x730)] public fixed byte InstanceContentExcelRow[0xA8];
-    /// <summary>
-    /// The remaining time in seconds for the instance.
-    /// </summary>
     [FieldOffset(0xCE4)] public InstanceContentType InstanceContentType;
 }
 
