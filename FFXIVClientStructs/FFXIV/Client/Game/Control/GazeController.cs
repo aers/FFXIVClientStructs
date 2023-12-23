43d07de3b17aca5da871d87dc64ddf39145ff506
Haselnussbomber <mail@haselnussbomber.de>
2023-12-23 23:58:34 +0100

    Use FixedSizeArray for Gazes

--- FFXIVClientStructs/FFXIV/Client/Game/Control/GazeController.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Control/GazeController.cs
@@ -7,22 +7,24 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Control;
 [VTableAddress("48 89 5C 24 ?? 48 8D 05 ?? ?? ?? ?? BA", 8)]
 [StructLayout(LayoutKind.Explicit, Size = 0x5E0)]
 public unsafe partial struct GazeController {
-    [FieldOffset(0x08)] public BattleChara* OwnerObject;
-    [FieldOffset(0x20)] public Gaze Torso;
-    [FieldOffset(0x200)] public Gaze Head;
-    [FieldOffset(0x3E0)] public Gaze Eyes;
+    [FieldOffset(0x10)] public BattleChara* OwnerObject;
+
+    /// <remarks>
+    /// 0 = Torso<br/>
+    /// 1 = Head<br/>
+    /// 2 = Eyes
+    /// </remarks>
+    [FixedSizeArray<Gaze>(3)]
+    [FieldOffset(0x20)] public fixed byte Gazes[3 * Gaze.Size];
+
+    [FieldOffset(0x5C4)] public int GazesCount;
 
     [VTableAddress("48 8D 05 ?? ?? ?? ?? 48 89 01 33 D2 89 51 18", 3)]
-    [StructLayout(LayoutKind.Explicit, Size = 0x1E0)]
+    [StructLayout(LayoutKind.Explicit, Size = Size)]
     public partial struct Gaze {
+        public const int Size = 0x1E0;
+
         [FieldOffset(0x10)] public TargetInformation TargetInfo;
-        // [FieldOffset(0x38)] public fixed byte Unk38[0x40]; // struct
-        // [FieldOffset(0x78)] public fixed byte Unk78[0x28]; // struct
-        // [FieldOffset(0xA0)] public Matrix4x4 UnkA0; // maybe?
-        // [FieldOffset(0xE0)] public Matrix4x4 UnkE0; // maybe?
-        // [FieldOffset(0x120)] public fixed byte Unk120[0x10]; // struct
-        // [FieldOffset(0x130)] public fixed byte Unk130[0x10]; // struct
-        // [FieldOffset(0x140)] public fixed byte Unk140[0x10]; // struct
 
         [VTableAddress("89 51 18 48 8D 05 ?? ?? ?? ?? 48 89 41 10 48 8D 05", 6)]
         [StructLayout(LayoutKind.Explicit, Size = 0x28)]
