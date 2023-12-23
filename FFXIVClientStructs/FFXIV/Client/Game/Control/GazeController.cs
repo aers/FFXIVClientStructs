1e36dc3d86975e6ba9a098bc5ed42ac1f554d3f2
Haselnussbomber <mail@haselnussbomber.de>
2023-12-23 22:54:05 +0100

    Add TargetInfoType enum

--- FFXIVClientStructs/FFXIV/Client/Game/Control/GazeController.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Control/GazeController.cs
@@ -1,5 +1,6 @@
 using FFXIVClientStructs.FFXIV.Client.Game.Character;
 using FFXIVClientStructs.FFXIV.Client.Game.Object;
+using FFXIVClientStructs.FFXIV.Common.Math;
 
 namespace FFXIVClientStructs.FFXIV.Client.Game.Control;
 
@@ -26,15 +27,31 @@ public unsafe partial struct GazeController {
         [VTableAddress("89 51 18 48 8D 05 ?? ?? ?? ?? 48 89 41 10 48 8D 05", 6)]
         [StructLayout(LayoutKind.Explicit, Size = 0x28)]
         public partial struct TargetInformation {
+            [FieldOffset(0x8)] public TargetInfoType Type;
+
             /// <summary>
             /// The current target for this character's gaze. Can be set independently of soft or hard targets, and may be set
             /// by NPCs or minions. For players, an action cast will generally target the LookTarget (which generally will be
             /// the soft target if set, then the hard target).
             /// </summary>
             /// <remarks>
+            /// Used when Type is <see cref="TargetInfoType.GameObjectID"/>.<br/>
             /// Unlike other GameObjectIDs, this one appears to be set to fully 0 if the player is not looking at anything.
             /// </remarks>
             [FieldOffset(0x10)] public GameObjectID TargetId;
+
+            /// <remarks>
+            /// Used when Type is <see cref="TargetInfoType.Unk2"/> or <see cref="TargetInfoType.Unk3"/>.
+            /// </remarks>
+            [FieldOffset(0x10)] public Vector3 Unk10;
+            [FieldOffset(0x20)] public int Unk20;
+
+            public enum TargetInfoType {
+                None = 0,
+                GameObjectID = 1,
+                Unk2 = 2,
+                Unk3 = 3,
+            }
         }
     }
 }
