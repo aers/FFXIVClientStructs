42efbb500c2dad943eca6dc7fa53a253f972a7f6
Koenari <koenari@koenari.de>
2023-12-28 12:17:54 +0100

    Add NetworkProxy member functions

--- FFXIVClientStructs/FFXIV/Client/Network/NetworkModuleProxy.cs
+++ FFXIVClientStructs/FFXIV/Client/Network/NetworkModuleProxy.cs
@@ -13,10 +13,17 @@ public unsafe partial struct NetworkModuleProxy {
     [FieldOffset(0x08)] public NetworkModule* NetworkModule;
     [FieldOffset(0x10)] public NetworkModulePacketReceiverCallback PacketReceiverCallback;
 
-    //[MemberFunction("")] Not siggable
-    //public partial void* FUN_140213c40(int[] p2, uint p3, uint p4);
-    //Calls NetworkModule.FUN_14157f880(p2,p3,p4);
-
     [MemberFunction("E8 ?? ?? ?? ?? 48 8B 9C 24 ?? ?? ?? ?? 0F B6 C8")]
     public partial void RequestCharacterData(byte idx, long contentId, long p4, long p5);
+
+    [MemberFunction("E8 ?? ?? ?? ?? EB ?? 49 8B 85")]
+    public partial bool IsInCrossWorlDuty();
+
+    /// <summary>
+    /// Gets current instance<br/>
+    /// Does NOT invoke network request
+    /// </summary>
+    /// <returns>Current instance or 0 for non instanced zones</returns>
+    [MemberFunction("E8 ?? ?? ?? ?? 48 8D 0D ?? ?? ?? ?? 0F B7 F0 E8 ?? ?? ?? ?? 8B D8")]
+    public partial short GetCurrentInstance();
 }
