fad006f944ffe6d0af04ef899f8c663037866cd7
Koenari <koenari@koenari.de>
2023-12-28 16:13:21 +0100

    rename

--- FFXIVClientStructs/FFXIV/Client/System/Timer/ClientTime.cs
+++ FFXIVClientStructs/FFXIV/Client/System/Timer/ClientTime.cs
@@ -11,7 +11,7 @@ namespace FFXIVClientStructs.FFXIV.Client.System.Timer;
 [StructLayout(LayoutKind.Explicit, Size = 0x40)]
 public struct ClientTime {
     [FieldOffset(0x8)] public long EorzeaTime;
-    [FieldOffset(0x10)] public float EorzeaTimeRemainder;
+    [FieldOffset(0x10)] public float EorzeaTimeMilliseconds;
 
     [FieldOffset(0x30)] public long EorzeaTimeOverride;
     [FieldOffset(0x38)] public bool IsEorzeaTimeOverridden;
