96fdb242e468c2c9d45e322c83c51cff00e027d3
WildWolf <magnus1997.mho@gmail.com>
2024-01-10 18:45:33 +0100

    add some fields and virtual function

--- FFXIVClientStructs/FFXIV/Client/Game/InstanceContent/ContentDirector.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/InstanceContent/ContentDirector.cs
@@ -3,8 +3,15 @@ using FFXIVClientStructs.FFXIV.Client.Game.Event;
 namespace FFXIVClientStructs.FFXIV.Client.Game.InstanceContent;
 
 [StructLayout(LayoutKind.Explicit, Size = 0xC48)]
-public struct ContentDirector {
+public partial struct ContentDirector {
     [FieldOffset(0x00)] public Director Director;
 
     [FieldOffset(0xC08)] public float ContentTimeLeft;
+
+    /// <summary>
+    /// Gets the max time for the content in seconds
+    /// </summary>
+    /// <returns>Time in seconds</returns>
+    [VirtualFunction(313)]
+    public partial uint GetContentTimeMax();
 }
