d4d6331c41ebca5b55e2a7269625c8d8232a143b
Haselnussbomber <mail@haselnussbomber.de>
2024-01-14 20:09:01 +0100

    Move enums out of RaptureTextModule

--- FFXIVClientStructs/FFXIV/Client/UI/Misc/RaptureTextModule.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Misc/RaptureTextModule.cs
@@ -102,46 +102,46 @@ public unsafe partial struct RaptureTextModule {
 
     public static byte* GetOrnamentName(uint id, int intParam2 = 1)
         => FormatName(NameFormatterPlaceholder.ActStr, id, NameFormatterIdConverter.ActStr_Ornament, intParam2);
+}
 
-    public enum NameFormatterPlaceholder : int {
-        ObjStr = 0,
-        Item = 1,   // bypasses IdConverter
-        ActStr = 2,
-    }
+public enum NameFormatterPlaceholder : int {
+    ObjStr = 0,
+    Item = 1,   // bypasses IdConverter
+    ActStr = 2,
+}
 
-    public enum NameFormatterIdConverter : uint {
-        None = 0,
+public enum NameFormatterIdConverter : uint {
+    None = 0,
 
-        // ObjStr
-        ObjStr_BNpcName = 2,
-        ObjStr_ENpcResident = 3,
-        ObjStr_Treasure = 4,
-        ObjStr_Aetheryte = 5,
-        ObjStr_GatheringPointName = 6,
-        ObjStr_EObjName = 7,
-        // ObjStr_Mount = 8, // does not work?
-        ObjStr_Companion = 9,
-        // 10-11 unused
-        // ObjStr_Item = 12, // does not work?
+    // ObjStr
+    ObjStr_BNpcName = 2,
+    ObjStr_ENpcResident = 3,
+    ObjStr_Treasure = 4,
+    ObjStr_Aetheryte = 5,
+    ObjStr_GatheringPointName = 6,
+    ObjStr_EObjName = 7,
+    // ObjStr_Mount = 8, // does not work?
+    ObjStr_Companion = 9,
+    // 10-11 unused
+    // ObjStr_Item = 12, // does not work?
 
-        // ActStr
-        ActStr_Trait = 0,
-        ActStr_Action = 1,
-        // ActStr_Item = 2, // does not work?
-        // ActStr_EventItem = 3, // does not work?
-        ActStr_EventAction = 4,
-        // ActStr_EObjName = 5, // does not work?
-        ActStr_GeneralAction = 5,
-        ActStr_BuddyAction = 6,
-        ActStr_MainCommand = 7,
-        // ActStr_Companion = 8, // unresolved, use ObjStr_Companion
-        ActStr_CraftAction = 9,
-        ActStr_Action2 = 10,
-        ActStr_PetAction = 11,
-        ActStr_CompanyAction = 12,
-        ActStr_Mount = 13,
-        // 14-18 unused
-        ActStr_BgcArmyAction = 19,
-        ActStr_Ornament = 20,
-    }
+    // ActStr
+    ActStr_Trait = 0,
+    ActStr_Action = 1,
+    // ActStr_Item = 2, // does not work?
+    // ActStr_EventItem = 3, // does not work?
+    ActStr_EventAction = 4,
+    // ActStr_EObjName = 5, // does not work?
+    ActStr_GeneralAction = 5,
+    ActStr_BuddyAction = 6,
+    ActStr_MainCommand = 7,
+    // ActStr_Companion = 8, // unresolved, use ObjStr_Companion
+    ActStr_CraftAction = 9,
+    ActStr_Action2 = 10,
+    ActStr_PetAction = 11,
+    ActStr_CompanyAction = 12,
+    ActStr_Mount = 13,
+    // 14-18 unused
+    ActStr_BgcArmyAction = 19,
+    ActStr_Ornament = 20,
 }
