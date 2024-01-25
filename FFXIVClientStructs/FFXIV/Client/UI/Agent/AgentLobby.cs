cf8f5b4ccaa533a65ba8a48b1080d5c0f8eef265
Haselnussbomber <mail@haselnussbomber.de>
2024-01-26 00:48:33 +0100

    Obsolete some vtbls of structs with VTableAddress

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentLobby.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentLobby.cs
@@ -60,7 +60,7 @@ public unsafe partial struct LobbyData {
 [VTableAddress("48 8D 05 ?? ?? ?? ?? 48 8B F9 48 89 01 48 81 C1 ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8D 8F ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8D 8F ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8D 8F", 3)]
 [StructLayout(LayoutKind.Explicit, Size = 0x848)]
 public unsafe partial struct LobbyUIClient {
-    [FieldOffset(0x00)] public void** vtbl;
+    [FieldOffset(0x00), Obsolete("Use LobbyUIClient.StaticAddressPointers.VTable")] public void** vtbl;
     [FieldOffset(0x10)] public NetworkModuleProxy* NetworkModuleProxy;
     //[FieldOffset(0x18)] public ?* NetworConfig; // contains hosts and ports
 
