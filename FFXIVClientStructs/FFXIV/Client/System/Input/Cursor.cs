9b98950b1466645185813b8a757c1f20c1023f6d
Koenari <koenari@koenari.de>
2024-01-28 14:15:52 +0100

    extend cursor

--- FFXIVClientStructs/FFXIV/Client/System/Input/Cursor.cs
+++ FFXIVClientStructs/FFXIV/Client/System/Input/Cursor.cs
@@ -7,13 +7,23 @@ namespace FFXIVClientStructs.FFXIV.Client.System.Input;
 [StructLayout(LayoutKind.Explicit, Size = 0x378)]
 public unsafe partial struct Cursor {
     [FieldOffset(0x000)] public void* vtbl;
+
     [FieldOffset(0x009)] public bool UseSoftwareCursor;
+    [FieldOffset(0x00A)] public byte SoftwareCursorScale;
     [FieldOffset(0x00B)] public bool IsCursorVisible;
     [FieldOffset(0x00C)] public bool MouseNotCpatured;
     [FieldOffset(0x00D)] public bool IsCursorOutsideViewPort;
+
     [FieldOffset(0x010)] public uint ActiveCursorType; //Hand,Cursor, Cross, etc
+
     [FieldOffset(0x1B0)] public fixed ulong CursorHandles[16]; //HCURSOR (winuser.h)
+
     [FixedSizeArray<Pointer<byte>>(16)]
     [FieldOffset(0x238)] public fixed byte CursorNames[8 * 16];
+    [FieldOffset(0x2C0)] public int HardwareCursorSize;
     [FieldOffset(0x2C8)] public TextureResourceHandle* SoftwareCursorTexture;
+
+    [FieldOffset(0x368)] public bool ShowSoftwareCursorTrajectory;
+
+    [FieldOffset(0x370)] public bool UseOsHardwareCursor;
 }
