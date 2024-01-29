bb1adc1581c1d13f79e9a40d809e983775cbf62b
Haselnussbomber <mail@haselnussbomber.de>
2024-01-29 20:44:26 +0100

    Update AgentTryon

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentTryon.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentTryon.cs
@@ -12,22 +12,22 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
 [StructLayout(LayoutKind.Explicit, Size = 0x670)]
 public unsafe partial struct AgentTryon {
     [FieldOffset(0)] public AgentInterface AgentInterface;
-    [FieldOffset(0x28)] public CharaViewTryOn CharaView;
+    [FieldOffset(0x28)] public TryonCharaView CharaView;
 
     // you can ignore the openerAddonId in the call and just set it to 0
     [MemberFunction("E8 ?? ?? ?? ?? EB 56 48 8B 49 10")]
     public static partial bool TryOn(uint openerAddonId, uint itemId, byte stainId, uint glamourItemId, byte glamourStainId);
-}
 
-// Client::UI::Agent::AgentTryon::TryonCharaView
-//   Client::UI::Misc::CharaView
-[StructLayout(LayoutKind.Explicit, Size = 0x2D0)]
-public unsafe partial struct CharaViewTryOn {
-    [FieldOffset(0)] public CharaView Base;
-    [FieldOffset(0x2C8)] public bool DoUpdate; // beware: fetches data from agent too, happens in vf10
-    [FieldOffset(0x2C9)] public bool HideOtherEquipment;
-    [FieldOffset(0x2CA)] public bool HideVisor;
-    [FieldOffset(0x2CB)] public bool HideWeapon;
-    [FieldOffset(0x2CC)] public bool CloseVisor;
-    [FieldOffset(0x2CD)] public bool DrawWeapon;
+    // Client::UI::Agent::AgentTryon::TryonCharaView
+    //   Client::UI::Misc::CharaView
+    [StructLayout(LayoutKind.Explicit, Size = 0x2D0)]
+    public unsafe partial struct TryonCharaView {
+        [FieldOffset(0)] public CharaView Base;
+        [FieldOffset(0x2C8)] public bool DoUpdate; // beware: fetches data from agent too, happens in vf10
+        [FieldOffset(0x2C9)] public bool HideOtherEquipment;
+        [FieldOffset(0x2CA)] public bool HideVisor;
+        [FieldOffset(0x2CB)] public bool HideWeapon;
+        [FieldOffset(0x2CC)] public bool CloseVisor;
+        [FieldOffset(0x2CD)] public bool DrawWeapon;
+    }
 }
