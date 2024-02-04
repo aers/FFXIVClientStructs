5178759f3982af81b78c884d8648568e76cce1af
Infi <infiziert@protonmail.ch>
2024-02-04 16:15:47 +0100

    Increase ContentsNote size for 19 item arrays

--- FFXIVClientStructs/FFXIV/Client/Game/UI/ContentsNote.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/ContentsNote.cs
@@ -1,7 +1,7 @@
 namespace FFXIVClientStructs.FFXIV.Client.Game.UI;
 
 // Client::Game::UI::ContentsNote
-[StructLayout(LayoutKind.Explicit, Size = 0xB8)]
+[StructLayout(LayoutKind.Explicit, Size = 0xC0)]
 public unsafe partial struct ContentsNote {
     [StaticAddress("48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 84 C0 75 0C 0F B6 43 41", 3)]
     public static partial ContentsNote* Instance();
@@ -9,8 +9,8 @@ public unsafe partial struct ContentsNote {
     [FieldOffset(0x08)] public fixed byte CompletionFlags[11];
     [FieldOffset(0x1C)] public byte SelectedTab;
     [FieldOffset(0x1D)] public byte DisplayCount;
-    [FieldOffset(0x20)] public fixed int DisplayID[17];
-    [FieldOffset(0x64)] public fixed int DisplayStatus[17];
+    [FieldOffset(0x20)] public fixed int DisplayID[19];
+    [FieldOffset(0x6C)] public fixed int DisplayStatus[19];
 
     public bool IsContentNoteComplete(int index) {
         var offsetIndex = index - 1;
