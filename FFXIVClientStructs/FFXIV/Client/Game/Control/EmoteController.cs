37ade540fa2e0983b172a81a38fc6f69c6c25004
Caraxi <git@caraxian.com>
2024-02-24 11:44:14 +1030

    Update EmoteController.cs

--- FFXIVClientStructs/FFXIV/Client/Game/Control/EmoteController.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Control/EmoteController.cs
@@ -11,4 +11,10 @@ public unsafe partial struct EmoteController {
     [FieldOffset(0x16)] public ushort Unk1; // Seems to be 1 when close enough to a target that height adjustment is needed, maybe.
     [FieldOffset(0x18)] public GameObjectID Target;
     [FieldOffset(0x21)] public byte CPoseState;
+    
+    [MemberFunction("E8 ?? ?? ?? ?? 8B F8 83 F8 FF 0F 84 ?? ?? ?? ?? 8B CF")]
+    public partial int GetPoseKind();
+
+    [MemberFunction("E8 ?? ?? ?? ?? FE C3 44 8B F0")]
+    public static partial byte GetAvailablePoses(int poseKind);
 }
