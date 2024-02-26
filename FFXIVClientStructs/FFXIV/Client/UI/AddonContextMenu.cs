04adab4cf5ef3cfec62155a093f8bc64dabb2586
Asriel Camora <asriel@camora.dev>
2024-02-25 20:26:43 -0800

    Some ContextMenu sigs

--- FFXIVClientStructs/FFXIV/Client/UI/AddonContextMenu.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonContextMenu.cs
@@ -4,6 +4,9 @@ namespace FFXIVClientStructs.FFXIV.Client.UI;
 
 [Addon("ContextMenu")]
 [StructLayout(LayoutKind.Explicit, Size = 0x2A0)]
-public struct AddonContextMenu {
+public partial struct AddonContextMenu {
     [FieldOffset(0x0)] public AtkUnitBase AtkUnitBase;
+
+    [VirtualFunction(72)]
+    public partial bool OnMenuSelected(int selectedIdx, byte a3);
 }
--- FFXIVClientStructs/FFXIV/Client/UI/RaptureAtkModule.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/RaptureAtkModule.cs
@@ -42,6 +42,12 @@ public unsafe partial struct RaptureAtkModule {
     [MemberFunction("E8 ?? ?? ?? ?? 48 8B 46 58 48 85 C0")]
     public partial bool DecRefStringArrayData(int index);
 
+    [MemberFunction("E8 ?? ?? ?? ?? 66 89 46 50")]
+    public partial nint OpenAddon(uint addonNameId, uint valueCount, AtkValue* values, AgentInterface* parentAgent, long unk, uint ownerAddonId, int unk2);
+
+    [MemberFunction("E8 ?? ?? ?? ?? 0F B7 C0 48 83 C4 60")]
+    public partial nint OpenAddonByAgent(byte* addonName, AtkUnitBase* addon, int valueCount, AtkValue* values, AgentInterface* agent, nint a7, ushort parentAddonId);
+
     [VirtualFunction(39)]
     public partial void SetUiVisibility(bool uiVisible);
 
