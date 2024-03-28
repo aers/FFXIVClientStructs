41dc8bbedb8f71b7baa7fc912ca42e8acb9b7979
Haselnussbomber <mail@haselnussbomber.de>
2024-03-28 20:08:48 +0100

    Fix size of Client::Game::UI::TripleTriad

--- FFXIVClientStructs/FFXIV/Client/Game/UI/TripleTriad.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/TripleTriad.cs
@@ -4,7 +4,7 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.UI;
 
 // Client::Game::UI::TripleTriad
 //   Component::GUI::AtkModuleInterface::AtkEventInterface
-[StructLayout(LayoutKind.Explicit, Size = 0x20)]
+[StructLayout(LayoutKind.Explicit, Size = 0x1510)]
 public struct TripleTriad {
     [FieldOffset(0)] public AtkEventInterface AtkEventInterface;
 }
