9b41e764dfa9a9ec8508932b1be38f1dfe150dee
Joshua Goins <josh@redstrate.com>
2024-04-13 17:31:59 -0400

    BoneSimulator: Document known physics groups and more flags
    These physics groups correspond to what the bone simulator is
    simulating. There's also three flags that control some of the state such
    as when the simulator starts, stops or needs to be reset.

--- FFXIVClientStructs/FFXIV/Client/Graphics/Physics/BoneSimulator.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Physics/BoneSimulator.cs
@@ -8,10 +8,28 @@ namespace FFXIVClientStructs.FFXIV.Client.Graphics.Physics;
 [StructLayout(LayoutKind.Explicit, Size = 0x100)]
 public unsafe struct BoneSimulator {
     [FieldOffset(0x00)] public void* vtbl;
-    [FieldOffset(0x10)] public uint PhysicsGroup;
+
+    /// <summary> Non-exhaustive list of physics groups </summary>
+    public enum PhysicsGroup : uint
+    {
+        Clothing = 2,
+        HairA = 3, // Usually the hair's bangs
+        HairB = 4, // Typically the back of the hair
+        HairC = 5, // The sides of the hair
+        HairD = 6, // Extraneous hair bits
+        Chest = 7,
+        Earrings = 8,
+        Ears = 18,
+    }
+
+    [FieldOffset(0x10)] public PhysicsGroup Group;
     [FieldOffset(0x18)] public Skeleton* Skeleton; // Client::Graphics::Render::Skeleton
     [FieldOffset(0x20)] public Vector3 CharacterPosition;
     [FieldOffset(0x30)] public Vector3 Gravity;
     [FieldOffset(0x40)] public Vector3 Wind;
     [FieldOffset(0x54)] public float Spring; // Default is ~60, intense jitter happens above that value. Lesser values remove the spring in the bone.
+    [FieldOffset(0xF6)] public bool Start; // Flag that is set to true when the simulator starts, and is quickly reset
+    [FieldOffset(0xF7)] public bool Stop; // Same as Start, but when the simulator is requested to stop
+    [FieldOffset(0xF8)] public bool Reset; // When set to true, resets the bone simulator
+
 }
