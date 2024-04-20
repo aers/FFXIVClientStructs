465a30745b02e74ce36ee70e0c0e7b723823fc29
WildWolf <magnus1997.mho@gmail.com>
2024-04-20 02:30:51 +0200

    fix some struct size issues

--- FFXIVClientStructs/FFXIV/Client/UI/AddonRetainerTaskList.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonRetainerTaskList.cs
@@ -6,7 +6,7 @@ namespace FFXIVClientStructs.FFXIV.Client.UI;
 //   Component::GUI::AtkUnitBase
 //     Component::GUI::AtkEventListener
 [Addon("RetainerTaskList")]
-[StructLayout(LayoutKind.Explicit, Size = 0x8)]
+[StructLayout(LayoutKind.Explicit, Size = 0x238)]
 [VTableAddress("48 8d 05 ?? ?? ?? ?? 48 89 03 33 c0 80 8b 88 01 00 00 20 48 89 83 20 02 00 00 48 89 83 28 02 00 00 88", 3)]
 public unsafe partial struct AddonRetainerTaskList {
     [FieldOffset(0x0)] public AtkUnitBase AtkUnitBase;
