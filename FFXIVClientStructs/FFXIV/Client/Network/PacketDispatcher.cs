5ff32057d592a778f448e99311b0c4c7f98482e5
Haselnussbomber <mail@haselnussbomber.de>
2024-04-20 04:42:46 +0200

    Obsolete PacketDispatcher.ReceiveEvent

--- FFXIVClientStructs/FFXIV/Client/Network/PacketDispatcher.cs
+++ FFXIVClientStructs/FFXIV/Client/Network/PacketDispatcher.cs
@@ -2,6 +2,7 @@ namespace FFXIVClientStructs.FFXIV.Client.Network;
 
 [StructLayout(LayoutKind.Explicit, Size = 0x8)]
 public unsafe partial struct PacketDispatcher {
+    [Obsolete("Invalid signature. This resolves to a member function of FateManager and is scheduled for removal.")]
     [MemberFunction("48 89 5C 24 ?? 56 48 83 EC 20 4C 8B 09")]
     public partial void ReceiveEvent(uint p2, void* p3);
 
