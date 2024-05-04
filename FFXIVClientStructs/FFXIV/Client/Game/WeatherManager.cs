13fcbc45398da3018f31d5b77155b90c55f28601
pohky <otoriosu@gmail.com>
2024-05-05 00:11:50 +0200

    Update WeatherManager.cs

--- FFXIVClientStructs/FFXIV/Client/Game/WeatherManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/WeatherManager.cs
@@ -6,8 +6,22 @@ namespace FFXIVClientStructs.FFXIV.Client.Game;
 /// </remarks>
 [StructLayout(LayoutKind.Explicit, Size = 0x70)]
 public unsafe partial struct WeatherManager {
-    [StaticAddress("48 8D 0D ?? ?? ?? ?? 44 0F B7 45", 3, true)]
+    [FixedSizeArray<Pointer<ServerWeather>>(3)]
+    [FieldOffset(0x00)] internal fixed byte WeatherPtr[3 * 0x08];
+    [FixedSizeArray<ServerWeather>(3)]
+    [FieldOffset(0x18)] internal fixed byte Weather[3 * 0x18];
+
+    [FieldOffset(0x60)] public byte WeatherIndex;
+    [FieldOffset(0x64)] public byte WeatherId;
+    [FieldOffset(0x65)] public byte WeatherOverride;
+    [FieldOffset(0x66)] public byte CurrentDaytimeOffset;
+    [FieldOffset(0x67)] public byte IndividualWeatherId;
+
+    [StaticAddress("48 8D 0D ?? ?? ?? ?? 44 0F B7 45", 3)]
     public static partial WeatherManager* Instance();
+
+    [MemberFunction("E8 ?? ?? ?? ?? 0F B6 C8 8D 41 ?? A9")]
+    public partial byte GetCurrentWeather();
 
     /// <summary>
     /// Checks if the specified TerritoryType has individual weather.
@@ -45,4 +59,12 @@ public unsafe partial struct WeatherManager {
     /// <returns> The id of a row in the Weather sheet. </returns>
     [MemberFunction("46 8D 04 C5 ?? ?? ?? ?? E9")]
     public partial byte GetWeatherForDaytime(ushort territoryTypeId, int daytimeOffset);
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x18)]
+    public unsafe partial struct ServerWeather {
+        [FieldOffset(0x08)] public byte NextWeatherId;
+        [FieldOffset(0x09)] public byte CurrentWeatherId;
+        [FieldOffset(0x0C)] public float DaytimeFadeTimeLeft;
+        [FieldOffset(0x10)] public float DaytimeFadeLength;
+    }
 }
