3005c0774bff422f26e08568dd764db63f7675b9
Haselnussbomber <mail@haselnussbomber.de>
2024-05-09 16:03:04 +0200

    Update AtkEventType

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkEvent.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkEvent.cs
@@ -3,6 +3,7 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI;
 // max known: 79
 // seems to have generic events followed by component-specific events
 public enum AtkEventType : byte {
+    // AtkResNode
     MouseDown = 3,
     MouseUp = 4,
     MouseMove = 5,
@@ -16,13 +17,13 @@ public enum AtkEventType : byte {
     // AtkComponentButton & children
     ButtonPress = 23, // sent on MouseDown on button
     ButtonRelease = 24, // sent on MouseUp and MouseOut
-    ButtonClick = 25, // sent on MouseUp and MouseClick on button     
+    ButtonClick = 25, // sent on MouseUp and MouseClick on button
 
     ListItemRollOver = 33,
     ListItemRollOut = 34,
     ListItemToggle = 35,
 
-    // AtkComponentDragDrop 
+    // AtkComponentDragDrop
     DragDropBegin = 47, // sent on MouseDown over a draggable icon (will NOT send for a locked icon)
     DragDropInsert = 50, // sent when dropping an icon into a hotbar/inventory slot or similar
     DragDropRollOver = 52,
@@ -40,14 +41,26 @@ public enum AtkEventType : byte {
     IconTextRollOut = 57,
     IconTextClick = 58,
 
+    // AtkTimer
+    TimerTick = 61,
+    TimerEnd = 62,
+
     // AtkSimpleTween
     TweenProgress = 64,
     TweenComplete = 65,
+
+    // AtkAddonControl
+    ChildAddonAttached = 66, // found inside AtkAddonControl_Update at "E8 ?? ?? ?? ?? 49 8B 4D 00 33 C0"
 
     // AtkComponentWindow
     WindowRollOver = 67,
     WindowRollOut = 68,
     WindowChangeScale = 69,
+
+    // AtkTextNode
+    LinkMouseClick = 72,
+    LinkMouseOver = 73,
+    LinkMouseOut = 74,
 }
 
 [StructLayout(LayoutKind.Explicit, Size = 0x30)]
