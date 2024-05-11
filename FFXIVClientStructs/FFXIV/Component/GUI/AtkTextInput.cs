2886c18e4ac77ef4e0c3c44210b67203d6315da0
Haselnussbomber <mail@haselnussbomber.de>
2024-05-11 23:50:45 +0200

    Update AtkInputManager

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkTextInput.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkTextInput.cs
@@ -1,14 +1,17 @@
 using FFXIVClientStructs.FFXIV.Client.System.Input;
 using FFXIVClientStructs.FFXIV.Client.System.String;
+using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Client.UI.Misc;
 using FFXIVClientStructs.FFXIV.Component.Completion;
 
 namespace FFXIVClientStructs.FFXIV.Component.GUI;
 
 [StructLayout(LayoutKind.Explicit, Size = 0xCC0)]
-public unsafe struct AtkTextInput {
+public unsafe partial struct AtkTextInput {
     [FieldOffset(0x10)] public CompletionModule* CompletionModule;
     [FieldOffset(0x18)] public TextService* TextService;
+    [FixedSizeArray<Pointer<RaptureAtkHistory>>(19)]
+    [FieldOffset(0x20)] public fixed byte RaptureAtkHistories[0x8 * 19];
     [FieldOffset(0xC0)] public RaptureTextModule* RaptureTextModule;
     [FieldOffset(0xD0)] public AtkFontCodeModule* AtkFontCodeModule;
     [FieldOffset(0x1C0)] public ClipBoard ClipboardData;
