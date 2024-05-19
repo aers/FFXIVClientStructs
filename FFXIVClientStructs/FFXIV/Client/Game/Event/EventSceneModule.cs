2d58fd206fe59d38d5bcd1f84eebd77a5593a8d8
Haselnussbomber <mail@haselnussbomber.de>
2024-05-19 04:22:45 +0200

    Update EventSceneModule

--- FFXIVClientStructs/FFXIV/Client/Game/Event/EventSceneModule.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Event/EventSceneModule.cs
@@ -4,8 +4,8 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Event;
 // ctor "E8 ?? ?? ?? ?? 33 F6 49 8D 8D ?? ?? ?? ?? 41 89 B5"
 [StructLayout(LayoutKind.Explicit, Size = 0x31C0)]
 public unsafe struct EventSceneModule {
-    [FieldOffset(0x00)] public EventSceneModuleUsualImpl EventSceneModuleUsualImpl;
-    [FieldOffset(0x10)] public EventSceneModuleImplBase EventSceneModuleImplBase;
-    [FieldOffset(0x20)] public EventSceneModuleImplBase* EventSceneModuleImpl;
+    [FieldOffset(0x18)] public EventSceneModuleUsualImpl EventSceneModuleUsualImpl;
+    [FieldOffset(0x28)] public EventSceneModuleImplBase EventSceneModuleImplBase;
+    [FieldOffset(0x38)] public EventSceneModuleImplBase* EventSceneModuleImpl;
     [FieldOffset(0x1F0)] public EventGPoseController EventGPoseController;
 }
