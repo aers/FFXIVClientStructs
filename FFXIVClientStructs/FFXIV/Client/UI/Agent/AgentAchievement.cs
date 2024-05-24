f409d235d67a3b66b111acd1bfa243b383dc9208
pohky <otoriosu@gmail.com>
2024-05-24 21:34:38 +0200

    Update AgentAchievement

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentAchievement.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentAchievement.cs
@@ -8,5 +8,5 @@ public partial struct AgentAchievement {
     [FieldOffset(0x00)] public AgentInterface AgentInterface;
 
     [MemberFunction("E8 ?? ?? ?? ?? EB ?? 48 8B 46 ?? 48 63 4E")]
-    public partial void OpenAchievement(uint achievementId);
+    public partial void OpenById(uint achievementId);
 }
