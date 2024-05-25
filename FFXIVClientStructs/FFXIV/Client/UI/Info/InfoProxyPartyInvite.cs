4ff07c83c0b52ee23750d9edbe1ec84a1d85a313
Infi <infiziert@protonmail.ch>
2024-05-25 11:20:50 +0200

    Add AgentPartyMember and add functions to InfoProxyPartyInvite

--- FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyPartyInvite.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyPartyInvite.cs
@@ -10,4 +10,13 @@ public unsafe partial struct InfoProxyPartyInvite {
     [FieldOffset(0x042)] public ushort InviterWorldID;
     [FieldOffset(0x048)] public Utf8String IviterName;
     [FieldOffset(0x0B0)] public Utf8String IviterNameWithHomeworld;
+
+    [MemberFunction("E8 ?? ?? ?? ?? 33 C0 EB 51")]
+    public partial byte InviteToParty(byte* name, ushort world, ulong contentId);
+
+    [MemberFunction("48 83 EC 38 41 B1 09")]
+    public partial byte InviteToPartyContentId(ulong contentId);
+
+    [MemberFunction("E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 48 8B 83 ?? ?? ?? ?? 48 85 C0 74 62")]
+    public partial byte InviteToPartyInInstance(ulong contentId);
 }
