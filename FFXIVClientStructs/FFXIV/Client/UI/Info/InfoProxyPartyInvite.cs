90a152f92e031c6fd5a14bf73fa66ac279138ae1
Infi <infiziert@protonmail.ch>
2024-05-25 13:03:52 +0200

    small fixes

--- FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyPartyInvite.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyPartyInvite.cs
@@ -12,11 +12,11 @@ public unsafe partial struct InfoProxyPartyInvite {
     [FieldOffset(0x0B0)] public Utf8String IviterNameWithHomeworld;
 
     [MemberFunction("E8 ?? ?? ?? ?? 33 C0 EB 51")]
-    public partial byte InviteToParty(ulong contentId, byte* name, ushort worldId);
+    public partial bool InviteToParty(ulong contentId, byte* name, ushort worldId);
 
     [MemberFunction("48 83 EC 38 41 B1 09")]
-    public partial byte InviteToPartyContentId(ulong contentId, ushort worldId);
+    public partial bool InviteToPartyContentId(ulong contentId, ushort worldId);
 
     [MemberFunction("E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 48 8B 83 ?? ?? ?? ?? 48 85 C0 74 62")]
-    public partial byte InviteToPartyInInstance(ulong contentId);
+    public partial bool InviteToPartyInInstance(ulong contentId);
 }
