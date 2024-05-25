2437dff13355cb0031b96c638d8db37973d2519a
Infi <infiziert@protonmail.ch>
2024-05-25 11:35:41 +0200

    add unknown ushort to promote and kick

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentPartyMember.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentPartyMember.cs
@@ -8,8 +8,8 @@ public unsafe partial struct AgentPartyMember {
     [FieldOffset(0x00)] public AgentInterface AgentInterface;
 
     [MemberFunction("E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 49 8B 56 20")]
-    public partial void Promote(byte* name, ulong contentId);
+    public partial void Promote(byte* name, ushort unknown, ulong contentId);
 
     [MemberFunction("E8 ?? ?? ?? ?? EB 66 49 8B 4E 20")]
-    public partial void Kick(byte* name, ulong contentId);
+    public partial void Kick(byte* name, ushort unknown, ulong contentId);
 }
