d38489ef3742bfdc4e044574dce45e4e88535732
Infi <infiziert@protonmail.ch>
2024-05-25 13:14:56 +0200

    Change return type to bool

--- FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyNoviceNetwork.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyNoviceNetwork.cs
@@ -5,5 +5,5 @@ public unsafe partial struct InfoProxyNoviceNetwork {
     [FieldOffset(0x00)] public InfoProxyInterface InfoProxyInterface;
 
     [MemberFunction("E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 48 8B CB E8 ?? ?? ?? ?? 45 33 C9")]
-    public partial byte InviteToNoviceNetwork(ulong contentId, ushort worldId, byte* name);
+    public partial bool InviteToNoviceNetwork(ulong contentId, ushort worldId, byte* name);
 }
