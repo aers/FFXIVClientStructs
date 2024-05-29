65c0fdd1745ad1fecbb95443de10f575617fcca2
pohky <otoriosu@gmail.com>
2024-05-29 15:17:25 +0200

    Update Demihuman.cs

--- FFXIVClientStructs/FFXIV/Client/Graphics/Scene/Demihuman.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Scene/Demihuman.cs
@@ -13,21 +13,14 @@ namespace FFXIVClientStructs.FFXIV.Client.Graphics.Scene;
 [GenerateInterop]
 [Inherits<CharacterBase>]
 public unsafe partial struct Demihuman {
-    [FieldOffset(0x928), CExportIgnore] private nint _slotDecalBase;
+    [FieldOffset(0x928), FixedSizeArray, CExportIgnore] internal FixedSizeArray5<Pointer<TextureResourceHandle>> _slotDecals;
     [FieldOffset(0x928)] public TextureResourceHandle* HeadDecal;
     [FieldOffset(0x930)] public TextureResourceHandle* TopDecal;
     [FieldOffset(0x938)] public TextureResourceHandle* ArmsDecal;
     [FieldOffset(0x940)] public TextureResourceHandle* LegsDecal;
     [FieldOffset(0x948)] public TextureResourceHandle* FeetDecal;
 
-    public ref TextureResourceHandle* SlotDecal(int slot) {
-        if (slot < 0 || slot > 4)
-            throw new ArgumentOutOfRangeException(nameof(slot));
-        return ref ((TextureResourceHandle**)Unsafe.AsPointer(ref _slotDecalBase))[slot];
-    }
-
-    public Span<Pointer<TextureResourceHandle>> SlotDecalsSpan
-        => new(Unsafe.AsPointer(ref _slotDecalBase), 5);
+    public TextureResourceHandle* SlotDecal(int slot) => SlotDecals[slot].Value;
 
     [FieldOffset(0x958)] public Texture* FreeCompanyCrest;
     [FieldOffset(0x960)] public uint SlotFreeCompanyCrestBitfield; // Only relevant bit is & 0x1
