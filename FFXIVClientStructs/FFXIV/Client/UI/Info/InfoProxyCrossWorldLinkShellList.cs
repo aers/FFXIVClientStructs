40934624a488fe4ac1884221930ea84b579104c4
Haselnussbomber <mail@haselnussbomber.de>
2024-05-30 04:43:57 +0200

    Fix capitalization

--- FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyCrossWorldLinkShellList.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyCrossWorldLinkShellList.cs
@@ -12,14 +12,14 @@ public unsafe partial struct InfoProxyCrossWorldLinkshell {
     [FieldOffset(0x30)] public byte Unk30;
     [FieldOffset(0x38)] public Utf8String InvitedName;
     [FieldOffset(0xA0)] public Utf8String UnkString0;
-    [FieldOffset(0x108), FixedSizeArray] internal FixedSizeArray8<CrossworldLinkshellEntry> _crossworldLinkshells;
+    [FieldOffset(0x108), FixedSizeArray] internal FixedSizeArray8<CrossWorldLinkshellEntry> _crossWorldLinkshells;
     //530 after
 
     [MemberFunction("E8 ?? ?? ?? ?? 48 8B C8 E8 ?? ?? ?? ?? 45 8D 46 FB")]
     public partial Utf8String* GetCrossworldLinkshellName(uint index);
 
     [StructLayout(LayoutKind.Explicit, Size = 0x88)]
-    public partial struct CrossworldLinkshellEntry {
+    public partial struct CrossWorldLinkshellEntry {
         [FieldOffset(0x00)] public Utf8String Name;
         //0x10 bytes
         [FieldOffset(0x78)] public uint FoundationTime;
