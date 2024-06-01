bc24c1770bb3df325bab84d1375ba0793de733db
WildWolf <magnus1997.mho@gmail.com>
2024-06-01 15:25:07 +0200

    fix some structs not getting their pointer types when converting to new fixed arrays

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkSimpleTweenHolder.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkSimpleTweenHolder.cs
@@ -3,6 +3,6 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI;
 [GenerateInterop]
 [StructLayout(LayoutKind.Explicit, Size = 0x4008)]
 public unsafe partial struct AtkSimpleTweenHolder {
-    [FieldOffset(0x00), FixedSizeArray] internal FixedSizeArray2048<AtkSimpleTween> _list;
+    [FieldOffset(0x00), FixedSizeArray] internal FixedSizeArray2048<Pointer<AtkSimpleTween>> _list;
     [FieldOffset(0x4000)] public uint NextId;
 }
