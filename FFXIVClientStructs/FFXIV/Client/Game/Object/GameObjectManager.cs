8708377dc9e0d4d282200c40fbaf0a03ad666792
aers <aers00@gmail.com>
2024-06-02 16:05:35 -0700

    update object arrays

--- FFXIVClientStructs/FFXIV/Client/Game/Object/GameObjectManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Object/GameObjectManager.cs
@@ -25,27 +25,35 @@ public unsafe partial struct GameObjectManager {
         // 289-428: ?
         // 429-528: ?
         // 529-598: something MJI (island sanctuary) related
-        [FieldOffset(0x0000), FixedSizeArray] internal FixedSizeArray599<Pointer<GameObject>> _all;
-
-        // compact list of valid (non-null) objects, sorted by GetObjectID() result
-        [FieldOffset(0x12B8), FixedSizeArray] internal FixedSizeArray599<Pointer<GameObject>> _filtered;
-
-        // compact list of valid networked objects (non-null with EntityId != 0xE0000000), sorted by EntityId
-        [FieldOffset(0x2570), FixedSizeArray] internal FixedSizeArray599<Pointer<GameObject>> _networked;
-
-        [FieldOffset(0x3828)] public int FilteredCount;
-        [FieldOffset(0x382C)] public int NetworkedCount;
+        
+        /// <summary>
+        /// Pointers to GameObjects, sorted by ObjectIndex.
+        /// </summary>
+        [FieldOffset(0x0000), FixedSizeArray] internal FixedSizeArray599<Pointer<GameObject>> _indexSorted;
 
         /// <summary>
-        /// Binary search for an object by id, using Filtered list.
+        /// Pointers to active GameObjects, sorted by GameObject.GetGameObjectId().
+        /// </summary>
+        [FieldOffset(0x12B8), FixedSizeArray] internal FixedSizeArray599<Pointer<GameObject>> _gameObjectIdSorted;
+
+        /// <summary>
+        /// Pointers to active GameObjects with a valid Entityid (!= E0000000), sorted by EntityId.
+        /// </summary>
+        [FieldOffset(0x2570), FixedSizeArray] internal FixedSizeArray599<Pointer<GameObject>> _entityIdSorted;
+
+        [FieldOffset(0x3828)] public int GameObjectIdSortedCount;
+        [FieldOffset(0x382C)] public int EntityIdSortedCount;
+
+        /// <summary>
+        /// Binary search for an object by GameObjectId, using the GameObjectId sorted list.
         /// </summary>
         [MemberFunction("E8 ?? ?? ?? ?? 48 8B D0 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 48 8B D3 48 8D 4C 24 ?? E8 ?? ?? ?? ?? 48 8D 54 24")]
-        public partial GameObject* GetFilteredObjectById(GameObjectId objectId);
+        public partial GameObject* GetObjectByGameObjectId(GameObjectId objectId);
 
         /// <summary>
-        /// Binary search for an object by entity id, using Networked list.
+        /// Binary search for an object by EntityId, using the EntityId sorted list.
         /// </summary>
         [MemberFunction("48 89 5C 24 ?? 44 8B 89")]
-        public partial GameObject* GetNetworkedObjectById(uint entityId);
+        public partial GameObject* GetObjectByEntityId(uint entityId);
     }
 }
