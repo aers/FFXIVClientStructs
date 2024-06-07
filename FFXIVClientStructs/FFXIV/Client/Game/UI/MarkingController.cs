eb98e8b8263745a82012b6716b3e2434adb1cd0c
Infi <infiziert@protonmail.ch>
2024-06-07 16:01:12 +0200

    Add func to place waymark presets

--- FFXIVClientStructs/FFXIV/Client/Game/UI/MarkingController.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/MarkingController.cs
@@ -15,6 +15,9 @@ public unsafe partial struct MarkingController {
     [FieldOffset(0x100), FixedSizeArray] internal FixedSizeArray17<long> _markerTimes; //(1000 * QueryPerformanceCounter / QueryPerformanceFrequency)
 
     [FieldOffset(0x1E0), FixedSizeArray] internal FixedSizeArray8<FieldMarker> _fieldMarkers;
+
+    [MemberFunction("E8 ?? ?? ?? ?? 84 C0 0F 94 C0 EB 19")]
+    public partial void PlacePreset(nint preset);
 }
 
 [StructLayout(LayoutKind.Explicit, Size = 0x20)]
