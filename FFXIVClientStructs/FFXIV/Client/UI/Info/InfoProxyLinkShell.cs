8791803658971bb7fab3b02ce129410ce14ec257
pohky <otoriosu@gmail.com>
2024-06-11 21:37:11 +0200

    Update InfoProxyLinkShell.cs

--- FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyLinkShell.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyLinkShell.cs
@@ -13,14 +13,20 @@ public unsafe partial struct InfoProxyLinkshell {
     [FieldOffset(0x0A0)] public Utf8String UnkString1;
 
     [FieldOffset(0x108), FixedSizeArray] internal FixedSizeArray8<Entry> _linkShells;
+    [FieldOffset(0x1C8)] public uint ActiveLinkShellIndex;
+
+    [FieldOffset(0x1D0)] public Entry ActiveLinkShell;
 
     [MemberFunction("3B 51 10 73 0F 8B C2 48 83 C0 0B")]
-    public partial ulong* GetLinkshellInfo(uint index);
+    public partial Entry* GetLinkshellInfo(uint index);
 
     [MemberFunction("E8 ?? ?? ?? ?? 4C 8B C8 44 8D 47 01")]
-    public partial byte* GetLinkshellName(ulong* info);
+    public partial byte* GetLinkshellName(ulong id);
 
-    //0x20 bytes
     [StructLayout(LayoutKind.Explicit, Size = 0x18)]
-    public struct Entry;
+    public struct Entry {
+        [FieldOffset(0x00)] public ulong Id;
+        [FieldOffset(0x08)] public ulong ChatId;
+        [FieldOffset(0x10)] public uint Flags;
+    }
 }
