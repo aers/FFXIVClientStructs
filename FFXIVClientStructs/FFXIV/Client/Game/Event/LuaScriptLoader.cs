420ed8774e0a4c603eba1bdca8111726e20ce77e
WildWolf <magnus1997.mho@gmail.com>
2024-06-12 23:19:30 +0200

    update the unknown return type

--- FFXIVClientStructs/FFXIV/Client/Game/Event/LuaScriptLoader.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Event/LuaScriptLoader.cs
@@ -14,7 +14,7 @@ public unsafe partial struct LuaScriptLoader {
     [FieldOffset(0x10)] public ModuleBase* Parent;
     // [FieldOffset(0x18)] public StdMap<> UnkMap // unsure of the type of map but is being called in multiple places where this struct is used. one which is sub_1409173E0
 
-    // unsure of the return type and what the function does but its specific to this class from what I find in the vtable references
+    // unsure of what the function does but its specific to this class from what I find in the vtable references
     // [VirtualFunction(3)]
-    // public partial ? vf3(ResourceHandle* handle);
+    // public partial void vf3(ResourceHandle* handle);
 }
