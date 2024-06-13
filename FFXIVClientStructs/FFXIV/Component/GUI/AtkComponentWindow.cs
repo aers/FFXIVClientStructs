737cf7ba21d84443f79daff14f7c7ec9c86ec529
Haselnussbomber <mail@haselnussbomber.de>
2024-06-14 00:17:31 +0200

    Update AtkComponentWindow

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentWindow.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentWindow.cs
@@ -8,4 +8,23 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI;
 [GenerateInterop]
 [Inherits<AtkComponentBase>]
 [StructLayout(LayoutKind.Explicit, Size = 0x108)]
-public partial struct AtkComponentWindow;
+public unsafe partial struct AtkComponentWindow {
+    [FieldOffset(0xC0)] public AtkUnitBase* UnitBase;
+    /// <remarks>
+    /// [0] = Title<br/>
+    /// [1] = Subtitle<br/>
+    /// [2] = CloseButton<br/>
+    /// [3] = SettingsButton<br/>
+    /// [4] = HelpButton<br/>
+    /// [5] = Unknown<br/>
+    /// [6] = TitleBar<br/>
+    /// [7] = Unknown
+    /// </remarks>
+    [FieldOffset(0xC8), FixedSizeArray] internal FixedSizeArray8<int> _nodeIds;
+    [FieldOffset(0xE8)] public AtkCollisionNode* WindowCollisionNode;
+    [FieldOffset(0xF0)] public AtkCollisionNode* TitlebarCollisionNode;
+    [FieldOffset(0xF8)] public uint TitleTextId;
+    [FieldOffset(0xFC)] public uint SubtitleTextId;
+    [FieldOffset(0x100)] public float SubtitleOffsetX;
+    [FieldOffset(0x104)] public byte ShowFlags;
+}
