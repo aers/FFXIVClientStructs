8889cfe15b019ef0681614862ea28e64d8b8429d
MidoriKami <mi_kami@outlook.com>
2024-06-21 13:42:46 -0700

    did it in probably the worst way possible, but its cleaner I guess?

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkEventData.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkEventData.cs
@@ -2,13 +2,15 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI;
 
 [CExporterStructUnion]
 [StructLayout(LayoutKind.Explicit, Size = 0x28)]
-public struct AtkEventData {
-    [FieldOffset(0x00)] public AtkListItemData AtkListItemData;
+public partial struct AtkEventData {
+    [FieldOffset(0x00)] public AtkListItemData ListItemData;
 }
 
-[StructLayout(LayoutKind.Explicit, Size = 0x28)]
-public unsafe struct AtkListItemData {
-    [FieldOffset(0x00)] public AtkComponentListItemRenderer* ListItemRenderer;
-    [FieldOffset(0x10)] public int SelectedIndex;
-    // [FieldOffset(0x16)] public ushort HoveredItemIndex3;
+public partial struct AtkEventData {
+    [StructLayout(LayoutKind.Explicit, Size = 0x28)]
+    public unsafe struct AtkListItemData {
+        [FieldOffset(0x00)] public AtkComponentListItemRenderer* ListItemRenderer;
+        [FieldOffset(0x10)] public int SelectedIndex;
+        // [FieldOffset(0x16)] public ushort HoveredItemIndex3;
+    }
 }
