c9065f4ae6ef91688dcd599120583407d18c5aef
MidoriKami <mi_kami@outlook.com>
2024-06-21 13:23:00 -0700

    Hasel told me to

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkEventData.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkEventData.cs
@@ -1,12 +1,12 @@
 namespace FFXIVClientStructs.FFXIV.Component.GUI;
 
 [CExporterStructUnion]
-[StructLayout(LayoutKind.Explicit, Size = 0x18)]
+[StructLayout(LayoutKind.Explicit, Size = 0x28)]
 public struct AtkEventData {
     [FieldOffset(0x00)] public AtkListItemData AtkListItemData;
 }
 
-[StructLayout(LayoutKind.Explicit, Size = 0x18)]
+[StructLayout(LayoutKind.Explicit, Size = 0x28)]
 public unsafe struct AtkListItemData {
     [FieldOffset(0x00)] public AtkComponentListItemRenderer* ListItemRenderer;
     [FieldOffset(0x10)] public int SelectedIndex;
