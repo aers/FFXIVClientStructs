e543c97abf44f62cdd52fefdf452c9afcba231f3
MidoriKami <mi_kami@outlook.com>
2024-06-21 12:45:36 -0700

    Add ListItemToggleData

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkEventData.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkEventData.cs
@@ -1,4 +1,12 @@
 namespace FFXIVClientStructs.FFXIV.Component.GUI;
 
+[CExporterStructUnion]
 [StructLayout(LayoutKind.Explicit, Size = 0x18)]
-public struct AtkEventData;
+public struct AtkEventData {
+    [FieldOffset(0x00)] public ListItemToggleData ListItemToggleData;
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0x18)]
+public struct ListItemToggleData {
+    [FieldOffset(0x10)] public int SelectedIndex;
+}
