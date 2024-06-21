38e007cfaf2f025316f6b0ca0869912e42e07112
Haselnussbomber <mail@haselnussbomber.de>
2024-06-21 20:41:57 +0200

    Add GetBannerConditionUnlockState

--- FFXIVClientStructs/FFXIV/Component/Exd/ExdModule.cs
+++ FFXIVClientStructs/FFXIV/Component/Exd/ExdModule.cs
@@ -41,4 +41,7 @@ public unsafe partial struct ExdModule {
 
     [MemberFunction("E8 ?? ?? ?? ?? 8B 75 00")]
     public static partial Item* GetItemRowById(uint itemId);
+
+    [MemberFunction("40 53 48 83 EC 20 0F B6 41 29")]
+    public static partial byte GetBannerConditionUnlockState(BannerCondition* row);
 }
