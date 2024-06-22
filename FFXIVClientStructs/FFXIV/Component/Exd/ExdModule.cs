8b6fe970f5317958db425915e2bd1a394ae2a5ae
Haselnussbomber <mail@haselnussbomber.de>
2024-06-22 02:22:20 +0200

    Update ExdModule

--- FFXIVClientStructs/FFXIV/Component/Exd/ExdModule.cs
+++ FFXIVClientStructs/FFXIV/Component/Exd/ExdModule.cs
@@ -7,11 +7,35 @@ namespace FFXIVClientStructs.FFXIV.Component.Exd;
 public unsafe partial struct ExdModule {
     [FieldOffset(0x20)] public ExcelModule* ExcelModule;
 
-    [MemberFunction("E8 ?? ?? ?? ?? 0F 57 F6 48 85 C0")]
-    public partial void* GetRowBySheetIndexAndRowIndex(uint sheetId, uint rowId);
+    [MemberFunction("40 53 48 83 EC 20 45 8B D0 4C 8B CA")]
+    public partial void* GetRowBySheetAndRowId(ExcelSheet* sheet, uint rowId);
 
     [MemberFunction("48 89 5C 24 ?? 57 48 83 EC 40 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 44 24 ?? 41 8B F8")]
-    public partial void* GetRowBySheetAndRowIndex(ExcelSheet* sheet, uint rowId);
+    public partial void* GetRowBySheetAndRowIndex(ExcelSheet* sheet, uint rowIndex);
+
+    [MemberFunction("E8 ?? ?? ?? ?? EB 11 33 C0")]
+    public partial void* GetRowBySheetIndexAndRowId(uint sheetIndex, uint rowId);
+
+    [MemberFunction("E8 ?? ?? ?? ?? 0F 57 F6 48 85 C0")]
+    public partial void* GetRowBySheetIndexAndRowIndex(uint sheetIndex, uint rowIndex);
+
+    [MemberFunction("48 85 D2 74 0D 48 8B 02 48 8B CA 48 FF A0 88 00 00 00")]
+    public partial int GetRowCountBySheet(ExcelSheet* sheet);
+
+    [MemberFunction("E8 ?? ?? ?? ?? 3B D8 73 35")]
+    public partial int GetRowCountBySheetIndex(uint sheetIndex);
+
+    [MemberFunction("48 89 5C 24 ?? 57 48 83 EC 20 48 8B D9 48 8B FA 48 8B 49 20 48 8B 01"), GenerateStringOverloads]
+    public partial int GetRowCountBySheetName(byte* sheetName);
+
+    [MemberFunction("E8 ?? ?? ?? ?? FE 47 30")]
+    public partial ExcelSheet* GetSheetByIndex(uint sheetIndex);
+
+    [MemberFunction("E8 ?? ?? ?? ?? 49 89 46 70"), GenerateStringOverloads]
+    public partial ExcelSheet* GetSheetByName(byte* sheetName);
+
+    [MemberFunction("48 83 EC 38 48 8B 05 ?? ?? ?? ?? 48 8B 88")]
+    public partial bool IsColumnRsv(uint sheetIndex, uint rowId, uint subRowId, uint columnIndex);
 
     [MemberFunction("E8 ?? ?? ?? ?? 49 63 DD")]
     public static partial void* GetItemRowById(uint itemId);
