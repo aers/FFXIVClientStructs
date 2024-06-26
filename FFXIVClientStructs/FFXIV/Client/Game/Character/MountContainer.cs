dec775ac6dbc098f975ae5426f61446242d980c3
Haselnussbomber <mail@haselnussbomber.de>
2024-06-26 15:44:11 +0200

    More FixedSizeArray conversions
    Fix build errors

--- FFXIVClientStructs/FFXIV/Client/Game/Character/MountContainer.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Character/MountContainer.cs
@@ -11,7 +11,7 @@ public unsafe partial struct MountContainer {
     [FieldOffset(0x1C)] public float DismountTimer;
     //1 in dismount animation, 4 = instant delete mount when dismounting (used for npcs and such)
     [FieldOffset(0x20)] public byte Flags;
-    [FieldOffset(0x24)] public fixed uint MountedObjectIds[7];
+    [FieldOffset(0x24), FixedSizeArray] internal FixedSizeArray7<uint> _mountedEntityIds;
 
     [MemberFunction("E8 ?? ?? ?? ?? 0F B7 56 66")]
     public partial void CreateAndSetupMount(short mountId, uint buddyModelTop, uint buddyModelBody, uint buddyModelLegs, byte buddyStain, byte unk6, byte unk7);
