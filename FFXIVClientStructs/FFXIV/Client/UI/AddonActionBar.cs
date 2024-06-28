9fccca09d51250de879012158c27455af6d475e2
ItsBexy <rebecca@honey-crab.com>
2024-06-27 19:47:57 -0600

    Updated offsets for ActionBar addons Selfishly prioritizing these because I work with them. Seemingly no changes to anything, just the standard 0x10 bump all addons need.

--- FFXIVClientStructs/FFXIV/Client/UI/AddonActionBar.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonActionBar.cs
@@ -10,13 +10,13 @@ namespace FFXIVClientStructs.FFXIV.Client.UI;
 [Addon("_ActionBar")]
 [GenerateInterop]
 [Inherits<AddonActionBarX>]
-[StructLayout(LayoutKind.Explicit, Size = 0x2B8)]
+[StructLayout(LayoutKind.Explicit, Size = 0x2C8)]
 public unsafe partial struct AddonActionBar {
-    [FieldOffset(0x298)] public AtkComponentBase* CycleUpArrow;
-    [FieldOffset(0x2A0)] public AtkComponentBase* CycleDownArrow;
-    [FieldOffset(0x2A8)] public AtkComponentCheckBox* PadlockCheckbox;
+    [FieldOffset(0x2A8)] public AtkComponentBase* CycleUpArrow;
+    [FieldOffset(0x2B0)] public AtkComponentBase* CycleDownArrow;
+    [FieldOffset(0x2B8)] public AtkComponentCheckBox* PadlockCheckbox;
 
-    [FieldOffset(0x2B1)] public byte HotbarIdPet; // usually both the same value, only difference is that this one changes to 18 when cycled to the pet bar
-    [FieldOffset(0x2B3)] public byte HotbarId;
-    [FieldOffset(0x2B5)] public bool IncludePetBarWhenCycling;
+    [FieldOffset(0x2C1)] public byte HotbarIdPet; // usually both the same value, only difference is that this one changes to 18 when cycled to the pet bar
+    [FieldOffset(0x2C3)] public byte HotbarId;
+    [FieldOffset(0x2C5)] public bool IncludePetBarWhenCycling;
 }
--- FFXIVClientStructs/FFXIV/Client/UI/AddonActionBarX.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonActionBarX.cs
@@ -9,20 +9,20 @@ namespace FFXIVClientStructs.FFXIV.Client.UI;
 [Addon("_ActionBar01", "_ActionBar02", "_ActionBar03", "_ActionBar04", "_ActionBar05", "_ActionBar06", "_ActionBar07", "_ActionBar08", "_ActionBar09")]
 [GenerateInterop(isInherited: true)]
 [Inherits<AddonActionBarBase>]
-[StructLayout(LayoutKind.Explicit, Size = 0x298)]
+[StructLayout(LayoutKind.Explicit, Size = 0x2A8)]
 public unsafe partial struct AddonActionBarX {
-    [FieldOffset(0x248)] public AtkTextNode* HotbarNumIconTextNode;
-    [FieldOffset(0x250)] public AtkCollisionNode* HotbarNumIconCollisionNode;
-    [FieldOffset(0x258)] public AtkResNode* ContainerNode;
-    [FieldOffset(0x260)] public AtkResNode* HotbarNumIconNode;
-    [FieldOffset(0x268)] public AtkResNode* PadlockNode;
+    [FieldOffset(0x258)] public AtkTextNode* HotbarNumIconTextNode;
+    [FieldOffset(0x260)] public AtkCollisionNode* HotbarNumIconCollisionNode;
+    [FieldOffset(0x268)] public AtkResNode* ContainerNode;
+    [FieldOffset(0x270)] public AtkResNode* HotbarNumIconNode;
+    [FieldOffset(0x278)] public AtkResNode* PadlockNode;
 
     /// <summary>
     /// The current layout (columns x rows) of this specific hotbar.
     /// </summary>
-    [FieldOffset(0x270)] public ActionBarLayout ActionBarLayout;
+    [FieldOffset(0x280)] public ActionBarLayout ActionBarLayout;
 
-    [FieldOffset(0x27C), FixedSizeArray] internal FixedSizeArray6<Dimensions> _layoutDimensions; // every hotbar stores the same pre-baked dimensions for each of the 6 layout options
+    [FieldOffset(0x28C), FixedSizeArray] internal FixedSizeArray6<Dimensions> _layoutDimensions; // every hotbar stores the same pre-baked dimensions for each of the 6 layout options
 
     [StructLayout(LayoutKind.Explicit, Size = 4)]
     public struct Dimensions {
