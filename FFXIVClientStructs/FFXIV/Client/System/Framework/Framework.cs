52cc62ff2192f8e436067755b45669c0d1036f0a
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2024-06-28 01:41:51 +0100

    Framework offset fixes.

--- FFXIVClientStructs/FFXIV/Client/System/Framework/Framework.cs
+++ FFXIVClientStructs/FFXIV/Client/System/Framework/Framework.cs
@@ -19,7 +19,7 @@ namespace FFXIVClientStructs.FFXIV.Client.System.Framework;
 // Client::System::Framework::Framework
 // ctor "E8 ?? ?? ?? ?? 48 8B C8 48 89 05 ?? ?? ?? ?? EB 0A 48 8B CE"
 [GenerateInterop]
-[StructLayout(LayoutKind.Explicit, Size = 0x35C8)]
+[StructLayout(LayoutKind.Explicit, Size = 0x35D0)]
 public unsafe partial struct Framework {
     [StaticAddress("49 8B DC 48 89 1D ?? ?? ?? ??", 6, true)]
     public static partial Framework* Instance();
@@ -34,86 +34,86 @@ public unsafe partial struct Framework {
     [FieldOffset(0x0598)] public FileAccessPath ConfigPath;
     [FieldOffset(0x07A8)] public GameWindow* GameWindow;
     //584 byte
-    [FieldOffset(0x09F8)] public int CursorPosX;
-    [FieldOffset(0x09FC)] public int CursorPosY;
+    [FieldOffset(0x09FC)] public int CursorPosX;
+    [FieldOffset(0x0A00)] public int CursorPosY;
 
-    [FieldOffset(0x1104)] public int CursorPosX2;
-    [FieldOffset(0x1108)] public int CursorPosY2;
+    [FieldOffset(0x110C)] public int CursorPosX2;
+    [FieldOffset(0x1110)] public int CursorPosY2;
 
-    [FieldOffset(0x1670)] public NetworkModuleProxy* NetworkModuleProxy;
-    [FieldOffset(0x1678)] public bool IsNetworkModuleInitialized;
-    [FieldOffset(0x1679)] public bool EnableNetworking;
-    [FieldOffset(0x1680)] public TimePoint UtcTime;
-    [FieldOffset(0x1698)] public uint TimerResolutionMillis;
-    [FieldOffset(0x16A0)] public long PerformanceCounterFrequency;
-    [FieldOffset(0x16A8)] public long PerformanceCounterValue;
+    [FieldOffset(0x1678)] public NetworkModuleProxy* NetworkModuleProxy;
+    [FieldOffset(0x1680)] public bool IsNetworkModuleInitialized;
+    [FieldOffset(0x1681)] public bool EnableNetworking;
+    [FieldOffset(0x1688)] public TimePoint UtcTime;
+    [FieldOffset(0x16A0)] public uint TimerResolutionMillis;
+    [FieldOffset(0x16A8)] public long PerformanceCounterFrequency;
+    [FieldOffset(0x16B0)] public long PerformanceCounterValue;
     /// <summary>
     /// Frame time (in seconds) to use for calculating animations, tasks, game logic and such. This is not necessarily the real time since the last frame.
     /// </summary>
-    [FieldOffset(0x16B8)] public float FrameDeltaTime;
+    [FieldOffset(0x16C0)] public float FrameDeltaTime;
     /// <summary>
     /// Holds the unaltered real time since last frame in seconds.
     /// </summary>
-    [FieldOffset(0x16BC)] public float RealFrameDeltaTime;
+    [FieldOffset(0x16C4)] public float RealFrameDeltaTime;
     /// <summary>
     /// If set to anything non-zero, overrides <see cref="FrameDeltaTime"/>. Has lower precedence than <see cref="NextFrameDeltaTimeOverride"/>.
     /// </summary>
-    [FieldOffset(0x16C0)] public float FrameDeltaTimeOverride;
+    [FieldOffset(0x16C8)] public float FrameDeltaTimeOverride;
     /// <summary>
     /// If non-zero <see cref="FrameDeltaTime"/> is multiplied with this.
     /// </summary>
-    [FieldOffset(0x16C4)] public float FrameDeltaFactor;
-    [FieldOffset(0x16C8)] public uint FrameCounter;
-    [FieldOffset(0x16D0)] public long FrameDeltaTimeMSInt; // FrameDeltaTime in milliseconds, rounded to integer
-    [FieldOffset(0x16D8)] public float FrameDeltaTimeMSRem; // sub-millisecond difference between real and rounded dt, added to the next frame
-    [FieldOffset(0x16E0)] public long FrameDeltaTimeUSInt; // FrameDeltaTime in microseconds, rounded to integer
-    [FieldOffset(0x16E8)] public float FrameDeltaTimeUSRem; // sub-microsecond difference between real and rounded dt, added to the next frame
-    [FieldOffset(0x16F8)] public TaskManager TaskManager;
-    [FieldOffset(0x1768)] public ClientTime ClientTime;
-    [FieldOffset(0x17B0)] public float GameSpeedMultiplier; // usually 1, but during recording replay could be different
-    [FieldOffset(0x17C4)] public float FrameRate;
+    [FieldOffset(0x16CC)] public float FrameDeltaFactor;
+    [FieldOffset(0x16D0)] public uint FrameCounter;
+    [FieldOffset(0x16D8)] public long FrameDeltaTimeMSInt; // FrameDeltaTime in milliseconds, rounded to integer
+    [FieldOffset(0x16E0)] public float FrameDeltaTimeMSRem; // sub-millisecond difference between real and rounded dt, added to the next frame
+    [FieldOffset(0x16E8)] public long FrameDeltaTimeUSInt; // FrameDeltaTime in microseconds, rounded to integer
+    [FieldOffset(0x16F0)] public float FrameDeltaTimeUSRem; // sub-microsecond difference between real and rounded dt, added to the next frame
+    [FieldOffset(0x1700)] public TaskManager TaskManager;
+    [FieldOffset(0x1770)] public ClientTime ClientTime;
+    [FieldOffset(0x17B8)] public float GameSpeedMultiplier; // usually 1, but during recording replay could be different
+    [FieldOffset(0x17CC)] public float FrameRate;
     /// <summary>
     /// If true <see cref="FrameDeltaTime"/> is set to 0.
     /// </summary>
-    [FieldOffset(0x17C8)] public bool DiscardFrame;
+    [FieldOffset(0x17D0)] public bool DiscardFrame;
     /// <summary>
     /// If set to anything non-zero, overrides <see cref="FrameDeltaTime"/>. If negative <see cref="FrameDeltaTimeOverride"/> is used and 60fps as a fallback.
     /// Unlike <see cref="FrameDeltaTimeOverride"/>, this applies only to the next frame, and is reset to zero on next tick.
     /// </summary>
-    [FieldOffset(0x17CC)] public float NextFrameDeltaTimeOverride;
-    [FieldOffset(0x17D0)] public bool WindowInactive;
+    [FieldOffset(0x17D4)] public float NextFrameDeltaTimeOverride;
+    [FieldOffset(0x17D8)] public bool WindowInactive;
 
-    [FieldOffset(0x17E0)] public int DataPathType;
+    [FieldOffset(0x17E8)] public int DataPathType;
 
-    [FieldOffset(0x19EC), FixedSizeArray(isString: true)] internal FixedSizeArray260<char> _gamePath;
-    [FieldOffset(0x1DFC), FixedSizeArray(isString: true)] internal FixedSizeArray260<char> _sqPackPath;
-    [FieldOffset(0x220C), FixedSizeArray(isString: true)] internal FixedSizeArray260<char> _userPath;
+    [FieldOffset(0x19F4), FixedSizeArray(isString: true)] internal FixedSizeArray260<char> _gamePath;
+    [FieldOffset(0x1E04), FixedSizeArray(isString: true)] internal FixedSizeArray260<char> _sqPackPath;
+    [FieldOffset(0x2214), FixedSizeArray(isString: true)] internal FixedSizeArray260<char> _userPath;
 
-    [FieldOffset(0x2B30)] public ExcelModuleInterface* ExcelModuleInterface;
-    [FieldOffset(0x2B38)] public ExdModule* ExdModule;
-    [FieldOffset(0x2B50)] public BGCollisionModule* BGCollisionModule;
-    [FieldOffset(0x2B60)] public UIModule* UIModule;
-    [FieldOffset(0x2B68)] public UIClipboard* UIClipboard;
-    [FieldOffset(0x2B78)] public EnvironmentManager* EnvironmentManager;
-    [FieldOffset(0x2B80)] public SoundManager* SoundManager;
-    [FieldOffset(0x2BC8)] public LuaState LuaState;
+    [FieldOffset(0x2B38)] public ExcelModuleInterface* ExcelModuleInterface;
+    [FieldOffset(0x2B40)] public ExdModule* ExdModule;
+    [FieldOffset(0x2B58)] public BGCollisionModule* BGCollisionModule;
+    [FieldOffset(0x2B68)] public UIModule* UIModule;
+    [FieldOffset(0x2B70)] public UIClipboard* UIClipboard;
+    [FieldOffset(0x2B80)] public EnvironmentManager* EnvironmentManager;
+    [FieldOffset(0x2B88)] public SoundManager* SoundManager;
+    [FieldOffset(0x2BD0)] public LuaState LuaState;
 
-    [FieldOffset(0x2BF0), FixedSizeArray(isString: true)] internal FixedSizeArray256<byte> _gameVersion;
+    [FieldOffset(0x2BF8), FixedSizeArray(isString: true)] internal FixedSizeArray256<byte> _gameVersion;
     // TODO: convert to array of 64 strings each 32 bytes long if possible
-    [FieldOffset(0x2CF0 + 0 * 0x20), FixedSizeArray(isString: true)] internal FixedSizeArray32<byte> _ex1Version; // Heavensward
-    [FieldOffset(0x2CF0 + 1 * 0x20), FixedSizeArray(isString: true)] internal FixedSizeArray32<byte> _ex2Version; // Stormblood
-    [FieldOffset(0x2CF0 + 2 * 0x20), FixedSizeArray(isString: true)] internal FixedSizeArray32<byte> _ex3Version; // Shadowbringers
-    [FieldOffset(0x2CF0 + 3 * 0x20), FixedSizeArray(isString: true)] internal FixedSizeArray32<byte> _ex4Version; // Endwalker
-    [FieldOffset(0x2CF0 + 4 * 0x20), FixedSizeArray(isString: true)] internal FixedSizeArray32<byte> _ex5Version; // Dawntrail
+    [FieldOffset(0x2CF8 + 0 * 0x20), FixedSizeArray(isString: true)] internal FixedSizeArray32<byte> _ex1Version; // Heavensward
+    [FieldOffset(0x2CF8 + 1 * 0x20), FixedSizeArray(isString: true)] internal FixedSizeArray32<byte> _ex2Version; // Stormblood
+    [FieldOffset(0x2CF8 + 2 * 0x20), FixedSizeArray(isString: true)] internal FixedSizeArray32<byte> _ex3Version; // Shadowbringers
+    [FieldOffset(0x2CF8 + 3 * 0x20), FixedSizeArray(isString: true)] internal FixedSizeArray32<byte> _ex4Version; // Endwalker
+    [FieldOffset(0x2CF8 + 4 * 0x20), FixedSizeArray(isString: true)] internal FixedSizeArray32<byte> _ex5Version; // Dawntrail
 
-    [FieldOffset(0x3500)] public bool UseWatchDogThread;
+    [FieldOffset(0x3508)] public bool UseWatchDogThread;
 
-    [FieldOffset(0x3510)] public int FramesUntilDebugCheck;
+    [FieldOffset(0x3518)] public int FramesUntilDebugCheck;
     /// <summary>
     /// Set if <c>IsSteam</c> was set for this instance as part of <c>SetupSteamApi</c>. Set even if loading the Steam API
     /// fails for some reason.
     /// </summary>
-    [FieldOffset(0x35B4)] public bool IsSteamGame;
+    [FieldOffset(0x35BC)] public bool IsSteamGame;
 
     /// <summary>
     /// Access the Steam API wrapper/interface.
@@ -121,12 +121,12 @@ public unsafe partial struct Framework {
     /// <remarks>
     /// The struct backed by this API should not be considered stable. If you use this, you are signing up for API breakage.
     /// </remarks>
-    [FieldOffset(0x35B8)] public SteamApi* SteamApi;
+    [FieldOffset(0x35C0)] public SteamApi* SteamApi;
 
     /// <summary>
     /// Handle (type HMODULE) of steam_api64.dll
     /// </summary>
-    [FieldOffset(0x35C0)] public nint SteamApiLibraryHandle;
+    [FieldOffset(0x35C8)] public nint SteamApiLibraryHandle;
 
     [MemberFunction("E8 ?? ?? ?? ?? 80 7B 1D 01")]
     public partial UIModule* GetUIModule();
