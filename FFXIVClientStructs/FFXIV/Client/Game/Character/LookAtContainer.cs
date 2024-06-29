89db13d66ff544cfeb8dbe7ee43a271f5ceee696
pohky <otoriosu@gmail.com>
2024-06-29 06:44:59 +0200

    update lookat

--- FFXIVClientStructs/FFXIV/Client/Game/Character/LookAtContainer.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Character/LookAtContainer.cs
@@ -14,13 +14,13 @@ public unsafe partial struct LookAtContainer {
     /// When using /facecamera, this is the rotation of the camera.<br/>
     /// When editing banner and the character is following the camera (either with head or eyes), this field holds the position of the camera.
     /// </summary>
-    [FieldOffset(0x5F0)] public Vector3 CameraVector; // maybe Vector4 with unused W field?
+    [FieldOffset(0xBA0)] public Vector3 CameraVector; // maybe Vector4 with unused W field?
 
-    [FieldOffset(0x600)] public byte FaceCameraFlag; // looks like a bitfield but only with one bit used
+    [FieldOffset(0xBB0)] public byte FaceCameraFlag; // looks like a bitfield but only with one bit used
 
-    [FieldOffset(0x604)] public Vector2 BannerHeadDirection;
-    [FieldOffset(0x60C)] public Vector2 BannerEyeDirection;
-    [FieldOffset(0x614)] public BannerCameraFollowFlags BannerCameraFollowFlag;
+    [FieldOffset(0xBB4)] public Vector2 BannerHeadDirection;
+    [FieldOffset(0xBBC)] public Vector2 BannerEyeDirection;
+    [FieldOffset(0xBC4)] public BannerCameraFollowFlags BannerCameraFollowFlag;
 
     public bool IsFacingCamera {
         get => (FaceCameraFlag & 0x1) == 0x1;
--- FFXIVClientStructs/FFXIV/Client/Game/Control/CharacterLookAtController.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Control/CharacterLookAtController.cs
@@ -6,7 +6,7 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Control;
 // ctor "48 8D 05 ?? ?? ?? ?? 48 8D 71 20"
 [GenerateInterop]
 [VirtualTable("48 8D 05 ?? ?? ?? ?? 48 8D 71 20", 3)]
-[StructLayout(LayoutKind.Explicit, Size = 0x5E0)]
+[StructLayout(LayoutKind.Explicit, Size = 0xB90)]
 public unsafe partial struct CharacterLookAtController {
     [FieldOffset(0x10)] public BattleChara* OwnerObject;
 
@@ -15,6 +15,6 @@ public unsafe partial struct CharacterLookAtController {
     /// 1 = Head<br/>
     /// 2 = Eyes
     /// </remarks>
-    [FieldOffset(0x20), FixedSizeArray] internal FixedSizeArray3<CharacterLookAtControlParam> _params;
-    [FieldOffset(0x5C4)] public int ParamCount;
+    [FieldOffset(0x20), FixedSizeArray] internal FixedSizeArray6<CharacterLookAtControlParam> _params;
+    [FieldOffset(0xB64)] public int ParamCount;
 }
