eda439c088376612c836525f9316a5aceab78b6a
Infi <infiziert@protonmail.ch>
2024-06-30 04:44:36 +0200

    Update InfoProxyFriendlist & InfoProxyCommonList

--- FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyFriendList.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyFriendList.cs
@@ -9,14 +9,14 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Info;
 [InfoProxy(InfoProxyId.FriendList)]
 [GenerateInterop]
 [Inherits<InfoProxyCommonList>]
-[StructLayout(LayoutKind.Explicit, Size = 0x3AD0)]
+[StructLayout(LayoutKind.Explicit, Size = 0x3AE8)]
 public unsafe partial struct InfoProxyFriendList {
-    [FieldOffset(0x0D8)] public Utf8String Str2;
-    [FieldOffset(0x140)] public Utf8String Str3;
+    [FieldOffset(0xF0)] public Utf8String Str2;
+    [FieldOffset(0x158)] public Utf8String Str3;
     //NExt 2: Set when seleccting a player (Name + 02 12 02 59 03 + WorldName)
-    [FieldOffset(0x1A8)] public Utf8String Str4;
-    [FieldOffset(0x210)] public Utf8String Str5;
-    [FieldOffset(0x278), FixedSizeArray] internal FixedSizeArray200<NameBuffer> _names;
+    [FieldOffset(0x1C0)] public Utf8String Str4;
+    [FieldOffset(0x228)] public Utf8String Str5;
+    [FieldOffset(0x290), FixedSizeArray] internal FixedSizeArray200<NameBuffer> _names;
 
     [GenerateInterop]
     [StructLayout(LayoutKind.Explicit, Size = 0x40)]
