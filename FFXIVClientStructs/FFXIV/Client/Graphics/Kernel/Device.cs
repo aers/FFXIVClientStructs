31afa02c8f423ad57a28fd6b9109e8da348ce528
pohky <otoriosu@gmail.com>
2024-06-30 16:41:56 +0200

    Update Device.cs

--- FFXIVClientStructs/FFXIV/Client/Graphics/Kernel/Device.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Kernel/Device.cs
@@ -5,7 +5,7 @@ namespace FFXIVClientStructs.FFXIV.Client.Graphics.Kernel;
 
 // Client::Graphics::Kernel::DeviceDX11
 [GenerateInterop]
-[StructLayout(LayoutKind.Explicit, Size = 0x258)]
+[StructLayout(LayoutKind.Explicit, Size = 0x8C8)]
 public unsafe partial struct Device {
     [StaticAddress("48 8B 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 80 7B 08 00", 3, isPointer: true)]
     public static partial Device* Instance();
@@ -14,23 +14,28 @@ public unsafe partial struct Device {
     [FieldOffset(0x10)] public void* RenderThread; // Client::Graphics::Kernel::RenderThread
     [FieldOffset(0x70)] public SwapChain* SwapChain;
     [FieldOffset(0x7A)] public byte RequestResolutionChange;
-    [FieldOffset(0x88)] public uint Width;
-    [FieldOffset(0x8C)] public uint Height;
-    [FieldOffset(0x90)] public float AspectRatio;
-    [FieldOffset(0x94)] public float GammaCorrection;
-    [FieldOffset(0x98)] public int ColorFilter;
-    [FieldOffset(0x9C)] public float ColorFilterRange;
-    [FieldOffset(0xA4)] public int FrameRateDivider;
-    [FieldOffset(0x1B0)] public void* hWnd;
-    [FieldOffset(0x1C0)] public uint NewWidth;
-    [FieldOffset(0x1C4)] public uint NewHeight;
-    [FieldOffset(0x1C8)] public int FrameRate;
-    [FieldOffset(0x220)] public int D3DFeatureLevel; // D3D_FEATURE_LEVEL enum
-    [FieldOffset(0x228)] public void* DXGIFactory; // IDXGIFactory1
-    [FieldOffset(0x230)] public void* DXGIOutput; // IDXGIOutput6
-    [FieldOffset(0x238)] public void* D3D11Forwarder; // CID3D11Forwarder (ID3D11Device vtbl present here)
-    [FieldOffset(0x240)] public void* D3D11DeviceContext; // ID3D11DeviceContext5
-    [FieldOffset(0x250)] public void* ImmediateContext; // Client::Graphics::Kernel::Device::ImmediateContext
+    [FieldOffset(0x8C)] public uint Width;
+    [FieldOffset(0x90)] public uint Height;
+    [FieldOffset(0x94)] public float AspectRatio;
+    [FieldOffset(0x98)] public float GammaCorrection;
+    [FieldOffset(0x9C)] public int ColorFilter;
+    [FieldOffset(0xA0)] public float ColorFilterRange;
+
+    [FieldOffset(0xAC)] public short FrameRateLimit;
+    // [FieldOffset(0xAE)] public short FrameRateLimit2; ?
+
+    [FieldOffset(0x820)] public void* hWnd;
+    [FieldOffset(0x830)] public uint NewWidth;
+    [FieldOffset(0x834)] public uint NewHeight;
+    [FieldOffset(0x838)] public int FrameRate;
+
+    [FieldOffset(0x890)] public int D3DFeatureLevel; // D3D_FEATURE_LEVEL enum
+    [FieldOffset(0x898)] public void* DXGIFactory; // IDXGIFactory1
+    [FieldOffset(0x8A0)] public void* DXGIOutput; // IDXGIOutput6
+    [FieldOffset(0x8A8)] public void* D3D11Forwarder; // CID3D11Forwarder (ID3D11Device vtbl present here)
+    [FieldOffset(0x8B0)] public void* D3D11DeviceContext; // ID3D11DeviceContext5
+
+    [FieldOffset(0x8C0)] public void* ImmediateContext; // Client::Graphics::Kernel::Device::ImmediateContext
 
     [MemberFunction("E8 ?? ?? ?? ?? 49 89 45 48")]
     public partial ConstantBuffer* CreateConstantBuffer(int byteSize, uint flags, uint unk);
