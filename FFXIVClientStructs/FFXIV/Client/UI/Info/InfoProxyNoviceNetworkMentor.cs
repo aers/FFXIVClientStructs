6b9df9b57b9916b8de032875384cfff55a8e8681
pohky <otoriosu@gmail.com>
2024-06-30 09:19:17 +0200

    update infoproxynovicenetwork

--- FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyNoviceNetworkMentor.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyNoviceNetworkMentor.cs
@@ -3,5 +3,5 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Info;
 [InfoProxy(InfoProxyId.NoviceNetworkMentor)]
 [GenerateInterop]
 [Inherits<InfoProxyNoviceNetworkList>]
-[StructLayout(LayoutKind.Explicit, Size = 0xC8)]
+[StructLayout(LayoutKind.Explicit, Size = 0xE0)]
 public unsafe partial struct InfoProxyNoviceNetworkMentor;
