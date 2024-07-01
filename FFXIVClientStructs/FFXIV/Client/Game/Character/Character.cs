19f089af17ca088527df4a4a5877e12dd535e242
Haselnussbomber <mail@haselnussbomber.de>
2024-07-01 23:19:18 +0200

    Move ForayInfo under Character struct

--- FFXIVClientStructs/FFXIV/Client/Game/Character/Character.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Character/Character.cs
@@ -154,3 +154,9 @@ public unsafe partial struct Character {
         Performance = 16, // Unknown
     }
 }
+
+[StructLayout(LayoutKind.Explicit, Size = 2)]
+public struct ForayInfo {
+    [FieldOffset(0x00)] public byte Level;
+    [FieldOffset(0x01)] public byte Element;
+}
