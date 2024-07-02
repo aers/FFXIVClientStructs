686ef772bb286d79aec46ab439cb52bd39fc64f6
pohky <otoriosu@gmail.com>
2024-07-03 01:20:10 +0200

    update EnvManager

--- FFXIVClientStructs/FFXIV/Client/Graphics/Environment/EnvManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Environment/EnvManager.cs
@@ -5,7 +5,7 @@ namespace FFXIVClientStructs.FFXIV.Client.Graphics.Environment;
 
 // Client::Graphics::Environment::EnvManager
 [GenerateInterop]
-[StructLayout(LayoutKind.Explicit, Size = 0x910)]
+[StructLayout(LayoutKind.Explicit, Size = 0xBF0)]
 public unsafe partial struct EnvManager {
     [StaticAddress("0F 28 F2 48 8B 05", 6, isPointer: true)]
     public static partial EnvManager* Instance();
@@ -19,12 +19,14 @@ public unsafe partial struct EnvManager {
     [FieldOffset(0x27)] public byte ActiveWeather;
     [FieldOffset(0x28)] public float TransitionTime;
 
-    [FieldOffset(0x30)] public EnvSpace* EnvSpace;
+    [FieldOffset(0x38)] public EnvSpace* EnvSpace;
 
-    [FieldOffset(0x50)] public EnvState EnvState;
-    [FieldOffset(0x388)] public EnvSoundState EnvSoundState;
-    [FieldOffset(0x400)] public EnvSimulator EnvSimulator;
-    [FieldOffset(0x750)] public ShadowCamera ShadowCamera;
+    [FieldOffset(0x58)] public EnvState EnvState;
+    
+    [FieldOffset(0x468)] public EnvSoundState EnvSoundState;
+    [FieldOffset(0x4E0)] public EnvSimulator EnvSimulator;
 
-    [FieldOffset(0x8F4)] public uint UnkFlags;
+    [FieldOffset(0x8E0)] public ShadowCamera ShadowCamera;
+
+    // [FieldOffset(0x8F4)] public uint UnkFlags;
 }
--- FFXIVClientStructs/FFXIV/Client/Graphics/Environment/EnvState.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Environment/EnvState.cs
@@ -1,7 +1,7 @@
 namespace FFXIVClientStructs.FFXIV.Client.Graphics.Environment;
 
 // Client::Graphics::Environment::EnvState
-[StructLayout(LayoutKind.Explicit, Size = 0x258)]
+[StructLayout(LayoutKind.Explicit, Size = 0x2F8)]
 public unsafe struct EnvState {
     [FieldOffset(0x12C)] public float Rain;
 }
