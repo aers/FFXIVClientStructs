d6e7aec3de67f2df3e4ea896f224060f53a869e5
Infi <infiziert@protonmail.ch>
2024-07-05 04:17:19 +0200

    fix doc

--- FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyBlacklist.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyBlacklist.cs
@@ -12,7 +12,7 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Info;
 public unsafe partial struct InfoProxyBlacklist {
     [FieldOffset(0x20)] public Utf8String LastBlockedNameContacts; // Gets set with contacts
     [FieldOffset(0x88)] public Utf8String LastBlockedNameChat; // Gets set with chat and contacts
-    [FieldOffset(0xF0), FixedSizeArray] internal FixedSizeArray200<BlockedCharacter> _blockedCharacters; // does not clean up removed characters, read only until TotalBlocks
+    [FieldOffset(0xF0), FixedSizeArray] internal FixedSizeArray200<BlockedCharacter> _blockedCharacters; // does not clean up removed characters, read only until BlockedCharactersCount
     [FieldOffset(0x13B0)] public int BlockedCharactersCount;
     [FieldOffset(0x13C0)] public StdMap<ulong, int> AccountIdMap;
     [FieldOffset(0x13D0)] public StdMap<ulong, int> ContentIdMap;
