6d840401f3b3a53423675e935efcd75ce599a5b1
pohky <otoriosu@gmail.com>
2024-07-08 13:20:21 +0200

    Update AddonNamePlate.cs

--- FFXIVClientStructs/FFXIV/Client/UI/AddonNamePlate.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonNamePlate.cs
@@ -86,8 +86,8 @@ public unsafe partial struct AddonNamePlate {
             [FieldOffset(0x0)] public UIObjectKind NamePlateKind;
             [FieldOffset(0x4)] public int HPLabelState;
             /// <summary>
-            /// & 0x1 - Update
-            /// & 0x2 - Update Colors
+            /// &amp; 0x1 - Update<br/>
+            /// &amp; 0x2 - Update Colors<br/>
             /// </summary>
             [FieldOffset(0x8)] public int UpdateFlags;
             [FieldOffset(0xC)] public int X;
@@ -105,17 +105,17 @@ public unsafe partial struct AddonNamePlate {
             [FieldOffset(0x3C)] public int NamePlateObjectIndex;
             // [FieldOffset(0x40)] public int Unk;
             /// <summary>
-            /// & 0x1 - Is prefix title
-            /// & 0x8 - Use Depth-based Priority (terrain obstruction)
-            /// & 0x80 - Hide title
-            /// & 0x100 - Disable Alternate Part Id
+            /// &amp; 0x1 - Is prefix title<br/>
+            /// &amp; 0x8 - Use Depth-based Priority (terrain obstruction)<br/>
+            /// &amp; 0x80 - Hide title<br/>
+            /// &amp; 0x100 - Disable Alternate Part Id<br/>
             /// </summary>
             [FieldOffset(0x44)] public int DrawFlags;
             // [FieldOffset(0x48)] public int Unk;
             /// <summary>
-            /// & 0x1 - Draw name text
-            /// & 0x2 - Draw gauge
-            /// & 0x4 - Unknown, seems to be set 1-2 frames after node appears
+            /// &amp; 0x1 - Draw name text<br/>
+            /// &amp; 0x2 - Draw gauge<br/>
+            /// &amp; 0x4 - Unknown, seems to be set 1-2 frames after node appears<br/>
             /// </summary>
             [FieldOffset(0x4C)] public int VisibilityFlags;
         }
