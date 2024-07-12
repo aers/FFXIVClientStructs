5c0dcbb4aac6bda78acd9275b3463bca6e1a1842
Haselnussbomber <mail@haselnussbomber.de>
2024-07-13 01:30:28 +0200

    Update FateContext

--- FFXIVClientStructs/FFXIV/Client/Game/Fate/FateContext.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Fate/FateContext.cs
@@ -5,9 +5,11 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Fate;
 
 // Client::Game::Fate::FateContext
 // ctor "48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 66 89 51 18"
+[GenerateInterop]
 [StructLayout(LayoutKind.Explicit, Size = 0x10A0)]
-public struct FateContext {
+public partial struct FateContext {
     [FieldOffset(0x18)] public ushort FateId;
+    [FieldOffset(0x1A)] public byte EurekaFate;
     [FieldOffset(0x20)] public int StartTimeEpoch;
     [FieldOffset(0x28)] public short Duration;
 
@@ -19,12 +21,24 @@ public struct FateContext {
     [FieldOffset(0x3AF)] public byte HandInCount;
     [FieldOffset(0x3B8)] public byte Progress;
     [FieldOffset(0x3C4)] public bool IsExpBonus;
+    [FieldOffset(0x3C6), FixedSizeArray] internal FixedSizeArray8<ushort> _objectiveIcons;
     [FieldOffset(0x3D8)] public uint IconId;
+    [FieldOffset(0x3DC)] public uint MapIconId;
+    [FieldOffset(0x3E0)] public uint InactiveMapIcon;
+    [FieldOffset(0x3E4)] public uint EventItem;
+    [FieldOffset(0x3EC)] public int Music;
+    [FieldOffset(0x3F0)] public ushort GivenStatus;
+    [FieldOffset(0x3F4)] public byte Rule;
+    [FieldOffset(0x3F6)] public byte FateRuleEx;
     [FieldOffset(0x3F9)] public byte Level;
     [FieldOffset(0x3FA)] public byte MaxLevel;
+    [FieldOffset(0x3FC)] public ushort ScreenImageAccept;
+    [FieldOffset(0x3FE)] public ushort ScreenImageComplete;
+    [FieldOffset(0x400)] public ushort ScreenImageFailed;
+    [FieldOffset(0x40A)] public ushort FATEChain;
+    [FieldOffset(0x430)] public uint RequiredQuest;
     [FieldOffset(0x450)] public Vector3 Location;
     [FieldOffset(0x464)] public float Radius;
 
-    [FieldOffset(0x760)] public uint MapIconId;
-    [FieldOffset(0x78E)] public ushort TerritoryId;
+    [FieldOffset(0x79A)] public ushort TerritoryId;
 }
