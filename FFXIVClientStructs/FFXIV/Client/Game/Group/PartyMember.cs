30934b05c7d0fedcfaf164c2f54f66f1ba4aa202
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2024-07-14 12:35:29 +0100

    Party updates

--- FFXIVClientStructs/FFXIV/Client/Game/Group/PartyMember.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Group/PartyMember.cs
@@ -26,13 +26,13 @@ public unsafe partial struct PartyMember {
     [FieldOffset(0x32A)] public ushort HomeWorld;
     // GroupManager::GetPartyMemberName: "E8 ? ? ? ? 4C 8B C5 4C 2B C0" or "48 8B 81 ? ? ? ? 48 85 C0 74 ? 48 8B C8 E9"
     [FieldOffset(0x32C), FixedSizeArray(isString: true)] internal FixedSizeArray64<byte> _name;
-    [FieldOffset(0x370)] public Utf8String* UnkName; // TODO: is this anonymous name?
+    [FieldOffset(0x370)] public Utf8String* NameOverride; // if non-null, replaces real name in ui (eg for blacklisted players)
     [FieldOffset(0x378)] public byte Sex;
     [FieldOffset(0x379)] public byte ClassJob;
     [FieldOffset(0x37A)] public byte Level;
     [FieldOffset(0x37B)] public byte DamageShield;
     [FieldOffset(0x37C), FixedSizeArray] internal FixedSizeArray3<ExtraProperty> _extraProperties;
-    [FieldOffset(0x394)] public byte Flags; // 0x01 == set for valid alliance members, 0x04 == set if XYZ is valid?
+    [FieldOffset(0x394)] public byte Flags; // 0x01 == set for valid alliance members, 0x04 == set if XYZ is valid?, 0x10 == in cutscene
 
     public bool IsValidAllianceMember => (Flags & 1) != 0;
 
