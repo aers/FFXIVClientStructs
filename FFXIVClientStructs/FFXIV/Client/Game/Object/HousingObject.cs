980ca64f46975a7002e1dba234547b2f13fdffb9
Haselnussbomber <mail@haselnussbomber.de>
2024-08-03 22:24:33 +0200

    Update HousingObject

--- FFXIVClientStructs/FFXIV/Client/Game/Object/HousingObject.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Object/HousingObject.cs
@@ -3,4 +3,21 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Object;
 [GenerateInterop(isInherited: true)]
 [Inherits<GameObject>]
 [StructLayout(LayoutKind.Explicit, Size = 0x1D0)]
-public unsafe partial struct HousingObject;
+public unsafe partial struct HousingObject {
+    [FieldOffset(0x1A8)] public HousingObjectId HousingObjectId;
+
+    /// <remarks> Index in <see cref="IndoorTerritory.Furniture"/> or <see cref="OutdoorTerritory.Furniture"/> depending on <see cref="HousingObjectId.Type"/>. </remarks>
+    [FieldOffset(0x1AE)] public short HousingFurnitureIndex;
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0x04)]
+public struct HousingObjectId {
+    [FieldOffset(0x00), CExportIgnore] public uint Id;
+    [FieldOffset(0x00)] public ushort EntryId;
+    [FieldOffset(0x02)] public HousingObjectType Type;
+}
+
+public enum HousingObjectType : ushort {
+    YardObject = 2,
+    Furniture = 3,
+}
