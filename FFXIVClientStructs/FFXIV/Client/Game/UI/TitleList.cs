4aaa7112c46c9c0cea7e90fa441248426dc2f1ad
Haselnussbomber <mail@haselnussbomber.de>
2024-08-07 16:02:53 +0200

    Update TitleList/TitleController

--- FFXIVClientStructs/FFXIV/Client/Game/UI/TitleList.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/TitleList.cs
@@ -9,6 +9,9 @@ public unsafe partial struct TitleList {
     [FieldOffset(0x69)] public bool DataReceived;
     [FieldOffset(0x6A)] public bool DataRequested;
 
+    [MemberFunction("40 53 48 83 EC 30 80 79 6A 00")]
+    public partial void RequestTitleList();
+
     [MemberFunction("E8 ?? ?? ?? ?? 84 C0 74 1A 8B CB")]
     public partial bool IsTitleUnlocked(ushort titleId);
 }
