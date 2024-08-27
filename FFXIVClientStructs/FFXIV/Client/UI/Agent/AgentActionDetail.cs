19764c2a28b88a4711fcdbb80f0731fcb7fc3938
Infi <infiziert@protonmail.ch>
2024-08-27 19:07:16 +0200

    Add AgentActionDetail

---
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentActionDetail.cs
@@ -0,0 +1,28 @@
+﻿namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+[Agent(AgentId.ActionDetail)]
+[GenerateInterop]
+[Inherits<AgentInterface>]
+[StructLayout(LayoutKind.Explicit, Size = 0x60)]
+public unsafe partial struct AgentActionDetail {
+    [FieldOffset(0x38)] public ActionKind ActionKind;
+    [FieldOffset(0x3C)] public uint ActionId;
+    [FieldOffset(0x40)] public uint OriginalId; // Example: Summon Topaz
+    [FieldOffset(0x44)] public uint AdjustedId; // Example: Summon Titan II
+}
+
+public enum ActionKind {
+    Action = 28,
+    GeneralAction = 30,
+    CompanionOrder = 31,
+    MainCommand = 32,
+    ExtraCommand = 33,
+    Minion = 34,
+    PetOrder = 35,
+    Trait = 36,
+    CompanionAction = 37,
+    Mount = 39,
+    SquadronAction = 48,
+    Performance = 49,
+}
+
