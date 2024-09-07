f5a8e7398ffc35735294a444d89d23411f435b9c
Aireil <33433913+Aireil@users.noreply.github.com>
2024-09-08 01:27:04 +0200

    Add AgentMutelist

---
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentMutelist.cs
@@ -0,0 +1,15 @@
+﻿using FFXIVClientStructs.FFXIV.Client.System.String;
+
+namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+// Client::UI::Agent::AgentMustlist
+//   Client::UI::Agent::AgentInterface
+//     Component::GUI::AtkModuleInterface::AtkEventInterface
+[Agent(AgentId.Mutelist)]
+[GenerateInterop]
+[Inherits<AgentInterface>]
+[StructLayout(LayoutKind.Explicit, Size = 0xD0)]
+public unsafe partial struct AgentMutelist {
+    [FieldOffset(0x68)] public Utf8String SelectedPlayerFullName; // includes cross world icon
+    [FieldOffset(0x58)] public ulong SelectedPlayerAccountId;
+}
