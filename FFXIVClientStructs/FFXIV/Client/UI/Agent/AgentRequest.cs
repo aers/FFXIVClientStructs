25b279f8f3a17bbcb5929f367473f42c3e0c4c53
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2024-09-20 00:54:32 +0100

    Some turn-in reversing.

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentRequest.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentRequest.cs
@@ -1,3 +1,5 @@
+using FFXIVClientStructs.FFXIV.Client.Game;
+
 namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
 
 // Client::UI::Agent::AgentRequest
@@ -6,9 +8,10 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
 [Agent(AgentId.Request)]
 [GenerateInterop]
 [Inherits<AgentInterface>]
-[StructLayout(LayoutKind.Explicit, Size = 0x460)]
+[StructLayout(LayoutKind.Explicit, Size = 0x850)]
 public unsafe partial struct AgentRequest {
 
     [FieldOffset(0x114)] public sbyte SelectedTurnInSlot; // you can have multiple items to turn in. this defaults to -1 and when you select one of them it becomes its index (starts at 0)
     [FieldOffset(0x124)] public short SelectedTurnInSlotItemOptions; // after you select a SelectedTurnInSlot, you can have multiple varieties of the item, like HQ and NQ. this is the amount of options you have for this slot
+    [FieldOffset(0x130), FixedSizeArray] internal FixedSizeArray140<Pointer<InventoryItem>> _selectedTurnInSlotItemOptionValues;
 }
