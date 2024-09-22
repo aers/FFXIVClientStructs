f2ac5dfe186b6ac88d939b65db89e6a405af5bce
Haselnussbomber <mail@haselnussbomber.de>
2024-09-22 02:55:02 +0200

    Update TerritoryInfo

--- FFXIVClientStructs/FFXIV/Client/Game/UI/TerritoryInfo.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/TerritoryInfo.cs
@@ -9,6 +9,9 @@ public unsafe partial struct TerritoryInfo {
     [StaticAddress("48 8D 0D ?? ?? ?? ?? BA ?? ?? ?? ?? F3 0F 5C 05", 3)]
     public static partial TerritoryInfo* Instance();
 
+    /// <summary>For example, used for housing subdivisions.</summary>
+    [FieldOffset(0x14)] public uint MapIdOverride;
+    [FieldOffset(0x18)] public uint ChatLinkMapIdOverride;
     [FieldOffset(0x1C)] public bool InSanctuary;
 
     [FieldOffset(0x24)] public uint AreaPlaceNameId;
