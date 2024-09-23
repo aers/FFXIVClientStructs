fc03da12ecea739d403e55d15e5134f9f4c2a77c
Haselnussbomber <mail@haselnussbomber.de>
2024-09-23 19:45:43 +0200

    Update UIState

--- FFXIVClientStructs/FFXIV/Client/Game/UI/QuestUI.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/QuestUI.cs
@@ -2,5 +2,5 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.UI;
 
 // Client::Game::UI::QuestUI
 //   Client::Game::UI::ScenarioTextReader
-[StructLayout(LayoutKind.Explicit, Size = 0xFD0)]
+[StructLayout(LayoutKind.Explicit, Size = 0xFF0)]
 public struct QuestUI;
