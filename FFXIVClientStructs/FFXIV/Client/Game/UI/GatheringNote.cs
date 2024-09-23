6294c4a9473bb380057556d6c571b12d0d76ceb5
Haselnussbomber <mail@haselnussbomber.de>
2024-09-23 19:45:43 +0200

    Update UIState

--- FFXIVClientStructs/FFXIV/Client/Game/UI/GatheringNote.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/GatheringNote.cs
@@ -1,5 +1,5 @@
 namespace FFXIVClientStructs.FFXIV.Client.Game.UI;
 
 // Client::Game::UI::GatheringNote
-[StructLayout(LayoutKind.Explicit, Size = 0x628)]
+[StructLayout(LayoutKind.Explicit, Size = 0x6A0)]
 public struct GatheringNote;
