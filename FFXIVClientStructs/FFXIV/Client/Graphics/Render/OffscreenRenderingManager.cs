9a957f62a4a410e3f981fc7bb1e07d46888f614e
Haselnussbomber <mail@haselnussbomber.de>
2024-10-10 02:50:51 +0200

    Update RenderTargetManager, add GraphicsConfig

--- FFXIVClientStructs/FFXIV/Client/Graphics/Render/OffscreenRenderingManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Render/OffscreenRenderingManager.cs
@@ -1,3 +1,6 @@
+using FFXIVClientStructs.FFXIV.Client.Graphics.Kernel;
+using FFXIVClientStructs.FFXIV.Client.System.Resource.Handle;
+
 namespace FFXIVClientStructs.FFXIV.Client.Graphics.Render;
 
 // Client::Graphics::Render::OffscreenRenderingManager
@@ -6,4 +9,9 @@ namespace FFXIVClientStructs.FFXIV.Client.Graphics.Render;
 [StructLayout(LayoutKind.Explicit, Size = 0x190)]
 public unsafe partial struct OffscreenRenderingManager {
     [FieldOffset(0xC8), FixedSizeArray] internal FixedSizeArray4<Pointer<Camera>> _cameras;
+
+    [FieldOffset(0x148)] public ShaderPackageResourceHandle* PrimitiveVSHandle;
+    [FieldOffset(0x150)] public ShaderPackageResourceHandle* PrimitivePSHandle;
+    [FieldOffset(0x158)] public ShaderPackage* PrimitiveVSPackage;
+    [FieldOffset(0x160)] public ShaderPackage* PrimitivePSPackage;
 }
