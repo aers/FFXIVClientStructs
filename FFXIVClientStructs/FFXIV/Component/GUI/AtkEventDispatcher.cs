7665369a8e38fb510d91e5cc3d8d9ef82b048585
Haselnussbomber <mail@haselnussbomber.de>
2024-11-07 23:04:19 +0100

    Update AtkEventDispatcher

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkEventDispatcher.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkEventDispatcher.cs
@@ -1,14 +1,27 @@
 namespace FFXIVClientStructs.FFXIV.Component.GUI;
 
+// Component::GUI::AtkEventDispatcher
+[GenerateInterop]
 [StructLayout(LayoutKind.Explicit, Size = 0x28)]
 public unsafe partial struct AtkEventDispatcher {
     [FieldOffset(0)] public AtkEventManager* AtkEventManager;
     [FieldOffset(0x8)] public StdVector<Pointer<AtkEvent>> Events;
-    // [FieldOffset(0x20)] public byte Unk20;
+    [FieldOffset(0x20)] public byte Unk20;
 
-    // [MemberFunction("E8 ?? ?? ?? ?? 48 8B 0D ?? ?? ?? ?? 4C 8B 74 24")]
-    // public partial void DispatchEvent(nint a2);
+    /// <returns>A bool indicating if the event was handled.</returns>
+    [MemberFunction("E8 ?? ?? ?? ?? 48 8B 0D ?? ?? ?? ?? 4C 8B 74 24")]
+    public partial bool DispatchEvent(Event* evt);
 
-    // [MemberFunction("E8 ?? ?? ?? ?? 48 8B 5B 20 41 B2 01")]
-    // public partial void RemoveEvent(nint a2);
+    [MemberFunction("E8 ?? ?? ?? ?? 48 8B 5B 20 41 B2 01")]
+    public partial void RemoveEvent(AtkEvent* atkEvent);
+
+    // Component::GUI::AtkEventDispatcher::Event
+    [GenerateInterop]
+    [StructLayout(LayoutKind.Explicit, Size = 0x30)]
+    public partial struct Event {
+        [FieldOffset(0x0)] public AtkEventState State;
+        // AtkEventState.UnkFlags1 are saved in this field (as uint), whenever AtkEventStateFlags.Unk4 is set.
+        [FieldOffset(0x4)] public uint UnkFlags;
+        [FieldOffset(0x8)] public AtkEventData EventData;
+    }
 }
