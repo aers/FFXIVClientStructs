14ef308061f6486d37205b40a739e397db7b17eb
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2024-11-13 01:00:37 +0000

    DutyActionManager fixes.

--- FFXIVClientStructs/FFXIV/Client/Game/DutyActionManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/DutyActionManager.cs
@@ -6,17 +6,17 @@ namespace FFXIVClientStructs.FFXIV.Client.Game;
 // In some contexts, game uses value of the PrimaryCostType (column in Action sheet) to identify duty actions, with 20 meaning 'slot 0' and 21 meaning 'slot 1'
 // The game sets available actions either by referencing ContentExAction sheet, or by specifying details explicitly
 [GenerateInterop]
-[StructLayout(LayoutKind.Explicit, Size = 0x58)]
+[StructLayout(LayoutKind.Explicit, Size = 0xA0)]
 public unsafe partial struct DutyActionManager {
-    [FieldOffset(0x00)] public byte PendingContentExActionRowId; // if not equat to current, on next tick game will start reading the sheet and will initialize set of actions when done
+    [FieldOffset(0x00)] public byte PendingContentExActionRowId; // if not equal to current, on next tick game will start reading the sheet and will initialize set of actions when done
     [FieldOffset(0x01)] public byte CurrentContentExActionRowId;
     [FieldOffset(0x08)] public ExcelSheet* ContentExActionSheet;
     [FieldOffset(0x10)] public nint RowReader;
     [FieldOffset(0x18)] public byte NumValidSlots;
-    [FieldOffset(0x1C), FixedSizeArray] internal FixedSizeArray2<uint> _actionId;
-    [FieldOffset(0x24), FixedSizeArray] internal FixedSizeArray2<byte> _maxCharges;
-    [FieldOffset(0x28)] public bool ActionsPresent; // see RaptureHotbarModule.SetDutyActionsPresent
-    [FieldOffset(0x29), FixedSizeArray] internal FixedSizeArray2<bool> _actionActive;
-    [FieldOffset(0x2C), FixedSizeArray] internal FixedSizeArray2<RecastDetail> _recast;
-    [FieldOffset(0x54), FixedSizeArray] internal FixedSizeArray2<byte> _curCharges;
+    [FieldOffset(0x19)] public bool ActionsPresent; // see RaptureHotbarModule.SetDutyActionsPresent
+    [FieldOffset(0x1A), FixedSizeArray] internal FixedSizeArray5<bool> _actionActive;
+    [FieldOffset(0x20), FixedSizeArray] internal FixedSizeArray5<uint> _actionId;
+    [FieldOffset(0x34), FixedSizeArray] internal FixedSizeArray5<RecastDetail> _recast;
+    [FieldOffset(0x98), FixedSizeArray] internal FixedSizeArray2<byte> _maxCharges;
+    [FieldOffset(0x9A), FixedSizeArray] internal FixedSizeArray2<byte> _curCharges;
 }
