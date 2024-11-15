14beed7a4008a996c26aa41dd73ba8a879379a3b
Haselnussbomber <mail@haselnussbomber.de>
2024-11-15 20:30:50 +0100

    Update Control

--- FFXIVClientStructs/FFXIV/Client/Game/Control/Control.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Control/Control.cs
@@ -5,15 +5,15 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Control;
 
 // Client::Game::Control::Control
 [GenerateInterop]
-[StructLayout(LayoutKind.Explicit, Size = 0x5F00)]
+[StructLayout(LayoutKind.Explicit, Size = 0x7655)]
 public unsafe partial struct Control {
     [FieldOffset(0x00)] public CameraManager CameraManager;
     [FieldOffset(0x180)] public TargetSystem TargetSystem;
 
-    [FieldOffset(0x5E43)] public bool IsWalking;
-    [FieldOffset(0x5EA8)] public uint LocalPlayerEntityId;
-    [FieldOffset(0x5EB0)] public BattleChara* LocalPlayer;
-    [FieldOffset(0x5EC0)] public Matrix4x4 ViewProjectionMatrix;
+    [FieldOffset(0x7603)] public bool IsWalking;
+    [FieldOffset(0x7668)] public uint LocalPlayerEntityId;
+    [FieldOffset(0x7670)] public BattleChara* LocalPlayer;
+    [FieldOffset(0x7680)] public Matrix4x4 ViewProjectionMatrix;
 
     [StaticAddress("4C 8D 35 ?? ?? ?? ?? 48 8B 09", 3)]
     public static partial Control* Instance();
