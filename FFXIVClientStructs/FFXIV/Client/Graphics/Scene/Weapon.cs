8678a89e844f9a5f0848d3048a5d986fbc9057cf
Ottermandias <70807659+Ottermandias@users.noreply.github.com>
2024-11-20 11:48:21 +0100

    Update Weapon.cs

--- FFXIVClientStructs/FFXIV/Client/Graphics/Scene/Weapon.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Scene/Weapon.cs
@@ -24,4 +24,15 @@ public unsafe partial struct Weapon {
     [FieldOffset(0xA28)] public TextureResourceHandle* Decal;
     [FieldOffset(0xA30)] public Texture* FreeCompanyCrest;
     [FieldOffset(0xA38)] public uint SlotFreeCompanyCrestBitfield; // Only relevant bit is & 0x1
+    [FieldOffset(0xA40)] public ChangedWeaponData* ChangedData;
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x06)] // Size unknown
+    public struct ChangedWeaponData
+    {
+        // No primary ID since this requires a new model to be setup.
+        [FieldOffset(0x00)] public ushort SecondaryId;
+        [FieldOffset(0x02)] public byte Variant;
+        [FieldOffset(0x03)] public byte Stain0;
+        [FieldOffset(0x04)] public byte Stain1;
+    }
 }
