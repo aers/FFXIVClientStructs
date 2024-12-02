6fbafeeb64955f8b2cf3a9c639d248f76dcdd28f
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2024-12-02 18:23:49 +0100

    Add sort reordering to party member proxy

--- FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyPartyMember.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyPartyMember.cs
@@ -8,4 +8,13 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Info;
 [GenerateInterop]
 [Inherits<InfoProxyCommonList>]
 [StructLayout(LayoutKind.Explicit, Size = 0x360)]
-public unsafe partial struct InfoProxyPartyMember;
+public unsafe partial struct InfoProxyPartyMember {
+    /// <summary>
+    /// Changes the order of the party members in the party list.
+    /// </summary>
+    /// <param name="selectedIndex">The person you want to change order of. Has to be lower than <paramref name="targetIndex"/></param>
+    /// <param name="targetIndex">Where you want them to end up</param>
+    /// <param name="unk1">Default true from PartySort command handler</param>
+    [MemberFunction("E8 ?? ?? ?? ?? B3 01 88 5C 24 60")]
+    public partial void ChangeOrder(int selectedIndex, int targetIndex, bool unk1 = true);
+}
