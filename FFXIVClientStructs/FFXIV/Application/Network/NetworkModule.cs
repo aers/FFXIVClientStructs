df45e5c555d385e01c032836bcd595fcc7b3486d
RedworkDE <10944644+RedworkDE@users.noreply.github.com>
2024-12-21 16:16:27 +0100

    add types for some NetworkModule fields

--- FFXIVClientStructs/FFXIV/Application/Network/NetworkModule.cs
+++ FFXIVClientStructs/FFXIV/Application/Network/NetworkModule.cs
@@ -1,5 +1,6 @@
 using FFXIVClientStructs.FFXIV.Client.Network;
 using FFXIVClientStructs.FFXIV.Client.System.String;
+using FFXIVClientStructs.FFXIV.Common;
 
 namespace FFXIVClientStructs.FFXIV.Application.Network;
 
@@ -29,19 +30,30 @@ public unsafe partial struct NetworkModule {
     [FieldOffset(0x8C8)] public Utf8String Ticket;
     [FieldOffset(0x930)] public Utf8String World;
     [FieldOffset(0x998)] public Utf8String ZoneName;
+    [FieldOffset(0xA00)] private Utf8String UnkA00;
 
     [FieldOffset(0xAA1)] public bool WinSockInitialized;
     [FieldOffset(0xAA8)] public NetworkModulePacketReceiverCallback* PacketReceiverCallback;
 
+    [FieldOffset(0xAD0)] private TimePoint UnkTimeAD0;
+    [FieldOffset(0xB28)] private TimePoint UnkTimeB28;
     [FieldOffset(0xB54)] public int CurrentDeviceTime; //Timestamp
     [FieldOffset(0xB58)] public int CurrentDeviceTimeMillis; //Milliseconds for CurrentDeviceTime
 
+    [FieldOffset(0xB68)] private TimePoint UnkTimeB68;
+    [FieldOffset(0xB88)] private TimePoint UnkTimeB88;
+    [FieldOffset(0xBB0)] private TimePoint UnkTimeBB0;
+    [FieldOffset(0xBC8)] private uint UnkTimeBB0_UpdateFrequency;
+
+    [FieldOffset(0xBD0)] private TimePoint UnkTimeBD0;
+    [FieldOffset(0xBE8)] private TimePoint UnkTimeBE8;
     [FieldOffset(0xC06)] public short CurrentInstance;
 
     [FieldOffset(0xC10)] public int KeepAliveZone;
     [FieldOffset(0xC14)] public int KeepAliveIntervalZone;
     [FieldOffset(0xC18)] public int KeepAliveChat;
     [FieldOffset(0xC1C)] public int KeepAliveIntervalChat;
+    [FieldOffset(0xC20)] private TimePoint UnkTimeC20;
 
     [FieldOffset(0xC38)] public bool IsInCrossWorldDuty;
 }
