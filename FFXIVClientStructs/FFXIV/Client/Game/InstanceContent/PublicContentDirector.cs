7f42290918410a0b5d9e07424377ad8ff0f3aca1
Haselnussbomber <mail@haselnussbomber.de>
2024-12-28 21:55:17 +0100

    Update PublicContentDirector

--- FFXIVClientStructs/FFXIV/Client/Game/InstanceContent/PublicContentDirector.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/InstanceContent/PublicContentDirector.cs
@@ -13,24 +13,24 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.InstanceContent;
 [StructLayout(LayoutKind.Explicit, Size = 0x11D0)]
 public unsafe partial struct PublicContentDirector {
     // fields from PublicContent sheet
-    [FieldOffset(0xD80)] public uint NameOffset; // quite useless here
-    [FieldOffset(0xD84)] public uint MapIcon;
-    [FieldOffset(0xD88)] public uint TextDataStart;
-    [FieldOffset(0xD8C)] public uint TextDataEnd;
-    [FieldOffset(0xD90)] public uint StartCutscene;
-    [FieldOffset(0xD94)] public uint LGBEventRange;
-    [FieldOffset(0xD98)] public uint LGBPopRange;
-    [FieldOffset(0xD9C)] public uint EndCutscene;
-    [FieldOffset(0xDA0)] public ushort Timelimit;
-    [FieldOffset(0xDA2)] public ushort ContentFinderCondition;
-    [FieldOffset(0xDA4)] public ushort AdditionalData;
-    [FieldOffset(0xDA6)] public ushort Unknown0;
-    [FieldOffset(0xDA8)] public ushort Unknown1;
-    [FieldOffset(0xDAA)] public ushort Unknown2;
-    [FieldOffset(0xDAC)] public ushort Unknown3;
-    [FieldOffset(0xDB0)] public ushort Unknown5;
-    [FieldOffset(0xDB2)] public PublicContentDirectorType Type;
-    [FieldOffset(0xDB3)] public byte Unknown4;
+    [FieldOffset(0xD80 + 0x00)] public uint NameOffset;
+    [FieldOffset(0xD80 + 0x04)] public uint MapIcon;
+    [FieldOffset(0xD80 + 0x08)] public uint TextDataStart;
+    [FieldOffset(0xD80 + 0x0C)] public uint TextDataEnd;
+    [FieldOffset(0xD80 + 0x10)] public uint StartCutscene;
+    [FieldOffset(0xD80 + 0x14)] public uint LGBEventRange;
+    [FieldOffset(0xD80 + 0x18)] public uint LGBPopRange;
+    [FieldOffset(0xD80 + 0x1C)] public uint EndCutscene;
+    [FieldOffset(0xD80 + 0x20)] public ushort Timelimit;
+    [FieldOffset(0xD80 + 0x22)] public ushort ContentFinderCondition;
+    [FieldOffset(0xD80 + 0x24)] public ushort AdditionalData;
+    [FieldOffset(0xD80 + 0x26)] public ushort Unknown0;
+    [FieldOffset(0xD80 + 0x28)] public ushort Unknown1;
+    [FieldOffset(0xD80 + 0x2A)] public ushort Unknown2;
+    [FieldOffset(0xD80 + 0x2C)] public ushort Unknown3;
+    [FieldOffset(0xD80 + 0x2E)] public ushort Unknown5;
+    [FieldOffset(0xD80 + 0x30)] public PublicContentDirectorType Type;
+    [FieldOffset(0xD80 + 0x31)] public byte Unknown4;
 
     [MemberFunction("40 53 57 48 83 EC 78 48 8B D9 48 8D 0D")]
     public static partial nint HandleEnterContentInfoPacket(EnterContentInfoPacket* packet);
