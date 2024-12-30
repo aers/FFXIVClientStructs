aca98a4f1b2d0394e76f7eb30e621554a1df5f77
Haselnussbomber <mail@haselnussbomber.de>
2024-12-30 20:50:41 +0100

    Add some placeholder Render::Manager structs

---
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Render/ShaderManager.cs
@@ -0,0 +1,5 @@
+namespace FFXIVClientStructs.FFXIV.Client.Graphics.Render;
+
+// Client::Graphics::Render::ShaderManager
+[StructLayout(LayoutKind.Explicit, Size = 0x1D8)]
+public struct ShaderManager;
