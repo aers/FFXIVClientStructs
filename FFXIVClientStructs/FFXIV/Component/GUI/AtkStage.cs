68509624f1c08ad7f00f8118e3cb3ac1857c5d09
Haselnussbomber <mail@haselnussbomber.de>
2025-01-09 13:54:06 +0100

    Update AtkStage

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkStage.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkStage.cs
@@ -10,6 +10,7 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI;
 [StructLayout(LayoutKind.Explicit, Size = 0x75E00)]
 public unsafe partial struct AtkStage {
     [FieldOffset(0x0)] public AtkEventTarget AtkEventTarget;
+    [FieldOffset(0x10)] public AtkFontManager* AtkFontManager;
     [FieldOffset(0x18)] public AtkTextureResourceManager* AtkTextureResourceManager;
     [FieldOffset(0x20)] public RaptureAtkUnitManager* RaptureAtkUnitManager;
     [FieldOffset(0x28)] public AtkInputManager* AtkInputManager;
