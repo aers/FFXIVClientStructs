fa2f0633ad352aa0dd6043bb622623033b7d213a
Haselnussbomber <mail@haselnussbomber.de>
2025-01-18 04:44:41 +0100

    Add AgentMiragePrismPrismSetConvert

---
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentMiragePrismPrismSetConvert.cs
@@ -0,0 +1,16 @@
+namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+// Client::UI::Agent::AgentMiragePrismPrismSetConvert
+//   Client::UI::Agent::AgentInterface
+//     Component::GUI::AtkModuleInterface::AtkEventInterface
+[Agent(AgentId.MiragePrismPrismSetConvert)]
+[GenerateInterop]
+[Inherits<AgentInterface>]
+[StructLayout(LayoutKind.Explicit, Size = 0x30)]
+public unsafe partial struct AgentMiragePrismPrismSetConvert {
+    [MemberFunction("E8 ?? ?? ?? ?? 48 8B 43 28 C6 40 02 0B")]
+    public partial void Open(uint itemId, InventoryType inventoryType, int slot, int openerAddonId, bool enableStoring);
+
+    // OpenPreview in data.yml
+    public void Open(uint itemId) => Open(itemId, InventoryType.Invalid, 0, 0, false);
+}
