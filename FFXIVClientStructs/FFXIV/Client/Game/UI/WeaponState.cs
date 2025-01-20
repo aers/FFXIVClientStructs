31908418bf9eb58025481962105be32ddb920a4b
awgil <andrew.gilewsky@gmail.com>
2025-01-20 10:46:12 +0300

    Auto-attack related functions. (#1279)

--- FFXIVClientStructs/FFXIV/Client/Game/UI/WeaponState.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/WeaponState.cs
@@ -7,5 +7,15 @@ public struct WeaponState {
     [FieldOffset(0x04)] public float SheatheCooldown;
     [FieldOffset(0x08)] public float AutoSheathTimer;
     [FieldOffset(0x0C)] public bool AutoSheatheState;
-    [FieldOffset(0x10)] public bool IsAutoAttacking;
+    [FieldOffset(0x10)] public AutoAttackState AutoAttackState; // note: not sure whether this is actually a part of this structure or next field of a parent
+    [FieldOffset(0x10), Obsolete("Use AutoAttackState.IsAutoAttacking")] public bool IsAutoAttacking;
+}
+
+[GenerateInterop]
+[StructLayout(LayoutKind.Explicit, Size = 1)]
+public partial struct AutoAttackState {
+    [FieldOffset(0)] public bool IsAutoAttacking;
+
+    [MemberFunction("E8 ?? ?? ?? ?? EB 15 41 B0 01")]
+    public partial bool SetImpl(bool value, bool sendPacket, bool isInstant);
 }
