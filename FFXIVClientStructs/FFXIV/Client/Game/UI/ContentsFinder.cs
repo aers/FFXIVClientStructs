946fc0a5dac40dd026a03013875874cbd5a1a313
Asriel <asriel@camora.dev>
2025-01-31 22:50:30 -0800

    Update ContentsFinder.cs

--- FFXIVClientStructs/FFXIV/Client/Game/UI/ContentsFinder.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/ContentsFinder.cs
@@ -45,7 +45,7 @@ public struct ContentsFinderQueueInfo {
     [FieldOffset(0x5B)] public sbyte ClampedPositionInQueue;
     [FieldOffset(0x5C)] public sbyte PositionInQueue;
 
-    [FieldOffset(0x62)] public QueueInfoState StateInfo;
+    [FieldOffset(0x62)] public QueueInfoState InfoState;
     [FieldOffset(0x67), Obsolete("Use StateInfo.AverageWaitTime")] public byte AverageWaitTime; // In minutes
 
     [FieldOffset(0x7C)] public PoppedContentTypes PoppedContentType;
@@ -64,6 +64,21 @@ public struct ContentsFinderQueueInfo {
 
     public DateTime GetEnteredQueueDateTime() => DateTime.UnixEpoch.AddSeconds(EnteredQueueTimestamp);
     public DateTime GetQueueReadyDateTime() => DateTime.UnixEpoch.AddSeconds(QueueReadyTimestamp);
+
+    [MemberFunction("40 53 57 41 57 48 83 EC 30 0F B6 41 55")]
+    public partial void ProcessInfoState(QueueStates newState, QueueInfoState* newInfoState);
+    
+    [MemberFunction("E8 ?? ?? ?? ?? 0F B6 13 44 0F B6 C7")]
+    public partial void SetQueuedLanguages(byte languageFlags);
+    
+    [MemberFunction("C6 01 00 44 8B CA")]
+    public partial void SetQueuedContentFinderConditions(uint conditionCount, int* conditionIds);
+    
+    [MemberFunction("41 0F B6 C0 89 51 28")]
+    public partial void SetQueuedJobAndRoulette(uint classJobId, byte a3, byte a4, byte contentRouletteId);
+    
+    [MemberFunction("48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 8B FA 48 8B D9 45 84 C0")]
+    public partial void UpdateQueueState(QueueStates newState, bool beganQueue);
 
     public enum QueueStates : byte {
         None = 0,
