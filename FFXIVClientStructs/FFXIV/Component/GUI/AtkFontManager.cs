1ed26c32991f8d7618d36dad9c430a1f7e08a505
Haselnussbomber <mail@haselnussbomber.de>
2025-02-04 13:28:54 +0100

    Add AtkFontManager.Icon2RemapTable (#1297)
    * Add AtkFontManager.Icon2RemapTable
    
    * Update data.yml

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkFontManager.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkFontManager.cs
@@ -14,6 +14,8 @@ public unsafe partial struct AtkFontManager {
     [FieldOffset(0x28)] public TextChecker* TextChecker;
     [FieldOffset(0x30)] public AtkTextureResourceManager* AtkTextureResourceManager;
     [FieldOffset(0x38)] public AtkFontAnalyzer* AtkFontAnalyzer;
+    /// <summary> An array that maps icon IDs used in <c>&lt;icon2(id)&gt;</c> macros to their remapped equivalents based on the controller's button mappings. </summary>
+    [FieldOffset(0x40)] public Icon2RemapEntry* Icon2RemapTable; // 30 * 0x08
     [FieldOffset(0x50)] public uint SetupState;
 
     [GenerateInterop]
@@ -29,5 +31,13 @@ public unsafe partial struct AtkFontManager {
         [FieldOffset(0x00)] public TextureResourceHandle* TextureResourceHandle;
         [FieldOffset(0x08)] private ResourceHandle* GfdResourceHandle; // probably only available during setup, because it's null afterwards
         [FieldOffset(0x10)] public Texture* Texture;
+    }
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x08)]
+    public struct Icon2RemapEntry {
+        /// <summary> The ID passed to the icon2 macro. </summary>
+        [FieldOffset(0x00)] public uint IconId;
+        /// <summary> The GFD Icon ID that should be displayed. </summary>
+        [FieldOffset(0x04)] public uint RemappedIconId;
     }
 }
