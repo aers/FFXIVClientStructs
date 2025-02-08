5c7517e6631ba529ea8b5181af64652bd65eac09
Haselnussbomber <mail@haselnussbomber.de>
2025-02-08 12:49:43 +0100

    Add LuaTexts to LuaEventHandler (#1298)
    * Update LuaEventHandler
    
    * Add CustomTalkEventHandler placeholder

---
+++ FFXIVClientStructs/FFXIV/Client/Game/Event/CustomTalkEventHandler.cs
@@ -0,0 +1,9 @@
+namespace FFXIVClientStructs.FFXIV.Client.Game.Event;
+
+// Client::Game::Event::CustomTalkEventHandler
+//   Client::Game::Event::LuaEventHandler
+//     Client::Game::Event::EventHandler
+[GenerateInterop]
+[Inherits<LuaEventHandler>]
+[StructLayout(LayoutKind.Explicit, Size = 0x4C0)]
+public partial struct CustomTalkEventHandler;
--- FFXIVClientStructs/FFXIV/Client/Game/Event/LuaEventHandler.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Event/LuaEventHandler.cs
@@ -13,4 +13,12 @@ public unsafe partial struct LuaEventHandler {
     [FieldOffset(0x218)] public LuaScriptLoader<LuaEventHandler> LuaScriptLoader;
     [FieldOffset(0x240)] public Utf8String LuaClass;
     [FieldOffset(0x2A8)] public Utf8String LuaKey;
+
+    [FieldOffset(0x310)] public StdMap<uint, LuaEventHandlerLuaText> LuaTexts;
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0xD0)]
+public struct LuaEventHandlerLuaText {
+    [FieldOffset(0x00)] public Utf8String Key;
+    [FieldOffset(0x68)] public Utf8String Value;
 }
