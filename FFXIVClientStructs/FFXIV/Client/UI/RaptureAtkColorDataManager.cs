5582d63210733d1c9078a8b3afdc9de3b2c30183
Haselnussbomber <mail@haselnussbomber.de>
2025-02-08 11:57:49 +0100

    Update RaptureAtkColorDataManager

--- FFXIVClientStructs/FFXIV/Client/UI/RaptureAtkColorDataManager.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/RaptureAtkColorDataManager.cs
@@ -1,5 +1,22 @@
 namespace FFXIVClientStructs.FFXIV.Client.UI;
 
 // Client::UI::RaptureAtkColorDataManager
+// Used by the ColorPicker addon
 [StructLayout(LayoutKind.Explicit, Size = 0x18)]
-public struct RaptureAtkColorDataManager;
+public unsafe struct RaptureAtkColorDataManager {
+    [FieldOffset(0x08)] public ColorSet* ColorSets;
+    [FieldOffset(0x10)] public int NumColorSets;
+    [FieldOffset(0x14)] public bool IsLoaded;
+
+    public Span<ColorSet> ColorSetsSpan => new(ColorSets, NumColorSets);
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x10)]
+    public struct ColorSet {
+        [FieldOffset(0x00)] public uint Id;
+        [FieldOffset(0x04)] public short NumColors;
+        [FieldOffset(0x06)] public bool UnkBool;
+        [FieldOffset(0x08)] public uint* Colors;
+
+        public Span<ColorSet> ColorSpan => new(Colors, NumColors);
+    }
+}
