f0f0a943987c87d2279925b3bf9667c432fac15f
Haselnussbomber <mail@haselnussbomber.de>
2025-02-15 10:52:57 +0100

    Update ShellCommandInterface

--- FFXIVClientStructs/FFXIV/Component/Shell/ShellCommandInterface.cs
+++ FFXIVClientStructs/FFXIV/Component/Shell/ShellCommandInterface.cs
@@ -1,4 +1,17 @@
+using FFXIVClientStructs.FFXIV.Client.System.String;
+
 namespace FFXIVClientStructs.FFXIV.Component.Shell;
 
+[GenerateInterop]
 [StructLayout(LayoutKind.Explicit, Size = 0x08)]
-public struct ShellCommandInterface;
+public unsafe partial struct ShellCommandInterface {
+    [VirtualFunction(1)]
+    public partial int ExecuteCommand(CommandData* commandData, void* source);
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x30)]
+    public struct CommandData {
+        [FieldOffset(0x00)] public ushort TextCommandId;
+
+        [FieldOffset(0x18)] public StdVector<Utf8String> StringArgs;
+    }
+}
