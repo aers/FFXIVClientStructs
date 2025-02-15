8bf82df7baa41a7c354f746a3a61fecbe6794f0f
Haselnussbomber <mail@haselnussbomber.de>
2025-02-15 14:30:06 +0100

    Slightly better name

--- FFXIVClientStructs/FFXIV/Component/Shell/ShellCommandInterface.cs
+++ FFXIVClientStructs/FFXIV/Component/Shell/ShellCommandInterface.cs
@@ -6,10 +6,10 @@ namespace FFXIVClientStructs.FFXIV.Component.Shell;
 [StructLayout(LayoutKind.Explicit, Size = 0x08)]
 public unsafe partial struct ShellCommandInterface {
     [VirtualFunction(1)]
-    public partial int ExecuteCommand(CommandData* commandData, void* source);
+    public partial int ExecuteCommand(CommandContext* context, void* source);
 
     [StructLayout(LayoutKind.Explicit, Size = 0x30)]
-    public struct CommandData {
+    public struct CommandContext {
         [FieldOffset(0x00)] public ushort TextCommandId;
 
         [FieldOffset(0x18)] public StdVector<Utf8String> StringArgs;
