ae163aa8b2c10f66ec9950326b94d50763d16abe
Haselnussbomber <mail@haselnussbomber.de>
2025-02-17 13:12:46 +0100

    Update GlobalParameters table

--- FFXIVClientStructs/FFXIV/Component/Text/MacroDecoder.cs
+++ FFXIVClientStructs/FFXIV/Component/Text/MacroDecoder.cs
@@ -15,77 +15,91 @@ public unsafe partial struct MacroDecoder {
 // GlobalParameters
 // Note that the StdDeque (and this list) is zero-based, so you need to subtract 1 from gnum/gstr.
 //
-// |-------|----------------------|----------------------------------------------------------------|
-// | Index | Type                 | Label                                                          |
-// |-------|----------------------|----------------------------------------------------------------|
-// |     0 | ReferencedUtf8String | Player Name                                                    |
-// |     1 | String               | Log Message Name 1                                             |
-// |     2 | String               | Log Message Name 2                                             |
-// |     3 | Integer              | Player Sex                                                     |
-// |    10 | Integer              | Eorzea Time Hours                                              |
-// |    11 | Integer              | Eorzea Time Minutes                                            |
-// |    12 | Integer              | Log Text Colors - Chat 1 - Say                                 |
-// |    13 | Integer              | Log Text Colors - Chat 1 - Shout                               |
-// |    14 | Integer              | Log Text Colors - Chat 1 - Tell                                |
-// |    15 | Integer              | Log Text Colors - Chat 1 - Party                               |
-// |    16 | Integer              | Log Text Colors - Chat 1 - Alliance                            |
-// |    17 | Integer              | Log Text Colors - Chat 2 - LS1                                 |
-// |    18 | Integer              | Log Text Colors - Chat 2 - LS2                                 |
-// |    19 | Integer              | Log Text Colors - Chat 2 - LS3                                 |
-// |    20 | Integer              | Log Text Colors - Chat 2 - LS4                                 |
-// |    21 | Integer              | Log Text Colors - Chat 2 - LS5                                 |
-// |    22 | Integer              | Log Text Colors - Chat 2 - LS6                                 |
-// |    23 | Integer              | Log Text Colors - Chat 2 - LS7                                 |
-// |    24 | Integer              | Log Text Colors - Chat 2 - LS8                                 |
-// |    25 | Integer              | Log Text Colors - Chat 2 - Free Company                        |
-// |    26 | Integer              | Log Text Colors - Chat 2 - PvP Team                            |
-// |    27 | Integer              | Log Text Colors - General - PvP Team Announcements             |
-// |    28 | Integer              | Log Text Colors - Chat 2 - Novice Network                      |
-// |    29 | Integer              | Log Text Colors - Chat 1 - Emotes (Personal Emotes)            |
-// |    30 | Integer              | Log Text Colors - Chat 1 - Emotes                              |
-// |    32 | Integer              | Log Text Colors - General - Free Company Announcements         |
-// |    33 | Integer              | Log Text Colors - General - Novice Network Announcements       |
-// |    34 | Integer              | Log Text Colors - Chat 2 - CWLS1                               |
-// |    35 | Integer              | Log Text Colors - Battle - Damage Dealt                        |
-// |    36 | Integer              | Log Text Colors - Battle - Missed Attacks                      |
-// |    37 | Integer              | Log Text Colors - Battle - Actions                             |
-// |    38 | Integer              | Log Text Colors - Battle - Items                               |
-// |    39 | Integer              | Log Text Colors - Battle - Healing                             |
-// |    40 | Integer              | Log Text Colors - Battle - Enchanting Effects                  |
-// |    41 | Integer              | Log Text Colors - Battle - Enfeebing Effects                   |
-// |    42 | Integer              | Log Text Colors - General - Echo                               |
-// |    43 | Integer              | Log Text Colors - General - System Messages                    |
-// |    54 | ReferencedUtf8String | Companion Name                                                 |
-// |    56 | Integer              | Log Text Colors - General - Battle System Messages             |
-// |    57 | Integer              | Log Text Colors - General - Gathering System Messages          |
-// |    58 | Integer              | Log Text Colors - General - Error Messages                     |
-// |    59 | Integer              | Log Text Colors - General - NPC Dialogue                       |
-// |    60 | Integer              | Log Text Colors - General - Item Drops                         |
-// |    61 | Integer              | Log Text Colors - General - Level Up                           |
-// |    62 | Integer              | Log Text Colors - General - Loot                               |
-// |    63 | Integer              | Log Text Colors - General - Synthesis                          |
-// |    64 | Integer              | Log Text Colors - General - Gathering                          |
-// |    67 | Integer              | Player ClassJobId                                              |
-// |    68 | Integer              | Player Level                                                   |
-// |    70 | Integer              | Player Race                                                    |
-// |    71 | Integer              | Player Synced Level                                            |
-// |    77 | Integer              | Client/Plattform?                                              |
-// |    82 | Integer              | Datacenter Region (see WorldDCGroupType sheet)                 |
-// |    83 | Integer              | Log Text Colors - Chat 2 - CWLS2                               |
-// |    84 | Integer              | Log Text Colors - Chat 2 - CWLS3                               |
-// |    85 | Integer              | Log Text Colors - Chat 2 - CWLS4                               |
-// |    86 | Integer              | Log Text Colors - Chat 2 - CWLS5                               |
-// |    87 | Integer              | Log Text Colors - Chat 2 - CWLS6                               |
-// |    88 | Integer              | Log Text Colors - Chat 2 - CWLS7                               |
-// |    89 | Integer              | Log Text Colors - Chat 2 - CWLS8                               |
-// |    92 | Integer              | TerritoryType Id                                               |
-// |    94 | Integer              | LogSetRoleColor 1: LogColorRoleTank                            |
-// |    95 | Integer              | LogSetRoleColor 2: LogColorRoleTank                            |
-// |    96 | Integer              | LogSetRoleColor 1: LogColorRoleHealer                          |
-// |    97 | Integer              | LogSetRoleColor 2: LogColorRoleHealer                          |
-// |    98 | Integer              | LogSetRoleColor 1: LogColorRoleDPS                             |
-// |    99 | Integer              | LogSetRoleColor 2: LogColorRoleDPS                             |
-// |   100 | Integer              | LogSetRoleColor 1: LogColorOtherClass                          |
-// |   101 | Integer              | LogSetRoleColor 2: LogColorOtherClass                          |
-// | ? 102 | Integer              | Has Login Security Token (set in UIModule.HandlePacket case 0) |
-// |-------|----------------------|----------------------------------------------------------------|
+// |-------|----------------------|---------------------------------------------|
+// | Index | Type                 | Label                                       |
+// |-------|----------------------|---------------------------------------------|
+// |     0 | ReferencedUtf8String | Player Name                                 |
+// |     1 | String               | Temp Player 1 Name                          |
+// |     2 | String               | Temp Player 2 Name                          |
+// |     3 | Integer              | Player Sex                                  |
+// |     4 | Integer              | Temp Player 1 Sex                           |
+// |     5 | Integer              | Temp Player 2 Sex                           |
+// |     6 | Integer              | Temp Player 1 Unk 1                         |
+// |     7 | Integer              | Temp Player 2 Unk 1                         |
+// |    10 | Integer              | Eorzea Time Hours                           |
+// |    11 | Integer              | Eorzea Time Minutes                         |
+// |    12 | Integer              | ColorSay                                    |
+// |    13 | Integer              | ColorShout                                  |
+// |    14 | Integer              | ColorTell                                   |
+// |    15 | Integer              | ColorParty                                  |
+// |    16 | Integer              | ColorAlliance                               |
+// |    17 | Integer              | ColorLS1                                    |
+// |    18 | Integer              | ColorLS2                                    |
+// |    19 | Integer              | ColorLS3                                    |
+// |    20 | Integer              | ColorLS4                                    |
+// |    21 | Integer              | ColorLS5                                    |
+// |    22 | Integer              | ColorLS6                                    |
+// |    23 | Integer              | ColorLS7                                    |
+// |    24 | Integer              | ColorLS8                                    |
+// |    25 | Integer              | ColorFCompany                               |
+// |    26 | Integer              | ColorPvPGroup                               |
+// |    27 | Integer              | ColorPvPGroupAnnounce                       |
+// |    28 | Integer              | ColorBeginner                               |
+// |    29 | Integer              | ColorEmoteUser                              |
+// |    30 | Integer              | ColorEmote                                  |
+// |    31 | Integer              | ColorYell                                   |
+// |    32 | Integer              | ColorFCAnnounce                             |
+// |    33 | Integer              | ColorBeginnerAnnounce                       |
+// |    34 | Integer              | ColorCWLS                                   |
+// |    35 | Integer              | ColorAttackSuccess                          |
+// |    36 | Integer              | ColorAttackFailure                          |
+// |    37 | Integer              | ColorAction                                 |
+// |    38 | Integer              | ColorItem                                   |
+// |    39 | Integer              | ColorCureGive                               |
+// |    40 | Integer              | ColorBuffGive                               |
+// |    41 | Integer              | ColorDebuffGive                             |
+// |    42 | Integer              | ColorEcho                                   |
+// |    43 | Integer              | ColorSysMsg                                 |
+// |    51 | Integer              | Player Grand Company Rank (Maelstrom)       |
+// |    52 | Integer              | Player Grand Company Rank (Twin Adders)     |
+// |    53 | Integer              | Player Grand Company Rank (Immortal Flames) |
+// |    54 | ReferencedUtf8String | Companion Name                              |
+// |    55 | ReferencedUtf8String | Content Name                                |
+// |    56 | Integer              | ColorSysBattle                              |
+// |    57 | Integer              | ColorSysGathering                           |
+// |    58 | Integer              | ColorSysErr                                 |
+// |    59 | Integer              | ColorNpcSay                                 |
+// |    60 | Integer              | ColorItemNotice                             |
+// |    61 | Integer              | ColorGrowup                                 |
+// |    62 | Integer              | ColorLoot                                   |
+// |    63 | Integer              | ColorCraft                                  |
+// |    64 | Integer              | ColorGathering                              |
+// |    65 | Integer              | Temp Player 1 Unk 2                         |
+// |    66 | Integer              | Temp Player 2 Unk 2                         |
+// |    67 | Integer              | Player ClassJobId                           |
+// |    68 | Integer              | Player Level                                |
+// |    70 | Integer              | Player Race                                 |
+// |    71 | Integer              | Player Synced Level                         |
+// |    77 | Integer              | Client/Plattform?                           |
+// |    78 | Integer              | Player BirthMonth                           |
+// |    82 | Integer              | Datacenter Region                           |
+// |    83 | Integer              | ColorCWLS2                                  |
+// |    84 | Integer              | ColorCWLS3                                  |
+// |    85 | Integer              | ColorCWLS4                                  |
+// |    86 | Integer              | ColorCWLS5                                  |
+// |    87 | Integer              | ColorCWLS6                                  |
+// |    88 | Integer              | ColorCWLS7                                  |
+// |    89 | Integer              | ColorCWLS8                                  |
+// |    91 | Integer              | Player Grand Company                        |
+// |    92 | Integer              | TerritoryType Id                            |
+// |    93 | Integer              | Is Soft Keyboard Enabled                    |
+// |    94 | Integer              | LogSetRoleColor 1: LogColorRoleTank         |
+// |    95 | Integer              | LogSetRoleColor 2: LogColorRoleTank         |
+// |    96 | Integer              | LogSetRoleColor 1: LogColorRoleHealer       |
+// |    97 | Integer              | LogSetRoleColor 2: LogColorRoleHealer       |
+// |    98 | Integer              | LogSetRoleColor 1: LogColorRoleDPS          |
+// |    99 | Integer              | LogSetRoleColor 2: LogColorRoleDPS          |
+// |   100 | Integer              | LogSetRoleColor 1: LogColorOtherClass       |
+// |   101 | Integer              | LogSetRoleColor 2: LogColorOtherClass       |
+// |   102 | Integer              | Has Login Security Token                    |
+// |-------|----------------------|---------------------------------------------|
