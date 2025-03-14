2c216a193abcf870d7f4f5d5802cf71330d437ff
Haselnussbomber <mail@haselnussbomber.de>
2025-03-14 13:14:15 +0100

    Fix ExcelSheet inheritance

--- FFXIVClientStructs/FFXIV/Common/Component/Excel/ExcelSheet.cs
+++ FFXIVClientStructs/FFXIV/Common/Component/Excel/ExcelSheet.cs
@@ -4,9 +4,10 @@ namespace FFXIVClientStructs.FFXIV.Common.Component.Excel;
 //   Common::Component::Excel::LinkList<Common::Component::Excel::ExcelSheet>
 //   Common::Component::Excel::ExcelSheetInterface
 [GenerateInterop]
-[Inherits<ExcelSheetInterface>(0x18)]
+[Inherits<ExcelSheetInterface>]
 [StructLayout(LayoutKind.Explicit, Size = 0x110)]
 public unsafe partial struct ExcelSheet {
+    [FieldOffset(0x8)] public ExcelModuleInterface.ExcelLanguage Language;
     [FieldOffset(0x10)] public StringPointer SheetName; // 32 Bytes
     [FieldOffset(0x20)] public uint RowCount;
     [FieldOffset(0x30)] public ColumnInfo* ColumnDefinitions;
@@ -15,6 +16,7 @@ public unsafe partial struct ExcelSheet {
 
     //[FieldOffset(0x68)] public LinkList<ExcelPage> ExcelPages;
 
+    [FieldOffset(0xC8)] public ExcelVariant Variant;
     [FieldOffset(0xCC)] public ushort ColumnCount;
 
     [FieldOffset(0xD0)] public ushort Version; //?
