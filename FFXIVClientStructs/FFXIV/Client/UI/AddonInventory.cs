a04ed49abc9b1efefb30b883be7134539dc456b0
Amber <AmberKortier@gmail.com>
2025-03-16 18:32:04 +0100

    InventoryNumberArray

--- FFXIVClientStructs/FFXIV/Client/UI/AddonInventory.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonInventory.cs
@@ -1,4 +1,5 @@
 using FFXIVClientStructs.FFXIV.Component.GUI;
+using static FFXIVClientStructs.FFXIV.Client.UI.AddonInventory.InventoryNumberArray.InventoryItemNumberArray.ItemDye;
 
 namespace FFXIVClientStructs.FFXIV.Client.UI;
 
@@ -16,4 +17,62 @@ public partial struct AddonInventory {
 
     [MemberFunction("E9 ?? ?? ?? ?? 83 FD 11")]
     public partial void SetTab(int tab);
+
+    [GenerateInterop]
+    [StructLayout(LayoutKind.Explicit, Size = 1351 * 4)]
+    public partial struct InventoryNumberArray {
+        [FieldOffset(0x00), FixedSizeArray] internal FixedSizeArray140<Pointer<InventoryItemNumberArray>> _items;
+
+        [GenerateInterop]
+        [StructLayout(LayoutKind.Explicit, Size = 6 * 4)]
+        public partial struct InventoryItemNumberArray {
+            [FieldOffset(0 * 4)] public uint IconId;
+            [FieldOffset(2 * 4)] internal int _stackCount;
+            [FieldOffset(3 * 4)] public ItemFlag ItemFlags;
+            [FieldOffset(4 * 4)] public ItemDye DyeSlot0;
+            [FieldOffset(5 * 4)] public ItemDye DyeSlot1;
+
+            public int StackCount => (_stackCount >> 16) & 0xFFFF;
+
+            [GenerateInterop]
+            [StructLayout(LayoutKind.Explicit, Size = 1 * 4)]
+            public partial struct ItemFlag {
+                [FieldOffset(0x0)] public ItemTypeFlag ItemType;
+                [FieldOffset(0x2)] public ItemWearableIcon Wearable;
+                [FieldOffset(0x3)] public ItemMirageFlag MirageFlag;
+
+                public enum ItemTypeFlag { 
+                    Gear = 1,
+                    Collectable = 16,
+                    Normal = 64
+                }
+
+                public enum ItemWearableIcon {
+                    Wearable = 1,
+                    Impossible = 2,
+                    Unavailable = 3
+                }
+
+                public enum ItemMirageFlag {
+                    IsGlamoured = 1,
+                    PartOfGearset = 2
+                }
+            }
+
+            [GenerateInterop]
+            [StructLayout(LayoutKind.Explicit, Size = 1 * 4)]
+            public partial struct ItemDye {
+                [FieldOffset(0x0)] public byte R;
+                [FieldOffset(0x1)] public byte G;
+                [FieldOffset(0x2)] public byte B;
+                [FieldOffset(0x3)] public ItemDyeFlags DyeFlags;
+
+                public enum ItemDyeFlags {
+                    CanBeDyed = 1,
+                    DyeLocked = 2,
+                    Dyed = 4
+                }
+            }
+        }
+    }
 }
--- FFXIVClientStructs/FFXIV/Client/UI/AddonPartyList.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonPartyList.cs
@@ -87,47 +87,47 @@ public unsafe partial struct AddonPartyList {
         [FieldOffset(7 * 4), FixedSizeArray] internal FixedSizeArray8<PartyListMemberNumberArray> _partyMembers;
         [FieldOffset(352 * 4), FixedSizeArray] internal FixedSizeArray7<PartyListMemberNumberArray> _trustMembers;
         [FieldOffset(653 * 4), FixedSizeArray] internal FixedSizeArray2<PartyListMemberNumberArray> _pets;
-    }
 
-    [GenerateInterop]
-    [StructLayout(LayoutKind.Explicit, Size = 43 * 4)]
-    public partial struct PartyListMemberNumberArray {
-        [FieldOffset(3 * 4)] public int Level;
-        [FieldOffset(4 * 4)] public int ClassIconId;
-        [FieldOffset(7 * 4)] public int CurrentHealth;
-        [FieldOffset(8 * 4)] public int MaxHealth;
-        /// <summary>
-        /// Goes from 0 to 200%
-        /// </summary>
-        [FieldOffset(9 * 4)] public int ShieldsPercentage;
-        [FieldOffset(10 * 4)] public int CurrentMana; 
-        [FieldOffset(11 * 4)] public int MaxMana;
-        /// <summary>
-        /// Goes from 0 to 100%
-        /// </summary>
-        [FieldOffset(13 * 4)] public int EnmityPercent;
-        /// <summary>
-        /// Starts at 1
-        /// <br/>[A]
-        /// <br/>[2]
-        /// <br/>[3]
-        /// <br/>...
-        /// <br/>[8]
-        /// </summary>
-        [FieldOffset(14 * 4)] public int EnmityLevel;
-        /// <summary>
-        /// Amount of Statuses applied to the player.
-        /// <br/>Max is 10.
-        /// </summary>
-        [FieldOffset(17 * 4)] public int StatusCount;
-        [FieldOffset(18 * 4), FixedSizeArray] internal FixedSizeArray10<int> _statusIconIds;
-        [FieldOffset(28 * 4), FixedSizeArray] internal FixedSizeArray10<bool> _statusIsDispellable;
-        /// <summary>
-        /// -1 if not active
-        /// </summary>
-        [FieldOffset(38 * 4)] public int CastTime;
-        [FieldOffset(39 * 4)] public int CastId;
-        [FieldOffset(41 * 4)] public int ContentId;
-        [FieldOffset(42 * 4)] public bool Targetable;
+        [GenerateInterop]
+        [StructLayout(LayoutKind.Explicit, Size = 43 * 4)]
+        public partial struct PartyListMemberNumberArray {
+            [FieldOffset(3 * 4)] public int Level;
+            [FieldOffset(4 * 4)] public int ClassIconId;
+            [FieldOffset(7 * 4)] public int CurrentHealth;
+            [FieldOffset(8 * 4)] public int MaxHealth;
+            /// <summary>
+            /// Goes from 0 to 200%
+            /// </summary>
+            [FieldOffset(9 * 4)] public int ShieldsPercentage;
+            [FieldOffset(10 * 4)] public int CurrentMana;
+            [FieldOffset(11 * 4)] public int MaxMana;
+            /// <summary>
+            /// Goes from 0 to 100%
+            /// </summary>
+            [FieldOffset(13 * 4)] public int EnmityPercent;
+            /// <summary>
+            /// Starts at 1
+            /// <br/>[A]
+            /// <br/>[2]
+            /// <br/>[3]
+            /// <br/>...
+            /// <br/>[8]
+            /// </summary>
+            [FieldOffset(14 * 4)] public int EnmityLevel;
+            /// <summary>
+            /// Amount of Statuses applied to the player.
+            /// <br/>Max is 10.
+            /// </summary>
+            [FieldOffset(17 * 4)] public int StatusCount;
+            [FieldOffset(18 * 4), FixedSizeArray] internal FixedSizeArray10<int> _statusIconIds;
+            [FieldOffset(28 * 4), FixedSizeArray] internal FixedSizeArray10<bool> _statusIsDispellable;
+            /// <summary>
+            /// -1 if not active
+            /// </summary>
+            [FieldOffset(38 * 4)] public int CastTime;
+            [FieldOffset(39 * 4)] public int CastId;
+            [FieldOffset(41 * 4)] public int ContentId;
+            [FieldOffset(42 * 4)] public bool Targetable;
+        }
     }
 }
