ca7a32efa6efcdf7e5dfb814df7caf6f8bf12a4f
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2025-03-22 14:54:54 +0100

    add option to rename image base in IDA

--- ida/ffxiv_fullrun_ida.py
+++ ida/ffxiv_fullrun_ida.py
@@ -2,6 +2,8 @@ from os import path
 import idautils
 import ida_auto
 import idc
+import ida_kerwin
+import idaapi
 
 print("Loading ffxiv_idarename.py")
 exec(open(path.join(path.dirname(path.realpath(__file__)), "ffxiv_idarename.py")).read())
@@ -11,6 +13,17 @@ exec(open(path.join(path.dirname(path.realpath(__file__)), "ffxiv_exdgetters.py"
 
 print("Loading ffxiv_structimporter.py")
 exec(open(path.join(path.dirname(path.realpath(__file__)), "ffxiv_structimporter.py")).read())
+
+ida_base = ida_kerwin.ask_buttons(
+    "ffxiv_dx11.exe",
+    "__ImageBase",
+    "",
+    ida_kernwin.ASKBTN_YES,
+    "HIDECANCEL\nWhich name would you prefer from base offset?\n\nffxiv_dx11.exe : CheatEngine and a few other utilities will use the offset of ffxiv_dx11.exe+ as the base.\n__ImageBase : This is the default IDA behavior.",
+) == ida_kernwin.ASKBTN_YES
+
+if (ida_base):
+    idc.set_name(idaapi.get_imagebase(), "ffxiv_dx11.exe")
 
 for s in idautils.Segments():
     start = idc.get_segm_start(s)
