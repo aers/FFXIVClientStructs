e86737efbac9e6f22e049a86dada5d124646c16b
Exter-N <exter-n@exter-n.fr>
2025-03-24 08:11:57 +0100

    Retype texture flags, add file header to TextureRH

--- FFXIVClientStructs/FFXIV/Client/System/Resource/Handle/TextureResourceHandle.cs
+++ FFXIVClientStructs/FFXIV/Client/System/Resource/Handle/TextureResourceHandle.cs
@@ -10,5 +10,25 @@ namespace FFXIVClientStructs.FFXIV.Client.System.Resource.Handle;
 [Inherits<ResourceHandle>]
 [StructLayout(LayoutKind.Explicit, Size = 0x140)]
 public unsafe partial struct TextureResourceHandle {
+    [FieldOffset(0xB0)] public TexFileHeader Header;
     [FieldOffset(0x118)] public Texture* Texture;
+    /// <remarks> Only known valid during <see cref="ResourceHandle.Load"/>. </remarks>
+    [FieldOffset(0x120)] public Texture* TextureWhileLoading;
+    /// <remarks> Only known valid during <see cref="ResourceHandle.Load"/>. </remarks>
+    [FieldOffset(0x138)] public void* DataWhileLoading;
+
+    // From Lumina.Data.Files.TexFile.TexHeader.
+    [GenerateInterop]
+    [StructLayout(LayoutKind.Explicit, Size = 0x50)]
+    public partial struct TexFileHeader {
+        [FieldOffset(0)] public TextureFlags Type;
+        [FieldOffset(0x4)] public TextureFormat Format;
+        [FieldOffset(0x8)] public ushort Width;
+        [FieldOffset(0xA)] public ushort Height;
+        [FieldOffset(0xC)] public ushort Depth;
+        [FieldOffset(0xE)] public byte MipCountAndFlag;
+        [FieldOffset(0xF)] public byte ArraySize;
+        [FieldOffset(0x10)] private FixedSizeArray3<uint> _lodOffsets;
+        [FieldOffset(0x1C)] private FixedSizeArray13<uint> _offsetsToSurfaces;
+    }
 }
