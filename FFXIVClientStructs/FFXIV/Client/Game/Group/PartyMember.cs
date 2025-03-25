0d1f79e9f91a903e9007914b2d7a4fb2ceb9e23c
Haselnussbomber <mail@haselnussbomber.de>
2025-03-25 21:38:20 +0100

    Update GroupManager, PartyMember

--- FFXIVClientStructs/FFXIV/Client/Game/Group/PartyMember.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Group/PartyMember.cs
@@ -4,41 +4,41 @@ using FFXIVClientStructs.FFXIV.Client.System.String;
 namespace FFXIVClientStructs.FFXIV.Client.Game.Group;
 
 [GenerateInterop]
-[StructLayout(LayoutKind.Explicit, Size = 0x3A0)]
+[StructLayout(LayoutKind.Explicit, Size = 0x490)]
 public unsafe partial struct PartyMember {
     [FieldOffset(0x0)] public StatusManager StatusManager;
-    [FieldOffset(0x2F0)] public float X;
-    [FieldOffset(0x2F4)] public float Y;
-    [FieldOffset(0x2F8)] public float Z;
+    [FieldOffset(0x3E0)] public float X;
+    [FieldOffset(0x3E4)] public float Y;
+    [FieldOffset(0x3E8)] public float Z;
 
     [Obsolete("Renamed to AccountId", true)]
-    [FieldOffset(0x300)] public ulong Unk300; // content id for anonymous players?
-    [FieldOffset(0x300)] public ulong AccountId;
+    [FieldOffset(0x3F0)] public ulong Unk300; // content id for anonymous players?
+    [FieldOffset(0x3F0)] public ulong AccountId;
 
-    [FieldOffset(0x308)] public ulong ContentId;
-    [FieldOffset(0x310)] public uint EntityId;
-    [FieldOffset(0x314)] public uint PetEntityId; // carbuncle, etc
-    [FieldOffset(0x318)] public uint CompanionEntityId; // chocobo
-    [FieldOffset(0x31C)] public uint CurrentHP;
-    [FieldOffset(0x320)] public uint MaxHP;
-    [FieldOffset(0x324)] public ushort CurrentMP;
-    [FieldOffset(0x326)] public ushort MaxMP;
-    [FieldOffset(0x328)] public ushort TerritoryType;
-    [FieldOffset(0x32A)] public ushort HomeWorld;
+    [FieldOffset(0x3F8)] public ulong ContentId;
+    [FieldOffset(0x400)] public uint EntityId;
+    [FieldOffset(0x404)] public uint PetEntityId; // carbuncle, etc
+    [FieldOffset(0x408)] public uint CompanionEntityId; // chocobo
+    [FieldOffset(0x40C)] public uint CurrentHP;
+    [FieldOffset(0x410)] public uint MaxHP;
+    [FieldOffset(0x414)] public ushort CurrentMP;
+    [FieldOffset(0x416)] public ushort MaxMP;
+    [FieldOffset(0x418)] public ushort TerritoryType;
+    [FieldOffset(0x41A)] public ushort HomeWorld;
     // GroupManager::GetPartyMemberName: "E8 ? ? ? ? 4C 8B C5 4C 2B C0" or "48 8B 81 ? ? ? ? 48 85 C0 74 ? 48 8B C8 E9"
-    [FieldOffset(0x32C), FixedSizeArray(isString: true)] internal FixedSizeArray64<byte> _name;
+    [FieldOffset(0x41C), FixedSizeArray(isString: true)] internal FixedSizeArray64<byte> _name;
 
     [Obsolete("Renamed to NameOverride", true)]
-    [FieldOffset(0x370)] public Utf8String* UnkName;
-    [FieldOffset(0x370)] public Utf8String* NameOverride; // if non-null, replaces real name in ui (eg for blacklisted players)
+    [FieldOffset(0x460)] public Utf8String* UnkName;
+    [FieldOffset(0x460)] public Utf8String* NameOverride; // if non-null, replaces real name in ui (eg for blacklisted players)
 
-    [FieldOffset(0x378)] public byte Sex;
-    [FieldOffset(0x379)] public byte ClassJob;
-    [FieldOffset(0x37A)] public byte Level;
-    [FieldOffset(0x37B)] public byte DamageShield;
+    [FieldOffset(0x468)] public byte Sex;
+    [FieldOffset(0x469)] public byte ClassJob;
+    [FieldOffset(0x46A)] public byte Level;
+    [FieldOffset(0x46B)] public byte DamageShield;
     /// <remarks> For easier access, use <see cref="GetContentValue"/>. </remarks>
-    [FieldOffset(0x37C), FixedSizeArray] internal FixedSizeArray3<StdPair<uint, uint>> _contentKeyValueData;
-    [FieldOffset(0x394)] public byte Flags; // 0x01 == set for valid alliance members, 0x04 == set if XYZ is valid?, 0x10 == in cutscene
+    [FieldOffset(0x46C), FixedSizeArray] internal FixedSizeArray3<StdPair<uint, uint>> _contentKeyValueData;
+    [FieldOffset(0x484)] public byte Flags; // 0x01 == set for valid alliance members, 0x04 == set if XYZ is valid?, 0x10 == in cutscene
 
     /// <inheritdoc cref="PlayerState.GetContentValue"/>
     public uint GetContentValue(uint key) {
