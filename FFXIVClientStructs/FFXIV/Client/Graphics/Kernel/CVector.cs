27858b10c10031640caf20a5ecdb8e7d614f88ef
Exter-N <exter-n@exter-n.fr>
2025-03-27 23:14:13 +0100

    Actually fix `CVector<T>`

--- FFXIVClientStructs/FFXIV/Client/Graphics/Kernel/CVector.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Kernel/CVector.cs
@@ -3,7 +3,8 @@ namespace FFXIVClientStructs.FFXIV.Client.Graphics.Kernel;
 // Client::Graphics::Kernel::CVector
 //   std::vector
 // this class inherits std::vector but adds a virtual function, so its just std::vector with a vtbl
-[StructLayout(LayoutKind.Explicit, Size = 0x20)]
-public struct CVector<T> where T : unmanaged {
-    [FieldOffset(8)] public StdVector<T> Vector;
+[StructLayout(LayoutKind.Sequential)]
+public unsafe struct CVector<T> where T : unmanaged {
+    private void* _vtable;
+    public StdVector<T> Vector;
 }
