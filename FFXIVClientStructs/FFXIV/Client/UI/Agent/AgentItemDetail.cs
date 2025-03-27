d49ca90ab0cc2245df61afec75d020bf93f6bd94
Infi <infiziert@protonmail.ch>
2025-03-27 15:56:54 +0100

    Update AgentItemDetail

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentItemDetail.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentItemDetail.cs
@@ -3,8 +3,9 @@ using FFXIVClientStructs.FFXIV.Client.System.String;
 namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
 
 public enum ItemDetailKind : byte {
-    ChatItem = 1,      // all items linked in chat, except event items
+    ChatItem = 1,      // all items linked in chat, except event items, also used for some shops
     InventoryItem = 2, // all(?) items outside of chat, including event items
+    ShopItem = 6,      // most shops use this, exceptions are grand company and sundry splendors
     ChatEventItem = 8  // event items linked in chat
 }
 
@@ -32,10 +33,15 @@ public unsafe partial struct AgentItemDetail {
     // - 71: PremiumSaddleBag1
     // - 72: PremiumSaddleBag2
     [FieldOffset(0x11C)] public uint TypeOrId;
-    [FieldOffset(0x120)] public uint Index; // The index of the item in the inventory. 0 for chat, ??? for KeyItems.
+    [FieldOffset(0x120)] public uint Index; // The index of the item in the inventory. 0 for chat, ??? for KeyItems, position top-bottom inside a shop.
+    [FieldOffset(0x124)] public int BuyQuantity; // The quantity selected for buying, which is previewed on the tooltip as "in bag" quantity, -1 outside shops
     [FieldOffset(0x128)] public byte Flag1; // Related to checking the inventory
+    [FieldOffset(0x134)] public ushort MaxStackSize;
     [FieldOffset(0x138)] public uint ItemId;
     [FieldOffset(0x148)] public Utf8String String1;
-    [FieldOffset(0x1B2)] public byte Flag2; // This needs to be set to 1 for the item detail tooltip to show
-    [FieldOffset(0x1B6)] public byte Flag3; // If set to zero, avoids an early return in addon->Show()
+    [FieldOffset(0x1B0)] public Utf8String String2;
+    [FieldOffset(0x218)] public byte Flag2; // This needs to be set to 1 for the item detail tooltip to show
+    [FieldOffset(0x219)] public byte Flag3; // If set to zero, avoids an early return in addon->Show()
+    [FieldOffset(0x21A)] public byte Flag4; 
+    [FieldOffset(0x21B)] public byte Flag5; // Set to 1 for some gear and all HQ items
 }
