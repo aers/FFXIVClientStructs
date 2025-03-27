39f946556c2b32d45d2bc4632ffe3faa5dcd558b
Haselnussbomber <mail@haselnussbomber.de>
2025-03-27 20:13:18 +0100

    Fix AgentTelepotTown

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentTelepotTown.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentTelepotTown.cs
@@ -10,15 +10,15 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
 public unsafe partial struct AgentTelepotTown {
     [FieldOffset(0x28)] public AgentTelepotTownData* Data;
 
-    [MemberFunction("48 89 5C 24 ?? 57 48 83 EC 50 0F B6 FA")]
+    [MemberFunction("E9 ?? ?? ?? ?? 83 F9 01 0F 85 ?? ?? ?? ?? 48 8B 4B 28")]
     public partial void TeleportToAetheryte(byte index);
 }
 
-[StructLayout(LayoutKind.Explicit, Size = 0xD168)]
+[StructLayout(LayoutKind.Explicit, Size = 0xDE20)]
 public struct AgentTelepotTownData {
     [FieldOffset(0x4)] public byte CurrentAetheryte; // the one you're standing at
 
-    [FieldOffset(0x70A)] public byte SelectedAetheryte;
+    [FieldOffset(0x70E)] public byte SelectedAetheryte;
 
-    [FieldOffset(0x70C)] public byte Flags;
+    [FieldOffset(0x710)] public byte Flags;
 }
