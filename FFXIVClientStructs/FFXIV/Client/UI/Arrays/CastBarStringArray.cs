132cbdcb83e3521d17858bf425a11269b0086610
Amber Kortier <AmberKortier@gmail.com>
2025-03-27 12:34:10 +0100

    CStringPointer

--- FFXIVClientStructs/FFXIV/Client/UI/Arrays/CastBarStringArray.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Arrays/CastBarStringArray.cs
@@ -8,7 +8,7 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Arrays;
 public unsafe partial struct CastBarStringArray {
     public static CastBarStringArray* Instance() => (CastBarStringArray*)AtkStage.Instance()->GetStringArrayData(StringArrayType.CastBar)->StringArray;
 
-    [FieldOffset(0), FixedSizeArray, CExportIgnore] internal FixedSizeArray1<StringPointer> _data;
+    [FieldOffset(0), FixedSizeArray, CExportIgnore] internal FixedSizeArray1<CStringPointer> _data;
 
     [FieldOffset(0 * 8)] public CStringPointer CastName;
 }
