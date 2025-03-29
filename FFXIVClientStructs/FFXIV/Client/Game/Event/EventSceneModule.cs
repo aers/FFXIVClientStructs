5ef0cfe2f1e543b8ea5ff437309e569b3c4adbf4
Asgard <95163444+AsgardXIV@users.noreply.github.com>
2025-03-29 01:18:40 -0600

    Fix EventGPoseController offset in EventSceneModule

--- FFXIVClientStructs/FFXIV/Client/Game/Event/EventSceneModule.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Event/EventSceneModule.cs
@@ -7,5 +7,5 @@ public unsafe struct EventSceneModule {
     [FieldOffset(0x28)] public EventSceneModuleImplBase EventSceneModuleImplBase;
     [FieldOffset(0x38)] public EventSceneModuleImplBase* EventSceneModuleImpl;
     [FieldOffset(0x98)] public EventSceneModuleTaskManager TaskManager;
-    [FieldOffset(0x1F0)] public EventGPoseController EventGPoseController;
+    [FieldOffset(0x200)] public EventGPoseController EventGPoseController;
 }
