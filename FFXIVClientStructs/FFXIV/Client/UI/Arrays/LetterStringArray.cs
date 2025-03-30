b41f215a68e76fd93054b0ae0263254eef41e083
Amber <78602618+Glyceri@users.noreply.github.com>
2025-03-31 00:05:09 +0200

    Update FFXIVClientStructs/FFXIV/Client/UI/Arrays/LetterStringArray.cs
    Co-authored-by: Haselnussbomber <mail@haselnussbomber.de>

--- FFXIVClientStructs/FFXIV/Client/UI/Arrays/LetterStringArray.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Arrays/LetterStringArray.cs
@@ -13,17 +13,17 @@ public unsafe partial struct LetterStringArray {
     /// <summary>
     /// Ordered based on date received. It doesn't care about which tab.
     /// </summary>
-    [FieldOffset(0 * 8), FixedSizeArray, CExportIgnore] internal FixedSizeArray130<CStringPointer> _mailSenders;
+    [FieldOffset(0 * 8), FixedSizeArray] internal FixedSizeArray130<CStringPointer> _mailSenders;
 
     /// <summary>
     /// Ordered based on date received. It doesn't care about which tab.
     /// </summary>
-    [FieldOffset(130 * 8), FixedSizeArray, CExportIgnore] internal FixedSizeArray130<CStringPointer> _timesPassed;
+    [FieldOffset(130 * 8), FixedSizeArray] internal FixedSizeArray130<CStringPointer> _timesPassed;
 
     /// <summary>
     /// Ordered based on date received. It doesn't care about which tab.
     /// </summary>
-    [FieldOffset(260 * 8), FixedSizeArray, CExportIgnore] internal FixedSizeArray130<CStringPointer> _mailTextPreviews;
+    [FieldOffset(260 * 8), FixedSizeArray] internal FixedSizeArray130<CStringPointer> _mailTextPreviews;
 
     [FieldOffset(391 * 8)] public CStringPointer FullMailText;
     [FieldOffset(392 * 8)] public CStringPointer UnclaimedGoodsCountText;
