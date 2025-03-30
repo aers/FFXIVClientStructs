a7b21053c8bd69becf6ca26022005288474c8fdb
Amber <78602618+Glyceri@users.noreply.github.com>
2025-03-31 00:03:57 +0200

    Update FFXIVClientStructs/FFXIV/Client/UI/Arrays/FreeCompanyMemberStringArray.cs
    Co-authored-by: Haselnussbomber <mail@haselnussbomber.de>

--- FFXIVClientStructs/FFXIV/Client/UI/Arrays/FreeCompanyMemberStringArray.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Arrays/FreeCompanyMemberStringArray.cs
@@ -9,11 +9,11 @@ public unsafe partial struct FreeCompanyMemberStringArray {
 
     [FieldOffset(0 * 8), FixedSizeArray, CExportIgnore] internal FixedSizeArray1401<CStringPointer> _data;
 
-    [FieldOffset(0 * 8), FixedSizeArray, CExportIgnore] internal FixedSizeArray200<SocialListMemberStringArray> _members;
+    [FieldOffset(0 * 8), FixedSizeArray] internal FixedSizeArray200<SocialListMemberStringArray> _members;
 
     [FieldOffset(1000 * 8)] public CStringPointer YourMemo;
 
-    [FieldOffset(1001 * 8), FixedSizeArray, CExportIgnore] internal FixedSizeArray200<FreeCompanyMemoStringArray> _memos;
+    [FieldOffset(1001 * 8), FixedSizeArray] internal FixedSizeArray200<FreeCompanyMemoStringArray> _memos;
 
     [CExportIgnore]
     [StructLayout(LayoutKind.Explicit, Size = 2 * 8)]
