cc348e3de0d3215045c09e5faaa66149fe8afe23
Amber <AmberKortier@gmail.com>
2025-03-31 14:06:07 +0200

    Couple Fixes

--- FFXIVClientStructs/FFXIV/Client/UI/Arrays/BlackListNumberArray.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Arrays/BlackListNumberArray.cs
@@ -5,8 +5,8 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Arrays;
 [CExportIgnore]
 [GenerateInterop]
 [StructLayout(LayoutKind.Explicit, Size = 8 * 4)]
-public unsafe partial struct BlackListNumberArrayArray {
-    public static BlackListNumberArrayArray* Instance() => (BlackListNumberArrayArray*)AtkStage.Instance()->GetNumberArrayData(NumberArrayType.BlackList)->IntArray;
+public unsafe partial struct BlackListNumberArray {
+    public static BlackListNumberArray* Instance() => (BlackListNumberArray*)AtkStage.Instance()->GetNumberArrayData(NumberArrayType.BlackList)->IntArray;
 
     [FieldOffset(0), FixedSizeArray, CExportIgnore] internal FixedSizeArray8<int> _data;
 
