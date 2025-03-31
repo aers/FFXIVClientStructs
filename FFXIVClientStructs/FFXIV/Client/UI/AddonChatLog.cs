4df7a587c5ba6c143cdd2e984be0e448ded043d6
Caraxi <git@caraxian.com>
2025-03-31 22:47:17 +1030

    Update AddonChatLog.cs

--- FFXIVClientStructs/FFXIV/Client/UI/AddonChatLog.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonChatLog.cs
@@ -15,4 +15,7 @@ public unsafe partial struct AddonChatLog {
     [FieldOffset(0x2AD)] public byte TabCount;
 
     [FieldOffset(0x2B0), FixedSizeArray] internal FixedSizeArray5<Utf8String> _tabNames;
+
+    [MemberFunction("E8 ?? ?? ?? ?? 48 8B CF 84 C0 74 07")]
+    public partial bool IsZoomed();
 }
