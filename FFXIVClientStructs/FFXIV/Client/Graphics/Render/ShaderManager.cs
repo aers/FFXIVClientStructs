32227f5a839dc75f50be0fe114e12f755834c2ae
Haselnussbomber <mail@haselnussbomber.de>
2025-04-16 00:26:53 +0200

    Add ShaderPackageResourceHandles to ShaderManager

--- FFXIVClientStructs/FFXIV/Client/Graphics/Render/ShaderManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Render/ShaderManager.cs
@@ -1,5 +1,12 @@
+using FFXIVClientStructs.FFXIV.Client.System.Resource.Handle;
+
 namespace FFXIVClientStructs.FFXIV.Client.Graphics.Render;
 
 // Client::Graphics::Render::ShaderManager
+[GenerateInterop]
 [StructLayout(LayoutKind.Explicit, Size = 0x1D0)]
-public struct ShaderManager;
+public unsafe partial struct ShaderManager {
+    // This is technically not an array, but we want to use it in Dalamud to show loaded Shader Packages.
+    [FieldOffset(0x08), FixedSizeArray] internal FixedSizeArray56<Pointer<ShaderPackageResourceHandle>> _shaderPackageResourceHandles;
+    [FieldOffset(0x1C8)] private ShaderCodeResourceHandle* CopyEnvLocationShaderCodeResourceHandle;
+}
