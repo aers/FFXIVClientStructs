5e7c5271b5cb5187547b14e1f6b7a97b6098b6cd
pohky <otoriosu@gmail.com>
2025-04-22 12:20:30 +0200

    Update SharedGroupResourceHandle.cs

--- FFXIVClientStructs/FFXIV/Client/System/Resource/Handle/SharedGroupResourceHandle.cs
+++ FFXIVClientStructs/FFXIV/Client/System/Resource/Handle/SharedGroupResourceHandle.cs
@@ -2,7 +2,7 @@ namespace FFXIVClientStructs.FFXIV.Client.System.Resource.Handle;
 
 [GenerateInterop]
 [Inherits<ResourceHandle>]
-[StructLayout(LayoutKind.Explicit, Size = 0xB8)]
+[StructLayout(LayoutKind.Explicit, Size = 0xC0)]
 public unsafe partial struct SharedGroupResourceHandle {
     [FieldOffset(0xB0)] public byte* SceneChunk;
 }
