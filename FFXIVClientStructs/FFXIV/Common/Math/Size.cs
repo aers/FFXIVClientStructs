c53fc88e6723ce828339d38d36848edc2068760a
Haselnussbomber <mail@haselnussbomber.de>
2025-05-14 11:55:31 +0200

    Add implicit Vector2 casts to Size

--- FFXIVClientStructs/FFXIV/Common/Math/Size.cs
+++ FFXIVClientStructs/FFXIV/Common/Math/Size.cs
@@ -6,6 +6,9 @@ public struct Size : IEquatable<Size>, IComparable<Size> {
     [FieldOffset(0x0)] public int Width;
     [FieldOffset(0x4)] public int Height;
 
+    public static implicit operator System.Numerics.Vector2(Size size) => new(size.Width, size.Height);
+    public static implicit operator Vector2(Size size) => new(size.Width, size.Height);
+
     public bool Equals(Size other) => Value == other.Value;
     public override bool Equals(object? obj) => obj is Size other && Equals(other);
     public override int GetHashCode() => Value.GetHashCode();
