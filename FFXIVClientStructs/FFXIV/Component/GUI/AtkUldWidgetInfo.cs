609b1f4efc383600b828437afe550f5f7b897e33
Haselnussbomber <mail@haselnussbomber.de>
2025-05-26 17:35:00 +0200

    Add AtkWidgetAlignment

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkUldWidgetInfo.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkUldWidgetInfo.cs
@@ -4,19 +4,27 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI;
 [Inherits<AtkUldObjectInfo>]
 [StructLayout(LayoutKind.Explicit, Size = 0x20)]
 public partial struct AtkUldWidgetInfo {
-    [FieldOffset(0x10)] public uint AlignmentType; // TODO: use AlignmentType enum
-    [FieldOffset(0x14)] public float X;
-    [FieldOffset(0x18)] public float Y;
+    [FieldOffset(0x10)] public AtkWidgetAlignment WidgetAlignment;
+    [FieldOffset(0x10), Obsolete("Use WidgetAlignment.AlignmentType")] public uint AlignmentType;
+    [FieldOffset(0x14), Obsolete("Use WidgetAlignment.X")] public float X;
+    [FieldOffset(0x18), Obsolete("Use WidgetAlignment.Y")] public float Y;
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0x10)]
+public struct AtkWidgetAlignment {
+    [FieldOffset(0x00)] public AlignmentType AlignmentType;
+    [FieldOffset(0x04)] public float X;
+    [FieldOffset(0x08)] public float Y;
 }
 
 public enum AlignmentType {
-    TopLeft = 0x0,
-    Top = 0x1,
-    TopRight = 0x2,
-    Left = 0x3,
-    Center = 0x4,
-    Right = 0x5,
-    BottomLeft = 0x6,
-    Bottom = 0x7,
-    BottomRight = 0x8
+    TopLeft,
+    Top,
+    TopRight,
+    Left,
+    Center,
+    Right,
+    BottomLeft,
+    Bottom,
+    BottomRight
 }
