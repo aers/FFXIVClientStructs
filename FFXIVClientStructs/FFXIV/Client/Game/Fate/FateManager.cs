29ef724cbcfe1b628d3bb79bf32bc023dfe32a1d
pohky <otoriosu@gmail.com>
2025-06-04 04:02:49 +0200

    Update FateManager.cs

--- FFXIVClientStructs/FFXIV/Client/Game/Fate/FateManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Fate/FateManager.cs
@@ -16,7 +16,7 @@ public unsafe partial struct FateManager {
     [FieldOffset(0x88)] public FateContext* CurrentFate;
     [FieldOffset(0x90)] public StdVector<Pointer<FateContext>> Fates;
     [FieldOffset(0xA8)] public ushort SyncedFateId;
-    [FieldOffset(0xAC)] public byte FateJoined;
+    [FieldOffset(0xAF)] public byte FateJoined;
 
     [MemberFunction("E8 ?? ?? ?? ?? 48 85 C0 48 8D 4C 24 ?? 0F 95 C2")]
     public partial FateContext* GetFateById(ushort fateId);
