4efc26c760fb9d8053f21413cf1537ade83aabe2
MidoriKami <mi_kami@outlook.com>
2025-07-01 08:29:02 -0700

    AtkComponentNumericInput fields

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentNumericInput.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentNumericInput.cs
@@ -12,6 +12,9 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI;
 public unsafe partial struct AtkComponentNumericInput : ICreatable {
     [FieldOffset(0x1F8)] public int Value; // Found through SetValue function
     [FieldOffset(0x2F8)] public AtkUldComponentDataNumericInput Data;
+    [FieldOffset(0x320)] public int Min;
+    [FieldOffset(0x324)] public int Max;
+    [FieldOffset(0x328)] public int Add;
 
     [MemberFunction("48 89 5C 24 ?? 48 89 74 24 ?? 57 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 84 24 ?? ?? ?? ?? 48 8B F1 E8 ?? ?? ?? ?? 48 8D 05 ?? ?? ?? ?? 48 C7 86 ?? ?? ?? ?? ?? ?? ?? ??")]
     public partial void Ctor();
