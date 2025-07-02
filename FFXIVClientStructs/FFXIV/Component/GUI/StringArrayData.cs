90f974aaff955fb3d51bf4e97b7a7252065c3f7f
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2025-07-02 10:32:49 +0200

    Add todo for 7.3

--- FFXIVClientStructs/FFXIV/Component/GUI/StringArrayData.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/StringArrayData.cs
@@ -6,7 +6,7 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI;
 [Inherits<AtkArrayData>]
 [StructLayout(LayoutKind.Explicit, Size = 0x38)]
 public unsafe partial struct StringArrayData {
-    [FieldOffset(0x28)] public byte** StringArray;
+    [FieldOffset(0x28)] public byte** StringArray; // TODO: change this to CStringPointer*
     [FieldOffset(0x30)] public byte** ManagedStringArray;
 
     /// <summary>
