b31f959482ebce8c14120a89c707e2e2529da4e2
Exter-N <exter-n@exter-n.fr>
2025-07-05 03:47:55 +0200

    Add Human skin material array and resolving vfunc

--- FFXIVClientStructs/FFXIV/Client/Graphics/Scene/Human.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Scene/Human.cs
@@ -47,6 +47,13 @@ public unsafe partial struct Human {
     [FieldOffset(0xB38)] public TextureResourceHandle* RFingerDecal;
     [FieldOffset(0xB40)] public TextureResourceHandle* LFingerDecal;
 
+    [FieldOffset(0xB48), CExportIgnore] private nint _slotSkinMaterialBase;
+    [FieldOffset(0xB48)] public MaterialResourceHandle* HeadSkinMaterial;
+    [FieldOffset(0xB50)] public MaterialResourceHandle* TopSkinMaterial;
+    [FieldOffset(0xB58)] public MaterialResourceHandle* ArmsSkinMaterial;
+    [FieldOffset(0xB60)] public MaterialResourceHandle* LegsSkinMaterial;
+    [FieldOffset(0xB68)] public MaterialResourceHandle* FeetSkinMaterial;
+
 
     public ref TextureResourceHandle* SlotDecal(int slot) {
         if (slot is < 0 or > 9)
@@ -54,8 +61,17 @@ public unsafe partial struct Human {
         return ref ((TextureResourceHandle**)Unsafe.AsPointer(ref _slotDecalBase))[slot];
     }
 
+    public ref MaterialResourceHandle* SlotSkinMaterial(int slot) {
+        if (slot is < 0 or > 4)
+            throw new ArgumentOutOfRangeException(nameof(slot));
+        return ref ((MaterialResourceHandle**)Unsafe.AsPointer(ref _slotSkinMaterialBase))[slot];
+    }
+
     public Span<Pointer<TextureResourceHandle>> SlotDecalsSpan
         => new(Unsafe.AsPointer(ref _slotDecalBase), 10);
+
+    public Span<Pointer<MaterialResourceHandle>> SlotSkinMaterialsSpan
+        => new(Unsafe.AsPointer(ref _slotSkinMaterialBase), 5);
 
     [FieldOffset(0xBF0)] public ConstantBuffer* CustomizeParameterCBuffer;
     [FieldOffset(0xBF8)] public ConstantBuffer* DecalColorCBuffer;
