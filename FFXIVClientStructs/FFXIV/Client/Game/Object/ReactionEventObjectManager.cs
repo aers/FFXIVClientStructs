e5115bc668c385de7d8ce78292b888fac37b1c33
Haselnussbomber <mail@haselnussbomber.de>
2025-07-12 22:20:38 +0200

    Fix ReactionEventObjectManager size

--- FFXIVClientStructs/FFXIV/Client/Game/Object/ReactionEventObjectManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Object/ReactionEventObjectManager.cs
@@ -3,7 +3,7 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Object;
 // Client::Game::Object::ReactionEventObjectManager
 // Gatherables/Farm in Island Sanctuary
 [GenerateInterop]
-[StructLayout(LayoutKind.Explicit, Size = 0x08)]
+[StructLayout(LayoutKind.Explicit, Size = 0x328)]
 public unsafe partial struct ReactionEventObjectManager {
     [StaticAddress("48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 33 C9", 3)]
     public static partial ReactionEventObjectManager* Instance();
