4855a0fca2885e48aa5c254bc40d36c76413da15
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2025-07-25 14:45:52 +0200

    Update thread

--- FFXIVClientStructs/FFXIV/Client/System/Threading/Thread.cs
+++ FFXIVClientStructs/FFXIV/Client/System/Threading/Thread.cs
@@ -4,10 +4,29 @@ namespace FFXIVClientStructs.FFXIV.Client.System.Threading;
 //   Client::System::Common::NonCopyable
 [GenerateInterop(isInherited: true)]
 [StructLayout(LayoutKind.Explicit, Size = 0x28)]
-public unsafe partial struct Thread {
-    [FieldOffset(0x00)] public byte** args;
+public unsafe partial struct Thread
+{
     [FieldOffset(0x08)] public nint EventHandle;
     [FieldOffset(0x10)] public nint ThreadHandle;
     [FieldOffset(0x18)] public int ThreadId;
     [FieldOffset(0x1C)] public int AffinityMask;
+    [FieldOffset(0x20)] public bool HasRequestedStop;
+
+    [VirtualFunction(0)]
+    public partial Thread* Dtor(byte flags);
+
+    [VirtualFunction(1)]
+    public partial void RequestStop();
+
+    // [VirtualFunction(2)]
+    // public partial bool vf2(); // mov al, 1
+    
+    // [VirtualFunction(3)]
+    // public partial int vf3(); // xor eax, eax
+
+    // [VirtualFunction(4)]
+    // public partial ? vf4(); // nullsub
+
+    [VirtualFunction(5)]
+    public partial void Run();
 }
