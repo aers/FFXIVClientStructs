67734537cde03c76aba0e3cc3319c731f33fefb3
Haselnussbomber <mail@haselnussbomber.de>
2025-07-29 17:32:09 +0200

    Update GameWindow

--- FFXIVClientStructs/FFXIV/Client/System/Framework/GameWindow.cs
+++ FFXIVClientStructs/FFXIV/Client/System/Framework/GameWindow.cs
@@ -7,8 +7,8 @@ public unsafe partial struct GameWindow {
     [StaticAddress("89 15 ?? ?? ?? ?? 48 F7 E1", 2)]
     public static partial GameWindow* Instance();
 
-    [FieldOffset(0x00)] public ulong ArgumentCount; // TODO: (u)int
-    [FieldOffset(0x08)] public byte** Arguments; // Points to an array that points to CStr // TODO: use CStringPointer, add Span
+    [FieldOffset(0x00)] public int ArgumentCount;
+    [FieldOffset(0x08)] public CStringPointer* Arguments;
     [FieldOffset(0x10)] public float FrameDeltaTime;
     [FieldOffset(0x18)] public nint WindowHandle;
     [FieldOffset(0x20)] public int WindowWidth; // Only used and correct if in Windowed Mode
@@ -20,5 +20,5 @@ public unsafe partial struct GameWindow {
     [FieldOffset(0x58)] public int MinWidth;
     [FieldOffset(0x5C)] public int MinHeight;
 
-    public string GetArgument(ulong idx) => Marshal.PtrToStringUTF8(idx >= ArgumentCount ? nint.Zero : (nint)Arguments[idx]) ?? string.Empty;
+    public Span<CStringPointer> ArgumentsSpan => new(Arguments, ArgumentCount);
 }
