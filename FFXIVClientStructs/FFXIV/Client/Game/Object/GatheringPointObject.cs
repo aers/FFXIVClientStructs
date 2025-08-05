b6fe8c00b2ff6b5c3eebcf9481a99a44211ae9aa
MidoriKami <mi_kami@outlook.com>
2025-08-05 16:22:33 -0700

    Update children

--- FFXIVClientStructs/FFXIV/Client/Game/Object/GatheringPointObject.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Object/GatheringPointObject.cs
@@ -4,10 +4,10 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Object;
 
 [GenerateInterop]
 [Inherits<GameObject>]
-[StructLayout(LayoutKind.Explicit, Size = 0x1E0)]
+[StructLayout(LayoutKind.Explicit, Size = 0x1F0)]
 public unsafe partial struct GatheringPointObject {
-    [FieldOffset(0x190)] public GatheringPointObjectImplBase ObjectImplBase;
-    [FieldOffset(0x1B0)] public GatheringPointObjectImpl ObjectImpl;
+    [FieldOffset(0x1A0)] public GatheringPointObjectImplBase ObjectImplBase;
+    [FieldOffset(0x1C0)] public GatheringPointObjectImpl ObjectImpl;
     [FieldOffset(0x1D0)] public GatheringPointObjectImplBase* Impl;
 
     [GenerateInterop(isInherited: true)]
