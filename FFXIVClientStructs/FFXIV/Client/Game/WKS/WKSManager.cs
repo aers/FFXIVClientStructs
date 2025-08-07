f0fd4f67260cb3a345e3d4b881de836e6ba9d72b
pohky <otoriosu@gmail.com>
2025-08-07 11:35:58 +0200

    Update WKSManager.cs

--- FFXIVClientStructs/FFXIV/Client/Game/WKS/WKSManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/WKS/WKSManager.cs
@@ -15,31 +15,30 @@ public unsafe partial struct WKSManager {
     [FieldOffset(0x18)] public ushort TerritoryId;
 
     /// <remarks> RowId of WKSDevGrade sheet. </remarks>
-    [FieldOffset(0x4A)] public ushort DevGrade;
+    [FieldOffset(0x52)] public ushort DevGrade;
 
     /// <remarks> For Hub upgrades. RowId of WKSFateControl sheet. </remarks>
-    [FieldOffset(0x50)] public ushort CurrentFateControlRowId;
+    [FieldOffset(0x54)] public ushort CurrentFateControlRowId;
     /// <remarks> For Hub upgrades. Id of Fate in FateManager. </remarks>
-    [FieldOffset(0x52)] public ushort CurrentFateId;
+    [FieldOffset(0x58)] public ushort CurrentFateId;
 
     /// <remarks> RowId of WKSMissionUnit sheet. </remarks>
-    [FieldOffset(0xC60)] public ushort CurrentMissionUnitRowId;
+    [FieldOffset(0xC10)] public ushort CurrentMissionUnitRowId;
 
-    [FieldOffset(0xC9C)] public uint FishingBait;
+    [FieldOffset(0xC4C)] public uint FishingBait;
 
-    [FieldOffset(0xD34), FixedSizeArray] internal FixedSizeArray11<int> _scores; // cosmic class scores
+    [FieldOffset(0xD68), FixedSizeArray] internal FixedSizeArray11<int> _scores; // cosmic class scores
 
-    [FieldOffset(0xDB0)] private void* UnkStructDB0;
-    [FieldOffset(0xDB8)] private void* UnkStructDB8;
-    [FieldOffset(0xDC0)] public WKSMechaEventModule* MechaEventModule;
-    [FieldOffset(0xDC8)] private void* UnkStructDC8;
-    [FieldOffset(0xDD0)] private void* UnkStructDD0;
-    [FieldOffset(0xDD8)] private void* EmergencyInfoModule; // Red Alert
-    [FieldOffset(0xDE0)] private void* UnkStructDE0;
-    [FieldOffset(0xDE8)] private void* UnkStructDE8;
-    [FieldOffset(0xDF0)] public WKSMissionModule* MissionModule; // Stellar Missions
-    [FieldOffset(0xDF8)] public WKSResearchModule* ResearchModule;
-    [FieldOffset(0xE00)] private void* UnkStructE00;
-
-    [FieldOffset(0xE08)] public StdVector<Pointer<WKSModuleBase>> Modules;
+    [FieldOffset(0xE10)] private void* UnkStructDB0;
+    [FieldOffset(0xE18)] private void* UnkStructDB8;
+    [FieldOffset(0xE20)] public WKSMechaEventModule* MechaEventModule;
+    [FieldOffset(0xE28)] private void* UnkStructDC8;
+    [FieldOffset(0xE30)] private void* UnkStructDD0;
+    [FieldOffset(0xE38)] private void* EmergencyInfoModule; // Red Alert
+    [FieldOffset(0xE40)] private void* UnkStructDE0;
+    [FieldOffset(0xE48)] private void* UnkStructDE8;
+    [FieldOffset(0xE50)] public WKSMissionModule* MissionModule; // Stellar Missions
+    [FieldOffset(0xE58)] public WKSResearchModule* ResearchModule;
+    [FieldOffset(0xE60)] private void* UnkStructE00;
+    [FieldOffset(0xE68)] public StdVector<Pointer<WKSModuleBase>> Modules;
 }
