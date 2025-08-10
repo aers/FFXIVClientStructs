ee118d445f9c57771b4746da84825f5c852720b5
MidoriKami <mi_kami@outlook.com>
2025-08-10 08:12:26 -0700

    Move to AtkEventTarget

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkEventTarget.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkEventTarget.cs
@@ -4,4 +4,5 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI;
 // no explicit constructor, just an event interface with 2 virtual functions
 [GenerateInterop(isInherited: true)]
 [StructLayout(LayoutKind.Explicit, Size = 0x8)]
+[VirtualTable("4C 8D 3D ?? ?? ?? ?? 4C 89 3F 41 F6 C4", 3)]
 public partial struct AtkEventTarget;
