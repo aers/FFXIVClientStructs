08480efce7323224e1c907d4621fb6864ea5dc3e
pohky <otoriosu@gmail.com>
2025-08-11 02:17:54 +0200

    Update InfoProxyBlacklist.cs

--- FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyBlacklist.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyBlacklist.cs
@@ -13,9 +13,9 @@ public unsafe partial struct InfoProxyBlacklist {
     [FieldOffset(0x20)] public Utf8String LastBlockedNameContacts; // Gets set with contacts
     [FieldOffset(0x88)] public Utf8String LastBlockedNameChat; // Gets set with chat and contacts
     [FieldOffset(0xF0), FixedSizeArray] internal FixedSizeArray200<BlockedCharacter> _blockedCharacters; // does not clean up removed characters, read only until BlockedCharactersCount
-    [FieldOffset(0x13B0)] public int BlockedCharactersCount;
-    [FieldOffset(0x13B8)] public StdMap<ulong, int> AccountIdMap;
-    [FieldOffset(0x13C8)] public StdMap<ulong, int> ContentIdMap;
+    [FieldOffset(0x19F0)] public int BlockedCharactersCount;
+    [FieldOffset(0x19F8)] public StdMap<ulong, int> AccountIdMap;
+    [FieldOffset(0x1A08)] public StdMap<ulong, int> ContentIdMap;
 
     [MemberFunction("E8 ?? ?? ?? ?? 83 7C 24 ?? ?? 75 ?? E8")]
     public partial void GetBlockResult(BlockResult* outBlockResult, ulong accountId, ulong contentId);
@@ -23,11 +23,11 @@ public unsafe partial struct InfoProxyBlacklist {
     [MemberFunction("48 83 EC 48 F6 81 ?? ?? ?? ?? ?? 75 ?? 33 C0 48 83 C4 48")]
     public partial BlockResultType GetBlockResultType(ulong accountId, ulong contentId);
 
-    [StructLayout(LayoutKind.Explicit, Size = 0x18)]
+    [StructLayout(LayoutKind.Explicit, Size = 0x20)]
     public struct BlockedCharacter {
         [FieldOffset(0x0)] public CStringPointer Name;
-        [FieldOffset(0x8)] public ulong Id; // accountId for new, contentId for old
-        [FieldOffset(0x10)] public byte Flag;
+        [FieldOffset(0x10)] public ulong Id; // accountId for new, contentId for old
+        [FieldOffset(0x18)] public byte Flag;
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x14)]
