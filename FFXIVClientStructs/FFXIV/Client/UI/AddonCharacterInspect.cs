48d437534817459db8593f1c8d80d79cb04dfc2c
Caraxi <git@caraxian.com>
2025-08-12 16:56:50 +0930

    Update AddonCharacterInspect.cs

--- FFXIVClientStructs/FFXIV/Client/UI/AddonCharacterInspect.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonCharacterInspect.cs
@@ -10,6 +10,6 @@ namespace FFXIVClientStructs.FFXIV.Client.UI;
 [Inherits<AtkUnitBase>]
 [StructLayout(LayoutKind.Explicit, Size = 0x528)]
 public unsafe partial struct AddonCharacterInspect {
-    [FieldOffset(0x450)] public PreviewController PreviewController;
-    [FieldOffset(0x460), Obsolete("Moved to PreviewController.Component", true)] public AtkComponentBase* PreviewComponent;
+    [FieldOffset(0x460)] public PreviewController PreviewController;
+    [FieldOffset(0x470), Obsolete("Moved to PreviewController.Component", true)] public AtkComponentBase* PreviewComponent;
 }
