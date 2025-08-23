fe61f7083bec6febad2fa3a35fd0aabdba189682
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2025-08-23 14:48:37 +0200

    fix attribute again

--- FFXIVClientStructs/Attributes/CExporterExcelAttribute.cs
+++ FFXIVClientStructs/Attributes/CExporterExcelAttribute.cs
@@ -18,7 +18,7 @@ internal class CExporterExcelBeginAttribute(string SheetName) : Attribute {
 [AttributeUsage(AttributeTargets.Field)]
 internal class CExporterExcelEndAttribute : Attribute;
 
-[AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Method | AttributeTargets.Property)]
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Delegate)]
 internal class CExporterExcelAttribute(string SheetName) : Attribute {
     public string SheetName { get; } = SheetName;
 }
