f16207e269345fb18112e67bc90bdc0ddbaaae6f
AtmoOmen <110901588+AtmoOmen@users.noreply.github.com>
2025-08-26 23:14:04 +0800

    Update InfoProxyNoviceNetwork.cs

--- FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyNoviceNetwork.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyNoviceNetwork.cs
@@ -12,4 +12,7 @@ public unsafe partial struct InfoProxyNoviceNetwork {
     [MemberFunction("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 48 89 7C 24 ?? 41 56 48 83 EC 50 48 8B 9C 24")]
     [GenerateStringOverloads]
     public partial bool InviteToNoviceNetwork(ulong accountId, ulong contentId, ushort worldId, CStringPointer name);
+
+    [MemberFunction("E9 ?? ?? ?? ?? 33 D2 48 83 C4 28")]
+    public partial void SendJoinRequest();
 }
