73df354f19fa7cf81f6610141d775f58a09f01ba
Haselnussbomber <mail@haselnussbomber.de>
2025-09-07 14:07:11 +0200

    Update WKSMechaEvent

--- FFXIVClientStructs/FFXIV/Client/Game/WKS/WKSMechaEventModule.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/WKS/WKSMechaEventModule.cs
@@ -8,13 +8,16 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.WKS;
 [StructLayout(LayoutKind.Explicit, Size = 0xA2B0)]
 public unsafe partial struct WKSMechaEventModule {
     [FieldOffset(0x30), FixedSizeArray] internal FixedSizeArray2<WKSMechaEvent> _events;
-    [FieldOffset(0xA0F0)] public WKSMechaEvent* CurrentEvent;
+    [FieldOffset(0xA290)] public WKSMechaEvent* CurrentEvent;
 
-    [FieldOffset(0xA104)] public WKSEventModuleFlag Flags;
+    [FieldOffset(0xA2A4)] public WKSEventModuleFlag Flags;
 }
 
 [Flags]
 public enum WKSEventModuleFlag : uint {
-    HasCurrentEvent = 1 << 0,
+    HasCurrentEvent = 1 << 0, // the pointer. doesn't mean it's active
+    PilotApplicationSubmitted = 1 << 1,
+    PilotApplicationAccepted = 1 << 2,
+    PilotCutscenePlaying = 1 << 3,
     IsJoined = 1 << 7,
 }
