87706f2e7abdbf488cfcdf12120b12ea620a6a08
Haselnussbomber <mail@haselnussbomber.de>
2025-09-17 00:05:03 +0200

    Fix inheritance of HousingCombinedObject

--- FFXIVClientStructs/FFXIV/Client/Game/Object/HousingCombinedObject.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Object/HousingCombinedObject.cs
@@ -1,10 +1,11 @@
 namespace FFXIVClientStructs.FFXIV.Client.Game.Object;
 
 // Client::Game::Object::HousingCombinedObject
-//   Client::Game::Object::HousingObject
-//     Client::Game::Object::GameObject
-//     Client::LayoutEngine::Housing::HousingEventListener
+//   Client::Game::Object::HousingEventObject
+//     Client::Game::Object::HousingObject
+//       Client::Game::Object::GameObject
+//       Client::LayoutEngine::Housing::HousingEventListener
 [GenerateInterop]
-[Inherits<HousingObject>]
+[Inherits<HousingEventObject>]
 [StructLayout(LayoutKind.Explicit, Size = 0x1E0)]
 public unsafe partial struct HousingCombinedObject;
