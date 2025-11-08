5577e3920c633235aa3eca2285ae8946ae8846c2
Haselnussbomber <mail@haselnussbomber.de>
2025-11-08 09:45:07 +0100

    Fix FishingState enum underlying type

--- FFXIVClientStructs/FFXIV/Client/Game/Event/FishingEventHandler.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Event/FishingEventHandler.cs
@@ -27,17 +27,17 @@ public unsafe partial struct FishingEventHandler {
         baitValue[1].SetBool(false);
         return ReceiveEvent(&returnValue, baitValue, 2, 2);
     }
+}
 
-    public enum FishingState : byte {
-        NotFishing = 0,
-        PoleOut = 1,
-        PullPoleIn = 2,
-        Quit = 3,
-        PoleReady = 4,
-        Bite = 5,
-        Reeling = 6,
-        Waiting = 8,
-        NormalFishing = 9,
-        LureFishing = 12,
-    }
+public enum FishingState {
+    NotFishing = 0,
+    PoleOut = 1,
+    PullPoleIn = 2,
+    Quit = 3,
+    PoleReady = 4,
+    Bite = 5,
+    Reeling = 6,
+    Waiting = 8,
+    NormalFishing = 9,
+    LureFishing = 12,
 }
