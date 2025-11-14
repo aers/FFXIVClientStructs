e568449071f45d1543dacfda9abde5a53c9c1761
Marci696 <maci.fischer@gmail.com>
2025-11-14 19:01:23 +0100

    working selectedId

---
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentMinionMountBase.cs
@@ -0,0 +1,12 @@
+﻿namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+// Client::UI::Agent::AgentMinionMountBase
+//   Client::UI::Agent::AgentInterface
+//     Component::GUI::AtkModuleInterface::AtkEventInterface
+[GenerateInterop(isInherited: true)]
+[Inherits<AgentInterface>]
+[StructLayout(LayoutKind.Explicit, Size = 0x60)]
+public partial struct AgentMinionMountBase {
+    // Selected mount or minion id in Guide.
+    [FieldOffset(0x5c)] public int SelectedId;
+}
---
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentMountNoteBook.cs
@@ -0,0 +1,14 @@
+﻿namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+
+// Client::UI::Agent::AgentMountNoteBook
+//   Client::UI::Agent::AgentMinionMountBase
+//     Client::UI::Agent::AgentInterface
+//       Component::GUI::AtkModuleInterface::AtkEventInterface
+[Agent(AgentId.MountNotebook)]
+[GenerateInterop]
+[Inherits<AgentMinionMountBase>]
+[StructLayout(LayoutKind.Explicit, Size = 0x0)]
+public partial struct AgentMountNoteBook {
+   
+}
