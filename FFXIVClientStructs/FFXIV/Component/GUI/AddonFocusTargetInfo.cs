61f19ab84f57709c20f70e343b94a4a01059c84b
Marci696 <maci.fischer@gmail.com>
2025-11-14 22:40:06 +0100

    added AddonFocusTargetInfo that i found

---
+++ FFXIVClientStructs/FFXIV/Component/GUI/AddonFocusTargetInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace FFXIVClientStructs.FFXIV.Component.GUI;
+
+[GenerateInterop]
+[Inherits<AtkUnitBase>]
+[StructLayout(LayoutKind.Explicit, Size = 0x2c0)]
+public partial struct AddonFocusTargetInfo {
+    [FieldOffset(0x240)] public AtkTextNode AtkTextNode;
+}
