01aca397f82da61107d6c345506a87b1706589f6
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2025-11-24 13:00:36 +0100

    fixup some todos

--- FFXIVClientStructs/FFXIV/Common/Component/BGCollision/Scene.cs
+++ FFXIVClientStructs/FFXIV/Common/Component/BGCollision/Scene.cs
@@ -100,9 +100,10 @@ public unsafe partial struct SceneWrapper {
     }
 }
 
+[GenerateInterop]
+[Inherits<Object>]
 [StructLayout(LayoutKind.Explicit, Size = 0xC0)]
 public unsafe partial struct Scene {
-    [FieldOffset(0x00), CExporterBaseType] public Object Object; // TODO: actual add as inheritance
     [FieldOffset(0x08)] public SceneManager* Manager;
     [FieldOffset(0x10)] public Collider* FirstCollider;
     [FieldOffset(0x18)] public int NumColliders;
