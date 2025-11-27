0237a5c1a9751fc619bcf374a6164b51cef914e2
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2025-11-27 16:35:11 -0500

    minor cleanup

--- FFXIVClientStructs/FFXIV/Client/System/Scheduler/Base/BaseInstanceObject.cs
+++ FFXIVClientStructs/FFXIV/Client/System/Scheduler/Base/BaseInstanceObject.cs
@@ -2,4 +2,4 @@ namespace FFXIVClientStructs.FFXIV.Client.System.Scheduler.Base;
 
 [GenerateInterop(isInherited: true)]
 [StructLayout(LayoutKind.Explicit, Size = 0x60)]
-public partial struct BaseInstanceObject {}
+public partial struct BaseInstanceObject;
