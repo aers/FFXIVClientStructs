8451b211fbae2566d019d865aab3f7de7d6fcec9
Haselnussbomber <mail@haselnussbomber.de>
2025-12-05 23:35:36 +0100

    Add AddonItemDetail

--- FFXIVClientStructs/FFXIV/Client/UI/AddonItemInspectionResult.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonItemInspectionResult.cs
@@ -3,9 +3,10 @@ using FFXIVClientStructs.FFXIV.Component.GUI;
 namespace FFXIVClientStructs.FFXIV.Client.UI;
 
 // Client::UI::AddonItemInspectionResult
-//   Component::GUI::AtkUnitBase
-//     Component::GUI::AtkEventListener
+//   Client::UI::AddonItemDetailBase
+//     Component::GUI::AtkUnitBase
+//       Component::GUI::AtkEventListener
 [GenerateInterop]
-[Inherits<AtkUnitBase>]
+[Inherits<AtkUnitBase>] // TODO: inherit from AddonItemDetailBase
 [StructLayout(LayoutKind.Explicit, Size = 0x310)]
 public partial struct AddonItemInspectionResult;
