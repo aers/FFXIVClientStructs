e44b387852fe27c23b32a6595e0a1d1c76ad55c6
chirp <72366111+chirpxiv@users.noreply.github.com>
2025-12-13 13:40:37 +0000

    Add `CExporterUnion` attribute for `VfxResourceHandle`

--- FFXIVClientStructs/FFXIV/Client/Graphics/Vfx/VfxResourceHandle.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Vfx/VfxResourceHandle.cs
@@ -2,8 +2,7 @@
 
 [StructLayout(LayoutKind.Explicit, Size = 8)]
 public struct VfxResourceHandle {
-    [FieldOffset(0)] public ulong Value;
-
-    [FieldOffset(0)] public uint Id;
-    [FieldOffset(4)] public uint Index;
+    [FieldOffset(0), CExporterUnion("Value")] public ulong Value;
+    [FieldOffset(0), CExporterUnion("Value")] public uint Id;
+    [FieldOffset(4), CExporterUnion("Value")] public uint Index;
 }
