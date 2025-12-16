38dcccc0e8faa6f6080710a3f5816114287b3511
Haselnussbomber <mail@haselnussbomber.de>
2025-12-16 14:30:50 +0100

    Update RaptureGearsetModule

--- FFXIVClientStructs/FFXIV/Client/UI/Misc/RaptureGearsetModule.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Misc/RaptureGearsetModule.cs
@@ -10,7 +10,7 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Misc;
 [GenerateInterop]
 [Inherits<UserFileEvent>]
 [VirtualTable("48 8D 05 ?? ?? ?? ?? 48 89 7B ?? 48 8D 73", 3)]
-[StructLayout(LayoutKind.Explicit, Size = 0xB810)]
+[StructLayout(LayoutKind.Explicit, Size = 0xB748)]
 public unsafe partial struct RaptureGearsetModule {
     public static RaptureGearsetModule* Instance() {
         var uiModule = UIModule.Instance();
@@ -20,10 +20,10 @@ public unsafe partial struct RaptureGearsetModule {
     [FieldOffset(0x48)] public UIModule* UIModulePtr;
     [FieldOffset(0x50), FixedSizeArray] internal FixedSizeArray100<GearsetEntry> _entries;
 
-    [FieldOffset(0xB5D0)] public int CurrentGearsetIndex;
+    [FieldOffset(0xB504)] public int CurrentGearsetIndex;
 
-    [FieldOffset(0xB7A4), FixedSizeArray] internal FixedSizeArray100<byte> _enabledGearsetIndex2EntryIndex;
-    [FieldOffset(0xB809)] public byte NumGearsets;
+    [FieldOffset(0xB6D8), FixedSizeArray] internal FixedSizeArray100<byte> _enabledGearsetIndex2EntryIndex;
+    [FieldOffset(0xB73D)] public byte NumGearsets;
 
     /// <summary>
     /// Return a pointer to a <see cref="GearsetEntry"/> by index/ID.
