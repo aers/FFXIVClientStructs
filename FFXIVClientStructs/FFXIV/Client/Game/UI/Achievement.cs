2f7a2fb131b5a9c7d9ab98a717969fa99939891c
Haselnussbomber <mail@haselnussbomber.de>
2025-12-16 21:28:10 +0100

    Update Achievement

--- FFXIVClientStructs/FFXIV/Client/Game/UI/Achievement.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/Achievement.cs
@@ -2,28 +2,28 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.UI;
 
 // Client::Game::UI::Achievement
 [GenerateInterop]
-[StructLayout(LayoutKind.Explicit, Size = 0x7E8)]
+[StructLayout(LayoutKind.Explicit, Size = 0x818)]
 public unsafe partial struct Achievement {
     [StaticAddress("48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 04 30 FF C3", 3)]
     public static partial Achievement* Instance();
 
     [FieldOffset(0x08)] public AchievementState State;
-    [FieldOffset(0x0C), FixedSizeArray(false, true, 3825)] internal FixedSizeArray479<byte> _completedAchievements;
+    [FieldOffset(0x0C), FixedSizeArray(false, true, 3901)] internal FixedSizeArray488<byte> _completedAchievements;
 
     /// <remarks> Last Five Achievement Ids </remarks>
-    [FieldOffset(0x1F2), FixedSizeArray] internal FixedSizeArray5<ushort> _history;
-    // [FieldOffset(0x1FC), FixedSizeArray] internal FixedSizeArray32<byte> _unk1FC; // BitArray, index is Achievement.Unknown1 or Unknown2
-    [FieldOffset(0x218)] public AchievementState ProgressRequestState;
-    [FieldOffset(0x21C)] public uint ProgressAchievementId;
-    [FieldOffset(0x220)] public uint ProgressCurrent;
-    [FieldOffset(0x224)] public uint ProgressMax;
+    [FieldOffset(0x1FE), FixedSizeArray] internal FixedSizeArray5<ushort> _history;
+    // [FieldOffset(0x208), FixedSizeArray] internal FixedSizeArray32<byte> _unk1FC; // BitArray, index is Achievement.Unknown1 or Unknown2
+    [FieldOffset(0x224)] public AchievementState ProgressRequestState;
+    [FieldOffset(0x228)] public uint ProgressAchievementId;
+    [FieldOffset(0x22C)] public uint ProgressCurrent;
+    [FieldOffset(0x230)] public uint ProgressMax;
 
     // Stuff for "Near Completion" tab and some other unimplemented tab, idk?
-    // [FieldOffset(0x22A)] 2x byte for opened state?
-    // [FieldOffset(0x22C)] 2x AchievementState
-    // [FieldOffset(0x234)] 2x struct 0x1E5 length with each 3x struct of 0x80 that contains an array of 100 bytes???
+    // [FieldOffset(0x236)] 2x byte for opened state?
+    // [FieldOffset(0x238)] 2x AchievementState
+    // [FieldOffset(0x240)] 2x struct 0x1E5 length with each 3x struct of 0x80 that contains an array of 100 bytes???
 
-    // [FieldOffset(0x5FE), FixedSizeArray] internal FixedSizeArray479<byte> _completedItemBarterWarningAchievements; // unsure, currently only for Phantom weapons?!
+    // [FieldOffset(0x60A), FixedSizeArray] internal FixedSizeArray479<byte> _completedItemBarterWarningAchievements; // unsure, currently only for Phantom weapons?!
 
     /// <summary> Requests Achievement Progress from the server. </summary>
     [MemberFunction("E8 ?? ?? ?? ?? 41 C6 44 24 ?? ?? E9 ?? ?? ?? ?? 48 8D 4F ?? E8 ?? ?? ?? ?? 88 43")]
