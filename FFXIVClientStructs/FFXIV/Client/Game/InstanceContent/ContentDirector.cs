2f0f4d2c86989a7ef04bdbf975d0569948582fc9
pohky <1510568+pohky@users.noreply.github.com>
2025-12-20 16:55:01 +0100

    fix ContentDirector vfuncs

--- FFXIVClientStructs/FFXIV/Client/Game/InstanceContent/ContentDirector.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/InstanceContent/ContentDirector.cs
@@ -20,24 +20,32 @@ public unsafe partial struct ContentDirector {
     /// <remarks> This might also be a countdown until the content starts (e.g. Frontlines), then the actual time left of the content. </remarks>
     [FieldOffset(0xCF0)] public float ContentTimeLeft;
 
+    [VirtualFunction(302)]
+    public partial uint GetCurrentLevel();
+    
+    [VirtualFunction(303)]
+    public partial uint GetMaxLevel();
+    
     /// <summary>
     /// Gets the max time for the content in seconds
     /// </summary>
     /// <returns>Time in seconds</returns>
-    [VirtualFunction(317)]
+    [VirtualFunction(328)]
     public partial uint GetContentTimeMax();
 
     [GenerateInterop]
     [StructLayout(LayoutKind.Explicit, Size = 0x608)]
     public partial struct MapEffectList {
         [FieldOffset(0x00), FixedSizeArray] internal FixedSizeArray128<MapEffectItem> _items;
+        [FieldOffset(0x600)] public ushort ContentDirectorManagedSGRowId;
         [FieldOffset(0x602)] public ushort ItemCount;
         [FieldOffset(0x604)] public byte Dirty;
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0xC)]
     public struct MapEffectItem {
-        [FieldOffset(0x00)] public uint LayoutId;
+        [FieldOffset(0x00)] public uint LayoutId; // ContentDirectorManagedSG.Unknown0
+        [FieldOffset(0x05)] public byte Unknown1; // ContentDirectorManagedSG.Unknown1
         [FieldOffset(0x08)] public ushort State;
         [FieldOffset(0x0A)] public byte Flags;
     }
