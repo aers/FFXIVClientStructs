733caeedbd3838b541b186c37830ed90a0f311ed
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2026-01-03 06:23:42 +0100

    update InfoProxyCommonList inheritance

--- FFXIVClientStructs/FFXIV/Client/Game/Character/CharacterManagerInterface.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Character/CharacterManagerInterface.cs
@@ -4,6 +4,8 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Character;
 [StructLayout(LayoutKind.Explicit, Size = 0x18)]
 [GenerateInterop(isInherited: true)]
 public unsafe partial struct CharacterManagerInterface {
+    // [FieldOffset(0x8)] public BlacklistManager BlacklistManager;
+
     [VirtualFunction(5)]
     public partial void Dtor(byte freeFlags);
 }
