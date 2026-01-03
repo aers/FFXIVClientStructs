30995d4d27c9f5fbe4a465a7b7d2cd89eae064a8
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2026-01-03 06:45:18 +0100

    update to more correct comments on CharacterManagerInterface

--- FFXIVClientStructs/FFXIV/Client/Game/Character/CharacterManagerInterface.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Character/CharacterManagerInterface.cs
@@ -4,7 +4,8 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Character;
 [StructLayout(LayoutKind.Explicit, Size = 0x18)]
 [GenerateInterop(isInherited: true)]
 public unsafe partial struct CharacterManagerInterface {
-    // [FieldOffset(0x8)] public BlacklistManager BlacklistManager;
+    // [FieldOffset(0x8)] public ??? SomeBlacklistManagerSecondInheritance; 
+    // [FieldOffset(0x10)] public void* CriticalSection; // seems to be needed for some std::vector handling related to BlackListManager
 
     [VirtualFunction(5)]
     public partial void Dtor(byte freeFlags);
