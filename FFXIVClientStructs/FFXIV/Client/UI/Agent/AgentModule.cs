db9195121388aa61920b43927edfb874b9a40ec9
MidoriKami <mi_kami@outlook.com>
2026-01-04 17:55:57 -0800

    Add AgentModule ctor

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentModule.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentModule.cs
@@ -19,6 +19,9 @@ public unsafe partial struct AgentModule {
 
     [MemberFunction("E8 ?? ?? ?? ?? 83 7B 48 00")]
     public partial AgentInterface* GetAgentByInternalId(AgentId agentId);
+
+    [MemberFunction("E8 ?? ?? ?? ?? 48 8B 85 ?? ?? ?? ?? 48 8B CE")]
+    public partial void Ctor();
 }
 
 [GenerateInterop]
