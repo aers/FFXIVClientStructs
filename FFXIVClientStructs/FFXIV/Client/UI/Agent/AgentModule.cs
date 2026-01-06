dfac28eee23cb6d82d63499423a336d653947720
pohky <1510568+pohky@users.noreply.github.com>
2026-01-07 00:01:18 +0100

    Update AgentModule.cs

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentModule.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentModule.cs
@@ -520,7 +520,7 @@ public enum AgentId : uint {
     MKDRelicGrowth = 482,
     MKDRelicGrowth2 = 483,
     QuickPanel = 484,
-    Unk485 = 485, // new in 7.4
+    EventTutorial = 485,
     ConfigCharacterChatLogMiniTalk = 486,
     ToggleContextMenu = 487,
     Unk488 = 488, // new in 7.3
