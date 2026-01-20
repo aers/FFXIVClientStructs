698aa9342d344bcd507a3a2a76122786a72a0531
pohky <1510568+pohky@users.noreply.github.com>
2026-01-20 22:25:58 +0100

    Update GameObject.cs

--- FFXIVClientStructs/FFXIV/Client/Game/Object/GameObject.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Object/GameObject.cs
@@ -108,7 +108,7 @@ public unsafe partial struct GameObject {
     public partial CharacterBase* GetCharacterBase();
 
     [VirtualFunction(26)]
-    public partial void Highlight(ObjectHighlightColor color);
+    public partial void Highlight(ObjectHighlightColor color); // TODO: add missing "bool includeMount = true" arg
 
     /// <param name="outHandlers">Should point to array that can fit up to 32 pointers.</param>
     /// <returns>Num elements filled.</returns>
@@ -123,6 +123,9 @@ public unsafe partial struct GameObject {
 
     [VirtualFunction(47)]
     public partial uint GetNameId();
+    
+    [VirtualFunction(53)]
+    public partial TargetType GetTargetType();
 
     [VirtualFunction(54)]
     public partial void PositionModified();
@@ -302,6 +305,16 @@ public enum BattleNpcSubKind : byte {
 
     /// <summary> Squadron, Trust, Duty Support </summary>
     NpcPartyMember = 9,
+}
+
+public enum TargetType {
+    NonPartyPc = 0,
+    Party = 1,
+    Alliance = 2,
+    Pet = 3,
+    Enemy = 4,
+    Minion = 5,
+    NpcOrObject = 6
 }
 
 [Flags]
