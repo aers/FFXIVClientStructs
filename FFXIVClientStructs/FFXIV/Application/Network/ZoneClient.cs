4c80f316bd4c3af29b00b18b04dc79d82247b638
Haselnussbomber <mail@haselnussbomber.de>
2026-01-21 18:59:47 +0100

    Fix wrong sig

--- FFXIVClientStructs/FFXIV/Application/Network/ZoneClient.cs
+++ FFXIVClientStructs/FFXIV/Application/Network/ZoneClient.cs
@@ -3,6 +3,6 @@ namespace FFXIVClientStructs.FFXIV.Application.Network;
 [GenerateInterop]
 [StructLayout(LayoutKind.Explicit, Size = 0xA0)]
 public unsafe partial struct ZoneClient {
-    [MemberFunction("48 89 5C 24 ?? 48 89 74 24 ?? 4C 89 64 24 ?? 55 41 56 41 57 48 8B EC 48 83 EC 70")]
+    [MemberFunction("48 83 EC ?? 48 8B 89 ?? ?? ?? ?? 48 85 C9 74 ?? 44 89 44 24 ?? 4C 8D 44 24 ?? 44 89 4C 24 ?? 44 0F B6 4C 24")]
     public partial bool SendPacket(nint packet, uint a3, uint a4, bool a5);
 }
