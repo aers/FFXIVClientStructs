c5bc105f403830c3e0706acf6c9688ed2a5ffee2
Haselnussbomber <mail@haselnussbomber.de>
2026-01-26 13:10:38 +0100

    Add some more fields

--- FFXIVClientStructs/FFXIV/Client/UI/AddonRecipeNote.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonRecipeNote.cs
@@ -12,17 +12,36 @@ namespace FFXIVClientStructs.FFXIV.Client.UI;
 public unsafe partial struct AddonRecipeNote {
     [FieldOffset(0x238)] public AtkTextNode* CurrentJobLevel; // Job Level String [ex: "Level 90"]
     [FieldOffset(0x240)] public AtkTextNode* CurrentJobName; // Job Name String [ex: "Carpenter"]
+
+    [FieldOffset(0x250)] public AtkImageNode* CurrentJobImage;
+
     /// <summary>
     /// Sibling container to scrollable list of recipes and parent container to recipe characteristics text panel
     /// </summary>
     [FieldOffset(0x268)] public AtkResNode* RecipeListAndCharacteristicsPanelContainer;
+
+    [FieldOffset(0x278), FixedSizeArray] internal FixedSizeArray9<Pointer<AtkComponentButton>> _tabButtons;
+    [FieldOffset(0x2E8), FixedSizeArray] internal FixedSizeArray9<Pointer<AtkImageNode>> _tabButtonImages;
     [FieldOffset(0x330)] public AtkComponentButton* TrialSynthesisButton;
     [FieldOffset(0x338)] public AtkComponentButton* QuickSynthesisButton;
     [FieldOffset(0x340)] public AtkComponentButton* SynthesizeButton;
+    [FieldOffset(0x348)] public AtkComponentButton* PreviousCategoryButton;
+    [FieldOffset(0x350)] public AtkComponentButton* NextCategoryButton;
+    [FieldOffset(0x358)] public AtkComponentButton* PreviousPageButton;
+    [FieldOffset(0x360)] public AtkComponentButton* NextPageButton;
+    [FieldOffset(0x368)] public AtkComponentButton* RecipeFilterButton;
+    [FieldOffset(0x370)] public AtkImageNode* RecipeFilterButtonImage;
     [FieldOffset(0x378)] public AtkTextNode* PaginationText; // Pagination Text [ex: "1-32"]
     [FieldOffset(0x380), FixedSizeArray] internal FixedSizeArray5<Pointer<AtkComponentBase>> _characteristicsComponents;
     [FieldOffset(0x3A8), FixedSizeArray] internal FixedSizeArray5<Pointer<AtkTextNode>> _characteristicsTexts;
+    [FieldOffset(0x3D0)] public AtkTextNode* SearchHintText;
+
     [FieldOffset(0x3E0)] public AtkTextNode* RecipeLevelLiteral; // Recipe Level Literal [ex: "Recipe Level"]
+
+    [FieldOffset(0x448)] public AtkComponentTreeList* RecipeList;
+    [FieldOffset(0x468)] public AtkComponentDropDownList* CategoryDropDown;
+    [FieldOffset(0x478)] public AtkComponentButton* SelectedRecipeIconButton;
+    [FieldOffset(0x480)] public AtkComponentIcon* SelectedRecipeIcon;
     [FieldOffset(0x488)] public AtkTextNode* SelectedRecipeName; // Selected Recipe Name [ex: "Water Otter Fountain Lumber"]
     [FieldOffset(0x490)] public AtkTextNode* SelectedRecipeDurability; // Durability [ex: "60"]
     [FieldOffset(0x498)] public AtkTextNode* SelectedRecipeDifficulty; // Difficulty [ex: "7480"]
@@ -39,8 +58,12 @@ public unsafe partial struct AddonRecipeNote {
     [FieldOffset(0x4D0)] public AtkTextNode* SelectedRecipeQuantityCraftableFromMaterialsInInventory; // Quantity Craftable from Inventory [ex: "10"]
     [FieldOffset(0x4E0)] public AtkTextNode* ProgressCalculationSentence; // Progress calculation sentence [ex: "At 100% efficiency, progress increases by 244."]
     [FieldOffset(0x4E8)] public AtkTextNode* QualityCalculationSentence; // Quality calculation sentence [ex: "At 100% efficiency, quality increases by 247."]
+    [FieldOffset(0x4F0)] public AtkComponentButton* NqFillButton;
+    [FieldOffset(0x4F8)] public AtkComponentButton* HqFillButton;
     [FieldOffset(0x500), FixedSizeArray] internal FixedSizeArray2<CrystalNodes> _crystals;
     [FieldOffset(0x540), FixedSizeArray] internal FixedSizeArray6<IngredientNodes> _ingredients;
+
+    [FieldOffset(0x908)] public AtkComponentTextInput* SearchTextInput;
 
     [FieldOffset(0x3A8), Obsolete("Use CharacteristicsTexts[0}")] public AtkTextNode* CharacteristicsTextLine1; // Characteristics Line 1 [ex: "Quick Synthesis Unavailable"]
     [FieldOffset(0x3B0), Obsolete("Use CharacteristicsTexts[1}")] public AtkTextNode* CharacteristicsTextLine2; // Characteristics Line 2 [ex: "Craftsmanship Required: 3700"]
