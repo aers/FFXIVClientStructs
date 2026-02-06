1bf397b853d2062578fc87e7c3c1083baf52f3b2
bwuny <102061785+sleepybnuuy@users.noreply.github.com>
2026-02-06 12:01:56 -0600

    Apply suggestion from @wolfcomp
    Co-authored-by: wolfcomp <4028289+wolfcomp@users.noreply.github.com>

--- FFXIVClientStructs/FFXIV/Client/Graphics/Scene/DrawObject.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Scene/DrawObject.cs
@@ -23,25 +23,4 @@ public unsafe partial struct DrawObject {
         get => (Flags & 0x09) == 0x09; // Unsure why two bits and what exactly they mean.
         set => Flags = (byte)(value ? Flags | 0x09 : Flags & ~0x09);
     }
-
-
-    /// <summary>
-    /// certain objects may require Visible bit set in addition to a color, without this they fail to draw entirely
-    /// </summary>
-    /// <remarks>
-    /// 0x70 and higher cause pink heatmap effects with motion, possibly a render glitch?
-    /// </remarks>
-    [Flags]
-    public enum Outline : byte {
-        /// <summary>
-        /// no color on its own, used in combination with other bits
-        /// </summary>
-        Default = 0x03,
-        Red = ObjectHighlightColor.Red << 4,
-        Green = ObjectHighlightColor.Green << 4,
-        Blue = ObjectHighlightColor.Blue << 4,
-        Yellow = ObjectHighlightColor.Yellow << 4,
-        Orange = ObjectHighlightColor.Orange << 4,
-        Magenta = ObjectHighlightColor.Magenta << 4
-    }
 }
