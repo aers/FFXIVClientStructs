a97e9f89d72d40eabd0f3b52266862dca3eba872
Haselnussbomber <mail@haselnussbomber.de>
2026-02-10 08:46:47 +0100

    More fixes

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentItemDetailBase.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentItemDetailBase.cs
@@ -3,8 +3,7 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
 // Client::UI::Agent::AgentItemDetailBase
 //   Client::UI::Agent::AgentInterface
 //     Component::GUI::AtkModuleInterface::AtkEventInterface
-[Agent(AgentId.ItemInspection)]
 [GenerateInterop(isInherited: true)]
 [Inherits<AgentInterface>]
 [StructLayout(LayoutKind.Explicit, Size = 0x50)]
-public unsafe partial struct AgentItemDetailBase;
+public partial struct AgentItemDetailBase;
--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentItemInspection.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentItemInspection.cs
@@ -11,14 +11,14 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
 [Inherits<AgentItemDetailBase>]
 [StructLayout(LayoutKind.Explicit, Size = 0x68)]
 public unsafe partial struct AgentItemInspection {
-    [FieldOffset(0x50)] public Data InspectionData;
+    [FieldOffset(0x50)] public Data* InspectionData;
 
+    [GenerateInterop]
     [StructLayout(LayoutKind.Explicit, Size = 0x668)]
-    public struct Data {
+    public partial struct Data {
         [FieldOffset(0x0)] public InventoryItem Item;
         [FieldOffset(0x50), FixedSizeArray] internal FixedSizeArray11<Utf8String> _unk50;
     }
-
 
     [MemberFunction("40 53 56 41 54 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 84 24 ?? ?? ?? ?? 48 8B 01")]
     public partial void OpenResult(int a2, InventoryItem* item);
