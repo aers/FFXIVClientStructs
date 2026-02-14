7176af97d71cd235a8074acfd73190c596198bca
bwuny <102061785+sleepybnuuy@users.noreply.github.com>
2026-02-14 12:26:24 -0600

    DrawObject outline offset (#1700)
    * add outline offset and byte flags
    
    * add remarks
    
    * add ObjectHighlightColor references, rename to Magneta
    
    * Apply suggestion from @wolfcomp
    
    Co-authored-by: wolfcomp <4028289+wolfcomp@users.noreply.github.com>
    
    * Apply suggestion from @wolfcomp
    
    Co-authored-by: wolfcomp <4028289+wolfcomp@users.noreply.github.com>
    
    * Apply suggestion from @wolfcomp
    
    Co-authored-by: wolfcomp <4028289+wolfcomp@users.noreply.github.com>
    
    * Generate a BitField instead
    
    ---------
    
    Co-authored-by: wolfcomp <4028289+wolfcomp@users.noreply.github.com>
    Co-authored-by: Haselnussbomber <mail@haselnussbomber.de>

--- FFXIVClientStructs/FFXIV/Client/Graphics/Scene/DrawObject.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Scene/DrawObject.cs
@@ -1,3 +1,5 @@
+using FFXIVClientStructs.FFXIV.Client.Game.Object;
+
 namespace FFXIVClientStructs.FFXIV.Client.Graphics.Scene;
 
 // Client::Graphics::Scene::DrawObject
@@ -9,6 +11,15 @@ namespace FFXIVClientStructs.FFXIV.Client.Graphics.Scene;
 public unsafe partial struct DrawObject {
     [BitField<bool>(nameof(IsCoveredFromRain), 4)]
     [FieldOffset(0x88)] public byte Flags;
+
+    [BitField<ObjectHighlightColor>(nameof(OutlineColor), 4, 4)]
+    [FieldOffset(0x89)] public byte OutlineFlags;
+
+    /// <summary>
+    /// Used to highlight potential targets and housing object outlines.<br/>
+    /// To set the color it is recommended to use <see cref="GameObject.Highlight" />, as it makes sure that it also highlights a characters weapon(s), mount and ornament, if available.
+    /// </summary>
+    public partial ObjectHighlightColor OutlineColor { get; set; }
 
     public bool IsVisible {
         get => (Flags & 0x09) == 0x09; // Unsure why two bits and what exactly they mean.
