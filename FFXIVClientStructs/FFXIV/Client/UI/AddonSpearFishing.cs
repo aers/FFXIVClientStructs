f197e430dadd875d898e947006e4e3659b697f4d
pohky <1510568+pohky@users.noreply.github.com>
2026-03-03 03:49:59 +0100

    Fix field offsets in AddonSpearFishing and FishInfo

--- FFXIVClientStructs/FFXIV/Client/UI/AddonSpearFishing.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonSpearFishing.cs
@@ -11,18 +11,18 @@ namespace FFXIVClientStructs.FFXIV.Client.UI;
 [StructLayout(LayoutKind.Explicit, Size = 0x908)]
 public unsafe partial struct AddonSpearFishing {
     /// <remarks> Order is bottom -> top </remarks>
-    [FieldOffset(0x294), FixedSizeArray] internal FixedSizeArray3<FishInfo> _fish;
+    [FieldOffset(0x29C), FixedSizeArray] internal FixedSizeArray3<FishInfo> _fish;
 
     [FieldOffset(0x8A8)] public AtkComponentGaugeBar* GaugeBar;
-}
 
-[StructLayout(LayoutKind.Explicit, Size = 0x1C)]
-public struct FishInfo {
-    [FieldOffset(0x8)] public bool Available;
-    [FieldOffset(0x10)] public bool InverseDirection;
-    [FieldOffset(0x11)] public bool GuaranteedLarge;
-    [FieldOffset(0x12)] public SpearfishSize Size;
-    [FieldOffset(0x14)] public SpearfishSpeed Speed;
+    [StructLayout(LayoutKind.Explicit, Size = 0x1C)]
+    public struct FishInfo {
+        [FieldOffset(0x00)] public bool Available;
+        [FieldOffset(0x08)] public bool InverseDirection;
+        [FieldOffset(0x09)] public bool GuaranteedLarge;
+        [FieldOffset(0x0A)] public SpearfishSize Size;
+        [FieldOffset(0x0C)] public SpearfishSpeed Speed;
+    }
 }
 
 public enum SpearfishSize : byte {
