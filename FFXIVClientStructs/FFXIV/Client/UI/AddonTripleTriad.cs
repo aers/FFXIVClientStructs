6300670e850ef64d01ea495b9446d46d91157c52
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-03-03 13:16:29 +0000

    nest struct

--- FFXIVClientStructs/FFXIV/Client/UI/AddonTripleTriad.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonTripleTriad.cs
@@ -15,22 +15,22 @@ public unsafe partial struct AddonTripleTriad {
     [FieldOffset(0x240), FixedSizeArray] internal FixedSizeArray5<TripleTriadCard> _blueDeck;
     [FieldOffset(0x588), FixedSizeArray] internal FixedSizeArray5<TripleTriadCard> _redDeck;
     [FieldOffset(0x8D0), FixedSizeArray] internal FixedSizeArray9<TripleTriadCard> _board;
-}
 
-[StructLayout(LayoutKind.Explicit, Size = 0xA8)]
-public unsafe partial struct TripleTriadCard {
-    [FieldOffset(0x8)] public AtkComponentBase* CardDropControl;
-    [FieldOffset(0x80)] public byte CardRarity; // 1..5
-    [FieldOffset(0x81)] public CardType CardType;
-    [FieldOffset(0x82)] public CardOwner CardOwner;
-    [FieldOffset(0x83)] public byte NumSideU;
-    [FieldOffset(0x84)] public byte NumSideD;
-    [FieldOffset(0x85)] public byte NumSideR;
-    [FieldOffset(0x86)] public byte NumSideL;
-    [FieldOffset(0x87)] private byte Unk87; // constant tied to npcs
-    [FieldOffset(0x88)] private byte Unk88; // fixed per card, not Id
-    [FieldOffset(0x89)] private byte Unk89; // fixed per card, 41/42
-    [FieldOffset(0xA4)] public bool HasCard;
+    [StructLayout(LayoutKind.Explicit, Size = 0xA8)]
+    public unsafe partial struct TripleTriadCard {
+        [FieldOffset(0x8)] public AtkComponentBase* CardDropControl;
+        [FieldOffset(0x80)] public byte CardRarity; // 1..5
+        [FieldOffset(0x81)] public CardType CardType;
+        [FieldOffset(0x82)] public CardOwner CardOwner;
+        [FieldOffset(0x83)] public byte NumSideU;
+        [FieldOffset(0x84)] public byte NumSideD;
+        [FieldOffset(0x85)] public byte NumSideR;
+        [FieldOffset(0x86)] public byte NumSideL;
+        [FieldOffset(0x87)] private byte Unk87; // constant tied to npcs
+        [FieldOffset(0x88)] private byte Unk88; // fixed per card, not Id
+        [FieldOffset(0x89)] private byte Unk89; // fixed per card, 41/42
+        [FieldOffset(0xA4)] public bool HasCard;
+    }
 }
 
 public enum CardType : byte {
