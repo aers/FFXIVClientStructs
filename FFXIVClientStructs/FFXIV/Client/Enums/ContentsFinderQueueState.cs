4015847134f457e548658993ec897017d3780ae0
Haselnussbomber <mail@haselnussbomber.de>
2026-03-11 19:24:31 +0100

    Rename and move EnterContentInfoPacket

---
+++ FFXIVClientStructs/FFXIV/Client/Enums/ContentsFinderQueueState.cs
@@ -0,0 +1,10 @@
+namespace FFXIVClientStructs.FFXIV.Client.Enums;
+
+public enum ContentsFinderQueueState : byte {
+    None = 0,
+    Pending = 1,
+    Queued = 2,
+    Ready = 3,
+    Accepted = 4,
+    InContent = 5
+}
