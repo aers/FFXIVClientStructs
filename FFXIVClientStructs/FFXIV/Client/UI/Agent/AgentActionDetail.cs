4be4f22677b7f810560453842499402aefa14593
Haselnussbomber <mail@haselnussbomber.de>
2026-03-11 20:23:14 +0100

    Replace ActionKind with DetailKind enum

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentActionDetail.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentActionDetail.cs
@@ -1,3 +1,4 @@
+using FFXIVClientStructs.FFXIV.Client.Enums;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
@@ -13,7 +14,7 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
 public unsafe partial struct AgentActionDetail {
     [FieldOffset(0x28)] public NumberArrayData* NumberArray; // NumberArrayType.ActionDetail
     [FieldOffset(0x30)] public StringArrayData* StringArray; // StringArrayType.ActionDetail
-    [FieldOffset(0x38)] public ActionKind ActionKind;
+    [FieldOffset(0x38)] public DetailKind ActionKind;
     [FieldOffset(0x3C)] public uint ActionId;
     [FieldOffset(0x40)] public uint OriginalId; // Example: Summon Topaz
     [FieldOffset(0x44)] public uint AdjustedId; // Example: Summon Titan II
@@ -31,41 +32,5 @@ public unsafe partial struct AgentActionDetail {
 
     // flag & 1 = get AdjustedActionId
     [MemberFunction("E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 83 F8 0F")]
-    public partial void HandleActionHover(ActionKind actionKind, uint actionId, int flag, bool isLovmActionDetail, int a5, int a6);
-}
-
-/// Keep in sync with <see cref="Enums.DetailKind"/>
-public enum ActionKind {
-    Action = 29,
-    CraftingAction = 30,
-    GeneralAction = 31,
-    BuddyOrder = 32,
-    MainCommand = 33,
-    ExtraCommand = 34,
-    Companion = 35,
-    PetOrder = 36,
-    Trait = 37,
-    BuddyAction = 38,
-    CompanyAction = 39,
-    Mount = 40,
-    ChocoboRaceAction = 41,
-    ChocoboRaceItem = 42,
-    DeepDungeonEquipment = 43,
-    DeepDungeonEquipment2 = 44,
-    DeepDungeonItem = 45,
-    QuickChat = 46,
-    ActionComboRoute = 47,
-    PvPSelectTrait = 48,
-    BgcArmyAction = 49,
-    Perform = 50,
-    DeepDungeonMagicStone = 51,
-    DeepDungeonDemiclone = 52,
-    DeepDungeon4GimmickEffect = 53,
-    EurekaMagiaAction = 54,
-    MYCTemporaryItem = 55,
-    Ornament = 56,
-    Glasses = 57,
-    PhantomAction = 58,
-    MKDTrait = 59,
-    Unk60 = 60,
+    public partial void HandleActionHover(DetailKind actionKind, uint actionId, int flag, bool isLovmActionDetail, int a5, int a6);
 }
