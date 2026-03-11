7f05de361dc42e63fa5042075f5313ad57be1d51
Haselnussbomber <mail@haselnussbomber.de>
2026-03-11 19:48:34 +0100

    Rename UpdateDataPacket as it's not a packet

--- FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyDetail.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyDetail.cs
@@ -7,21 +7,11 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Info;
 [Inherits<InfoProxyInterface>]
 [StructLayout(LayoutKind.Explicit, Size = 0x240)]
 public unsafe partial struct InfoProxyDetail {
-    [FieldOffset(0x28)] public UpdateDataPacket UpdateData;
+    [FieldOffset(0x28)] public DetailUpdateData UpdateData;
 
     [FieldOffset(0x100)] public Utf8String SearchComment;
 
     [FieldOffset(0x238)] public bool HasUpdateData;
-
-    [GenerateInterop]
-    [StructLayout(LayoutKind.Explicit, Size = 0xD8)]
-    public partial struct UpdateDataPacket {
-        [FieldOffset(0x00)] public InfoProxyCommonList.CharacterData.OnlineStatus OnlineStatusMask;
-        [FieldOffset(0x08)] public ulong LookingForPartyClassJobIdMask;
-        [FieldOffset(0x10)] public byte ClassJobId;
-        [FieldOffset(0x11)] public InfoProxyCommonList.CharacterData.LanguageMask LanguageMask;
-        [FieldOffset(0x12), FixedSizeArray(isString: true)] internal FixedSizeArray193<byte> _searchComment;
-    }
 
     [MemberFunction("38 51 38 74 0A")]
     public partial void SetUpdateClassJobId(byte classJobId);
@@ -43,8 +33,18 @@ public unsafe partial struct InfoProxyDetail {
 
     /// <param name="updateData">The packet to send. If <c>null</c>, it will use <see cref="UpdateData"/>.</param>
     [MemberFunction("E8 ?? ?? ?? ?? C6 83 ?? ?? ?? ?? ?? 40 84 FF")]
-    public partial void SendUpdateData(UpdateDataPacket* updateData = null);
+    public partial void SendUpdateData(DetailUpdateData* updateData = null);
 
     [MemberFunction("48 89 5C 24 ?? 57 48 83 EC 20 4C 8B 49 28 48 8B D9")]
     public partial void RefreshOnlineStatus();
+
+    [GenerateInterop]
+    [StructLayout(LayoutKind.Explicit, Size = 0xD8)]
+    public partial struct DetailUpdateData {
+        [FieldOffset(0x00)] public InfoProxyCommonList.CharacterData.OnlineStatus OnlineStatusMask;
+        [FieldOffset(0x08)] public ulong LookingForPartyClassJobIdMask;
+        [FieldOffset(0x10)] public byte ClassJobId;
+        [FieldOffset(0x11)] public InfoProxyCommonList.CharacterData.LanguageMask LanguageMask;
+        [FieldOffset(0x12), FixedSizeArray(isString: true)] internal FixedSizeArray193<byte> _searchComment;
+    }
 }
