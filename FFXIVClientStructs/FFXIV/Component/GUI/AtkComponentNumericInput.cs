b8528ed67a1c777678a7840c4be6e2d1053fadcc
MidoriKami <mi_kami@outlook.com>
2026-03-14 09:47:29 -0700

    Add AtkComponentNumericInput Component Pointers

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentNumericInput.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentNumericInput.cs
@@ -10,6 +10,9 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI;
 [Inherits<AtkComponentInputBase>]
 [StructLayout(LayoutKind.Explicit, Size = 0x338)]
 public unsafe partial struct AtkComponentNumericInput : ICreatable {
+    [FieldOffset(0x1E8)] public AtkComponentButton* PlusButtonComponent;
+    [FieldOffset(0x1F0)] public AtkComponentButton* MinusButtonComponent;
+
     [FieldOffset(0x1F8)] public int Value; // Found through SetValue function
     [FieldOffset(0x2F8)] public AtkUldComponentDataNumericInput Data;
 
