78b32fffecf0d627a39b70adb4b6db6036aabd61
MidoriKami <mi_kami@outlook.com>
2026-03-14 11:03:32 -0700

    Correct offset

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentNumericInput.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentNumericInput.cs
@@ -14,7 +14,7 @@ public unsafe partial struct AtkComponentNumericInput : ICreatable {
     [FieldOffset(0x1F0)] public AtkComponentButton* MinusButtonComponent;
     [FieldOffset(0x1F8)] public int Value; // Found through SetValue function
     [FieldOffset(0x200)] public int Digits;
-    [FieldOffset(0x210)] public bool AddZeroPadding;
+    [FieldOffset(0x212)] public bool AddZeroPadding;
     [FieldOffset(0x2F8)] public AtkUldComponentDataNumericInput Data;
 
     [MemberFunction("48 89 5C 24 ?? 48 89 74 24 ?? 57 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 84 24 ?? ?? ?? ?? 48 8B F1 E8 ?? ?? ?? ?? 48 8D 05 ?? ?? ?? ?? 48 C7 86 ?? ?? ?? ?? ?? ?? ?? ??")]
