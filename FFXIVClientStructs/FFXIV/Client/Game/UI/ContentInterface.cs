91c23b700a968c566af2044651ac74fb95f576bb
Haselnussbomber <mail@haselnussbomber.de>
2026-03-15 02:50:30 +0100

    Fix build

--- FFXIVClientStructs/FFXIV/Client/Game/UI/ContentInterface.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/ContentInterface.cs
@@ -3,7 +3,7 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.UI;
 // Client::Game::UI::ContentInterface
 [GenerateInterop(isInherited: true)]
 [StructLayout(LayoutKind.Explicit, Size = 0x08)]
-public partial struct ContentInterface {
+public unsafe partial struct ContentInterface {
     [VirtualFunction(0)]
     public partial ContentInterface* Dtor(byte freeFlag);
 
