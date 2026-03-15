62380f50816374ddb6f924ac3d2f4722046a6524
Haselnussbomber <mail@haselnussbomber.de>
2026-03-15 12:52:43 +0100

    Update DetailKind/ActionKind

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentActionDetail.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentActionDetail.cs
@@ -1,3 +1,5 @@
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
 namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
 
 // Client::UI::Agent::AgentActionDetail
@@ -9,10 +11,23 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
 [VirtualTable("48 8D 05 ?? ?? ?? ?? 48 89 01 8B FA 48 8B D9 74 ?? 0F B6 41", 3)]
 [StructLayout(LayoutKind.Explicit, Size = 0x68)]
 public unsafe partial struct AgentActionDetail {
+    [FieldOffset(0x28)] public NumberArrayData* NumberArray; // NumberArrayType.ActionDetail
+    [FieldOffset(0x30)] public StringArrayData* StringArray; // StringArrayType.ActionDetail
     [FieldOffset(0x38)] public ActionKind ActionKind;
     [FieldOffset(0x3C)] public uint ActionId;
     [FieldOffset(0x40)] public uint OriginalId; // Example: Summon Topaz
     [FieldOffset(0x44)] public uint AdjustedId; // Example: Summon Titan II
+    [FieldOffset(0x48)] private uint Unk48;
+    [FieldOffset(0x4C)] private uint Unk4C;
+    [FieldOffset(0x50)] private int Unk50;
+    [FieldOffset(0x54)] private uint Unk54;
+    [FieldOffset(0x58)] private int Unk58;
+    [FieldOffset(0x5C)] private int Unk5C;
+    [FieldOffset(0x60)] private int Unk60;
+    [FieldOffset(0x64)] private bool Unk64;
+    [FieldOffset(0x65)] public bool IsLovmActionDetail;
+    [FieldOffset(0x66)] private byte Unk66;
+    [FieldOffset(0x67)] private byte Unk67;
 
     // flag & 1 = get AdjustedActionId
     [MemberFunction("E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 83 F8 0F")]
@@ -45,11 +60,12 @@ public enum ActionKind {
     Perform = 50,
     DeepDungeonMagicStone = 51,
     DeepDungeonDemiclone = 52,
-    EurekaMagiaAction = 53,
-    MYCTemporaryItem = 54,
-    Ornament = 55,
-    Glasses = 56,
-    Unk57 = 57,
-    MKDTrait = 58,
-    Unk59 = 59,
+    DeepDungeon4GimmickEffect = 53,
+    EurekaMagiaAction = 54,
+    MYCTemporaryItem = 55,
+    Ornament = 56,
+    Glasses = 57,
+    PhantomAction = 58,
+    MKDTrait = 59,
+    Unk60 = 60,
 }
