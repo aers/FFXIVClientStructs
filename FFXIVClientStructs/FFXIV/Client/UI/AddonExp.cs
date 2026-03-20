8182c94c37ab4c39890eec58829deb52752fa670
MidoriKami <mi_kami@outlook.com>
2026-03-20 09:45:55 -0700

    Exp

--- FFXIVClientStructs/FFXIV/Client/UI/AddonExp.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonExp.cs
@@ -9,9 +9,12 @@ namespace FFXIVClientStructs.FFXIV.Client.UI;
 [GenerateInterop]
 [Inherits<AtkUnitBase>]
 [StructLayout(LayoutKind.Explicit, Size = 0x2A8)]
-public partial struct AddonExp {
+public unsafe partial struct AddonExp {
+    [FieldOffset(0x268)] public AtkComponentGaugeBar* ExperienceBarComponent;
+    [FieldOffset(0x270)] private AtkTextNode* UnkTextNode; // Reads string from StringArrays[2][70] (Hud string array at index 70)
+    [FieldOffset(0x278)] public AtkImageNode* MoonIconNode;
+    [FieldOffset(0x280)] public AtkImageNode* SwordsIconNode;
     [FieldOffset(0x288)] public byte ClassJob;
-
     [FieldOffset(0x290)] public uint CurrentExp;
     [FieldOffset(0x294)] public uint RequiredExp;
     [FieldOffset(0x298)] public uint RestedExp;
