511dbb89182896bdb3daf238e5c0d215bbef8cf3
MidoriKami <mi_kami@outlook.com>
2026-03-21 15:20:23 -0700

    Add other texture to AtkComponentMap

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentMap.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentMap.cs
@@ -22,7 +22,8 @@ public unsafe partial struct AtkComponentMap : ICreatable {
     [FieldOffset(0x138)] public AtkResourceRendererManager ResourceRendererManager;
     [FieldOffset(0x250)] public DiscoveryRenderer m_DiscoveryRenderer;
     // [FieldOffset(0x268)] public AtkRenderTexture RenderTexture;
-    [FieldOffset(0x270)] public Texture* Texture;
+    [FieldOffset(0x270)] public Texture* FogOfWarMaskingTexture;
+    [FieldOffset(0x310)] public Texture* DiscoveryPartsTexture;
 
     [FieldOffset(0x374)] public float MapScale;
 
