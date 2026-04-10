6b97e73a1b897e91e4fdc69f2925b2b1a925965f
bilk <cR0N7CXAC9O0yJ9dd@gmail.com>
2026-04-10 18:00:23 +0100

    TofuHelper Additional

--- FFXIVClientStructs/FFXIV/Client/UI/Misc/TofuHelper.Data.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Misc/TofuHelper.Data.cs
@@ -2,7 +2,7 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Misc;
 
 [GenerateInterop]
 [StructLayout(LayoutKind.Explicit, Size = 0x378)]
-public unsafe partial struct TofuTransferObject {
+public partial struct TofuTransferObject {
     // Different structure to TofuModule's TofuObject
     [FieldOffset(0x10)] public Utf8String Name;
     [FieldOffset(0x78)] public ushort PosX;
@@ -24,19 +24,29 @@ public struct TofuObjectArgData {
     [FieldOffset(0x0)] public Utf8String Name;
 }
 
+/// <summary>
+/// The packed board is initially constructed in <see cref="TofuBoardOverview.ConstructPackedBoard"/> and then compressed via zlib.
+/// Useful Information: https://github.com/xivdev/file-formats/blob/main/imhex/stgy.hexpat
+/// </summary>
 [GenerateInterop]
 [StructLayout(LayoutKind.Explicit, Size = 0x450)]
 public partial struct TofuPackedBoard {
-    // Packed Board to be sent as packet to server. Structure changes dramatically with small object changes
-    // Manipulating bytes here before packet send has a high chance of being rejected by the server, thus not being sent to receiver
-    [FieldOffset(0x0)] private ushort TotalObjectSize; // increments by 0x11 with number of objects starting at 0x03
+    [FieldOffset(0x0)] private ushort Unk0; // increments by 0x11 with number of objects starting at 0x03
     [FieldOffset(0x2)] public SharingType SharingType;
-    [FieldOffset(0x6)] private byte Background; // lower nibble
+    [FieldOffset(0x6)] private byte Unk6; // lower nibble changes with background
     [FieldOffset(0x410), FixedSizeArray(isString: true)] internal FixedSizeArray60<byte> _boardName;
 }
 
+[Flags]
 public enum SharingType : byte {
     Normal = 0,
     RealTime = 1,
     Folder = 4,
+}
+
+[Flags]
+public enum SendState : uint {
+    None = 0,
+    Intermediate = 1,
+    Last = 2,
 }
