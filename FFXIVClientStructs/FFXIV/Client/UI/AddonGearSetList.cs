435f465d4b3bfe01426dc46baef99aa23dab9b3d
bilk <cR0N7CXAC9O0yJ9dd@gmail.com>
2026-04-18 01:43:22 +0100

    AddonGearSetList GearSet Array

--- FFXIVClientStructs/FFXIV/Client/UI/AddonGearSetList.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonGearSetList.cs
@@ -1,4 +1,5 @@
 using FFXIVClientStructs.FFXIV.Component.GUI;
+using static FFXIVClientStructs.FFXIV.Client.UI.Misc.RaptureGearsetModule;
 
 namespace FFXIVClientStructs.FFXIV.Client.UI;
 
@@ -10,5 +11,17 @@ namespace FFXIVClientStructs.FFXIV.Client.UI;
 [Inherits<AtkUnitBase>]
 [StructLayout(LayoutKind.Explicit, Size = 0x3AA8)]
 public partial struct AddonGearSetList {
+    [FieldOffset(0x260), FixedSizeArray] internal FixedSizeArray100<GearSet> _gearsets;
     [FieldOffset(0x3AA5)] public bool ShouldResetPosition;
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0x90)]
+public partial struct GearSet {
+    [FieldOffset(0x0)] public int Index; // -1 for empty
+    [FieldOffset(0x4)] public uint JobIconId; // icon without background
+    [FieldOffset(0x8)] public uint JobIconId2; // icon with background
+    [FieldOffset(0xC)] public byte GlamourPlateId;
+    [FieldOffset(0x10)] public Utf8String ItemLevelText;
+    [FieldOffset(0x78)] public CStringPointer Name;
+    [FieldOffset(0x84)] public GearsetItemFlag Flag;
 }
