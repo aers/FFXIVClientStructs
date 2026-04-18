0ca7b8da6e5d751228a52de612c5c28397760ad6
Haselnussbomber <mail@haselnussbomber.de>
2026-04-18 14:00:53 +0200

    CI test commit

---
+++ FFXIVClientStructs/FFXIV/Client/CiTest.cs
@@ -0,0 +1,7 @@
+namespace FFXIVClientStructs.FFXIV.Client;
+
+[GenerateInterop]
+[StructLayout(LayoutKind.Explicit, Size = 0x02)]
+public partial struct CiTest {
+    [FieldOffset(0x10)] public byte Test;
+}
