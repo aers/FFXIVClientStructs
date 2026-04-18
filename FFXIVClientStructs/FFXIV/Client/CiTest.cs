2c75e22cddae76fd3f9700798ff737dddf3b7006
Haselnussbomber <mail@haselnussbomber.de>
2026-04-18 14:01:02 +0200

    Revert "CI test commit"
    This reverts commit 0ca7b8da6e5d751228a52de612c5c28397760ad6.

--- FFXIVClientStructs/FFXIV/Client/CiTest.cs
+++
@@ -1,7 +0,0 @@
-namespace FFXIVClientStructs.FFXIV.Client;
-
-[GenerateInterop]
-[StructLayout(LayoutKind.Explicit, Size = 0x02)]
-public partial struct CiTest {
-    [FieldOffset(0x10)] public byte Test;
-}
