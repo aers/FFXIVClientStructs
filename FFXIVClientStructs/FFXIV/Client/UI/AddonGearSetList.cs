37ce353c7ff38f81071b7a11f2257b54dc920fc1
Haselnussbomber <mail@haselnussbomber.de>
2026-04-24 16:32:34 +0200

    Move GearSet struct into AddonGearSetList

--- FFXIVClientStructs/FFXIV/Client/UI/AddonGearSetList.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonGearSetList.cs
@@ -11,17 +11,17 @@ namespace FFXIVClientStructs.FFXIV.Client.UI;
 [Inherits<AtkUnitBase>]
 [StructLayout(LayoutKind.Explicit, Size = 0x3AA8)]
 public partial struct AddonGearSetList {
-    [FieldOffset(0x260), FixedSizeArray] internal FixedSizeArray100<GearSet> _gearsets;
+    [FieldOffset(0x260), FixedSizeArray] internal FixedSizeArray100<GearsetEntry> _gearsets;
     [FieldOffset(0x3AA5)] public bool ShouldResetPosition;
-}
 
-[StructLayout(LayoutKind.Explicit, Size = 0x90)]
-public partial struct GearSet {
-    [FieldOffset(0x0)] public int Index; // -1 for empty
-    [FieldOffset(0x4)] public uint JobIconId; // icon without background
-    [FieldOffset(0x8)] public uint JobIconId2; // icon with background
-    [FieldOffset(0xC)] public byte GlamourPlateId;
-    [FieldOffset(0x10)] public Utf8String ItemLevelText;
-    [FieldOffset(0x78)] public CStringPointer Name;
-    [FieldOffset(0x84)] public GearsetItemFlag Flag;
+    [StructLayout(LayoutKind.Explicit, Size = 0x90)]
+    public struct GearsetEntry {
+        [FieldOffset(0x0)] public int Index; // -1 for empty
+        [FieldOffset(0x4)] public uint JobIconId; // icon without background
+        [FieldOffset(0x8)] public uint JobWithBackgroundIconId; // icon with background, used for DragDrop
+        [FieldOffset(0xC)] public byte GlamourPlateId;
+        [FieldOffset(0x10)] public Utf8String ItemLevelText;
+        [FieldOffset(0x78)] public CStringPointer Name;
+        [FieldOffset(0x84)] public GearsetItemFlag Flags;
+    }
 }
