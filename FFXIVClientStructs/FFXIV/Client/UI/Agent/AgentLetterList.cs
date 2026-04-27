56c9d307367deac460509496abfe8e1473a04d49
Knightmore <35805408+Knightmore@users.noreply.github.com>
2026-04-27 09:28:23 +0200

    Update FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentLetterList.cs
    Co-authored-by: wolfcomp <4028289+wolfcomp@users.noreply.github.com>

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentLetterList.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentLetterList.cs
@@ -2,14 +2,14 @@ using FFXIVClientStructs.FFXIV.Client.UI.Info;
 
 namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
 
-// Client::UI::Agent::AgentLetterList
+// Client::UI::Agent::AgentLetter
 //   Client::UI::Agent::AgentInterface
 //     Component::GUI::AtkModuleInterface::AtkEventInterface
 [Agent(AgentId.LetterList)]
 [GenerateInterop]
 [Inherits<AgentInterface>]
 [StructLayout(LayoutKind.Explicit, Size = 0x60)]
-public unsafe partial struct AgentLetterList {
+public unsafe partial struct AgentLetter {
     [FieldOffset(0x30)] public InfoProxyLetter* InfoProxyLetter;
     [FieldOffset(0x38)] public uint OpenLetterIndex;
     [FieldOffset(0x48)] public float RewardRequestCountdown;  // 10 Seconds
