02c272dde0fca911db789ae3af4e17a5a7acb4aa
Universal Conquistador <universalconquistador@gmail.com>
2026-04-27 03:54:40 -0700

    Straightens out a sig I got mixed up with the penumbra name

--- FFXIVClientStructs/FFXIV/Client/System/File/FileThread.cs
+++ FFXIVClientStructs/FFXIV/Client/System/File/FileThread.cs
@@ -8,6 +8,9 @@ namespace FFXIVClientStructs.FFXIV.Client.System.File;
 public unsafe partial struct FileThread {
     [FieldOffset(0x0008)] public void* SecurityAttributes; // https://learn.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-createeventa
 
-    [MemberFunction("40 56 41 56 48 83 EC ?? 0F BE 02")]
+    [MemberFunction("44 88 4C 24 ?? 44 89 44 24 ?? 53 56")]
     public partial byte ReadSqPack(FileDescriptor* fileDescriptor, int priority, bool isSync);
+
+    [MemberFunction("40 56 41 56 48 83 EC ?? 0F BE 02")]
+    public partial byte DoFileJob(FileDescriptor* fileDescriptor, int priority, bool isSync);
 }
