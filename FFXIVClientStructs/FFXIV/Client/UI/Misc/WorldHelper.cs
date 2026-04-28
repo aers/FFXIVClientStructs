55ce90dc4c4239d4e4aba50cf25b5c28932d6375
Haselnussbomber <mail@haselnussbomber.de>
2026-04-28 20:37:43 +0200

    Update WorldHelper

--- FFXIVClientStructs/FFXIV/Client/UI/Misc/WorldHelper.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Misc/WorldHelper.cs
@@ -2,7 +2,7 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Misc;
 
 // Client::UI::Misc::WorldHelper
 [GenerateInterop]
-[StructLayout(LayoutKind.Explicit, Size = 0x40)]
+[StructLayout(LayoutKind.Explicit, Size = 0x50)]
 public unsafe partial struct WorldHelper {
     public static WorldHelper* Instance() {
         var uiModule = UIModule.Instance();
@@ -13,7 +13,8 @@ public unsafe partial struct WorldHelper {
     /// <summary> Contains all worlds of the current data center. </summary>
     [FieldOffset(0x18)] public StdMap<ushort, World> DataCenterWorlds;
     // [FieldOffset(0x28)] public ClassThatLoadsWorlds WorldLoader; // size: 0x10
-    [FieldOffset(0x38)] public bool IsLoaded;
+
+    [FieldOffset(0x48)] public bool IsLoaded;
 
     [MemberFunction("E8 ?? ?? ?? ?? 48 85 F6 75 17")]
     public partial World GetWorldById(ushort world);
@@ -24,12 +25,13 @@ public unsafe partial struct WorldHelper {
     [MemberFunction("E8 ?? ?? ?? ?? 66 41 89 07 66 85 C0")]
     public partial ushort GetWorldIdByName(CStringPointer worldName);
 
-
     [GenerateInterop]
-    [StructLayout(LayoutKind.Explicit, Size = 0x24)]
+    [StructLayout(LayoutKind.Explicit, Size = 0x48)]
     public partial struct World {
         [FieldOffset(0x00)] public ushort RowId;
         [FieldOffset(0x02)] public byte DataCenter;
-        [FieldOffset(0x03), FixedSizeArray(isString: true)] internal FixedSizeArray32<byte> _name;
+        [FieldOffset(0x03)] private byte Unk3;
+        [FieldOffset(0x04)] private byte Unk4;
+        [FieldOffset(0x05), FixedSizeArray(isString: true)] internal FixedSizeArray32<byte> _name;
     }
 }
