bca70526520ecf22c0d57638b89adb13ca825bda
Haselnussbomber <mail@haselnussbomber.de>
2026-04-30 00:38:45 +0200

    Update TitleList

--- FFXIVClientStructs/FFXIV/Client/Game/TitleList.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/TitleList.cs
@@ -3,11 +3,11 @@ namespace FFXIVClientStructs.FFXIV.Client.Game;
 // Client::Game::TitleList
 [GenerateInterop]
 [StructLayout(LayoutKind.Explicit, Size = 0x80)]
-public unsafe partial struct TitleList {
-    [FieldOffset(0x8), FixedSizeArray] internal FixedSizeArray111<byte> _titlesUnlockBitmask;
-    [FieldOffset(0x77)] public bool DataPending;
-    [FieldOffset(0x78)] public bool DataReceived;
-    [FieldOffset(0x79)] public bool DataRequested;
+public partial struct TitleList {
+    [FieldOffset(0x8), FixedSizeArray(isBitArray: true, bitCount: 894)] internal FixedSizeArray112<byte> _titlesUnlockBitmask;
+    [FieldOffset(0x7C)] public bool DataPending;
+    [FieldOffset(0x7D)] public bool DataReceived;
+    [FieldOffset(0x7E)] public bool DataRequested;
 
     [MemberFunction("E8 ?? ?? ?? ?? 89 6E 58")]
     public partial void RequestTitleList();
