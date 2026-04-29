531bb659f8cda229f0582a1142b85641ac5d52c6
pohky <1510568+pohky@users.noreply.github.com>
2026-04-30 01:51:46 +0200

    Update KeyboardDevice.cs

--- FFXIVClientStructs/FFXIV/Client/System/Input/KeyboardDevice.cs
+++ FFXIVClientStructs/FFXIV/Client/System/Input/KeyboardDevice.cs
@@ -5,5 +5,5 @@ namespace FFXIVClientStructs.FFXIV.Client.System.Input;
 //   Client::System::Input::RepeatCounter
 [GenerateInterop]
 [Inherits<KeyboardDeviceInterface>, Inherits<RepeatCounter>]
-[StructLayout(LayoutKind.Explicit, Size = 0x38)]
+[StructLayout(LayoutKind.Explicit, Size = 0x40)]
 public partial struct KeyboardDevice;
