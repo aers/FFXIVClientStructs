82a57c20919c8134cce910f58e0076ee9603f93c
pohky <1510568+pohky@users.noreply.github.com>
2026-04-30 06:02:53 +0200

    Update AgentWKSHud.cs

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentWKSHud.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentWKSHud.cs
@@ -12,7 +12,7 @@ public unsafe partial struct AgentWKSHud {
 
     public readonly bool IsReady => Info != null && Info->State == 3;
 
-    [StructLayout(LayoutKind.Explicit, Size = 0x270)]
+    [StructLayout(LayoutKind.Explicit, Size = 0x798)]
     public unsafe partial struct HudInfo {
         [FieldOffset(0)] public byte State; // 1/2 = fetching data for current zone, 3 = ready
 
@@ -24,7 +24,7 @@ public unsafe partial struct AgentWKSHud {
         [FieldOffset(0x1C)] public int ZoneCreditsItemId;
         [FieldOffset(0x20)] public int ZoneCreditsIcon;
 
-        [FieldOffset(0x40)] public Utf8String CosmoCreditsText;
-        [FieldOffset(0xA8)] public Utf8String ZoneCreditsText;
+        [FieldOffset(0x568)] public Utf8String CosmoCreditsText;
+        [FieldOffset(0x5D0)] public Utf8String ZoneCreditsText;
     }
 }
