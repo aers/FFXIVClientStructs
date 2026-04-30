2ca16145f54e712c7bd0d7b11615eeda43b82f43
pohky <1510568+pohky@users.noreply.github.com>
2026-04-30 11:03:06 +0200

    Update AddonHudLayoutScreen.cs

--- FFXIVClientStructs/FFXIV/Client/UI/AddonHudLayoutScreen.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonHudLayoutScreen.cs
@@ -8,18 +8,18 @@ namespace FFXIVClientStructs.FFXIV.Client.UI;
 [Addon("_HudLayoutScreen")]
 [GenerateInterop]
 [Inherits<AtkUnitBase>]
-[StructLayout(LayoutKind.Explicit, Size = 0x980)]
+[StructLayout(LayoutKind.Explicit, Size = 0x990)]
 public unsafe partial struct AddonHudLayoutScreen {
     [FieldOffset(0x2E0)] public AddonHudLayoutWindow* HudLayoutWindow;
-
-    [FieldOffset(0x558)] public AtkComponentNode* SelectedOverlayNode; // actually an array of active overlay nodes here, but this should be the selected one in theory
-
-    [FieldOffset(0x7C8)] public MoveableAddonInfoStruct* SelectedAddon;
+    //[FieldOffset(0x2E8), FixedSizeArray] internal FixedSizeArray103<Pointer<MoveableAddonInfoStruct>> _addons;
+    //[FieldOffset(0x620), FixedSizeArray] internal FixedSizeArray103<Pointer<AtkComponentNode>> _overlayNodes;
+    [FieldOffset(0x620)] public AtkComponentNode* SelectedOverlayNode; // <-- wrong
+    [FieldOffset(0x958)] public MoveableAddonInfoStruct* SelectedAddon;
 }
 
 [StructLayout(LayoutKind.Explicit, Size = 0x50)]
 public unsafe struct MoveableAddonInfoStruct {
-    [FieldOffset(0x20)] public AddonHudLayoutScreen* hudLayoutScreen;
+    [FieldOffset(0x20)] public AddonHudLayoutScreen* HudLayoutScreen;
     [FieldOffset(0x28)] public AtkUnitBase* SelectedAtkUnit;
     [FieldOffset(0x3C)] public int Flags;
     [FieldOffset(0x44)] public short XOffset;
