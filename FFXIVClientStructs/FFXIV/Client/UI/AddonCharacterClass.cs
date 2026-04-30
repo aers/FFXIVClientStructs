d4a96497feb3c2e75d2088e59886e5fadf8785fe
pohky <1510568+pohky@users.noreply.github.com>
2026-04-30 11:14:06 +0200

    Update AddonCharacterClass.cs

--- FFXIVClientStructs/FFXIV/Client/UI/AddonCharacterClass.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonCharacterClass.cs
@@ -9,17 +9,17 @@ namespace FFXIVClientStructs.FFXIV.Client.UI;
 [GenerateInterop]
 [Inherits<AtkUnitBase>]
 [VirtualTable("48 8D 05 ?? ?? ?? ?? 0F 57 C0 ?? ?? ?? 48 8B CF", 3)]
-[StructLayout(LayoutKind.Explicit, Size = 0x8A8)]
+[StructLayout(LayoutKind.Explicit, Size = 0x8D8)]
 public unsafe partial struct AddonCharacterClass {
-    [FieldOffset(0x238), FixedSizeArray] internal FixedSizeArray33<Pointer<AtkComponentBase>> _classComponents;
-    [FieldOffset(0x340)] public AtkComponentButton* TabsNode;
-    [FieldOffset(0x348)] public AtkTextNode* CurrentXPTextNode;
-    [FieldOffset(0x350)] public AtkTextNode* MaxXPTextNode;
-    [FieldOffset(0x358)] public AtkTextNode* CurrentDesynthesisLevelTextNode;
-    [FieldOffset(0x360)] public AtkTextNode* MaxDesynthesisLevelTextNode;
-    [FieldOffset(0x368), FixedSizeArray] internal FixedSizeArray33<ClassEntry> _classEntries;
+    [FieldOffset(0x238), FixedSizeArray] internal FixedSizeArray34<Pointer<AtkComponentBase>> _classComponents;
+    [FieldOffset(0x348)] public AtkComponentButton* TabsNode;
+    [FieldOffset(0x350)] public AtkTextNode* CurrentXPTextNode;
+    [FieldOffset(0x358)] public AtkTextNode* MaxXPTextNode;
+    [FieldOffset(0x360)] public AtkTextNode* CurrentDesynthesisLevelTextNode;
+    [FieldOffset(0x368)] public AtkTextNode* MaxDesynthesisLevelTextNode;
+    [FieldOffset(0x370), FixedSizeArray] internal FixedSizeArray34<ClassEntry> _classEntries;
 
-    [FieldOffset(0x8A0)] public int TabIndex;
+    [FieldOffset(0x8D0)] public int TabIndex;
 
     [MemberFunction("48 89 6C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 48 8B 81 ?? ?? ?? ?? 8B F2")]
     public partial void SetTab(int tab);
