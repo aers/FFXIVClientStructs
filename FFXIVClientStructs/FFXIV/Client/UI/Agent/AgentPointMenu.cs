2a07e8dd3b2902ef2f63efcdb10e41a273b66ca4
AtmoOmen <atmoomen2004@outlook.com>
2026-05-03 21:26:16 +0800

    PointMenu

---
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentPointMenu.cs
@@ -0,0 +1,89 @@
+namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+// Client::UI::Agent::AgentPointMenu
+//   Client::UI::Agent::AgentInterface
+//     Component::GUI::AtkModuleInterface::AtkEventInterface
+[Agent(AgentId.PointMenu)]
+[GenerateInterop]
+[Inherits<AgentInterface>]
+[StructLayout(LayoutKind.Explicit, Size = 0x48)]
+public unsafe partial struct AgentPointMenu {
+    [FieldOffset(0x28)] public PointMenuContext* Context;
+    [FieldOffset(0x30)] public CompletionTree* CompletionData;
+    [FieldOffset(0x38)] public int Phase;
+    [FieldOffset(0x3C)] public int SelectedIndex;
+    [FieldOffset(0x40)] private uint unkInt40;
+    [FieldOffset(0x44)] private byte unkByte44;
+
+    // Allocates a PointMenuContext and loads Excel Sheet PointMenu (1181)
+    [MemberFunction("40 55 41 56 41 57 48 83 EC ?? 48 8B 01 45 8B F8")]
+    public partial PointMenuContext* CreateContext(uint eventType, int unk);
+    
+    [MemberFunction("48 89 5C 24 ?? 48 89 74 24 ?? 55 57 41 54 41 56 41 57 48 8D 6C 24 ?? 48 81 EC ?? ?? ?? ?? 0F 29 B4 24")]
+    public partial void SendEntryToAddon(uint index);
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x140)]
+    public struct PointMenuContext {
+        [FieldOffset(0x00)] public AgentPointMenu* Agent;
+        [FieldOffset(0x08)] private void* unk08;
+        [FieldOffset(0x10)] private void* unk10;
+        [FieldOffset(0x18)] public ExcelSheetWaiter SheetWaiter;
+        [FieldOffset(0xB8)] public Utf8String TitleText;
+        [FieldOffset(0x120)] public StdVector<PointMenuEntry> Entries;
+        [FieldOffset(0x138)] public uint EventType;
+        [FieldOffset(0x13C)] public bool IsLoaded;
+    }
+
+    [StructLayout(LayoutKind.Explicit, Size = 0xA0)]
+    public struct ExcelSheetWaiter {
+        [FieldOffset(0x30)] public void* ScratchBuffer;
+        [FieldOffset(0x38)] private void* unk38;
+        [FieldOffset(0x40)] private void* unk40;
+        [FieldOffset(0x48)] private void* unkData48;
+        [FieldOffset(0x50)] private void* unkData50;
+        [FieldOffset(0x58)] private void* unk58;
+        [FieldOffset(0x60)] private void* unk60;
+        [FieldOffset(0x68)] private void* unk68;
+        [FieldOffset(0x70)] public void* OnLoadedCallback;
+        [FieldOffset(0x78)] public void* OnFailedCallback;
+        [FieldOffset(0x80)] public void* CallbackParam;
+        [FieldOffset(0x88)] public int SheetNumber;
+        [FieldOffset(0x8C)] private int unk8C;
+        [FieldOffset(0x90)] private void* unk90;
+        [FieldOffset(0x98)] private ushort flags98;
+    }
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x10)]
+    public struct CompletionTree {
+        [FieldOffset(0x00)] public CompletionTreeNode* Head;
+        [FieldOffset(0x08)] public ulong Count;
+    }
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x24)]
+    public struct CompletionTreeNode {
+        [FieldOffset(0x00)] public CompletionTreeNode* Left;
+        [FieldOffset(0x08)] public CompletionTreeNode* Parent;
+        [FieldOffset(0x10)] public CompletionTreeNode* Right;
+        [FieldOffset(0x18)] public byte Color;
+        [FieldOffset(0x19)] public byte IsNil;
+        [FieldOffset(0x1C)] public int Key;
+        [FieldOffset(0x20)] public int Bitfield;
+    }
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x88)]
+    public struct PointMenuEntry {
+        [FieldOffset(0x00)] public float X;
+        [FieldOffset(0x04)] public float Y;
+        [FieldOffset(0x08)] public float ClickAreaX;
+        [FieldOffset(0x0C)] public float ClickAreaY;
+        [FieldOffset(0x10)] public float ClickAreaWidth;
+        [FieldOffset(0x14)] public float ClickAreaHeight;
+        [FieldOffset(0x1A)] public byte State;
+        [FieldOffset(0x1B)] public byte IsElliptical;
+        [FieldOffset(0x1C)] public byte NavUp;
+        [FieldOffset(0x1D)] public byte NavDown;
+        [FieldOffset(0x1E)] public byte NavLeft;
+        [FieldOffset(0x1F)] public byte NavRight;
+        [FieldOffset(0x20)] public Utf8String Text;
+    }
+}
