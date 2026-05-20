5bca22100e4dae391727adadc8eaf9a218b9888f
Knightmore <35805408+Knightmore@users.noreply.github.com>
2026-05-20 22:32:24 +0200

    WKS JobState offset fix

--- FFXIVClientStructs/FFXIV/Client/Game/WKS/WKSManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/WKS/WKSManager.cs
@@ -86,22 +86,39 @@ public unsafe partial struct WKSManager {
     /// <summary>Per-job state block. WKSState contains 11 of these (one per cosmic class job).</summary>
     [StructLayout(LayoutKind.Explicit, Size = 0x148)]
     public struct WKSJobState {
-        [FieldOffset(0xF0)] public StdVector<WKSMissionState> ProvisionalTimeWeatherMissions;
-        [FieldOffset(0x108)] public StdVector<WKSMissionState> CriticalMissions;
+        [FieldOffset(0xB0)] public WKSMissionState ExtraBasicMission; // This held a single extra A-rank mission entry. Emitted by AgentWKSMission.GetBasicMissions. Purpose yet unclear.
+        [FieldOffset(0xC0)] public StdVector<WKSMissionState> SequentialMissions;
+        [FieldOffset(0xD8)] public StdVector<WKSMissionState> ProvisionalMissions;
+        [FieldOffset(0xF0)] public StdVector<WKSMissionState> CriticalMissions;
+        [FieldOffset(0x108)] public uint CriticalMissionData; // All jobs with currently open critical missions hold the same value. (e.g. 1779307018). Purpose yet unclear.
     }
 
     [GenerateInterop]
     [StructLayout(LayoutKind.Explicit, Size = 0x1088)]
     public partial struct WKSState {
-        [FieldOffset(0x00), FixedSizeArray] internal FixedSizeArray11<WKSJobState> _jobStates;
+        [FieldOffset(0x00)] private ushort Unk00;
+        [FieldOffset(0x04)] private byte Unk04;
+        [FieldOffset(0x08)] private ushort Unk08;
 
         /// <remarks>RowId of WKSDevGrade sheet.</remarks>
         [FieldOffset(0x0A)] public ushort DevGrade;
+
+        [FieldOffset(0x0C)] private uint Unk0C;
 
         /// <remarks>For Hub upgrades. RowId of WKSFateControl sheet.</remarks>
         [FieldOffset(0x10)] public ushort CurrentFateControlRowId;
         /// <remarks>For Hub upgrades. Id of Fate in FateManager.</remarks>
         [FieldOffset(0x12)] public ushort CurrentFateId;
+
+        [FieldOffset(0x14)] private byte Unk14; // Seems like some state flags which are checked in IsFunctionUnlocked. Bit 0 seems like "hub is built/active" 
+		
+        /// <remarks>0-based WKSPioneeringTrail row index for the currently visited planet (sheet row = this + 1).</remarks>
+        [FieldOffset(0x15)] public byte CurrentPlanetIndex; // Full row is highest subrow where WKSPioneeringTrail[row][sub].ActivationStage <= WKSState.DevGrade
+		
+        /// <remarks>0-based WKSPioneeringTrail row index for the latest unlocked planet. (sheet row = this + 1)</remarks>
+        [FieldOffset(0x16)] public byte LatestPlanetIndex;
+
+        [FieldOffset(0x18), FixedSizeArray] internal FixedSizeArray11<WKSJobState> _jobStates;
 
         /// <remarks> RowId of WKSMissionUnit sheet. </remarks>
         [FieldOffset(0xE30)] public ushort CurrentMissionUnitRowId;
