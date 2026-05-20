6d1a6a38090d8351cfe3afb4001f5df4fa28c707
RokasKil <rokas.kil5@gmail.com>
2026-05-20 20:03:48 +0300

    Update EffectContainer with tilt attributes

--- FFXIVClientStructs/FFXIV/Client/Game/Character/EffectContainer.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Character/EffectContainer.cs
@@ -15,11 +15,21 @@ public unsafe partial struct EffectContainer {
     [FieldOffset(0x30)] public StatusEffect StatusEffects;
     [FieldOffset(0x34)] public int MountTiltSetupState1;
     [FieldOffset(0x38)] public int MountTiltSetupState2;
-    // [FieldOffset(0x3C)] private byte Unk3C;
-    [FieldOffset(0x40)] public byte TiltParam1Type;
-    [FieldOffset(0x44)] public float TiltParam1Value;
-    [FieldOffset(0x48)] public byte TiltParam2Type;
-    [FieldOffset(0x4C)] public float TiltParam2Value;
+    [FieldOffset(0x40)] public float GroundTiltAngle;
+    [FieldOffset(0x44)] public float GroundTiltSpeed;
+    [FieldOffset(0x50)] public float FlightTiltAngle;
+    [FieldOffset(0x54)] public float FlightTiltSpeed;
+    
+    [FieldOffset(0x40), Obsolete("Invalid since 7.1")] public byte TiltParam1Type;
+    [FieldOffset(0x44), Obsolete("Invalid since 7.1")] public float TiltParam1Value;
+    [FieldOffset(0x48), Obsolete("Invalid since 7.1")] public byte TiltParam2Type;
+    [FieldOffset(0x4C), Obsolete("Invalid since 7.1")] public float TiltParam2Value;
+    
+    /// <summary>
+    /// Called when mounting/dismounting and maybe other state changes to set new tilt values 
+    /// </summary>
+    [MemberFunction("48 89 5C 24 ?? 55 48 83 EC ?? C6 41")]
+    public partial void LoadTiltData();
 
     [Flags]
     public enum StatusEffect : byte {
