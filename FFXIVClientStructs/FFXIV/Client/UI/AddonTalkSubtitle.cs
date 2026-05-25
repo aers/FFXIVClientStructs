c314fba6043b8c5320edbc59b3ceaf42994b2b4f
Haselnussbomber <mail@haselnussbomber.de>
2026-05-25 17:31:50 +0200

    Update AddonTalkSubtitle

--- FFXIVClientStructs/FFXIV/Client/UI/AddonTalkSubtitle.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonTalkSubtitle.cs
@@ -9,10 +9,12 @@ namespace FFXIVClientStructs.FFXIV.Client.UI;
 [GenerateInterop]
 [Inherits<AtkUnitBase>]
 [StructLayout(LayoutKind.Explicit, Size = 0x2B0)]
-public unsafe partial struct AddonTalkSubtitle {
+public partial struct AddonTalkSubtitle {
     [FieldOffset(0x238)] public Utf8String SubtitleText;
     [FieldOffset(0x2A0)] public uint SubtitleMaxWidth;
     [FieldOffset(0x2A4)] public float SubtitleScale;
-    [FieldOffset(0x2A8)] private bool Unk2A8;    //  If set, vf40 gets called when setting subtitle.
-    [FieldOffset(0x2A9)] private bool Unk2A9;    //  Checked and set in vf45 to call vf25.
+    /// <remarks> Used in OnSetup to not call Show when setting initial AtkValues. </remarks>
+    [FieldOffset(0x2A8)] public bool IsShowSuppressed;
+    /// <remarks> Checked and set in vf45 to call vf25. Something related to position (HudAnchoringTable). </remarks>
+    [FieldOffset(0x2A9)] private bool UnkIsAnchorPending; // not tested
 }
