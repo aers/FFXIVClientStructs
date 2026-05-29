612d57bdcb88a381a40241a6a44627fd16d837ea
pohky <1510568+pohky@users.noreply.github.com>
2026-05-29 15:45:04 +0200

    Update AgentPointMenu.cs

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentPointMenu.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentPointMenu.cs
@@ -14,12 +14,12 @@ public unsafe partial struct AgentPointMenu {
     [FieldOffset(0x30)] public StdMap<int, int>* CompletionData;
     [FieldOffset(0x38)] public int Phase;
     [FieldOffset(0x3C)] public int SelectedIndex;
-    [FieldOffset(0x40)] private uint unkInt40;
+    [FieldOffset(0x40)] private uint DefaultChoice;
     [FieldOffset(0x44)] private byte unkByte44;
 
     // Allocates a PointMenuContext and loads Excel Sheet PointMenu (1181)
     [MemberFunction("40 55 41 56 41 57 48 83 EC ?? 48 8B 01 45 8B F8")]
-    public partial void CreateContext(int eventType, int unk);
+    public partial void CreateContext(int pointMenu, int pointMenuChoiceIdx);
     
     [MemberFunction("E8 ?? ?? ?? ?? FF C3 3B DE 72 F0")]
     public partial void SendEntryToAddon(uint index);
@@ -30,26 +30,28 @@ public unsafe partial struct AgentPointMenu {
         [FieldOffset(0x08)] private void* unk08;
         [FieldOffset(0x10)] private void* unk10;
         [FieldOffset(0x18)] private ExcelSheetWaiter SheetWaiter; // SheetWaiter is in a substruct size of 0xA0
+
         [FieldOffset(0xB8)] public Utf8String TitleText;
         [FieldOffset(0x120)] public StdVector<PointMenuEntry> Entries;
-        [FieldOffset(0x138)] public int EventType;
+        [FieldOffset(0x138)] public int PointMenuId;
         [FieldOffset(0x13C)] public bool IsLoaded;
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x88)]
     public struct PointMenuEntry {
-        [FieldOffset(0x00)] public float X;
+        [FieldOffset(0x00), CExporterExcelBegin("PointMenuChoice")] public float X;
         [FieldOffset(0x04)] public float Y;
         [FieldOffset(0x08)] public float ClickAreaX;
         [FieldOffset(0x0C)] public float ClickAreaY;
         [FieldOffset(0x10)] public float ClickAreaWidth;
         [FieldOffset(0x14)] public float ClickAreaHeight;
-        [FieldOffset(0x1A)] public byte State;
-        [FieldOffset(0x1B)] public byte IsElliptical;
+        [FieldOffset(0x18)] public ushort PointMenuStringId;
+        [FieldOffset(0x1A)] public byte Shape;
+        [FieldOffset(0x1B)] private byte IsElliptical; // is what???
         [FieldOffset(0x1C)] public byte NavUp;
         [FieldOffset(0x1D)] public byte NavDown;
         [FieldOffset(0x1E)] public byte NavLeft;
-        [FieldOffset(0x1F)] public byte NavRight;
+        [FieldOffset(0x1F), CExporterExcelEnd] public byte NavRight;
         [FieldOffset(0x20)] public Utf8String Text;
     }
 }
