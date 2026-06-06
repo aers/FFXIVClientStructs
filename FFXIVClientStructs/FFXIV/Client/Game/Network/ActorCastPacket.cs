a0bb600cb7b1d60ada082c070c0069498ac1c44d
WorstAquaPlayer <maxwellaquaruiz@gmail.com>
2026-06-06 01:29:40 -0300

    Add ActorCastPacket and Handler

---
+++ FFXIVClientStructs/FFXIV/Client/Game/Network/ActorCastPacket.cs
@@ -0,0 +1,16 @@
+namespace FFXIVClientStructs.FFXIV.Client.Game.Network;
+
+[GenerateInterop]
+[StructLayout(LayoutKind.Explicit, Size = 0x20)]
+public partial struct ActorCastPacket {
+    [FieldOffset(0x00)] public ushort ActionId;
+    [FieldOffset(0x02)] public byte ActionType;
+    [FieldOffset(0x03)] public byte OmenDelay; // The value gets divided by 10.0f
+    [FieldOffset(0x04)] public uint ActionId_2;
+    [FieldOffset(0x08)] public float CastTime;
+    [FieldOffset(0x0C)] public uint TargetEntityId;
+    [FieldOffset(0x10)] public ushort RotationInt; // Quantized Rotation
+    [FieldOffset(0x12)] public bool Interruptible;
+    [FieldOffset(0x14)] public uint BallistaEntityId;
+    [FieldOffset(0x18), FixedSizeArray] internal FixedSizeArray3<ushort> _positionInt; // Quantized Position
+}
