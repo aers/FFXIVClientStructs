1931e016ccd3313e34c597fb253fde253f0af461
WorstAquaPlayer <maxwellaquaruiz@gmail.com>
2026-06-10 23:19:40 -0300

    Better name for ActorCastPacket Spell and Action IDs

--- FFXIVClientStructs/FFXIV/Client/Game/Network/ActorCastPacket.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Network/ActorCastPacket.cs
@@ -3,10 +3,10 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Network;
 [GenerateInterop]
 [StructLayout(LayoutKind.Explicit, Size = 0x20)]
 public partial struct ActorCastPacket {
-    [FieldOffset(0x00)] public ushort ActionId;
+    [FieldOffset(0x00)] public ushort SpellId;
     [FieldOffset(0x02)] public byte ActionType;
     [FieldOffset(0x03)] public byte OmenDelay; // The value gets divided by 10.0f
-    [FieldOffset(0x04)] public uint ActionId_2;
+    [FieldOffset(0x04)] public uint ActionId;
     [FieldOffset(0x08)] public float CastTime;
     [FieldOffset(0x0C)] public uint TargetEntityId;
     [FieldOffset(0x10)] public ushort RotationInt; // Quantized Rotation
