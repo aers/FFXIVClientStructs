679a710b96e87f559448afa06ba710168d27c95c
Universal Conquistador <universalconquistador@gmail.com>
2026-06-11 13:23:23 -0700

    Fixes threadlocals typo

--- FFXIVClientStructs/Interop/ThreadLocals.cs
+++ FFXIVClientStructs/Interop/ThreadLocals.cs
@@ -3,7 +3,7 @@ using FFXIVClientStructs.FFXIV.Client.Graphics.Kernel;
 namespace FFXIVClientStructs.Interop;
 
 [GenerateInterop]
-[StructLayout(LayoutKind.Explicit, Size = 0x203C4)]
+[StructLayout(LayoutKind.Explicit, Size = 0x203C5)]
 public unsafe partial struct ThreadLocals {
 
     [FieldOffset(0x238)] public Context* GraphicsKernelContext;
@@ -11,7 +11,7 @@ public unsafe partial struct ThreadLocals {
     /// <summary>
     /// Whether these thread locals have been initialized by <c>TlsCallback_0</c>.
     /// </summary>
-    [FieldOffset(0x203C3)] public bool IsInitialized;
+    [FieldOffset(0x203C4)] public bool IsInitialized;
 
     // Not in data.yml as this is already named by IDA and isn't technically a class member
     [MemberFunction("65 48 8B 04 25 30 00 00 00 C3")]
