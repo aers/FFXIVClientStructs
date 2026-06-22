b61092b51769d2921e19db82e149f8a58b507969
Haselnussbomber <mail@haselnussbomber.de>
2026-06-22 11:31:35 +0200

    Update LineVfxLayoutInstance

--- FFXIVClientStructs/FFXIV/Client/LayoutEngine/Layer/LineVfxLayoutInstance.cs
+++ FFXIVClientStructs/FFXIV/Client/LayoutEngine/Layer/LineVfxLayoutInstance.cs
@@ -1,13 +1,18 @@
 namespace FFXIVClientStructs.FFXIV.Client.LayoutEngine.Layer;
 
 // Client::LayoutEngine::Layer::LineVfxLayoutInstance
-//   Client::LayoutEngine::ILayoutInstance
-/// <summary>
-/// Encapsulates the Vfx for zone lines.
-/// </summary>
+//   Client::LayoutEngine::Layer::RangeLayoutInstance
+//     Client::LayoutEngine::ILayoutInstance
+//       Client::System::Common::NonCopyable
 [GenerateInterop]
-[Inherits<ILayoutInstance>]
+[Inherits<RangeLayoutInstance>]
 [StructLayout(LayoutKind.Explicit, Size = 0xA0)]
-public unsafe partial struct LineVfxLayoutInstance {
-    [FieldOffset(0x30)] public Transform Transform;
+public partial struct LineVfxLayoutInstance {
+    [FieldOffset(0x70)] public LineVfxStyle LineStyle;
+}
+
+public enum LineVfxStyle {
+    Red = 1,
+    Blue = 2,
+    RedFar = 3, // sets LayoutWorld.StreamingRadiusPerType[InstanceType.LineVfx] to 40.0f
 }
