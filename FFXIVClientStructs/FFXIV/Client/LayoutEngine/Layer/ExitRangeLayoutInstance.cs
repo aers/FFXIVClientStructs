d377c81309e3c899cc35f003c8fa15509d6d087a
Abyeon <17396444+Abyeon@users.noreply.github.com>
2026-06-21 23:58:13 -0400

    Add ExitRangeLayoutInstance.cs and LineVfxLayoutInstance.cs

---
+++ FFXIVClientStructs/FFXIV/Client/LayoutEngine/Layer/ExitRangeLayoutInstance.cs
@@ -0,0 +1,16 @@
+namespace FFXIVClientStructs.FFXIV.Client.LayoutEngine.Layer;
+
+// Client::LayoutEngine::Layer::ExitRangeLayoutInstance
+//   Client::LayoutEngine::ILayoutInstance
+/// <summary>
+/// BoxCollider that triggers loading into another zone.
+/// </summary>
+[GenerateInterop]
+[Inherits<ILayoutInstance>]
+[StructLayout(LayoutKind.Explicit, Size = 0xA0)]
+public unsafe partial struct ExitRangeLayoutInstance {
+    /// <summary>
+    /// TerritoryType that this exit leads to.
+    /// </summary>
+    [FieldOffset(0x86)] public ushort DestinationId;
+}
