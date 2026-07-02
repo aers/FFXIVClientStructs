72118edb63a871b93299a4f3dc2353bdacd7423e
Haselnussbomber <mail@haselnussbomber.de>
2026-07-02 21:03:31 +0200

    Add TODO

--- FFXIVClientStructs/FFXIV/Client/Network/PacketDispatcher.cs
+++ FFXIVClientStructs/FFXIV/Client/Network/PacketDispatcher.cs
@@ -14,7 +14,7 @@ public unsafe partial struct PacketDispatcher {
     [FieldOffset(0x10)] public NetworkModuleProxy* NetworkModuleProxy;
     [FieldOffset(0x18)] public uint GameSessionRandom;
     [FieldOffset(0x1C)] public uint LastPacketRandom;
-    [FieldOffset(0x20)] public uint Key0;
+    [FieldOffset(0x20)] public uint Key0; // TODO: array of 3
     [FieldOffset(0x24)] public uint Key1;
     [FieldOffset(0x28)] public uint Key2;
 
