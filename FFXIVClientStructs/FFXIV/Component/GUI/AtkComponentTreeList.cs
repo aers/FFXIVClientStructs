7f69e46a467f6e7a84bb204022280f912eb1bed0
Haselnussbomber <mail@haselnussbomber.de>
2026-07-06 16:46:09 +0200

    Update AtkComponentTreeList

--- FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentTreeList.cs
+++ FFXIVClientStructs/FFXIV/Component/GUI/AtkComponentTreeList.cs
@@ -15,7 +15,11 @@ namespace FFXIVClientStructs.FFXIV.Component.GUI;
 public unsafe partial struct AtkComponentTreeList : ICreatable<AtkComponentTreeList> {
     [FieldOffset(0x1B0)] public StdVector<Pointer<AtkComponentTreeListItem>> Items;
 
-    [FieldOffset(0x1E8)] private float Unk1E8; // ScrollMultiplier?
+    [FieldOffset(0x1E4)] public uint VisibleItemHeight;
+    [FieldOffset(0x1E8)] public uint MaxScrollPosition;
+    [FieldOffset(0x1EC)] public uint ItemCount;
+    [FieldOffset(0x1F0)] public uint VisibleItemCount;
+
     [FieldOffset(0x224)] public bool LayoutRefreshPending;
 
     [MemberFunction("40 53 48 83 EC 20 48 8B D9 E8 ?? ?? ?? ?? 33 C9 C7 83 ?? ?? ?? ?? ?? ?? ?? ?? 48 89 8B ?? ?? ?? ?? 48 8D 05 ?? ?? ?? ?? 48 89 03 0F 57 C0")]
@@ -29,7 +33,7 @@ public unsafe partial struct AtkComponentTreeList : ICreatable<AtkComponentTreeL
     public partial AtkComponentTreeListItem* GetItem(int index);
 
     [MemberFunction("E8 ?? ?? ?? ?? 84 C0 74 25 85 ED")]
-    public partial bool IsItemGroupHeader(int index);
+    public partial bool IsItemSectionHeader(int index);
 
     [MemberFunction("E8 ?? ?? ?? ?? 44 3B FF 75")]
     public partial void LoadAtkValues(
@@ -47,7 +51,13 @@ public unsafe partial struct AtkComponentTreeList : ICreatable<AtkComponentTreeL
     /// After calling this, you might also want to set <see cref="LayoutRefreshPending"/> to <c>true</c>.
     /// </remarks>
     [MemberFunction("E8 ?? ?? ?? ?? 44 88 A3 ?? ?? ?? ?? 80 4F")]
-    public partial void ExpandGroupExclusively(AtkComponentTreeListItem* groupHeaderItem, bool a3 = false);
+    public partial void ExpandGroupExclusively(AtkComponentTreeListItem* groupItem, bool suppressUpdate = false);
+
+    [MemberFunction("E8 ?? ?? ?? ?? 48 3B D8 74 ?? 48 8B 8F")]
+    public partial AtkComponentTreeListItem* FindItemGroup(AtkComponentTreeListItem* item);
+
+    [MemberFunction("E8 ?? ?? ?? ?? 48 8B 87 ?? ?? ?? ?? 48 8B 74 24 ?? 48 8B 5C 24 ?? 48 85 C0 74")]
+    public partial void CalculateVisibleItemHeightAndCount(uint* outItemHeight, uint* outItemCount);
 }
 
 [StructLayout(LayoutKind.Explicit, Size = 0x48)]
@@ -55,11 +65,34 @@ public unsafe struct AtkComponentTreeListItem {
     [FieldOffset(0)] public StdVector<uint> UIntValues; // first entry should be AtkComponentTreeListItemType
     [FieldOffset(0x18)] public StdVector<CStringPointer> StringValues; // first entry should be the displayed text
     [FieldOffset(0x30)] public AtkComponentListItemRenderer* Renderer;
-
-    // [FieldOffset(0x42)] public byte Flags; // for groups: 1 = expanded
-    // [FieldOffset(0x43)] public byte Flags2;
+    [FieldOffset(0x38)] private void* Unk38;
+    [FieldOffset(0x40)] public ushort Height;
+    [FieldOffset(0x42)] public TreeListItemState State;
+    [FieldOffset(0x43)] public TreeListItemType Type;
+    [FieldOffset(0x44)] public byte Depth;
+    [FieldOffset(0x45)] public bool IsHidden;
 }
 
+[Flags]
+public enum TreeListItemState : byte {
+    None = 0,
+    Expanded = 1 << 0,
+    Unk2 = 1 << 1,
+    Unk4 = 1 << 2,
+    Unk8 = 1 << 3,
+    Expanding = 1 << 4,
+    Disabled = 1 << 5,
+}
+
+[Flags]
+public enum TreeListItemType : byte {
+    None = 0,
+    LastItemInGroup = 1 << 0,
+    Group = 1 << 1,
+    SectionHeader = 1 << 2,
+}
+
+[Obsolete("Use TreeListItemType (exposed via AtkComponentTreeListItem.Type)")]
 public enum AtkComponentTreeListItemType : uint {
     Leaf = 0,
     LastLeafInGroup = 1,
