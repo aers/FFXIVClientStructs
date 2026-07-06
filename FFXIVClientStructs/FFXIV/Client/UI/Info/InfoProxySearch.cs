79ea4dfea55c0fc14f9471e2399c2ef7ffd90250
AtmoOmen <atmoomen2004@outlook.com>
2026-07-06 23:00:42 +0800

    comment

--- FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxySearch.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxySearch.cs
@@ -16,7 +16,7 @@ public unsafe partial struct InfoProxySearch {
     [FieldOffset(0xE8)] public byte LanguageMask; // bit1=JP, bit2=EN, bit3=DE, bit4=FR, 0xFF = all
     [FieldOffset(0xF0)] public byte GrandCompanyMask; // bit0=Maelstrom, bit1=TwinAdder, bit2=ImmortalFlames, 0xFF = all
     [FieldOffset(0xF8)] public ulong OnlineStatusMask; // 0 = unset (defaults to 1ul << 47 = Online only)
-    [FieldOffset(0x100), FixedSizeArray] internal FixedSizeArray50<ushort> _locationIDs;
+    [FieldOffset(0x100), FixedSizeArray] internal FixedSizeArray50<ushort> _locationIDs; // PlaceName IDs from PlayerSearchSubLocation.PlaceName
     [FieldOffset(0x164), FixedSizeArray(isString: true)] internal FixedSizeArray32<byte> _name;
     [FieldOffset(0x184)] private uint Unk184;
     [FieldOffset(0x188)] private uint Unk188;
