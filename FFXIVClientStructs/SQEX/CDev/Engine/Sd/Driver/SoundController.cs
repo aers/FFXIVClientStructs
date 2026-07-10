4ec014c37cf4265d08c23798d0012544ffb634e6
Haselnussbomber <mail@haselnussbomber.de>
2026-07-10 16:51:29 +0200

    Add unsafe modifier to SoundController

--- FFXIVClientStructs/SQEX/CDev/Engine/Sd/Driver/SoundController.cs
+++ FFXIVClientStructs/SQEX/CDev/Engine/Sd/Driver/SoundController.cs
@@ -7,7 +7,7 @@ namespace FFXIVClientStructs.SQEX.CDev.Engine.Sd.Driver;
 [GenerateInterop(isInherited: true)]
 [Inherits<SdMemoryAllocator>]
 [StructLayout(LayoutKind.Explicit, Size = 0x20)]
-public partial struct SoundController {
+public unsafe partial struct SoundController {
     [FieldOffset(0x08)] private int Unk08;
     [FieldOffset(0x10)] private nint Unk10; // CDev.Engine.Sd.SoundLayout*?
     [FieldOffset(0x18)] private nint SharedMutex; // std::shared_mutex?
