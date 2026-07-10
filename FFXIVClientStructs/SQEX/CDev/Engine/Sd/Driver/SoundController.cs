d0df2e40aa82224eb704312f9bb9b8c4f5add442
Universal Conquistador <universalconquistador@gmail.com>
2026-07-09 23:39:39 -0700

    Adds SoundController positioning funcs.

--- FFXIVClientStructs/SQEX/CDev/Engine/Sd/Driver/SoundController.cs
+++ FFXIVClientStructs/SQEX/CDev/Engine/Sd/Driver/SoundController.cs
@@ -1,3 +1,5 @@
+using FFXIVClientStructs.FFXIV.Common.Math;
+
 namespace FFXIVClientStructs.SQEX.CDev.Engine.Sd.Driver;
 
 // SQEX::CDev::Engine::Sd::Driver::SoundController
@@ -9,4 +11,10 @@ public partial struct SoundController {
     [FieldOffset(0x08)] private int Unk08;
     [FieldOffset(0x10)] private nint Unk10; // CDev.Engine.Sd.SoundLayout*?
     [FieldOffset(0x18)] private nint SharedMutex; // std::shared_mutex?
+
+    [MemberFunction("E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 41 8B BE")]
+    public partial void SetPosition(Vector4 position);
+
+    [MemberFunction("E8 ?? ?? ?? ?? 48 8D 4B 38 40 84 FF")] // 0x141E6D600
+    public partial void SetIsNonPositional(bool isPositional);
 }
