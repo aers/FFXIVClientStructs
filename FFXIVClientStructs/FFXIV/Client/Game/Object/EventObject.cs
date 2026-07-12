0bc864f8665e0e8cb1a8c899947b9ff18aaf4e84
Joshua Goins <josh@redstrate.com>
2026-07-12 19:29:44 -0400

    Add GetEventHandlers proxy function for EventObject
    This is used in EObj animation code, and pretty much just proxies what
    the GameObject function does.

--- FFXIVClientStructs/FFXIV/Client/Game/Object/EventObject.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Object/EventObject.cs
@@ -1,3 +1,5 @@
+using EventHandler = FFXIVClientStructs.FFXIV.Client.Game.Event.EventHandler;
+
 namespace FFXIVClientStructs.FFXIV.Client.Game.Object;
 
 // Client::Game::Object::EventObject
@@ -5,7 +7,7 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Object;
 [GenerateInterop]
 [Inherits<GameObject>]
 [StructLayout(LayoutKind.Explicit, Size = 0x1C0)]
-public partial struct EventObject {
+public unsafe partial struct EventObject {
     [FieldOffset(0x1A0), CExporterExcel("EObj")] public nint EObjRowPtr;
     [FieldOffset(0x1A8), CExporterExcel("ExportedSG")] public nint ExportedSGRowPtr;
     [FieldOffset(0x1B2)] public ushort SharedTimelineState; // ActorControl category 0x199 (SetSharedTimelineState) sets this field
@@ -13,4 +15,9 @@ public partial struct EventObject {
 
     [MemberFunction("E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 4D 85 F6 0F 84 ?? ?? ?? ?? 8B 44 24 70 BE ?? ?? ?? ??")]
     public partial void PlayAnimation(uint entityId, uint actionId, ulong unknown);
+
+    /// <param name="outHandlers">Should point to array that can fit up to 32 pointers.</param>
+    /// <returns>Num elements filled.</returns>
+    [MemberFunction("E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 4D 85 F6 0F 84 ?? ?? ?? ?? 8B 44 24 70 BE ?? ?? ?? ??")]
+    public partial int GetEventHandlers(EventHandler** outHandlers);
 }
