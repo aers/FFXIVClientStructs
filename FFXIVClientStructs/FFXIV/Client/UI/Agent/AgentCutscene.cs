6b7ba9d6ddda56def9db19314800adf9aebbbdf5
Haselnussbomber <mail@haselnussbomber.de>
2026-07-15 12:55:23 +0200

    Update AgentCutscene

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentCutscene.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentCutscene.cs
@@ -12,6 +12,11 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
 public unsafe partial struct AgentCutscene {
     [FieldOffset(0x30)] public uint SkipDialogAddonId;
     [FieldOffset(0x38)] public AtkModuleInterface.AtkEventInterface* SkipCallback;
+    [FieldOffset(0x40)] public uint TalkAutoMessageSettingAddonId;
+    [FieldOffset(0x44)] public uint TalkAutoMessageSelectorAddonId;
+    [FieldOffset(0x48)] public uint TalkAutoMessageSelectorCancelAddonId; // SelectYesno when closing without saving
+    [FieldOffset(0x4C)] public byte PendingTextAutoAdvanceSetting; // 0 = Apply to All Dialogue, 1 = Apply to Voiced Dialogue Only
+    [FieldOffset(0x4D)] public byte PendingUnvoicedAutoAdvanceSpeed;
 
     [MemberFunction("4C 8B DC 56 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 84 24 ?? ?? ?? ?? 83 79 ?? 00 48 8B F1 0F 87")]
     public partial bool OpenSkipDialog(AtkModuleInterface.AtkEventInterface* callback);
