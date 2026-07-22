0b41db3e531fc5e08d61156a2abd78ed99456839
Dante Souza e Souza <lokinmodar@gmail.com>
2026-07-22 12:06:02 -0300

    Correct item detail flag mapping

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentItemDetail.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentItemDetail.cs
@@ -30,7 +30,7 @@ public unsafe partial struct AgentItemDetail {
     [FieldOffset(0x11C)] public uint TypeOrId;
     [FieldOffset(0x120)] public uint Index; // The index of the item in the inventory. 0 for chat, ??? for KeyItems, position top-bottom inside a shop.
     [FieldOffset(0x124)] public int BuyQuantity; // The quantity selected for buying, which is previewed on the tooltip as "in bag" quantity, -1 outside shops
-    [FieldOffset(0x128)] public byte Flag1; // Related to checking the inventory
+    [FieldOffset(0x128)] public uint Flag1; // Related to checking the inventory
     [FieldOffset(0x134)] public int MaxStackSize;
     [FieldOffset(0x138)] public uint ItemId;
     [FieldOffset(0x13C)] private int Unk13C;
@@ -41,10 +41,10 @@ public unsafe partial struct AgentItemDetail {
     [FieldOffset(0x21E)] public byte Flag3; // If set to zero, avoids an early return in addon->Show()
 
     /// <remarks>
-    /// <paramref name="unk128"/> is written as a 32-bit value at offset <c>0x128</c>.
+    /// <paramref name="flag1"/> is written as a 32-bit value at offset <c>0x128</c>.
     /// </remarks>
     [MemberFunction("48 89 5C 24 08 48 89 6C 24 10 48 89 74 24 18 57 48 83 EC 20 8B FA 41 8B E9 BA A1 00 00 00 41 8B F0 48 8B D9 E8 ?? ?? ?? ??")]
-    public partial void HandleItemHover(DetailKind detailKind, uint typeOrId, uint index, int buyQuantity, uint unk128);
+    public partial void HandleItemHover(DetailKind detailKind, uint typeOrId, uint index, int buyQuantity, uint flag1);
 
     [MemberFunction("40 55 53 56 57 41 54 41 55 41 56 41 57 48 8D 6C 24 F9 48 81 EC A8 00 00 00 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 45 F7 48 8B 5D 7F 4C 8B F1 8B 4D 77 49 8B F9 44 8B 6D 6F 49 8B F0 4C 89 4D AF 4C 8B FA")]
     public partial bool OnItemHovered(InventoryItem** item, InventoryType* inventoryType, ushort* slot, uint index, uint typeOrId, InventoryItem* fallbackItem);
