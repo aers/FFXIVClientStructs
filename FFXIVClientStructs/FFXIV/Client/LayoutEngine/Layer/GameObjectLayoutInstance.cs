3c6503e06f3a25b3aa5705ec74d9aea162df14be
Haselnussbomber <mail@haselnussbomber.de>
2026-07-28 16:36:42 +0200

    Add GameObjectLayoutInstance

---
+++ FFXIVClientStructs/FFXIV/Client/LayoutEngine/Layer/GameObjectLayoutInstance.cs
@@ -0,0 +1,11 @@
+namespace FFXIVClientStructs.FFXIV.Client.LayoutEngine.Layer;
+
+// Client::LayoutEngine::Layer::GameObjectLayoutInstance
+//   Client::LayoutEngine::ILayoutInstance
+[GenerateInterop(isInherited: true)]
+[Inherits<ILayoutInstance>]
+[StructLayout(LayoutKind.Explicit, Size = 0xA0)]
+public partial struct GameObjectLayoutInstance {
+    /// <remarks> Which sheet this RowId is part of most likely depends on <see cref="ILayoutInstance.Identifier.Type"/>. </remarks>
+    [FieldOffset(0x30)] private uint BaseId;
+}
