59665e6ad2db63a824f03a926bcfc6c36c4c08ec
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-07-29 00:08:08 +0100

    Update AddonSalvageDialog.cs

--- FFXIVClientStructs/FFXIV/Client/UI/AddonSalvageDialog.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonSalvageDialog.cs
@@ -14,15 +14,15 @@ public unsafe partial struct AddonSalvageDialog {
 
     [FieldOffset(0x248)] private byte Unk248;
     [FieldOffset(0x250)] public AtkComponentCheckBox* GuaranteeNQResultsCheckboxNode;
-    [FieldOffset(0x258)] private byte GuaranteeNQResultsEnabled; // this is cached and has no bearing on the checkbox itself
+    [FieldOffset(0x258)] public bool GuaranteeNQResultsEnabled; // this is cached and has no bearing on the checkbox itself
 
     [FieldOffset(0x260)] private byte Unk260;
     [FieldOffset(0x268)] public AtkComponentCheckBox* BulkDesynthCheckboxNode;
-    [FieldOffset(0x270)] private byte BulkDesynthEnabled;
+    [FieldOffset(0x270)] public bool BulkDesynthEnabled;
 
     [FieldOffset(0x278)] private byte Unk278;
     [FieldOffset(0x280)] private AtkComponentCheckBox* Unk280; // Unused
-    [FieldOffset(0x288)] private byte Unk288Checked;
+    [FieldOffset(0x288)] private bool Unk288Checked;
 
     [FieldOffset(0x290)] public AtkComponentButton* DesynthesizeButton;
     [FieldOffset(0x298)] public AtkComponentButton* CancelButtonNode;
