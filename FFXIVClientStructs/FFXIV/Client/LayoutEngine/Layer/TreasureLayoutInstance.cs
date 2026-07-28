e3ce3338941b737ffeab888a74013a392613b711
Haselnussbomber <mail@haselnussbomber.de>
2026-07-28 16:36:46 +0200

    Add TreasureLayoutInstance

---
+++ FFXIVClientStructs/FFXIV/Client/LayoutEngine/Layer/TreasureLayoutInstance.cs
@@ -0,0 +1,9 @@
+namespace FFXIVClientStructs.FFXIV.Client.LayoutEngine.Layer;
+
+// Client::LayoutEngine::Layer::TreasureLayoutInstance
+//   Client::LayoutEngine::GameObjectLayoutInstance
+//     Client::LayoutEngine::ILayoutInstance
+[GenerateInterop]
+[Inherits<GameObjectLayoutInstance>]
+[StructLayout(LayoutKind.Explicit, Size = 0xA0)]
+public partial struct TreasureLayoutInstance;
