29181ca71d3f9c535e4dd4c3dc02c99f5e6cfc00
Zeffuro <Jeffroiscool@gmail.com>
2026-08-02 01:46:20 +0200

    Changes

--- FFXIVClientStructs/FFXIV/Client/UI/Arrays/PartyListNumberArray.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Arrays/PartyListNumberArray.cs
@@ -44,20 +44,20 @@ public unsafe partial struct PartyListNumberArray {
     public partial struct PartyListMemberNumberArray {
         [FieldOffset(0), FixedSizeArray, CExporterIgnore] internal FixedSizeArray43<int> _data;
 
-        [FieldOffset(3 * 4)] public int Level;
-        [FieldOffset(4 * 4)] public int ClassIconId;
-        [FieldOffset(7 * 4)] public int CurrentHealth;
-        [FieldOffset(8 * 4)] public int MaxHealth;
+        [FieldOffset(2 * 4)] public int Level;
+        [FieldOffset(3 * 4)] public int ClassIconId;
+        [FieldOffset(6 * 4)] public int CurrentHealth;
+        [FieldOffset(7 * 4)] public int MaxHealth;
         /// <summary>
         /// Goes from 0 to 200%
         /// </summary>
-        [FieldOffset(9 * 4)] public int ShieldsPercentage;
-        [FieldOffset(10 * 4)] public int CurrentMana;
-        [FieldOffset(11 * 4)] public int MaxMana;
+        [FieldOffset(8 * 4)] public int ShieldsPercentage;
+        [FieldOffset(9 * 4)] public int CurrentMana;
+        [FieldOffset(10 * 4)] public int MaxMana;
         /// <summary>
         /// Goes from 0 to 100%
         /// </summary>
-        [FieldOffset(13 * 4)] public int EnmityPercent;
+        [FieldOffset(12 * 4)] public int EnmityPercent;
         /// <summary>
         /// Starts at 1
         /// <br/>[A]
@@ -66,22 +66,23 @@ public unsafe partial struct PartyListNumberArray {
         /// <br/>...
         /// <br/>[8]
         /// </summary>
-        [FieldOffset(14 * 4)] public int EnmityLevel;
+        [FieldOffset(13 * 4)] public int EnmityLevel;
         /// <summary>
         /// Amount of Statuses applied to the player.
         /// <br/>Max is 10.
         /// </summary>
-        [FieldOffset(17 * 4)] public int StatusCount;
-        [FieldOffset(18 * 4), FixedSizeArray] internal FixedSizeArray10<int> _statusIconIds;
-        [FieldOffset(28 * 4), FixedSizeArray] internal FixedSizeArray10<bool> _statusIsDispellable;
+        [FieldOffset(16 * 4)] public int StatusCount;
+        [FieldOffset(17 * 4), FixedSizeArray] internal FixedSizeArray10<int> _statusIconIds;
+        [FieldOffset(27 * 4), FixedSizeArray] internal FixedSizeArray10<bool> _statusIsDispellable;
         /// <summary>
         /// -1 if not active
         /// </summary>
-        [FieldOffset(38 * 4)] public int CastTime;
-        [FieldOffset(39 * 4)] public int CastId;
+        [FieldOffset(37 * 4)] public int CastTime;
+        [FieldOffset(38 * 4)] public int CastId;
         [Obsolete("Use EntityId Instead.")]
-        [FieldOffset(41 * 4)] public int ContentId;
-        [FieldOffset(41 * 4)] public uint EntityId;
-        [FieldOffset(42 * 4)] public bool Targetable;
+        [FieldOffset(40 * 4)] public int ContentId;
+        [FieldOffset(40 * 4)] public uint EntityId;
+        [FieldOffset(41 * 4)] public bool Targetable;
+        [FieldOffset(42 * 4)] public int DisplayRow;
     }
 }
