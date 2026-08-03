b6762f71646cfb6ebf672460c047fa24a2e833bf
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-08-03 23:29:57 +0100

    add fish stuff

--- FFXIVClientStructs/FFXIV/Client/Game/UI/FishingNote.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/FishingNote.cs
@@ -1,5 +1,29 @@
+using FFXIVClientStructs.FFXIV.Common.Component.Excel;
+
 namespace FFXIVClientStructs.FFXIV.Client.Game.UI;
 
 // Client::Game::UI::FishingNote
 [StructLayout(LayoutKind.Explicit, Size = 0xE0)]
-public struct FishingNote;
+public unsafe struct FishingNote {
+    [FieldOffset(0x50)] public FishingNoteMode Mode;
+
+    [FieldOffset(0x48)] public FishingNoteLoadState FishingLoadState;
+    [FieldOffset(0x4C)] public FishingNoteLoadState SpearfishingLoadState;
+
+    [FieldOffset(0x70)] public ExcelSheetWaiter* FishingSpotWaiter;
+    [FieldOffset(0x78)] public ExcelSheetWaiter* FishParameterWaiter;
+    [FieldOffset(0x80)] public ExcelSheetWaiter* SpearfishingNotebookWaiter;
+    [FieldOffset(0x88)] public ExcelSheetWaiter* SpearfishingItemWaiter;
+    [FieldOffset(0x90)] public ExcelSheetWaiter* FishingNoteInfoWaiter;
+}
+
+public enum FishingNoteMode : byte {
+    Fishing = 0,
+    Spearfishing = 1,
+}
+
+public enum FishingNoteLoadState {
+    None = 0,
+    Loading = 1, // waiters are loading
+    Ready = 2,
+}
--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentFishGuide.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentFishGuide.cs
@@ -8,6 +8,56 @@ namespace FFXIVClientStructs.FFXIV.Client.UI.Agent;
 [Inherits<AgentInterface>]
 [StructLayout(LayoutKind.Explicit, Size = 0x1A8)]
 public unsafe partial struct AgentFishGuide {
+    [FieldOffset(0x28)] public uint SelectedItemId;
+    [FieldOffset(0x2C), FixedSizeArray] internal FixedSizeArray2<TabSelection> _tabSelections; // index w/ IsSpearfishing
+    [FieldOffset(0x30)] private byte Unk30;
+    [FieldOffset(0x31)] public bool IsSpearfishing;
+    [FieldOffset(0x32)] public bool IsSpearfishingUnlocked; // checks quest 2922
+
+    [FieldOffset(0x40)] private uint Unk40; // some addonid
+
+    [FieldOffset(0x48)] public FishGuideData* Data;
+
+    // some filter stuff
+    [FieldOffset(0x50)] private ushort Unk50;
+    [FieldOffset(0x52)] private ushort Unk52;
+    [FieldOffset(0x54)] private byte Unk54;
+    [FieldOffset(0x55)] private byte Unk55;
+    [FieldOffset(0x56)] private byte Unk56;
+
+    [FieldOffset(0x60)] private nint Unk60;
+    [FieldOffset(0x68)] private nint Unk68;
+
     [MemberFunction("E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 44 0F B6 C3 33 D2")]
     public partial void OpenForItemId(uint itemId, bool isSpearfishing);
+
+    [MemberFunction("E8 ?? ?? ?? ?? 48 8B CB E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 48 8B 4B 48")]
+    public partial void UpdateFishList(); // sets entries to NumberArray #67 and then refreshes the icons
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x02)]
+    public struct TabSelection {
+        [FieldOffset(0x00)] public byte PageIndex;
+        [FieldOffset(0x01)] public sbyte SelectedIndex;
+    }
+
+    [GenerateInterop]
+    [StructLayout(LayoutKind.Explicit, Size = 0x5A0)]
+    public unsafe partial struct FishGuideData {
+        [FieldOffset(0x08)] public AgentFishGuide* Agent;
+        [FieldOffset(0x10)] public StdVector<FishGuideListEntry> Entries;
+        [FieldOffset(0x28)] public StdVector<Pointer<FishGuideListEntry>> EntryPointers;
+        [FieldOffset(0x40), FixedSizeArray] internal FixedSizeArray100<int> _pageIcons;
+    }
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x14)]
+    public struct FishGuideListEntry {
+        [FieldOffset(0x00)] public uint ItemId;
+        [FieldOffset(0x04)] public ushort FishParameterId;
+        [FieldOffset(0x06)] private ushort Unk6;
+        [FieldOffset(0x08)] public ushort GatheringSubCategoryId;
+        [FieldOffset(0x0A)] private ushort UnkA;
+        [FieldOffset(0x0E)] public bool IsCaught;
+        [FieldOffset(0x10)] public bool HasItem;
+        [FieldOffset(0x11)] private bool Unk11;
+    }
 }
