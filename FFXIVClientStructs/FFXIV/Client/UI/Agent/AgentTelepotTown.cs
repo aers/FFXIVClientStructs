b3d7abb120543089d28fe8f34783d20cdc24ad1e
AtmoOmen <atmoomen2004@outlook.com>
2026-08-05 00:44:53 +0800

    some other fields

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentTelepotTown.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentTelepotTown.cs
@@ -29,15 +29,15 @@ public unsafe partial struct AgentTelepotTownData {
 [StructLayout(LayoutKind.Explicit, Size = 0x18)]
 public struct AetheryteEntry {
     [FieldOffset(0x00)] public uint AetheryteID;
-    [FieldOffset(0x04)] private uint Unk04;
-    [FieldOffset(0x08)] public ushort PlaceNameID;
-    [FieldOffset(0x0A)] private ushort Unk0A;
-    [FieldOffset(0x0C)] private uint Unk0C;
-    [FieldOffset(0x10)] public ushort TerritoryTypeID;
-    [FieldOffset(0x12)] private byte Unk12;
+    [FieldOffset(0x04)] private uint Unk04; // only written when marker below is not found
+    [FieldOffset(0x08)] public ushort PlaceNameID; // Aetheryte.AethernetName
+    [FieldOffset(0x0A)] private ushort PlaceNameID2; // written together with PlaceNameID
+    [FieldOffset(0x0C)] public ushort MarkerIndex; // index of the matching map marker, 100 when not found
+    [FieldOffset(0x0E)] public ushort MapID; // Aetheryte.Map
+    [FieldOffset(0x10)] public ushort TerritoryTypeID; // Aetheryte.Territory
+    [FieldOffset(0x12)] public byte GroupIndex; // index into the group table at AgentTelepotTownData+0x60C
     [FieldOffset(0x13)] public bool IsLocked;
-    [FieldOffset(0x14)] private byte Unk14;
+    [FieldOffset(0x14)] public bool IsUnusable;
     [FieldOffset(0x15)] public bool IsAetheryte;
     [FieldOffset(0x16)] public bool IsCurrent;
-    [FieldOffset(0x17)] private byte Unk17;
 }
