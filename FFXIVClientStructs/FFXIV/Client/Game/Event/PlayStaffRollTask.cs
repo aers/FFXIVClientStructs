a109242ce308f1f690a2084a26b57848cf596b82
pohky <1510568+pohky@users.noreply.github.com>
2026-08-07 10:20:07 +0200

    Update PlayStaffRollTask.cs

--- FFXIVClientStructs/FFXIV/Client/Game/Event/PlayStaffRollTask.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Event/PlayStaffRollTask.cs
@@ -6,7 +6,7 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Event;
 //   Client::Game::Event::EventSceneTaskInterface
 [GenerateInterop]
 [Inherits<EventSceneTaskInterface>]
-[StructLayout(LayoutKind.Explicit, Size = 0x90)]
+[StructLayout(LayoutKind.Explicit, Size = 0x20)]
 public partial struct PlayStaffRollTask {
     [FieldOffset(0x10)] public uint StaffRollType; // not sure what id this is, but it also controls which BGM is used (hardcoded)
     [FieldOffset(0x14)] public CreditMode Mode;
