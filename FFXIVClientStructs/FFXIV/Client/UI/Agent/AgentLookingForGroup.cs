cb911c2bc3640c4ef42040095f83dd90ff12e029
MidoriKami <mi_kami@outlook.com>
2026-08-08 21:52:08 -0700

    Add Populate recruitment criteria popup

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentLookingForGroup.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentLookingForGroup.cs
@@ -60,6 +60,15 @@ public unsafe partial struct AgentLookingForGroup {
     [MemberFunction("40 55 53 41 54 41 57 48 8D AC 24 ?? ?? ?? ?? B8")]
     public partial void PopulateListingData(Detailed* listingData);
 
+    /// <summary>
+    /// Populates the UI fields for AddonLookingForGroupCondition with data from <see cref="StoredRecruitmentInfo"/>.
+    /// </summary>
+    /// <remarks>
+    /// Doesn't seem to respect the role flags, but seems to respect everything else.
+    /// </remarks>
+    [MemberFunction("48 89 5C 24 ?? 48 89 74 24 ?? 48 89 7C 24 ?? 55 41 54 41 55 41 56 41 57 48 8D AC 24 ?? ?? ?? ?? B8 ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 2B E0 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 85 ?? ?? ?? ?? 8B 99")]
+    public partial void PopulateRecruitmentCriteriaPopup(bool unknown1, bool unknown2);
+
     [StructLayout(LayoutKind.Explicit, Size = 0x78)]
     public unsafe partial struct TreasureMapDetail {
         [FieldOffset(0x00)] public Utf8String String;
