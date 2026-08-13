b146318f029b631c7997d5accd6ab316ec5fa25e
Haselnussbomber <mail@haselnussbomber.de>
2026-08-13 16:24:28 +0200

    Update SendEventCompletePacket param a3

--- FFXIVClientStructs/FFXIV/Client/Network/PacketDispatcher.cs
+++ FFXIVClientStructs/FFXIV/Client/Network/PacketDispatcher.cs
@@ -88,7 +88,7 @@ public unsafe partial struct PacketDispatcher {
     public static partial void HandleContentsFinderNotificationPacket(ContentsFinderNotificationPacket* packet);
 
     [MemberFunction("E8 ?? ?? ?? ?? EB 10 48 8B 0D")]
-    public static partial void SendEventCompletePacket(EventId eventId, short scene, byte a3, uint* payload, byte payloadSize, void* a6);
+    public static partial void SendEventCompletePacket(EventId eventId, short scene, byte luaStatus, uint* payload, byte payloadSize, void* a6);
 
     [MemberFunction("40 53 57 48 81 EC ?? ?? ?? ?? 48 8B FA 8B")]
     public static partial void HandleActorCastPacket(uint entityId, ActorCastPacket* packet);
