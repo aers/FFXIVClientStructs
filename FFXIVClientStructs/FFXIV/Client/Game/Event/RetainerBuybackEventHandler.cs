5cd8fb645be58b16903f3c2ecd05c0d87d891a0e
Haselnussbomber <mail@haselnussbomber.de>
2026-08-18 16:49:47 +0200

    Add RetainerBuybackEventHandler.UIListener

--- FFXIVClientStructs/FFXIV/Client/Game/Event/RetainerBuybackEventHandler.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Event/RetainerBuybackEventHandler.cs
@@ -6,10 +6,17 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Event;
 // Client::Game::Event::RetainerBuybackEventHandler
 //   Client::Game::Event::EventHandler
 //   Client::UI::Agent::AgentContext::AgentContextUpdateChecker
-//   Component::GUI::AtkModuleInterface::AtkEventInterface
+//   Client::Game::Event::RetainerBuybackEventHandler::UIListener
+//     Component::GUI::AtkModuleInterface::AtkEventInterface
 [GenerateInterop]
 [Inherits<EventHandler>]
 [Inherits<AgentContextUpdateChecker>]
-[Inherits<AtkModuleInterface.AtkEventInterface>]
+[Inherits<RetainerBuybackEventHandler.UIListener>]
 [StructLayout(LayoutKind.Explicit, Size = 0x1D8)]
-public partial struct RetainerBuybackEventHandler;
+public partial struct RetainerBuybackEventHandler {
+    [GenerateInterop(isInherited: true)]
+    [Inherits<AtkModuleInterface.AtkEventInterface>]
+    [StructLayout(LayoutKind.Explicit, Size = 0x18)]
+    public partial struct UIListener;
+}
+
