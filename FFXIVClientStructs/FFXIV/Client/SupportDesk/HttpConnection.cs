2781aacf45d9cfef4e226efacf6f7d1ac467b99c
Joshua Goins <josh@redstrate.com>
2026-08-20 15:22:15 -0400

    Add remaining interesting bits for the SupportDesk HTTP API
    This will enable us to finally introspect the data for these requests!
    The most important bit was figuring out the stages and then making up a
    sensible delegate to hook into.

--- FFXIVClientStructs/FFXIV/Client/SupportDesk/HttpConnection.cs
+++ FFXIVClientStructs/FFXIV/Client/SupportDesk/HttpConnection.cs
@@ -7,11 +7,20 @@ public unsafe partial struct HttpConnection {
     [VirtualFunction(0)]
     public partial HttpRequest* Dtor(byte freeFlags);
 
-    // Initializes the HTTP session.
+    /// <summary> Initializes the HTTP session. </summary>
     [VirtualFunction(1)]
     public partial void OpenSession();
 
-    // Cleans up the HTTP session.
+    /// <summary> Cleans up the HTTP session. </summary>
     [VirtualFunction(2)]
     public partial void CloseSession();
+
+    /// <summary> Creates a new HTTPRequest. </summary>
+    /// <param name="url">URL to make a request to.</param>
+    /// <param name="verb">The HTTP verb, such as "GET" or "POST".</param>
+    /// <param name="data">Any request data to send (only relevant for POST requests really).</param>
+    /// <param name="dataLength">Length of the data buffer.</param>
+    /// <returns>The new HTTPRequest handle if it could be successfully created.</returns>
+    [VirtualFunction(4)]
+    public partial HttpRequest* MakeRequest(byte* url, byte* verb, byte* data, ulong dataLength);
 }
