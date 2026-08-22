2effb9e067d0e98a263c46d01edaa8e18d12d756
Haselnussbomber <mail@haselnussbomber.de>
2026-08-22 03:13:54 +0200

    Update ReactionEventObject(Manager)

--- FFXIVClientStructs/FFXIV/Client/Game/Object/ReactionEventObject.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Object/ReactionEventObject.cs
@@ -1,3 +1,5 @@
+using FFXIVClientStructs.FFXIV.Common.Math;
+
 namespace FFXIVClientStructs.FFXIV.Client.Game.Object;
 
 // Client::Game::Object::ReactionEventObject
@@ -6,6 +8,14 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Object;
 [Inherits<GameObject>]
 [StructLayout(LayoutKind.Explicit, Size = 0x240)]
 public unsafe partial struct ReactionEventObject {
+    [FieldOffset(0x1A0), CExporterExcel("EObj")] public nint EObjRowPtr;
+    [FieldOffset(0x1A8), CExporterExcel("ExportedSG")] public nint ExportedSGRowPtr;
+
+    [FieldOffset(0x1B4)] private byte Unk1B4;
     [FieldOffset(0x1B8)] public Utf8String SgbPath;
+    [FieldOffset(0x220)] public Quaternion RotationQuaternion;
+    [FieldOffset(0x230)] private byte Unk230;
+    [FieldOffset(0x234)] private float Unk234;
+    [FieldOffset(0x238)] private float Unk238;
     [FieldOffset(0x23C)] public uint EObjNameId;
 }
--- FFXIVClientStructs/FFXIV/Client/Game/Object/ReactionEventObjectManager.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Object/ReactionEventObjectManager.cs
@@ -1,3 +1,5 @@
+using FFXIVClientStructs.FFXIV.Common.Math;
+
 namespace FFXIVClientStructs.FFXIV.Client.Game.Object;
 
 // Client::Game::Object::ReactionEventObjectManager
@@ -10,4 +12,15 @@ public unsafe partial struct ReactionEventObjectManager {
 
     [FieldOffset(0x00)] public ReactionEventObject* ReactionEventObjectMemory;
     [FieldOffset(0x08), FixedSizeArray] internal FixedSizeArray100<Pointer<ReactionEventObject>> _reactionEventObjects;
+
+    /// <summary>
+    /// Creates a new <see cref="ReactionEventObject"/>.
+    /// </summary>
+    /// <param name="baseId">The EObj RowId.</param>
+    /// <param name="layoutId">A SharedGroupLayoutInstance Id.</param>
+    /// <param name="position">The position of the object.</param>
+    /// <param name="rotation">The rotation of the object.</param>
+    /// <returns>The object index (in the <see cref="ReactionEventObjectManager"/>).</returns>
+    [MemberFunction("E8 ?? ?? ?? ?? 0F 28 74 24 ?? 83 F8 ?? 74")]
+    public partial int CreateObject(uint baseId, uint layoutId, Vector3* position, float rotation);
 }
