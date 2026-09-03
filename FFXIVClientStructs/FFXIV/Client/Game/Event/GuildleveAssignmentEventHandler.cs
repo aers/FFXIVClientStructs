e32c3df5a8d475ce0abe9cdb96bc520425e83a78
Haselnussbomber <mail@haselnussbomber.de>
2026-09-03 22:12:03 +0200

    Update GuildleveAssignmentEventHandler

--- FFXIVClientStructs/FFXIV/Client/Game/Event/GuildleveAssignmentEventHandler.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Event/GuildleveAssignmentEventHandler.cs
@@ -9,61 +9,55 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Event;
 [Inherits<LuaEventHandler>]
 [StructLayout(LayoutKind.Explicit, Size = 0x768)]
 public unsafe partial struct GuildleveAssignmentEventHandler {
-	// Type Column of its GuildleveAssignment Row
+    /// <summary>
+    /// Type Column of its GuildleveAssignment Row
+    /// </summary>
     [FieldOffset(0x2D8)] public Utf8String Type;
-	
     [FieldOffset(0x340)] public GuildleveType CurrentLeveType;
     [FieldOffset(0x344), FixedSizeArray] internal FixedSizeArray6<int> _categorySelection;
-	
-	// Shows the first leveid of the category.
+    /// <summary>
+    /// Shows the first leveid of the category.
+    /// </summary>
     [FieldOffset(0x35C), FixedSizeArray] internal FixedSizeArray6<ushort> _listLeveId;
-    
-    [FieldOffset(0x368)] public uint GuildleveAssignmentRowId;
-    [FieldOffset(0x36C)] public uint GuildleveAssignmentTalkRowId;
-	
-	// Both QuestIds are in the same GuildleveAssignment Row
-    [FieldOffset(0x370)] public ushort UnlockQuestId1;
-    [FieldOffset(0x374)] public ushort UnlockQuestId2;
-
-    [FieldOffset(0x376)] private ushort Unk376;
-    [FieldOffset(0x378)] private byte Unk378; // Some flags
-    [FieldOffset(0x379)] public byte RequiredGrandCompanyRank;
-
-    [FieldOffset(0x37A)] public byte CompanyLeveFlags;
+    [FieldOffset(0x368 + 0x00), CExporterExcelBegin("GuildleveAssignment")] private uint TypeStringOffset;
+    [FieldOffset(0x368 + 0x04)] public uint AssignmentTalk;
+    [FieldOffset(0x368 + 0x08)] public uint Quest0;
+    [FieldOffset(0x368 + 0x0C)] public uint Quest1;
+    [FieldOffset(0x368 + 0x10)] private byte Unknown0;
+    [FieldOffset(0x368 + 0x11)] private byte RequiredGrandCompanyRank;
+    [FieldOffset(0x368 + 0x12), CExporterExcelEnd] private byte PackedBool12;
     [FieldOffset(0x380), CExporterExcel("GuildleveAssignmentTalk")] public nint AssignmentTalkRow;
 
     [FieldOffset(0x388), CExporterExcel("EventIconType")] public nint EventIconTypeRow;
-    [FieldOffset(0x390), CExporterExcel("CraftLeve")] private nint SelectedCraftLeveRow;
-	
+    [FieldOffset(0x390), CExporterExcel("CraftLeve")] public nint SelectedCraftLeveRow;
     [FieldOffset(0x398)] private int Unk398;
+
     [FieldOffset(0x3A0), FixedSizeArray] internal FixedSizeArray4<RewardItem> _rewardItems;
     [FieldOffset(0x6A0)] private int Unk6A0;
+
     [FieldOffset(0x6A8), CExporterExcel("GatheringLeve")] public nint SelectedGatheringLeveRow;
     [FieldOffset(0x6B0)] public ushort SelectedGatheringLeveId;
     [FieldOffset(0x6B2)] public ushort SelectedLeveId;
     [FieldOffset(0x6B4)] private ushort Unk6B4;
+
     [FieldOffset(0x6B8)] public GuildleveAssignmentLeve* SelectedLeveEntry;
     [FieldOffset(0x6C0)] public StdVector<GuildleveAssignmentCategoryList> AssignmentLists;
-	
-	// No idea about this one. Showed ids (probably leve ids)
-	//[FieldOffset(0x6D8)] private StdMap<ushort, GuildleveLeveInfo> LeveInfoMap;
-
-	// No idea if the following three are correct. They temporarily get set when you open a new tab when it is loaded. 
+    // No idea about this one. Showed ids (probably leve ids)
+    //[FieldOffset(0x6D8)] private StdMap<ushort, GuildleveLeveInfo> LeveInfoMap;
+    // No idea if the following three are correct. They temporarily get set when you open a new tab when it is loaded. 
     [FieldOffset(0x6E8)] private StdVector<ushort> PendingLeveIds;
     [FieldOffset(0x700)] private long PendingLeveCursor;
     [FieldOffset(0x708)] private ExcelSheet* PendingSheet;
-	
     [FieldOffset(0x710)] public ExcelSheetWaiter* SheetWaiter;
     [FieldOffset(0x718)] public StdVector<uint> RewardLeveIds;
     [FieldOffset(0x730)] public StdMap<ushort, uint> RewardItemMap;
     [FieldOffset(0x740)] public StdVector<Utf8String> DifficultyLabels;
-
     [FieldOffset(0x758)] private int Unk758;
     [FieldOffset(0x75C)] private bool Unk75C;
     [FieldOffset(0x75D)] private bool Unk75D; // Something about the HowTo window
-  
+
     [StructLayout(LayoutKind.Explicit, Size = 0xC0)]
-    public unsafe partial struct RewardItem {
+    public struct RewardItem {
         [FieldOffset(0x00)] public Utf8String Name;
         [FieldOffset(0x68)] public InventoryItem Item;
         [FieldOffset(0xB0)] private ushort Unk0B0;
@@ -73,23 +67,23 @@ public unsafe partial struct GuildleveAssignmentEventHandler {
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x18)]
-    public partial struct GuildleveAssignmentCategoryList {
+    public struct GuildleveAssignmentCategoryList {
         [FieldOffset(0x00)] public StdVector<GuildleveAssignmentGroup> Groups;
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x20)]
-    public partial struct GuildleveAssignmentGroup {
+    public struct GuildleveAssignmentGroup {
         [FieldOffset(0x00)] public StdVector<GuildleveAssignmentSubList> SubLists;
         [FieldOffset(0x18)] public uint Flags;
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x18)]
-    public partial struct GuildleveAssignmentSubList {
+    public struct GuildleveAssignmentSubList {
         [FieldOffset(0x00)] public StdVector<GuildleveAssignmentLeve> Leves;
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0xE8)]
-    public partial struct GuildleveAssignmentLeve {
+    public struct GuildleveAssignmentLeve {
         [FieldOffset(0x00)] public ushort LeveId;
         [FieldOffset(0x08)] public Utf8String Name;
         [FieldOffset(0x70)] public Utf8String ClientName;
@@ -98,13 +92,13 @@ public unsafe partial struct GuildleveAssignmentEventHandler {
         [FieldOffset(0xDC)] public uint GenreIcon;
         [FieldOffset(0xE0)] private int Unk0E0;
     }
-	
-	public enum GuildleveType : int {
-		Battlecraft,
-		Fieldcraft,
-		Tradecraft,
-		Maelstrom,
-		OrderOfTheTwinAdder,
-		ImmortalFlames
-	}
+
+    public enum GuildleveType {
+        Battlecraft,
+        Fieldcraft,
+        Tradecraft,
+        Maelstrom,
+        OrderOfTheTwinAdder,
+        ImmortalFlames
+    }
 }
