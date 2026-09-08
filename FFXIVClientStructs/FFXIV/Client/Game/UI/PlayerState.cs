1f10edb6290395b4fd33f1a56844382ac6497530
Haselnussbomber <mail@haselnussbomber.de>
2026-09-08 12:51:18 +0200

    Update bitcount of PlayerState.UnlockedMounts

--- FFXIVClientStructs/FFXIV/Client/Game/UI/PlayerState.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/PlayerState.cs
@@ -87,7 +87,7 @@ public unsafe partial struct PlayerState {
     [FieldOffset(0x300)] private sbyte Unk2DC; // GCSupply stuff
     // BitCount: MountSheet.Where(row => row.ModelChara.RowId != 0).Max(row => row.Order))
     /// <remarks> Use <see cref="IsMountUnlocked"/>. </remarks>
-    [FieldOffset(0x301), FixedSizeArray(isBitArray: true, bitCount: 358)] internal FixedSizeArray45<byte> _unlockedMounts;
+    [FieldOffset(0x301), FixedSizeArray(isBitArray: true, bitCount: 359)] internal FixedSizeArray45<byte> _unlockedMounts;
     // BitCount: OrnamentSheet.RowCount
     /// <remarks> Use <see cref="IsOrnamentUnlocked"/>. </remarks>
     [FieldOffset(0x32E), FixedSizeArray(isBitArray: true, bitCount: 59)] internal FixedSizeArray8<byte> _unlockedOrnaments;
