6179f0bef55fb6bde7cdbcc3d85be2754daaa02d
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2026-09-11 01:38:19 +0200

    Add ContextTabOwnerAddonId field to AgentChatLog

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentChatLog.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentChatLog.cs
@@ -19,6 +19,7 @@ public unsafe partial struct AgentChatLog {
     [FieldOffset(0x120)] public Utf8String TellPlayerName;
     [FieldOffset(0x188)] public ushort TellWorldId;
 
+    [FieldOffset(0x194)] public int ContextTabOwnerAddonId;
     [FieldOffset(0x198)] public int ContextTabIndex;
 
     [FieldOffset(0x1A0), FixedSizeArray] internal FixedSizeArray8<Utf8String> _channelSelectorLSNames;
