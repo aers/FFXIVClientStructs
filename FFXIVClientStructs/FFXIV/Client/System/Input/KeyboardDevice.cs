927b0bca31422b198c0d7ab1c47b1ec54669cf14
Haselnussbomber <mail@haselnussbomber.de>
2026-09-16 22:23:41 +0200

    Update KeyboardDevice(Interface)

--- FFXIVClientStructs/FFXIV/Client/System/Input/KeyboardDevice.cs
+++ FFXIVClientStructs/FFXIV/Client/System/Input/KeyboardDevice.cs
@@ -1,3 +1,5 @@
+using FFXIVClientStructs.FFXIV.Client.System.Framework;
+
 namespace FFXIVClientStructs.FFXIV.Client.System.Input;
 
 // Client::System::Input::KeyboardDevice
@@ -6,7 +8,11 @@ namespace FFXIVClientStructs.FFXIV.Client.System.Input;
 [GenerateInterop]
 [Inherits<KeyboardDeviceInterface>, Inherits<RepeatCounter>]
 [StructLayout(LayoutKind.Explicit, Size = 0x40)]
-public partial struct KeyboardDevice {
+public unsafe partial struct KeyboardDevice {
+    [FieldOffset(0x28)] public GameWindow* GameWindow;
+    [FieldOffset(0x30)] public bool IsKeyboardConnected;
+    [FieldOffset(0x38)] private Threading.Thread* UnkThread;
+
     [MemberFunction("48 89 5C 24 ?? 55 56 57 41 56 41 57 48 8D 6C 24 ?? 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 45 ?? 4D 8B F9")]
     public static partial void ProcessKeyboardInputMessage(nint hWnd, uint uMsg, nint wParam, nint lParam);
 }
--- FFXIVClientStructs/FFXIV/Client/System/Input/KeyboardDeviceInterface.cs
+++ FFXIVClientStructs/FFXIV/Client/System/Input/KeyboardDeviceInterface.cs
@@ -3,4 +3,28 @@ namespace FFXIVClientStructs.FFXIV.Client.System.Input;
 // Client::System::Input::KeyboardDeviceInterface
 [GenerateInterop(isInherited: true)]
 [StructLayout(LayoutKind.Explicit, Size = 0x08)]
-public unsafe partial struct KeyboardDeviceInterface;
+public unsafe partial struct KeyboardDeviceInterface {
+    [VirtualFunction(0)]
+    public partial KeyboardDeviceInterface* Dtor(byte freeFlags);
+
+    [VirtualFunction(1)]
+    public partial bool Initialize();
+
+    [VirtualFunction(2)]
+    public partial void Update(float dt);
+
+    [VirtualFunction(3)]
+    public partial void Deinitialize();
+
+    [VirtualFunction(4)]
+    public partial KeyboardInputData* GetData();
+
+    [VirtualFunction(5)]
+    public partial void ResetState(bool clearDown);
+
+    [VirtualFunction(6)]
+    public partial void SendKey(SeVirtualKey key);
+
+    [VirtualFunction(7)]
+    public partial bool GetIsKeyboardConnected();
+}
