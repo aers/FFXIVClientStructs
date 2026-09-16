1a666560896a712edc7d6f2d730946081d091e19
Haselnussbomber <mail@haselnussbomber.de>
2026-09-16 19:18:27 +0200

    Add mouse and keyboard wndproc handlers

--- FFXIVClientStructs/FFXIV/Client/System/Input/KeyboardDevice.cs
+++ FFXIVClientStructs/FFXIV/Client/System/Input/KeyboardDevice.cs
@@ -6,4 +6,7 @@ namespace FFXIVClientStructs.FFXIV.Client.System.Input;
 [GenerateInterop]
 [Inherits<KeyboardDeviceInterface>, Inherits<RepeatCounter>]
 [StructLayout(LayoutKind.Explicit, Size = 0x40)]
-public partial struct KeyboardDevice;
+public partial struct KeyboardDevice {
+    [MemberFunction("48 89 5C 24 ?? 55 56 57 41 56 41 57 48 8D 6C 24 ?? 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 45 ?? 4D 8B F9")]
+    public static partial void ProcessKeyboardInputMessage(nint hWnd, uint uMsg, nint wParam, nint lParam);
+}
--- FFXIVClientStructs/FFXIV/Client/System/Input/MouseDevice.cs
+++ FFXIVClientStructs/FFXIV/Client/System/Input/MouseDevice.cs
@@ -11,6 +11,9 @@ namespace FFXIVClientStructs.FFXIV.Client.System.Input;
 [Inherits<MouseDeviceInterface>, Inherits<RepeatCounter>, Inherits<InputDevice>]
 [StructLayout(LayoutKind.Explicit, Size = 0x48)]
 public partial struct MouseDevice {
+    [MemberFunction("48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC ?? 49 8B D8 C6 05")]
+    public static partial void ProcessMouseInputMessage(nint hWnd, uint uMsg, nint wParam);
+
     /// <remarks>
     /// Schedules the cursor to move to the specified coordinates on the next mouse device update.
     /// </remarks>
