69563dfb94adf9207b8d4e3a37c35268ce58d0b1
AtmoOmen <atmoomen2004@outlook.com>
2026-09-17 18:02:20 +0800

    Fix pet health function name to address mapping

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentXBMPetParty.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentXBMPetParty.cs
@@ -68,11 +68,12 @@ public unsafe partial struct AgentXBMPetParty {
     [MemberFunction("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 48 83 EC ?? 8B 41 ?? 48 8B F1 FF C8")]
     public partial bool HasUnlockedPets();
 
+    /// <remarks>0 - current health</remarks>
     [MemberFunction("CC CC CC CC CC CC CC CC CC CC CC CC CC CC CC CC 41 55 41 57 48 83 EC 28 4C 8B E9 E8")]
-    public partial uint GetPetStatValue(uint petId, byte statIndex);
+    public partial uint GetPetHealth(uint petId, byte index);
 
     [MemberFunction("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 48 83 EC ?? 48 8B 59 ?? 0F B6 EA")]
-    public partial void UpdatePetStatValues(byte statIndex);
+    public partial void UpdatePetsHealth(byte index);
 
     [MemberFunction("48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC ?? 8B 41 ?? 48 8B F9")]
     public partial bool IsPetAvailable(uint petId);
