8651034d714a6f5f9b070a538e932415acdbe89a
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2026-09-19 15:45:43 +0200

    change to non instance specific for max level grab

--- FFXIVClientStructs/FFXIV/Client/Game/UI/PlayerState.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/PlayerState.cs
@@ -351,7 +351,7 @@ public unsafe partial struct PlayerState {
     [MemberFunction("E8 ?? ?? ?? ?? 8B E8 F3 0F 10 35")]
     public partial uint GetCurrentClassJobNeededExp();
 
-    [MemberFunction("E8 ?? ?? ?? ?? 0F B6 C8 66 3B F1")]
+    [MemberFunction("0F B6 51 7E 80 FA 24")]
     public partial byte GetCurrentClassJobMaxLevel();
 
     #region Unlocks
