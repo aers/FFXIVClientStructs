3d70fa4c8ee973473eca48903da2d7f439eb0083
AtmoOmen <110901588+AtmoOmen@users.noreply.github.com>
2026-09-22 01:52:07 +0800

    Apply suggestion from @pohky
    Co-authored-by: pohky <1510568+pohky@users.noreply.github.com>

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentHousingPortal.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentHousingPortal.cs
@@ -21,7 +21,7 @@ public unsafe partial struct AgentHousingPortal {
     [FieldOffset(0x3C)] public int SelectedWardIndex;
     
     [FieldOffset(0x40)] public uint PlotCount;
-    [FieldOffset(0x44), FixedSizeArray] internal FixedSizeArray60<byte> _plotOccupied;
+    [FieldOffset(0x44), FixedSizeArray] internal FixedSizeArray60<bool> _plotOccupied;
     
     /// <summary>
     /// 0 - Personal Estate <br/>
