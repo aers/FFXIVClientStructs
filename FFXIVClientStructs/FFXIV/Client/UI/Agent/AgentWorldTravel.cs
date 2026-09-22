8b4fd7dee0ace80c88cffffd7ae1304bddb6f75e
AtmoOmen <atmoomen2004@outlook.com>
2026-09-22 20:22:07 +0800

    queue

--- FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentWorldTravel.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Agent/AgentWorldTravel.cs
@@ -12,11 +12,22 @@ public unsafe partial struct AgentWorldTravel {
     [FieldOffset(0x40)] public ushort* HomeWorldId;
     [FieldOffset(0x48)] private uint QueueTimer1; // only active during waiting
     [FieldOffset(0x4C)] public ushort DestinationWorldId;
+    
     [FieldOffset(0x50)] public Utf8String CurrentWorldName;
     [FieldOffset(0xB8)] public Utf8String DestinationWorldName;
-    [FieldOffset(0x120)] private ushort TransportFlag; // InQueue vs Ready
+
+    [FieldOffset(0x120)] public byte IsWorldTraveling;
+    [FieldOffset(0x121)] public byte IsInWorldTravelQueue;
+    
+    [FieldOffset(0x120), Obsolete("Use IsWorldTraveling and IsInWorldTravelQueue")] private ushort TransportFlag; // InQueue vs Ready
+    
     [FieldOffset(0x124)] private void* QueueTimer2; // only resets on each new travel, counts up in ~seconds
-    [FieldOffset(0x138)] private byte BetweenAreas; // Roughly corresponds to the BetweenAreas condition
+    
+    [FieldOffset(0x128)] public uint ElapsedQueueTimeSecond;
+    [FieldOffset(0x12C)] public uint QueuePosition;
+    [FieldOffset(0x130)] public uint EstimatedWaitTimeMinute;
+    
+    [FieldOffset(0x138)] public byte BetweenAreas; // Roughly corresponds to the BetweenAreas condition
 
     [MemberFunction("48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC ?? 48 8B F9 41 0F B7 F0 48 8B 49")]
     public partial void SetupWorldTravelInfo(ushort currentWorld, ushort targetWorld);
