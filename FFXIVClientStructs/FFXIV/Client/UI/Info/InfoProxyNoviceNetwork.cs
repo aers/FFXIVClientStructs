0aa83ac7b21bf2a8851aad0e762b5234b39ea0c9
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-09-26 12:50:24 +0100

    Update InfoProxyNoviceNetwork.cs

--- FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyNoviceNetwork.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/Info/InfoProxyNoviceNetwork.cs
@@ -10,8 +10,6 @@ public unsafe partial struct InfoProxyNoviceNetwork {
     [BitField<bool>(nameof(IsInNoviceNetwork), 0)]
     [FieldOffset(0x18)] public byte Flags;
 
-    public partial bool IsInNoviceNetwork { readonly get; set; }
-
     [MemberFunction("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 48 89 7C 24 ?? 41 56 48 83 EC 50 48 8B 9C 24")]
     [GenerateStringOverloads]
     public partial bool InviteToNoviceNetwork(ulong accountId, ulong contentId, ushort worldId, CStringPointer name);
